646b7867ed29a7c1cc2de94177ee2c6a58f4782b
MrNopi <Smart.like1998@gmail.com>
2019-12-03 16:47:12 +0300

    Finished project

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,16 +1,29 @@
 package core.basesyntax;
 
-/**
- * - Создать класс Machine с методами public abstract void doWork()
- * и public abstract void stopWork().
- * - Создать классы Track, Bulldozer, Excavator которые унаследуют Machine.
- * - Переопределить метод doWork() и вывести в консоль строку с сообщением что та или иная
- * машина начала свою работу.
- * - Переопределить метод stopWork() и вывести в консоль строку с сообщением что та или иная
- * машина прекратила свою работу.
- * - В классе Main создать добавить по одному екземпляру каждого класса.
- * - Отправить все машини на работу и остановить работу всех машин.
- */
 public class MainApp {
-
+    /**
+     * - Создать класс Machine с методами public abstract void doWork()
+     * и public abstract void stopWork().
+     * - Создать классы Track, Bulldozer, Excavator которые унаследуют Machine.
+     * - Переопределить метод doWork() и вывести в консоль строку с сообщением что та или иная
+     * машина начала свою работу.
+     * - Переопределить метод stopWork() и вывести в консоль строку с сообщением что та или иная
+     * машина прекратила свою работу.
+     * - В классе Main создать добавить по одному екземпляру каждого класса.
+     * - Отправить все машини на работу и остановить работу всех машин.
+     */
+    public static void main(String[] args) {
+        Excavator excavator = new Excavator();
+        //Excavator
+        excavator.doWork();
+        excavator.stopWork();
+        Bulldozer bulldozer = new Bulldozer();
+        //Bulldozer
+        bulldozer.doWork();
+        bulldozer.stopWork();
+        Track     track = new Track();
+        //Track
+        track.doWork();
+        track.stopWork();
+    }
 }
