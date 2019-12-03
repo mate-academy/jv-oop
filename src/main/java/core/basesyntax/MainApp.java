cd9d9bcdfc540b50a1cf4f3b88f566e3b2a75dab
Tsehelnyk <tsegelnikoff@gmail.com>
2019-12-03 20:33:52 +0200

    added solution in class MainApp, created classes Machine, Bulldozer, Excavator, Track

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -13,4 +13,22 @@ package core.basesyntax;
  */
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[3];
+        machines[0] = new Bulldozer();
+        machines[1] = new Excavator();
+        machines[2] = new Bulldozer();
+
+        for (Machine machine :
+                machines) {
+            machine.doWork();
+        }
+
+        for (Machine machine :
+                machines) {
+            machine.stopWork();
+        }
+
+    }
+
 }
