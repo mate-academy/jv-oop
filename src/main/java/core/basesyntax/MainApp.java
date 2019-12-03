da08dd1798afd7d7112044fd298e328d895249b7
IvanKrivoruchko <krivoruchko.ivan.ukr.net>
2019-12-03 17:41:18 +0200

    changed the child class to parent when objects were are declared

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -13,14 +13,14 @@ package core.basesyntax;
  */
 public class MainApp {
     public static void main(String[] args) {
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
-        Track track = new Track();
-        bulldozer.doWork();
-        excavator.doWork();
-        track.doWork();
-        bulldozer.stopWork();
-        excavator.stopWork();
-        track.stopWork();
+        Machine machine1 = new Bulldozer();
+        Machine machine2 = new Excavator();
+        Machine machine3 = new Track();
+        machine1.doWork();
+        machine2.doWork();
+        machine3.doWork();
+        machine1.stopWork();
+        machine2.stopWork();
+        machine3.stopWork();
     }
 }
