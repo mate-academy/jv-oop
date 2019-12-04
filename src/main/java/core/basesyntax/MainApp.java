08c582a5f6229e4550af5c7d120e1574f70181ca
anyapress777@gmail.com <golovinzodchiy95>
2019-12-04 16:05:30 +0200

    Task 2.3 was finished

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -12,5 +12,15 @@ package core.basesyntax;
  * - Отправить все машини на работу и остановить работу всех машин.
  */
 public class MainApp {
-
+    public static void main(String[] args) {
+        Track track1 = new Track("track1");
+        track1.doWork();
+        track1.stopWork();
+        Bulldozer bulldozer1 = new Bulldozer("bulldozer1");
+        bulldozer1.doWork();
+        bulldozer1.stopWork();
+        Excavator excavator1 = new Excavator("excavator1");
+        excavator1.doWork();
+        excavator1.stopWork();
+    }
 }
