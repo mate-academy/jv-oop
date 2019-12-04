eb700e8dc5dca54423cb0ef363bd34c3fc7dc2e5
Vadim Chaika <proxik777@gmail.com>
2019-12-04 02:14:43 +0200

    added a solution for machines homework

---
+++ src/main/java/core/basesyntax/Track.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Track extends Machine {
+
+    public void doWork() {
+        System.out.println("Track is started.");
+    }
+
+    public void stopWork() {
+        System.out.println("Track is stopped.");
+    }
+}
