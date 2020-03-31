d8b3d9407311333af4adf1620f15e43768af6412
Maksym Vakuliuk <vakuliukmaksym@mail.com>
2020-03-31 17:30:24 +0300

    change MainApp, changed machines initialization

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+
 /**
  * - Создать класс Machine с методами public abstract void doWork()
  * и public abstract void stopWork().
@@ -12,16 +14,16 @@ package core.basesyntax;
  * - Отправить все машини на работу и остановить работу всех машин.
  */
 public class MainApp {
-    Track track = new Track();
-    Bulldozer bulldozer = new Bulldozer();
-    Excavator excavator = new Excavator();
 
-    public void main() {
-        track.doWork();
-        bulldozer.doWork();
-        excavator.doWork();
-        track.stopWork();
-        bulldozer.stopWork();
-        excavator.stopWork();
+    public void main(String[] args) {
+        ArrayList<Machine> machines = new ArrayList<>();
+        machines.add(new Track());
+        machines.add(new Track());
+        machines.add(new Track());
+
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
