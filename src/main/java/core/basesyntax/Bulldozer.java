37b9224bb17be1306c30b370ca61856d03c25692
My <vasylnv19@gmail.com>
2020-03-31 17:19:26 +0300

    Solution_HW_2.3

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Bulldozer extends Machine {
     @Override
     public void doWork() {
-        System.out.println("I'm working!!!");
+        System.out.println("I'm work!!!");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("I'm don't working");
+        System.out.println("I'm don't work");
     }
 }
--- src/main/java/core/basesyntax/Track.java
+++ src/main/java/core/basesyntax/Track.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 public class Track extends Machine {
     @Override
     public void doWork() {
-        System.out.println("I'm working!!!");
+        System.out.println("I'm work!!!");
 
     }
 
     @Override
     public void stopWork() {
-        System.out.println("I'm don't working");
+        System.out.println("I'm don't work");
 
     }
 }
