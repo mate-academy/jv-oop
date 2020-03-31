090ce94cb78ae884549c86e9e48a2d49083ee94f
Allduiin <vovapro01@gmail.com>
2020-03-31 14:58:09 +0300

    Added all what need

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -13,14 +13,14 @@ package core.basesyntax;
  */
 public class MainApp {
     public static void main(String[] args) {
-        Machine Track1 = new Track();
-        Machine Bulldozer1 = new Bulldozer();
-        Machine Excavator1 = new Excavator();
-        Track1.doWork();
-        Bulldozer1.doWork();
-        Excavator1.doWork();
-        Track1.stopWork();
-        Bulldozer1.stopWork();
-        Excavator1.stopWork();
+        Machine track1 = new Track();
+        Machine bulldozer1 = new Bulldozer();
+        Machine excavator1 = new Excavator();
+        track1.doWork();
+        bulldozer1.doWork();
+        excavator1.doWork();
+        track1.stopWork();
+        bulldozer1.stopWork();
+        excavator1.stopWork();
     }
 }
