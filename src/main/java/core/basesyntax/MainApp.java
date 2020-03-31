9703c2fdbe3a093f7caf8529d5588b38c53f55c3
olehedza <olehedza@gmail.com>
2020-03-31 19:14:56 +0300

    empty lines deleted

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -16,11 +16,9 @@ public class MainApp {
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
         Machine track = new Track();
-
         bulldozer.doWork();
         excavator.doWork();
         track.doWork();
-
         bulldozer.stopWork();
         excavator.stopWork();
         track.stopWork();
