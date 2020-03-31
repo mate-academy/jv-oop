9c0eb165807087c082353f5600ecda68d631777d
Максим Матуляк <maksymmatuliak@gmail.com>
2020-03-31 19:19:13 +0300

    deleted copypast

--- src/main/java/core/basesyntax/Track.java
+++ src/main/java/core/basesyntax/Track.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Track extends Machine {
     @Override
     void doWork() {
-        System.out.println("Buldozer is working...");
+        System.out.println("Track is working...");
     }
 
     @Override
     void stopWork() {
-        System.out.println("Buldozer is not working...");
+        System.out.println("Track is not working...");
     }
 }
