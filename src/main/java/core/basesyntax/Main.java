76777c3206890c773717f357e3c9f8d4f78e5b13
maks45 <maksim.durov45@gmail.com>
2020-03-31 15:55:07 +0300

    added solution

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Machine track = new Track();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        track.doWork();
+        excavator.doWork();
+        bulldozer.doWork();
+        track.stopWork();
+        excavator.stopWork();
+        bulldozer.stopWork();
+    }
+}
