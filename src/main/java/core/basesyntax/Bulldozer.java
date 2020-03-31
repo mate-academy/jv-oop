2278b1c2d3f5f2a626e492090a8a96487c4861cb
Polina Kulyk <polina.kulyk@protonmail.com>
2020-03-31 14:44:00 +0300

    added child class Bulldoxer and implemented methods from parent Machine(minor)

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 public class Bulldozer extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Track started its work.");
+        System.out.println("Bulldozer started its work.");
     }
     @Override
     public void stopWork() {
-        System.out.println("Track stopped its work.");
+        System.out.println("Bulldozer stopped its work.");
     }
 }
