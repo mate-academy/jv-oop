05f844a1c4a636523f45bc2e374d9b13c4e2d5cb
Polina Kulyk <polina.kulyk@protonmail.com>
2020-03-31 14:42:01 +0300

    added child class Track and implemented methods from parent Machine

---
+++ src/main/java/core/basesyntax/Track.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+class Track extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Track started its work.");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Track stopped its work.");
+    }
+}
