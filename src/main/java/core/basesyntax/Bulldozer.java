f0b256db49d88705546a3f3d99191eaff6d1753c
6ALLIKA <skorik009@gmail.com>
2020-03-31 14:48:17 +0300

    added solution

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Bulldoze is working");
+
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldoze isn't working");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -12,5 +12,18 @@ package core.basesyntax;
  * - Отправить все машини на работу и остановить работу всех машин.
  */
 public class MainApp {
+    public static void main(String[] args) {
+        Machine bulldozer = new Bulldozer();
+        Machine track = new Track();
+        Machine excavator = new Excavator();
+
+        bulldozer.doWork();
+        track.doWork();
+        excavator.doWork();
+
+        bulldozer.stopWork();
+        track.stopWork();
+        excavator.stopWork();
+    }
 
 }
