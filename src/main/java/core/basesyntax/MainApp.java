4cecbb532963e63ca0545c656829cd3ea56f2fbe
NastiaFasova <fasovaaa@fmail.com>
2020-03-31 16:50:52 +0300

    Added solution to the 3-rd task of hw-2

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,21 +1,10 @@
 package core.basesyntax;
 
-/**
- * - Создать класс Machine с методами public abstract void doWork()
- * и public abstract void stopWork().
- * - Создать классы Track, Bulldozer, Excavator которые унаследуют Machine.
- * - Переопределить метод doWork() и вывести в консоль строку с сообщением что та или иная
- * машина начала свою работу.
- * - Переопределить метод stopWork() и вывести в консоль строку с сообщением что та или иная
- * машина прекратила свою работу.
- * - В классе Main создать добавить по одному екземпляру каждого класса.
- * - Отправить все машини на работу и остановить работу всех машин.
- */
 public class MainApp {
     public static void main(String[] args) {
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
-        Machine track = new Track();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        Track track = new Track();
         bulldozer.doWork();
         excavator.doWork();
         track.doWork();
