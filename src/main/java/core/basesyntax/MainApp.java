202514a9f39de8f977d7c4269be2bc79c8a722d0
Leonid <rommel@ukr.net>
2020-07-27 20:45:22 +0300

    oop task done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -12,5 +12,15 @@ package core.basesyntax;
  * - Отправить все машини на работу и остановить работу всех машин.
  */
 public class MainApp {
-
+    public static void main(String[] args) {
+        Track track = new Track();
+        track.doWork();
+        track.stopWork();
+        Bulldozer bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+        Excavator excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
+    }
 }
