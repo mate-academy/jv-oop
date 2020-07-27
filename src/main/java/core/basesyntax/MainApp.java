ae71fc8fe503d0fb9785d176ccbaf48528b9bd3d
Andriy <andriy.lagun@gmail.com>
2020-07-27 21:17:21 +0300

    Added abstract class Machine, classes Track,Bulldozer, Excavator. Implemented methods doWork and stopWork for all classes. Created MainApp class.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -11,6 +11,56 @@ package core.basesyntax;
  * - В классе Main создать добавить по одному екземпляру каждого класса.
  * - Отправить все машини на работу и остановить работу всех машин.
  */
-public class MainApp {
+abstract class Machine {
+    public abstract void doWork();
+    public abstract void stopWork();
+}
+class Track extends Machine{
 
+    @Override
+    public void doWork() {
+        System.out.println("The track started working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The track stopped working");
+    }
+}
+class Bulldozer extends Machine{
+
+    @Override
+    public void doWork() {
+        System.out.println("The bulldozer started working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The bulldozer stopped working");
+    }
+}
+class Excavator extends Machine{
+
+    @Override
+    public void doWork() {
+        System.out.println("The excavator started working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The excavator stopped working");
+    }
+}
+public class MainApp {
+    public static void main(String[] args) {
+        Track track = new Track();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        track.doWork();
+        bulldozer.doWork();
+        excavator.doWork();
+        track.stopWork();
+        bulldozer.stopWork();
+        excavator.stopWork();
+    }
 }
