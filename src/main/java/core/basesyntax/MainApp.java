62943d5e4a64588f01160184e37dd84eda52bdd2
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-07-28 18:12:00 +0300

    changes to abstract

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,15 +3,15 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
-        Track track = new Track();
+        Machine track = new Track();
         track.doWork();
         track.stopWork();
 
-        Bulldozer bulldozer = new Bulldozer();
+        Machine bulldozer = new Bulldozer();
         bulldozer.doWork();
         bulldozer.stopWork();
 
-        Excavator excavator = new Excavator();
+        Machine excavator = new Excavator();
         excavator.doWork();
         excavator.stopWork();
     }
