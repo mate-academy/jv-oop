237f2df26b02622261680df8eafcba3dfa7b30ee
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-07-28 17:37:21 +0300

    changes made

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,16 +3,16 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
-        Track track1 = new Track();
-        track1.doWork();
-        track1.stopWork();
+        Track track = new Track();
+        track.doWork();
+        track.stopWork();
 
-        Bulldozer bulldozer1 = new Bulldozer();
-        bulldozer1.doWork();
-        bulldozer1.stopWork();
+        Bulldozer bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        bulldozer.stopWork();
 
-        Excavator excavator1 = new Excavator();
-        excavator1.doWork();
-        excavator1.stopWork();
+        Excavator excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
     }
 }
