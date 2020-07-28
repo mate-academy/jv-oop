2345cb47d6e953d7d353e9d4049eabf028a4ca24
Fedia <fedia.rudiachenko@gmail.com>
2020-07-28 14:41:44 +0300

    added new classes Excavator, Bulldozer, Track which extends Machine

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -8,9 +8,19 @@ package core.basesyntax;
  * машина начала свою работу.
  * - Переопределить метод stopWork() и вывести в консоль строку с сообщением что та или иная
  * машина прекратила свою работу.
- * - В классе Main создать добавить по одному екземпляру каждого класса.
- * - Отправить все машини на работу и остановить работу всех машин.
+ * - В классе Main создать по одному экземпляру каждого класса.
+ * - Отправить все машины на работу и остановить работу всех машин.
  */
 public class MainApp {
-
+    public static void main(String[] args) {
+        Excavator excavator = new Excavator();
+        Bulldozer bulldozer = new Bulldozer();
+        Track track = new Track();
+        excavator.doWork();
+        bulldozer.doWork();
+        track.doWork();
+        excavator.stopWork();
+        bulldozer.stopWork();
+        track.stopWork();
+    }
 }
