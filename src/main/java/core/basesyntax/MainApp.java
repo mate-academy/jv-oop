948c1e850fd568be7d83761bb2c40efdf9476506
Oleksii Tromsa <tromsaalex@gmail.com>
2020-07-28 17:20:34 +0300

    Change variables declaration

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class MainApp {
     public static void main(String[] args) {
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
-        Track track = new Track();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine track = new Track();
 
         List<Machine> machines = new ArrayList<>();
         machines.add(bulldozer);
