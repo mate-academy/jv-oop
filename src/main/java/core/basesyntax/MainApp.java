f15ac166be5c2b088ffed7456d2afcedd3fca94c
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-07-28 15:05:37 +0300

    First commit. Machines are defined and tested.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -11,6 +11,16 @@ package core.basesyntax;
  * - В классе Main создать добавить по одному екземпляру каждого класса.
  * - Отправить все машини на работу и остановить работу всех машин.
  */
-public class MainApp {
 
+public class MainApp {
+    public void mainApp() {
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine track = new Track();
+        Machine[] park = {excavator, bulldozer, track};
+        for (Machine machine : park) {
+            machine.doWork();
+            machine.stopWork(); //Надеюсь, не принципиально, что бы все работали одновременно. Так они по очереди вкл/выкл.
+        }
+    }
 }
---
+++ src/main/java/core/basesyntax/Track.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Track extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Track is working!");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Track just stopped to work!");
+    }
+}
