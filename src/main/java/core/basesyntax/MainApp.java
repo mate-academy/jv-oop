91660fcbee0e2ccda4d233d38ff2932d4dfa8995
Martynice <marticiy@ukr.net>
2020-07-28 15:48:07 +0300

    fixed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -13,9 +13,9 @@ package core.basesyntax;
  */
 public class MainApp {
     public static void main(String[] args) {
-        Track track = new Track();
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
+        Machine track = new Track();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
         track.doWork();
         bulldozer.doWork();
         excavator.doWork();
