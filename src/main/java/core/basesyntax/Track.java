c6b11371283717fa458f0a30c9581ca76b64963d
Borisko130 <krimenyukb@gmail.com>
2020-07-28 13:41:39 +0300

    Completed homework 2.2

---
+++ src/main/java/core/basesyntax/Track.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Track extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Truck started work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped work");
+    }
+}
