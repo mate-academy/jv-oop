7fb8a33bae5f7ad1d2719b9c81b17824df50843e
ROMASHSX <andriiromash@gmail.com>
2020-07-28 12:54:08 +0300

    Task 2 is done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,16 +1,17 @@
 package core.basesyntax;
 
-/**
- * - Создать класс Machine с методами public abstract void doWork()
- * и public abstract void stopWork().
- * - Создать классы Track, Bulldozer, Excavator которые унаследуют Machine.
- * - Переопределить метод doWork() и вывести в консоль строку с сообщением что та или иная
- * машина начала свою работу.
- * - Переопределить метод stopWork() и вывести в консоль строку с сообщением что та или иная
- * машина прекратила свою работу.
- * - В классе Main создать добавить по одному екземпляру каждого класса.
- * - Отправить все машини на работу и остановить работу всех машин.
- */
 public class MainApp {
+    public static void main(String[] args){
+        Bulldozer machine1 = new Bulldozer();
+        Excavator machine2 = new Excavator();
+        Track machine3 = new Track();
 
+        machine1.doWork();
+        machine2.doWork();
+        machine3.doWork();
+
+        machine1.stopWork();
+        machine2.stopWork();
+        machine3.stopWork();
+    }
 }
