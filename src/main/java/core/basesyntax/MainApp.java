a419872195565ed7e1b7014d83b1a8b718c64a0b
OlessiaKharchenko <olessia.kharchenko@gmail.com>
2020-07-28 15:48:00 +0300

    renamed objects in main method

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -13,15 +13,15 @@ package core.basesyntax;
  */
 public class MainApp {
     public static void main(String [] args) {
-        Track track = new Track();
-        track.doWork();
-        track.stopWork();
-        Bulldozer bulldozer = new Bulldozer();
-        bulldozer.doWork();
-        bulldozer.stopWork();
-        Excavator excavator = new Excavator();
-        excavator.doWork();
-        excavator.stopWork();
+        Machine trackMachine = new Track();
+        trackMachine.doWork();
+        trackMachine.stopWork();
+        Machine bulldozerMachine = new Bulldozer();
+        bulldozerMachine.doWork();
+        bulldozerMachine.stopWork();
+        Machine excavatorMachine = new Excavator();
+        excavatorMachine.doWork();
+        excavatorMachine.stopWork();
     }
 }
 
