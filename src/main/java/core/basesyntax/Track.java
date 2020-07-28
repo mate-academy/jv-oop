08f0fa9974f2e68be0b2a106934fa9349d304a69
Ilya.Kvitkovskyi <ilya.kvitkovskyi@gmail.com>
2020-07-28 13:46:10 +0300

    added solution

---
+++ src/main/java/core/basesyntax/Track.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Track extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("The Track started working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The Track has finished working");
+    }
+}
