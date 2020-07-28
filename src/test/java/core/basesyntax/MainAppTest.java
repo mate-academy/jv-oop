a0171d7cc1af499a912e8ed28ed2c6b17d9fd6f8
Kostian <pyteec.ua@i.ua>
2020-07-28 15:56:26 +0300

    added solution

--- src/test/java/core/basesyntax/MainAppTest.java
+++ src/test/java/core/basesyntax/MainAppTest.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainAppTest {
-
+    public static void main(String[] args) {
+        Excavator excavator = new Excavator();
+        Track track = new Track();
+        Bulldozer bulldozer = new Bulldozer();
+        excavator.doWork();
+        track.doWork();
+        bulldozer.doWork();
+        excavator.stopWork();
+        track.stopWork();
+        bulldozer.stopWork();
+    }
 }
