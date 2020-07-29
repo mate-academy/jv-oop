8b37afaa6b046fd866ba111b8bc159119f363318
Vas35 <vas0977515005@gmail.com>
2020-07-29 23:51:36 +0300

    solution

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public abstract class Machine {
+
+    public abstract void startWork();
+
+    public abstract void stopWork();
+
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -12,5 +12,16 @@ package core.basesyntax;
  * - Отправить все машини на работу и остановить работу всех машин.
  */
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
+        for (Machine m : machines) {
+            m.startWork();
+        }
+        for (Machine m : machines) {
+            m.stopWork();
+        }
+    }
 }
