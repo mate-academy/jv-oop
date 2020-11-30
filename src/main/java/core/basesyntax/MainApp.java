cc618050c4a888547bc8cc5f65c015d9687bf487
bogdan <b.kostevich@gmail.com>
2020-11-30 23:26:53 +0200

    Added classes and realized methods of the each class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
 
+        Machine[] machinesWork = new Machine[] {bulldozer, excavator, truck};
+        Machine[] machinesStop = new Machine[] {bulldozer, excavator, truck};
+
+        for(Machine machine : machinesWork) {work(machine);}
+        System.out.println();
+        for(Machine machine : machinesStop) {stop(machine);}
+    }
+
+    public static void work (Machine machine) {
+        machine.doWork();
+    }
+
+    public static void stop (Machine machine) {
+        machine.stopWork();
+    }
 }
