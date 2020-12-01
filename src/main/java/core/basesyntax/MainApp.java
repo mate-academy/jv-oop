12a756ff30040726d5a2bcf9dc9f7fe5a469a69d
YuriiVern <yuriy.vernygorov@gmail.com>
2020-12-01 17:19:26 +0200

    final afterchecking

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,28 +2,18 @@ package core.basesyntax;
 
 public class MainApp {
 
-    public static void main(String...args){
+    public static void main(String[] args) {
 
-            Machine truck = new Truck();
-            truck.doWork();
-            truck.stopWork();
+        Machine truck = new Truck();
+        truck.doWork();
+        truck.stopWork();
 
-            Machine excavator = new Excavator();
-            excavator.doWork();
-            excavator.stopWork();
+        Machine excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
 
-            Machine bulldozer = new Bulldozer();
-            bulldozer.doWork();;
-            bulldozer.stopWork();
-
-
-
-
-
+        Machine bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        bulldozer.stopWork();
     }
-
-
-
-
-
 }
