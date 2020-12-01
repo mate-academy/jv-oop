64cd6671b9a3f3e7ec9f2a2d089223099818281c
KostyantynProskura <kostyantyn.proskura@gmail.com>
2020-12-01 15:07:09 +0200

    1). Added classes Machine,Truck, Bulldozer, Excavator, with extensions; Implemented code in MainApp.main - works with no mistakes.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
 
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+
+        Machine[] machinesArr = new Machine[]{truck,bulldozer,excavator};
+
+        for (Machine i : machinesArr) {
+            i.doWork();
+        }
+
+        for (Machine i : machinesArr) {
+            i.stopWork();
+        }
+
+    }
 }
