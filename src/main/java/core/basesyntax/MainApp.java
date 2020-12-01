53efcf5704dda26ecc29fdccb039688c54a43e7a
engineer027 <dp270597dma@gmail.com>
2020-12-01 14:37:08 +0200

    added solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck1 = new Truck();
+        truck1.doWork();
+        truck1.stopWork();
 
+        Machine bulldozer1 = new Bulldozer();
+        bulldozer1.doWork();
+        bulldozer1.stopWork();
+
+        Machine excavator1 = new Excavator();
+        excavator1.doWork();
+        excavator1.stopWork();
+    }
 }
