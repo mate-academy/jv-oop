70ec329015104f7c4b2b6db3616131f4cd60cc02
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2020-12-01 15:31:28 +0200

    all done, style fixed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,20 +1,21 @@
 package core.basesyntax;
 
 public class MainApp {
+
     public static void main(String[] args) {
 
-    Bulldozer bulldozer = new Bulldozer();
-    Excavator excavator = new Excavator();
-    Truck truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
 
-    bulldozer.doWork();
-    excavator.doWork();
-    truck.doWork();
+        bulldozer.doWork();
+        excavator.doWork();
+        truck.doWork();
 
-    System.out.println();
+        System.out.println();
 
-    bulldozer.stopWork();
-    excavator.stopWork();
-    truck.stopWork();
+        bulldozer.stopWork();
+        excavator.stopWork();
+        truck.stopWork();
     }
 }
