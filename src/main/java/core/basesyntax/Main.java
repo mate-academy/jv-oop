308bc1dcf8452ce861d4e48c9f82a3a834acea9e
Vitalik Kopach <apple_air@ukr.net>
2020-12-01 13:32:41 +0200

    fixed checkStyle in Main.class

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 public class Main {
 
     public static void main(String[] args) {
-
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
@@ -15,6 +14,5 @@ public class Main {
         truck.spotWork();
         bulldozer.spotWork();
         excavator.spotWork();
-
     }
 }
