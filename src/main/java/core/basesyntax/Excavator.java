32a3826d19fa06f92cca2ab7e9b55e221e9d185a
Tonitrus <grom4er@gmail.com>
2020-12-01 13:14:47 +0200

    Task complete

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Excavator extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println(getClass().getSimpleName() + " start work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println(getClass().getSimpleName() + " stop work");
+    }
+}
