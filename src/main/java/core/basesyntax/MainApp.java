e5165427eb0c17583382735cd17fd3e8f473137e
antonpanchishko <PanchishkoMayor@gmail.com>
2020-12-01 16:52:24 +0200

    2) amended code

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
-        Truck truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
 
         bulldozer.doWork();
         excavator.doWork();
