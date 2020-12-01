70b9c49104ffea132f8323f214de002d0ec39c78
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2020-12-01 13:29:04 +0200

    In MainApp class create objects Excavator, Bulldozer, Truck and called their methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
 
+        excavator.doWork();
+        bulldozer.doWork();
+        truck.doWork();
+
+        excavator.stopWork();
+        excavator.stopWork();
+        excavator.stopWork();
+    }
 }
