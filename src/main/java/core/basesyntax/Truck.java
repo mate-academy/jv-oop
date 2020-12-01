436cf4e2477b16db24f1e8bdaec5ca1c30ab4ee4
YuraBondarenko <yuriybondarenko2001@gmail.com>
2020-12-01 14:43:05 +0200

    added solution

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck is ending working!");
+    }
+
+    @Override
+    public void doWork() {
+        System.out.println("Truck is starting working!");
+
+    }
+}
