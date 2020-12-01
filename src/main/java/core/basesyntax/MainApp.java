b916e553b90f9d01956b94e1c0461d6ec861b449
OleksandrPokotylenko <aln.nko.of@gmail.com>
2020-12-01 18:20:08 +0200

    done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
 
+        Machine[] machines = new Machine[] {truck, excavator, bulldozer};
+
+        for (Machine m : machines) {
+            m.doWork();
+        }
+        for (Machine m : machines) {
+            m.stopWork();
+        }
+    }
 }
