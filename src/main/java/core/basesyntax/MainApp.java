8e961e8134a2088735e6b7f4652b2ce1087995f2
vsak <vsak@itsmartflex.com>
2021-02-27 21:06:31 +0200

    Fixed code style

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,15 +3,14 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine bulldozer = new Bulldozer();
-        Machine truck = new Truck();
-        Machine excavator = new Excavator();
-
         bulldozer.doWork();
         bulldozer.stopWork();
 
+        Machine truck = new Truck();
         truck.doWork();
         truck.stopWork();
 
+        Machine excavator = new Excavator();
         excavator.doWork();
         excavator.stopWork();
     }
