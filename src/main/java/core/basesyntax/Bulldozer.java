c844c80232749075bd25a708e0230df8ed0045c5
Maksim <max.freedman.main@gmail.com>
2021-03-20 03:31:38 +0200

    added solution

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("The bulldozer started working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The bulldozer stopped working");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : machines) {
+            startWork(machine);
+        }
+        for (Machine machine : machines) {
+            stopWork(machine);
+        }
+    }
 
+    private static void startWork(Machine machine) {
+        machine.doWork();
+    }
+
+    private static void stopWork(Machine machine) {
+        machine.stopWork();
+    }
 }
+
