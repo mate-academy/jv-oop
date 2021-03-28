a580835315e78b8839c9251757d68c1c9ca372d7
Aalderaan <chyregd@gmail.com>
2021-03-28 12:28:07 +0200

    added solutions for OOP basic

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public abstract class Machine {
+    public void doWork() {
+        System.out.println("Machine started work");
+    }
+
+    public void stopWork() {
+        System.out.println("Machine stopped work");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine macT = new Truck();
+        Machine macB = new Bulldozer();
+        Machine macE = new Excavator();
 
+        macT.doWork();
+        macB.doWork();
+        macE.doWork();
+
+        macT.stopWork();
+        macB.stopWork();
+        macE.stopWork();
+    }
 }
