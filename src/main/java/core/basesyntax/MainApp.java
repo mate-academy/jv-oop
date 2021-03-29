1039965c82bd1664c3bcbd514a295ddca1803ef8
Maksim <max.freedman.main@gmail.com>
2021-03-29 11:34:48 +0300

    fixed all style violations

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,20 +3,14 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
-        for (Machine machine : machines) {
-            startWork(machine);
-        }
-        for (Machine machine : machines) {
-            stopWork(machine);
-        }
-    }
 
-    private static void startWork(Machine machine) {
-        machine.doWork();
-    }
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
 
-    private static void stopWork(Machine machine) {
-        machine.stopWork();
+        for (Machine machine : machines) {
+            machine.stopWork();
+        }
     }
 }
 
