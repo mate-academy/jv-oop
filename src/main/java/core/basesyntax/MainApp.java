0e69c1e6b11b03f3c94e5860ea314267c638171c
Vlad-Andr <andreev.v99@gmail.com>
2021-03-29 18:41:09 +0300

    fixed empty lines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
-
         Machine excavator = new Excavator();
         excavator.doWork();
         excavator.stopWork();
