705885607c93d019c4b3524c28996e5ef729a971
Nikber5 <nikber198@gmail.com>
2021-03-29 11:42:36 +0300

    Redundant empty lines removed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
@@ -11,7 +10,7 @@ public class Main {
         bulldozer.doWork();
         excavator.doWork();
 
-        truck.stopWork(); 
+        truck.stopWork();
         bulldozer.stopWork();
         excavator.stopWork();
     }
