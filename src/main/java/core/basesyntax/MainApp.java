972e2fb8f1124bdde7b8d59519828546b04c8b46
redxp <redxpertus1@gmail.com>
2021-03-29 17:49:36 +0300

    changed to references on abstract class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,21 +2,17 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Truck truck = new Truck();
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
 
         Machine[]machines = {truck,bulldozer,excavator};
 
-        for (int i = 0; i < machines.length; i++) {
-            for (int j = 0; j < machines.length; j++) {
-                if (i > 0) {
-                    break;
-                } else {
-                    machines[j].doWork();
-                }
-            }
-            machines[i].stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+        for (Machine machine : machines) {
+            machine.stopWork();
         }
     }
 }
