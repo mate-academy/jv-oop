21d1a11d4e638735d82e035e2b153cbd2c723835
redxp <redxpertus1@gmail.com>
2021-03-29 18:01:37 +0300

    sorry :) change it

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -6,7 +6,7 @@ public class MainApp {
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
 
-        Machine[]machines = {truck,bulldozer,excavator};
+        Machine[] machines = { truck, bulldozer, excavator };
 
         for (Machine machine : machines) {
             machine.doWork();
