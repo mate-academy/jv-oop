4dbead7d6c62aac2ed04e7d72681b052e072df89
redxp <redxpertus1@gmail.com>
2021-03-29 16:37:13 +0300

    added solution for jv-oop hw

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
 
+        Machine[]machines = {truck,bulldozer,excavator};
+
+        for (int i = 0; i < machines.length; i++) {
+            for (int j = 0; j < machines.length; j++) {
+                if (i > 0) {
+                    break;
+                } else {
+                    machines[j].doWork();
+                }
+            }
+            machines[i].stopWork();
+        }
+    }
 }
