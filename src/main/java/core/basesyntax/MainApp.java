98c934eb80ca1cc30a8f031fc933153682aed3fc
Andrii Romaniuk <romaniukandrii7@gmail.com>
2021-03-30 01:02:40 +0300

    task completed, checks passed.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truckMachine = new Truck();
+        Machine bulldozerMachine = new Bulldozer();
+        Machine excavatorMachine = new Excavator();
 
+        truckMachine.doWork();
+        bulldozerMachine.doWork();
+        excavatorMachine.doWork();
+
+        truckMachine.stopWork();
+        bulldozerMachine.stopWork();
+        excavatorMachine.stopWork();
+    }
 }
