caaef7ea22c6c1fce539b8fd1dbb6f6d0074cf2a
Darina <to.darina.to>
2021-03-30 13:51:11 +0300

    3) work of all machines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    void main(String[] args) {
+        Machine truck = new Truck();
+        truck.doWork();
+        truck.stopWork();
+        Machine bulldozer = new Bulldozer();
+        bulldozer.stopWork();
+        bulldozer.doWork();
+        Machine excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
+    }
 }
