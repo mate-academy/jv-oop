08d130c5d245fb4b4a4dbcb66ff9ed4585e126a5
Nikita <kupora3@gmail.com>
2021-03-30 17:47:13 +0300

    rename messages for machines

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Bulldozer extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Started work...");
+        System.out.println("Bulldozer started work...");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Stopped work...");
+        System.out.println("Bulldozer stopped work...");
     }
 }
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Started work...");
+        System.out.println("Excavator started work...");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Stopped work...");
+        System.out.println("Excavator stopped work...");
     }
 }
