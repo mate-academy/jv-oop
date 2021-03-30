6430f787b9ec78d3e5a92bc091a00e05d29329b7
Alina <vasylyshyn.alina@gmail.com>
2021-03-30 15:56:01 +0300

    added all classes, methods

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        truck.doWork();
+        bulldozer.doWork();
+        excavator.doWork();
+        truck.stopWork();
+        bulldozer.stopWork();
+        excavator.stopWork();
+    }
+}
