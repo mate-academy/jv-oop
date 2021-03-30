415d47a89fe38797a970aa863de4358eed256850
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-03-30 19:29:54 +0300

    fixed for using polymorphism

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,15 +2,15 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Bulldozer bulldozer = new Bulldozer();
+        Machine bulldozer = new Bulldozer();
         bulldozer.doWork();
         bulldozer.stopWork();
 
-        Excavator excavator = new Excavator();
+        Machine excavator = new Excavator();
         excavator.doWork();
         excavator.stopWork();
 
-        Truck truck = new Truck();
+        Machine truck = new Truck();
         truck.doWork();
         truck.stopWork();
     }
