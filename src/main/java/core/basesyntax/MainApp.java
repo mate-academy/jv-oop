92b9351f41d0fe692ae0935c84762814ff772642
anufriiev <dimaanufriev1012@gmail.com>
2021-03-30 13:55:30 +0300

    HW-1. OOP

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+
+        Machine bulldozer = new Bulldozer();
+          bulldozer.doWork();
+
+        Machine track = new Track();
+          track.doWork();
+
+        Machine excavator = new Excavator();
+          excavator.doWork();
+
+          bulldozer.stopWork();
+
+          track.stopWork();
+
+          excavator.stopWork();
+
+
+    }
 
 }
