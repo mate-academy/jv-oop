a8a3f2804055f2676489d3286810a21fb161309c
Mariia Chekmak <mariiachekmak@MacBook-Pro-Mariia.local>
2021-03-30 17:06:07 +0300

    finished hw

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,18 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+
+        System.out.println(truck.doWork());
+        System.out.println(excavator.doWork());
+        System.out.println(bulldozer.doWork());
+        System.out.println(truck.stopWork());
+        System.out.println(excavator.stopWork());
+        System.out.println(bulldozer.stopWork());
+
+    }
 }
