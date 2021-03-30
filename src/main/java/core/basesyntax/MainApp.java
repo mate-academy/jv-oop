64110bf0d2e992bfaa4eeff329d789406d531a11
RomaIlchyshyn <ilchyshynroma@gmail.com>
2021-03-30 23:43:36 +0300

    first commit

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
 
+        Machine truck = new Truck();
+        Machine buldozer = new Buldozer();
+        Machine excavator = new Excavator();
+
+        truck.doWork();
+        buldozer.doWork();
+        excavator.doWork();
+
+        truck.stopWork();
+        buldozer.stopWork();
+        excavator.stopWork();
+    }
 }
