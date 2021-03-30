79c2ef019913a8b49842d15a6f4a666b6c735149
Koziarevskyi <koziarevskyi@gmail.com>
2021-03-30 21:55:28 +0300

    C_001 - Created new project. Created Classes Machine, Truck, Bulldozer and Excavator. First Commit.

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public class Machine {
+
+    public abstract void doWork();
+
+    public abstract void stopWork();
+
+}
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,5 @@
+package core.basesyntax;
+
+public class Truck extends Machine{
+
+}
