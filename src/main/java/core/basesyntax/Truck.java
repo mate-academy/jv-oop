a4b1f7e7136b542be09404b332992d36d057e68a
Veronika <koraf9712@gmail.com>
2021-03-30 15:20:42 +0300

    Added all methods

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,5 +1,13 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck stared working!");
+    }
 
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped working!");
+    }
 }
