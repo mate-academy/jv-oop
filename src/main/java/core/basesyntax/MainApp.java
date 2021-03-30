1c7435fc86071a1d1a0f9c5946ba0ad5c0d588cd
anufriiev <dimaanufriev1012@gmail.com>
2021-03-30 14:28:36 +0300

    HW-1. OOP. Commit 2

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-
         Machine bulldozer = new Bulldozer();
         bulldozer.doWork();
 
@@ -13,11 +12,7 @@ public class MainApp {
         excavator.doWork();
 
         bulldozer.stopWork();
-
         track.stopWork();
-
         excavator.stopWork();
-
     }
-
 }
