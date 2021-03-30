1a951a52c3b66631c2f0ea1f8c434b193773e4af
Lugos <lugos94@gmail.com>
2021-03-30 21:57:02 +0300

    Create class Machine containing methods public abstract void doWork() and public abstract void stopWork(). Create classes Truck, Bulldozer, Excavator that will inherit from Machine.

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("The Bulldozer started to work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The Bulldozer stopped working");
+    }
+}
---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("The Excavator started to work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The Excavator stopped working");
+    }
+}
