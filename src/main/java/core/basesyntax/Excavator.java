f992456ec4d13673dc4ecf0a3277455c941591c3
Nikita <kupora3@gmail.com>
2021-03-30 15:10:17 +0300

    all classes

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Started work...");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Stopped work...");
+    }
+}
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Started work...");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Stopped work...");
+    }
+}
