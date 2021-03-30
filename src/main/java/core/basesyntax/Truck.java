a9c8d96cccc745c7daf4d473ad8bbcd483d90b5f
Iryna Ustymenko <i.ustimenko18@gmail.com>
2021-03-30 14:48:00 +0300

    Created classes Truck, Bulldozer, Excavator and overrided doWork(), stopWork()

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+
+    public void doWork() {
+        System.out.println("Truck started working");
+    }
+
+    public void stopWork() {
+        System.out.println("Truck stopped working");
+    }
+}
