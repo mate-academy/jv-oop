de357043389f8f813a1ed06fd9812cc6bbbc0217
Dalai <semenov.dalay@gmail.com>
2021-03-30 09:26:58 +0300

    Done

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("A bulldozer starts working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("A bulldozer stop working");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
-public class MainApp {
-
+public class  MainApp {
+    public static void main(String[] args){
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        excavator.doWork();
+        excavator.stopWork();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+        truck.doWork();
+        truck.stopWork();
+    }
 }
