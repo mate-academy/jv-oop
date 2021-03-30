da45c030bc3c5c3c78f3e4d66d67db5ebdf57a12
Дмитро <dima4994@gmail.com>
2021-03-30 14:29:09 +0300

    Created classes: Machine, Truck, Bulldozer, Excavator. Created methods doWork(),stopWork() in class Machine and override these for classes Truck, Bulldozer, Excavator. Also In Main class created object of each class.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,18 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+
+        truck.doWork();
+        excavator.doWork();
+        bulldozer.doWork();
+
+        truck.stopWork();
+        excavator.stopWork();
+        bulldozer.stopWork();
+    }
 }
