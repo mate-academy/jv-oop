6380975e88ca49d171f5fdf325a6d6bb1d99eb29
Lugos <lugos94@gmail.com>
2021-03-31 08:56:16 +0300

    Redundant empty line in Classes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
-
         Machine truck = new Truck();
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
@@ -16,5 +15,4 @@ public class MainApp {
         excavator.stopWork();
         bulldozer.stopWork();
     }
-
 }
