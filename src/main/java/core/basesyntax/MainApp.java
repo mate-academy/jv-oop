d67ea566fe23e8ff1c9898e0a33e6adcc558d19f
Bohdan Chupika <chupika.bohdan@ukr.net>
2021-04-27 14:09:39 +0300

    added fixes after review

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,6 +3,6 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Valid solution!");
     }
 }
