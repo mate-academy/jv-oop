52480205e4482b1d01de3dbe6c8fc3b24ae7c32f
Ruslan Malyavsky <ruslan-gorod@ukr.net>
2021-05-11 17:45:04 +0300

    Created classes Excavator, Bulldozer, Truck and created Main method in  MainApp class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine e = new Excavator();
+        Machine b = new Bulldozer();
+        Machine t = new Truck();
+        e.doWork();
+        b.doWork();
+        t.doWork();
+        e.stopWork();
+        b.stopWork();
+        t.stopWork();
+    }
 }
