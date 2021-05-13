ed5a86b35c88e8b3723437534977671783338481
Ruslan Malyavsky <ruslan-gorod@ukr.net>
2021-05-13 14:29:50 +0300

    Renamed variables

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,14 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine e = new Excavator();
-        Machine b = new Bulldozer();
-        Machine t = new Truck();
-        e.doWork();
-        b.doWork();
-        t.doWork();
-        e.stopWork();
-        b.stopWork();
-        t.stopWork();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        excavator.doWork();
+        bulldozer.doWork();
+        truck.doWork();
+        excavator.stopWork();
+        bulldozer.stopWork();
+        truck.stopWork();
     }
 }
