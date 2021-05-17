385c672e6043bc3b1c498744b198c1bc7419fb08
alexpogrebniy <aleksandr_pogreb@ukr.net>
2021-05-17 17:25:16 +0300

    task fixed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Truck alfa = new Truck();
-        Bulldozer beta = new Bulldozer();
-        Excavator gamma = new Excavator();
+        Machine alfa = new Truck();
+        Machine beta = new Bulldozer();
+        Machine gamma = new Excavator();
 
         alfa.doWork();
         beta.doWork();
