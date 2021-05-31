e068506d4a17b159772c8b313ae663ea7ccda7f4
Daniel Kuruch <kuruch1004@gmail.com>
2021-05-31 15:06:57 +0300

    added new classes Bulldozer, Truck, Excavator , test these classes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,17 @@ package core.basesyntax;
 
 public class MainApp {
 
+  public static void main(String[] args) {
+    Machine[] machines = new Machine[]{
+        new Bulldozer(),
+        new Excavator(),
+        new Truck()
+    };
+    for (Machine machine : machines) {
+      machine.doWork();
+    }
+    for (Machine machine : machines){
+      machine.stopWork();
+    }
+  }
 }
