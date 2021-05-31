fdc7fd26372f849cbeb11479e9706ff6a7e4452b
yuriiadam <dr.damba@outlook.com>
2021-05-31 22:29:29 +0300

    added new classes of machines, class machine with abstract methods.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine firstCare = new Excavator();
+
+        Machine secondCare = new Bulldozer();
+
+        Machine thirdCare = new Truck();
+
+        System.out.println(firstCare.doWork());
+        System.out.println(secondCare.doWork());
+        System.out.println(thirdCare.doWork());
+        System.out.println(firstCare.stopWork());
+        System.out.println(secondCare.stopWork());
+        System.out.println(thirdCare.stopWork());
+
+    }
 
 }
