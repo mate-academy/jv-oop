eb6fc9960109f8eb6963086ea8721809af55a66c
vKutik <84785564+vKutik@users.noreply.github.com>
2021-06-01 17:59:34 +0300

    Create  classes Machine, Truck, Bulldozer, Excavator.
    Add abstract methods doWork(), stopWork() for class Machine.
    
    Override abstract methods for classes Truck, Excavator, Bulldozer.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine excavatorOne = new Excavator();
+        Machine truckOne = new Truck();
+        Machine bulldozerOne = new Bulldozer();
 
+        excavatorOne.doWork();
+        truckOne.doWork();
+        bulldozerOne.doWork();
+
+        excavatorOne.stopWork();
+        truckOne.stopWork();
+        bulldozerOne.stopWork();
+    }
 }
