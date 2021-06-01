6ce4309aa2e1f63059f5682abaa44f1b93e08783
Coyotious <coyotious@gmail.com>
2021-06-01 18:36:51 +0300

    added classes: Truck, Bulldozer, Excavator; added do and stop methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine volvo = new Truck();
+        Machine ain = new Bulldozer();
+        Machine doosan = new Excavator();
+        volvo.doWork();
+        ain.doWork();
+        doosan.doWork();
+        volvo.stopWork();
+        ain.stopWork();
+        doosan.stopWork();
+    }
 }
