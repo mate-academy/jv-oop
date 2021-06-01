55895b2a987cb1605c2a6484c418e52dfd38754c
sSlonIk <kryachok@gmail.com>
2021-06-01 17:22:09 +0300

    fixef mvn

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,16 +1,15 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
-        @Override
-        public void doWork() {
-                System.out.println("Starting Truck: 1... 2... 3... Oooops, error:\"ratatatata\".");
+    @Override
+    public void doWork() {
+        System.out.println("Starting Truck: 1... 2... 3... Oooops, error:\"ratatatata\".");
 
-        }
+    }
 
-        @Override
-        public void stopWork() {
-                System.out.println("Fuel out. Truck is not working.");
-
-        }
+    @Override
+    public void stopWork() {
+        System.out.println("Fuel out. Truck is not working.");
+    }
 
 }
