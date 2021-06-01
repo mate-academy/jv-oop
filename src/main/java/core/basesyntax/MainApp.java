156151017bb8c163e00191d9b1689c2fc57a3be8
Chupiak <chupyak@gmail.com>
2021-06-01 19:07:39 +0300

    All worked All stop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine buldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        buldozer.doWork();
+        excavator.doWork();
+        truck.doWork();
+        buldozer.stopWork();
+        excavator.stopWork();
+        truck.stopWork();
+    }
 }
