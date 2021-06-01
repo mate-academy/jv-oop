1b13d1ecb079bf97d970e0555957357051d2a442
Vladyslav Baydak <74090608+Optipist@users.noreply.github.com>
2021-06-01 19:20:45 +0300

    Deleted whitespaces

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,19 +1,13 @@
 package core.basesyntax;
 
 public class Excavator extends Machine {
-
     @Override
     public void doWork() {
-
         System.out.println("\nExcavator is starting it's work now!\n");
-
     }
 
     @Override
     public void stopWork() {
-
         System.out.println("\nExcavator is not available now!\n");
-
     }
-
 }
