920f9a211a87c01581aabd112c63898ea6f2b855
yuriiadam <dr.damba@outlook.com>
2021-06-01 13:01:34 +0300

    remove some empty lines is done finally

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,11 +3,8 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine firstCare = new Excavator();
-
         Machine secondCare = new Bulldozer();
-
         Machine thirdCare = new Truck();
-
         System.out.println(firstCare.doWork());
         System.out.println(secondCare.doWork());
         System.out.println(thirdCare.doWork());
