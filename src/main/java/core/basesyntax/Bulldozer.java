4190be0188658cee7c3d4940679fdbe8026f0c12
Tamara-Paskalova <forptv@gmail.com>
2021-06-01 16:22:44 +0300

    created classes Machine, Truck, Bulldozer, Excavator, added doWork, stopWork methods at all classes.

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class Bulldozer extends Machine {
+public class Bulldozer extends Machine{
     @Override
     public void doWork() {
         System.out.println("I'm a bossy bulldozer. I lead everyone!");
