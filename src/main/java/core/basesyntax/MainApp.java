f84ff1ec0c3d9d76f13f9b90c924ab189e8d0d87
“Valerii <torsher09@gmail.com>
2021-06-01 12:58:35 +0300

    changed type of objects to abstract reference

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,13 +2,13 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Truck truck = new Truck();
+        Machine truck = new Truck();
         truck.doWork();
         truck.stopWork();
-        Bulldozer bulldozer = new Bulldozer();
+        Machine bulldozer = new Bulldozer();
         bulldozer.doWork();
         bulldozer.stopWork();
-        Excavator excavator = new Excavator();
+        Machine excavator = new Excavator();
         excavator.doWork();
         excavator.stopWork();
     }
