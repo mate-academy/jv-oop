6b4b069425ca12a7a936212a7fe903b63bdda94f
Vladyslav Baydak <74090608+Optipist@users.noreply.github.com>
2021-06-01 19:19:16 +0300

    Deleted whitespaces

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,19 +1,13 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
-
     @Override
     public void doWork() {
-
         System.out.println("\nTruck is starting it's work now!\n");
-
     }
-
+    
     @Override
     public void stopWork() {
-
         System.out.println("\nTruck is not available now!\n");
-
     }
-
 }
