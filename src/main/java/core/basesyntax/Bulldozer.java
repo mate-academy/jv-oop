e0ff755c88c45ade7d253de02afa140d37d1cba3
Vladyslav Baydak <74090608+Optipist@users.noreply.github.com>
2021-06-01 19:21:08 +0300

    Deleted whitespaces

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,19 +1,13 @@
 package core.basesyntax;
 
 public class Bulldozer extends Machine {
-
     @Override
     public void doWork() {
-
         System.out.println("\nBulldozer is starting it's work now!\n");
-
     }
 
     @Override
     public void stopWork() {
-
         System.out.println("\nBulldozer is not available now!\n");
-
     }
-
 }
