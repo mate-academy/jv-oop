8d1b8772cb132d88af4f1d8cb8549d71f79939da
dmitriy tverdokhlibov <dmitriytverdokhlibov@yahoo.com>
2021-06-08 09:34:37 +0200

    create new branch develop and make solution jv-oop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Excavator excavator = new Excavator();
+        Bulldozer bulldozer = new Bulldozer();
 
+        truck.doWork();
+        excavator.doWork();
+        bulldozer.doWork();
+
+        truck.stopWork();
+        excavator.stopWork();
+        bulldozer.stopWork();
+    }
 }
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("start work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("stop work");
+    }
+}
