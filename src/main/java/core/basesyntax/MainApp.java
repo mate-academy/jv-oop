ed7a4b48ad84c94ec79f415832357218b5a422b6
ablindaruk <85460808+ablindaruk@users.noreply.github.com>
2021-06-16 14:46:36 +0300

    It was created: classes -  Machine, Bulldozer, Excavator, Truck. it was creted method public static void main() inside class MainApp has got two loops: one - for start working, second - to stop working.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine escavator = new Excavator();
+
+        Machine[] machines = new Machine[]{truck, bulldozer, escavator};
+
+        //start working of all machines
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+
+        //stop to work for all machines
+        for (Machine machine : machines) {
+            machine.stopWork();
+        }
+    }
+
+
 
 }
