e8c1574bf66d04fffa2a0855e19a068c3e0b936a
Olexandr <olesukmail@gmail.com>
2021-06-18 18:36:19 +0300

    Added 4 java classes (Excavator, Bulldozer, Truck, Machine), override methods "doWork" and "stopWork" and create 3 objects in MainApp.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,10 +2,9 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Excavator excavator = new Excavator();
-        Bulldozer bulldozer= new Bulldozer();
-        Truck truck = new Truck();
-
+        Machine excavator = new Excavator();
+        Machine bulldozer= new Bulldozer();
+        Machine truck = new Truck();
         excavator.doWork();
         excavator.stopWork();
         bulldozer.doWork();
