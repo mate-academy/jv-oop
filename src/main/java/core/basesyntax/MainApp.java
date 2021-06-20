8f5c363fbf1b724a950010c716cc4cb87c1adb22
Alex Pravda <kabinkos@gmail.com>
2021-06-20 18:13:01 +0300

    OOP Basic HW, rev.1

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,7 +3,15 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
-        String result = "Test GIT";
-    }
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
 
+        Machine[] machines = new Machine[] {bulldozer, excavator, truck};
+
+        for (Machine checkAll : machines) {
+            checkAll.doWork();
+            checkAll.stopWork();
+        }
+    }
 }
