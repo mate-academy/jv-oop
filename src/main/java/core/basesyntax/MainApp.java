6d1c3acb2eba9bf90c90edbfd2c4918865ef42fb
vbuchii <volodymyrb78@gmail.com>
2021-06-29 21:18:56 +0300

    create objects, start/stop work

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,25 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.machines.Bulldozer;
+import core.basesyntax.machines.Excavator;
+import core.basesyntax.machines.Machine;
+import core.basesyntax.machines.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+
+        System.out.println("START:");
+        truck.doWork();
+        bulldozer.doWork();
+        excavator.doWork();
+
+        System.out.println();
+        System.out.println("STOP:");
+        truck.stopWork();
+        bulldozer.stopWork();
+        excavator.stopWork();
+    }
 }
