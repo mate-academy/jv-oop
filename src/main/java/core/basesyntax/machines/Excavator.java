d8a0f9ae47ae1a54cd71aaf1f2952cfac0697a09
vbuchii <volodymyrb78@gmail.com>
2021-06-29 21:18:19 +0300

    add required classes

---
+++ src/main/java/core/basesyntax/machines/Excavator.java
@@ -0,0 +1,13 @@
+package core.basesyntax.machines;
+
+public class Excavator extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("The excavator has started to work.");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The excavator finished working.");
+    }
+}
