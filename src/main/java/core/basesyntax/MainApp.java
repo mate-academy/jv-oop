e5866b954d5e30f2cb7833ac0d368508c5973e87
Dana Khromenko <danadiadius@gmail.com>
2021-07-04 08:24:55 +0300

    Added psvm method, where created an array of machines, called their soWork() and stopWork() methods.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        // Array of different machines
+        Machine[] machines = new Machine[3];
+        machines[0] = new Truck();
+        machines[1] = new Bulldozer();
+        machines[2] = new Excavator();
 
+        // Start work of all machines
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+
+        // Stop work of all machines
+        for (Machine machine : machines) {
+            machine.stopWork();
+        }
+    }
 }
