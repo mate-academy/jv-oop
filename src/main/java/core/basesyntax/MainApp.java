42e895ba2c1e8ff4c3df0955d68798b7333fbfa3
Dana Khromenko <danadiadius@gmail.com>
2021-07-04 08:32:52 +0300

    Separated the creating of the machines and filling the array

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,11 +2,16 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        // Array of different machines
+        // Create different machines
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+
+        // Create and fill the machines array
         Machine[] machines = new Machine[3];
-        machines[0] = new Truck();
-        machines[1] = new Bulldozer();
-        machines[2] = new Excavator();
+        machines[0] = truck;
+        machines[1] = bulldozer;
+        machines[2] = excavator;
 
         // Start work of all machines
         for (Machine machine : machines) {
