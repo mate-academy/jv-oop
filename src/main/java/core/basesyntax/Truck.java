9631651565df051017652ede19dd7c743ccc5226
Dana Khromenko <danadiadius@gmail.com>
2021-07-04 08:19:40 +0300

    Changed printed info in the methods doWork() and stopWork()

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Truck extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Truck started its work");
+        System.out.println("The truck started its work.");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Truck stopped its work");
+        System.out.println("The truck stopped its work.");
     }
 }
