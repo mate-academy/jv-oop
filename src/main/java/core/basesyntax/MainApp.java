adfb0d02fcffa48d3c278864e00fc3c00b1cf7ae
Roman Novikov <novikovr121281@gmail.com>
2021-07-19 21:02:33 +0300

    my home work is done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Excavator excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
+        System.out.println();
 
+        Bulldozer bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+        System.out.println();
+
+        Truck truck = new Truck();
+        truck.doWork();
+        truck.stopWork();
+        System.out.println();
+    }
 }
