b100532e57d02e4a10993c925a9c72de3c8858da
Viktor Yukhno <pingvinomania@gmail.com>
2021-07-19 19:57:14 +0300

    Added class Machine with abstract methods doWork() and stopWork(). Added classes Truck, Bulldozer and Excavator that inherit from class Machine. Added override methods in those classes.

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public abstract class Machine {
+    public String model;
+
+    public abstract void doWork();
+
+    public abstract void stopWork();
+
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Excavator excavator = new Excavator();
+        excavator.model = "Cat";
+        Bulldozer bulldozer = new Bulldozer();
+        bulldozer.model = "John Deere";
+        Truck truck = new Truck();
+        truck.model = "Ford";
+
+        bulldozer.doWork();
+        excavator.doWork();
+        truck.doWork();
+
+        bulldozer.stopWork();
+        excavator.stopWork();
+        truck.stopWork();
+    }
 
 }
