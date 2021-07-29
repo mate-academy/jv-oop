75d4af063014ff42e8bd76304a69334ba3a09874
Pavlo Heorh <georg_pasha@ukr.net>
2021-07-29 22:51:37 +0300

    Problem was solved in mentioned order: new classes were created; methods  doWork() and stopWork were override; In Main class objects of each class were created; All machines starts and stops

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,11 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
-Truck ford = new Truck();
+    public static void main(String[] args) {
+        Truck ford = new Truck();
+          ford.doWork();
+          ford.stopWork();
+        Bulldozer graider = new Bulldozer();
+          graider.doWork();
+          graider.stopWork();
+        Excavator CAT = new Excavator();
+        CAT.doWork();
+        CAT.stopWork();
+    }
 
-Bulldozer graider = new Bulldozer();
-
-Excavator CAT = new Excavator();
 
 
 }
