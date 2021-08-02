f044871014b2789a1d73835b00fea0214db0ab12
Pavlo Heorh <georg_pasha@ukr.net>
2021-08-02 19:32:56 +0300

    checkstyle mistakes were corrected

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine caterpillar = new Excavator();
-          caterpillar.doWork();
-          caterpillar.stopWork();
+        caterpillar.doWork();
+        caterpillar.stopWork();
         Machine graider = new Bulldozer();
-          graider.doWork();
-          graider.stopWork();
+        graider.doWork();
+        graider.stopWork();
         Machine ford = new Truck();
-          ford.doWork();
-          ford.stopWork();
+        ford.doWork();
+        ford.stopWork();
     }
 
 }
