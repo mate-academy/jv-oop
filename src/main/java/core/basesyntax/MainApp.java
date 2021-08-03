e4cf37377c4871f83176d6149cb0fd7e20d097d0
Yurii <wervmer>
2021-08-03 18:04:57 +0300

    Solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public int matchResult(int result1, int result2, int bet1, int bet2) {
+        return ((result1 == bet1 && result2 == bet2) ? 2 : ((result1 > result2 && bet1 > bet2) ? 1 : ((result1 < result2 && bet1 < bet2) ? 1 : ((result1 == result2 && bet1 == bet2) ? 1 : 0))));
+    }
 }
