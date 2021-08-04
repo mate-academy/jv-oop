157e01ca09db552f37ad4e9e0bac2df8fe761a02
Yurii <wervmer>
2021-08-04 15:04:58 +0300

    Deleted empty lines and used polymorphism (when defining objects)

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,14 +1,7 @@
 package core.basesyntax;
 
 public class Bulldozer extends Machine{
-
     @Override
-    public void doWork() {
-        System.out.println("Bulldozer started work");
-    }
-
-    @Override
-    public void stopWork() {
-        System.out.println("Bulldozer stopped work");
-    }
+    public void doWork() { System.out.println("Bulldozer started work"); }
+    public void stopWork() { System.out.println("Bulldozer stopped work"); }
 }
