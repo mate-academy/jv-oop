19b6ddbb5a47938807de9af44e61d31feffbae6f
Oleg Kvasha <oleg.kvasha.00@gmail.com>
2021-08-10 14:23:54 +0300

    Added Machine, Truck, Excavator, Bulldozer

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machineArray = new Machine[3];
+        machineArray[0] = new Truck();
+        machineArray[1] = new Bulldozer();
+        machineArray[2] = new Excavator();
 
+        for (Machine el : machineArray) {
+            el.doWork();
+            el.stopWork();
+        }
+    }
 }
