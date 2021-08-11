4f8bee99ba1798d9ac736ffc2407f3c4212a5684
ASelivonchyk <aselivonchyk@gmail.com>
2021-08-11 18:14:36 +0300

    исправлены ошибки код сайла. изменет цикл for на foreach.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -11,7 +11,7 @@ public class MainApp {
             name.doWork();
         }
         for (Machine name : machines) {
-            name.doWork();
+            name.stopWork();
         }
     }
 }
