7dcb12cff4f97f8ab99031bd84d10b91969c7c37
mishamolnar <michaellmuller423@gmail.com>
2021-08-11 19:06:04 +0300

    truck excavator and bulldozer created

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,8 @@
+package core.basesyntax;
+
+public abstract class Machine {
+
+    abstract public void doWork();
+    abstract public void stopWork();
+
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,9 @@
 package core.basesyntax;
 
 public class MainApp {
+    Machine excavator = new Excavator();
+    Machine bulldozer = new Bulldozer();
+    Machine truck = new Truck();
 
+    
 }
