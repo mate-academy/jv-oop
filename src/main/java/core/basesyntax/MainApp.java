3bb6dafb9701be077493157e5531f24459c925a4
Dima Petrushenko <dmytro.petrushenko@gmail.com>
2021-08-12 13:05:57 +0300

    added new classes and made new objects

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
+import core.garage.Bulldozer;
+import core.garage.Excavator;
+import core.garage.Machine;
+import core.garage.Truck;
+
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truckMan = new Truck();
+        Machine bulldozerCaterpillar =new Bulldozer();
+        Machine excavatorKomatsu = new Excavator();
+
+        truckMan.doWork();
+        bulldozerCaterpillar.doWork();
+        excavatorKomatsu.doWork();
+
+        truckMan.stopWork();
+        bulldozerCaterpillar.stopWork();
+        excavatorKomatsu.stopWork();
+    }
 
 }
