305026cc33e4806c2b3857e604dd9b47eff459c0
Oleksandr Sydorskyi <oleksandrsydorskyi@gmail.com>
2021-08-13 16:44:24 +0300

    Used abstract reference instead of specific one: Deleted redundant empty lines from code.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,15 +2,12 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-
-        Truck truck = new Truck();
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
-
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
         truck.doWork();
         excavator.doWork();
         bulldozer.doWork();
-
         truck.stopWork();
         excavator.stopWork();
         bulldozer.stopWork();
