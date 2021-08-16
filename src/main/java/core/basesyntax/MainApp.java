9a453b0ed1cd7bc02ad47c244362675247541f62
vitalii_yelinskyi <yelinskyizp@gmail.com>
2021-08-16 17:20:24 +0300

    rename m to machine & delete some empty line

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -8,9 +8,9 @@ public class MainApp {
         machines[1] = new Bulldozer();
         machines[2] = new Truck();
 
-        for (Machine m: machines) {
-            m.doWork();
-            m.stopWork();
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
             System.out.println();
         }
     }
