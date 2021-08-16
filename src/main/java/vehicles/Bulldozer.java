286dc3d0ce71ff7e141807977725c5d56579381a
Vladyslav Gural <vladgural@gmail.com>
2021-08-16 17:35:10 +0300

    created class Bulldozer with implementing class Machine

---
+++ src/main/java/vehicles/Bulldozer.java
@@ -0,0 +1,14 @@
+package vehicles;
+
+public class Bulldozer extends Machine{
+
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer has started its working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer has stopped its working");
+    }
+}
