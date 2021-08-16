d2a47ac9df47452e39eb145b4fae6fbfd91fc61a
mishamolnar <michaellmuller423@gmail.com>
2021-08-16 15:43:14 +0300

    empty lines deleted

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,11 +3,9 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
-
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
-
         excavator.doWork();
         bulldozer.doWork();
         truck.doWork();
