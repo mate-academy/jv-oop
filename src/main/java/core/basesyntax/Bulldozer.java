5de5e4100535ab62bad9451ec30791f82d18a058
vitalii_yelinskyi <yelinskyizp@gmail.com>
2021-08-16 12:49:15 +0300

    edit method stopWork

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -13,6 +13,6 @@ public class Bulldozer extends Machine {
 
     @Override
     public void stopWork() {
-        System.out.println("Bulldozer is working");
+        System.out.println("Bulldozer is stopped");
     }
 }
