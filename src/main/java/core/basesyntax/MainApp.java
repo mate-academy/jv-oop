07444b7cc33c58a713e30e562ffdcbaca168b0c0
Vladyslav Gural <vladgural@gmail.com>
2021-08-16 17:47:35 +0300

    realized tast in MainApp class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
-public class MainApp {
+import vehicles.Bulldozer;
+import vehicles.Excavator;
+import vehicles.Machine;
+import vehicles.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine[] arrayOfVeihcles = {new Truck(), new Bulldozer(), new Excavator()};
+
+        for (Machine veihcle : arrayOfVeihcles) {
+            veihcle.doWork();
+        }
+        System.out.println();
+        for (Machine veihcle : arrayOfVeihcles) {
+            veihcle.stopWork();
+        }
+    }
 }
