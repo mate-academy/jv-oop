072f9261a9aa2292b9ffb0552464d926915fe1eb
Alexandr <netiazhevskyi@gmail.com>
2021-08-16 21:02:53 +0300

    Created new classes, methods, overrided accordingly

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Bulldozer bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        Excavator excavator = new Excavator();
+        excavator.doWork();
+        Truck truck = new Truck();
+        truck.doWork();
+        bulldozer.stopWork();
+        excavator.stopWork();
+        truck.stopWork();
+    }
 }
+
