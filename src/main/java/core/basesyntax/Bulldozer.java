c12b8881dd756dd8bfeb6556c2fb8b8f9173a31f
SonyaPakhomova <88778908+SonyaPakhomova@users.noreply.github.com>
2021-08-17 13:37:46 +0300

    All machines started them work and then finished(1)

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+    public void doWork() {
+        System.out.println("bulldozer started its work");
+    }
+
+    public void stopWork() {
+        System.out.println("bulldozer finished its work");
+    }
+}
+
+
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine buildozer = new Bulldozer();
+        buildozer.doWork();
+        Machine excavator = new Excavator();
+        excavator.doWork();
+        Machine truck = new Truck();
+        truck.doWork();
+        buildozer.stopWork();
+        excavator.stopWork();
+        truck.stopWork();
+    }
 
 }
