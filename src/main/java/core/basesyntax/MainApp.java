5a065624d430c8660834453d9e1e893ca4a80619
Nazar Hetun <nazarhetun@gmail.com>
2021-08-17 16:42:11 +0300

    fixed some errors

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,9 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
-
     public static void main(String[] args) {
-
         Machine excavator = new Excavator();
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
