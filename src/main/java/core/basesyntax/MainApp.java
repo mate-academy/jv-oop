bd4672aa6e94d2c3989d5067d825019a0d975cfe
Svyatos_Furs <50773032+Svyatos-dev@users.noreply.github.com>
2021-08-17 17:40:02 +0300

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     
+    
     public static void main(String[] args) {
         Machine bulldozer = new Bulldozer();
         bulldozer.doWork();
