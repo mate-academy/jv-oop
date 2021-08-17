cb0710816497d0ce67c003497ca3c5f7d1394d46
Andrii Tyndiuk <andriytundyk@gmail.com>
2021-08-17 15:49:31 +0300

    added getters and setters

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,6 +1,11 @@
 package core.basesyntax;
 
 public abstract class Machine {
+    private String name;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
     public abstract void doWork();
     public abstract void stopWork();
 }
