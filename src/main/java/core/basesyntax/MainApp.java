c2bfac888da66a0a56b7d5db89de945ecd04cd26
Nazar Hetun <nazarhetun@gmail.com>
2021-08-17 16:37:47 +0300

    All tasks in readme have been completed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,19 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+
+        Machine[] machines = new Machine[]{excavator, truck, bulldozer};
+
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+        for (Machine machine : machines) {
+            machine.stopWork();
+        }
+    }
 }
