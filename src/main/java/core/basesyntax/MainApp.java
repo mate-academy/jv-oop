88c454cd0d617ab484dd410a129a6f8979d4fc24
Andruwa808 <legacy112233@gmail.com>
2021-08-17 14:33:22 +0300

    completed the task

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,20 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+
+        //Start
+        excavator.doWork();
+        truck.doWork();
+        bulldozer.doWork();
+
+        //Stop
+        excavator.stopWork();
+        truck.stopWork();
+        bulldozer.stopWork();
+    }
+
 }
