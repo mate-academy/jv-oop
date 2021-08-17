1c8a858472637dd098f403d1127bc551d35bfb03
misha8096 <sichmisha@gmail.com>
2021-08-17 09:47:21 +0300

    created new classes and methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
-        Truck truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
         bulldozer.doWork();
         excavator.doWork();
         truck.doWork();
@@ -14,3 +14,4 @@ public class MainApp {
         truck.stopWork();
     }
 }
+
