f36e9fbfa36f2aecb57a241333769f6f8cc8c141
Stas Shepet <milka33349@gmail.com>
2021-08-17 14:52:24 +0300

    names changed, empty lines removed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,16 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine bmw = new Excavator();
-        Machine volkswagen = new Bulldozer();
-        Machine audi = new Truck();
-
-        bmw.doWork();
-        volkswagen.doWork();
-        audi.doWork();
-
-        bmw.stopWork();
-        volkswagen.stopWork();
-        audi.stopWork();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        excavator.doWork();
+        bulldozer.doWork();
+        truck.doWork();
+        excavator.stopWork();
+        bulldozer.stopWork();
+        truck.stopWork();
     }
 }
