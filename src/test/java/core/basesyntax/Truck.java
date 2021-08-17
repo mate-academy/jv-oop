9def1d39d0dd6317d540f2c5455bfca5beea39b0
pustoj0 <pustoj0@gmail.com>
2021-08-17 17:50:16 +0300

    inheritance system was added, new methods and their implementation

--- src/test/java/core/basesyntax/Truck.java
+++ src/test/java/core/basesyntax/Truck.java
@@ -1,4 +1,13 @@
 package core.basesyntax;
 
-public class Truck {
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck started work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped work");
+    }
 }
