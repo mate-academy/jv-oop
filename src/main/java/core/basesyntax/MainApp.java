2907e7202b272fcbfa679315a0cceead522805bf
Denys Smei <denys.smei@artcodevision.com>
2021-08-17 17:35:23 +0300

    Adding all Machines such as Bulldozer, Bulldozer, Truck and father class Machine. Also adding methods doWork() and stopWork() to each class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.machines.*;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine[] arrOfMachines = {new Bulldozer(), new Excavator(), new Truck()};
+        for (Machine machine : arrOfMachines) {
+            machine.doWork();
+        }
+        for (Machine machine : arrOfMachines) {
+            machine.stopWork();
+        }
+    }
 }
