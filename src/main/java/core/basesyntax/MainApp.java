f50524b65c4880fc960b2653d34e57be12f5c12f
V <v.ivashchenko42@protonmail.com>
2021-08-17 20:17:12 +0300

    Fixed checkstyle mistakes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -7,18 +7,14 @@ import core.basesyntax.model.Truck;
 
 public class MainApp {
     public static void main(String[] args) {
-
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
         Machine truck = new Truck();
-
         bulldozer.doWork();
         excavator.doWork();
         truck.doWork();
-
         bulldozer.stopWork();
         excavator.stopWork();
         truck.stopWork();
     }
 }
-
