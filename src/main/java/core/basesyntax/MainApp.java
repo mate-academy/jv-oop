6c2695ebe5f578b96f9d45ed763c916444c739dc
Roman Radikhovskiy <radihovskiyroman@gmail.com>
2021-08-17 14:33:45 +0300

    removed machine method constructor in Main class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,18 +2,6 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine machine = new Machine() {
-            @Override
-            public void doWork() {
-
-            }
-
-            @Override
-            public void stopWork() {
-
-            }
-        };
-
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
