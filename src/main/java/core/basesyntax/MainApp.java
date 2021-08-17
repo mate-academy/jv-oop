dfc4435b16bbc674527a4de3ebaa5e897fbdc203
Evgen Krasnenkov <krasnenkov.evgen@gmail.com>
2021-08-17 13:24:15 +0300

    abstract overwriting methods were implemented

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine noisy = new Bulldozer();
+        Machine quicky = new Excavator();
+        Machine speedy = new Truck();
+
+        noisy.doWork();
+        noisy.stopWork();
+        quicky.doWork();
+        quicky.stopWork();
+        speedy.doWork();
+        speedy.stopWork();
+    }
 
 }
