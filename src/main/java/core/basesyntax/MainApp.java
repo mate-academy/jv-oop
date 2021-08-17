788902f8eeb9f389acae277892b73ffbfcaea9ec
Roman Radikhovskiy <radihovskiyroman@gmail.com>
2021-08-17 14:10:36 +0300

    classes are created and methods are implemented

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,29 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine machine = new Machine() {
+            @Override
+            public void doWork() {
 
+            }
+
+            @Override
+            public void stopWork() {
+
+            }
+        };
+        
+        Excavator excavator = new Excavator();
+        Bulldozer bulldozer = new Bulldozer();
+        Truck truck = new Truck();
+
+        excavator.doWork();
+        bulldozer.doWork();
+        truck.doWork();
+
+        excavator.stopWork();
+        bulldozer.stopWork();
+        truck.stopWork();
+    }
 }
