13e418c20b87b18698d76228b82707c93e431a79
Denys Stefanko <denys.stefanko@gmail.com>
2021-08-17 14:57:28 +0300

    added fixes after review

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,7 +5,6 @@ public class MainApp {
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
-
         Machine[] machines = new Machine[] {excavator, bulldozer, truck};
         for (Machine machine : machines) {
             machine.doWork();
