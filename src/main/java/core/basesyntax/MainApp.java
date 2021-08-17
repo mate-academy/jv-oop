64baebb79876318f4ee45b96a30fe0027a8ba2d1
Serhii Orlov <orlov30@gmail.com>
2021-08-17 17:31:47 +0300

    fix accept: empty lines was remove.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,11 +5,9 @@ public class MainApp {
         Machine truck = new Truck();
         truck.doWork();
         truck.stopWork();
-
         Machine bulldozer = new Bulldozer();
         bulldozer.doWork();
         bulldozer.stopWork();
-
         Machine excavator = new Excavator();
         excavator.doWork();
         excavator.stopWork();
