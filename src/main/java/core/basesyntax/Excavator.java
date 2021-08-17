fba565bb27a321d217b616df4823b9ef70b989e1
Stas Shepet <milka33349@gmail.com>
2021-08-17 14:12:52 +0300

    added all clases

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public class Excavator extends Machine{
+    public void doWork(){
+        System.out.println("Excavator started its work");
+    }
+    public void stopWork(){
+        System.out.println("Excavator stopped its work");
+    }
+}
