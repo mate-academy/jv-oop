9b6313cd0583989d017a60cc1e4bc918137187a3
inbox-reva <inbox.reva@gmail.com>
2021-08-21 15:42:35 +0300

    all done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,18 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        // Create an object of each class
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        // Start work of all machines
+        truck.doWork();
+        bulldozer.doWork();
+        excavator.doWork();
+        // End work of all machines
+        truck.stopWork();
+        bulldozer.stopWork();
+        excavator.stopWork();
+    }
 }
