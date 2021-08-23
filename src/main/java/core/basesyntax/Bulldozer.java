3d40d5dc1fcfb1e8c55954f54e7ed016043bff4f
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2021-08-23 09:06:57 +0300

    Solution

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,5 @@
+package core.basesyntax;
+
+public class Bulldozer extends Truck{
+
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine man = new Truck();
+        man.setName("Man");
+        man.doWork();
+        man.stopWork();
+
+        Machine bulldozer = new Bulldozer();
+        bulldozer.setName("MTZ");
+        bulldozer.doWork();
+        bulldozer.stopWork();
+
+        Machine excavator = new Excavator();
+        excavator.setName("Caterpillar");
+        excavator.doWork();
+        excavator.stopWork();
+
+    }
+
 
 }
