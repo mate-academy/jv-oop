47eebc8b74120edb28be7fc2c8d629ede4331b2b
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2021-08-23 10:43:31 +0300

    HW JV OOP Solution v1

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
+
         Machine man = new Truck();
         man.setName("Man");
         man.doWork();
