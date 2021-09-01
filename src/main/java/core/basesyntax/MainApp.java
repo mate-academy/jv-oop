35315b1b8d330ffc8bf210ad48329d02f9e0105e
Serhii Petrenko <Poshtasp76@gmail.com>
2021-09-01 20:25:09 +0300

    - Created class Machine  containing methods `public abstract void doWork ()` and `public abstract void stopWork ()`. - Created classes `Truck`,` Bulldozer`, `Excavator` that inherit from` Machine`. - In those classes overrided `doWork () and stopWork (). - In `Main` class created an object of each class. - Started work of all machines and then stopped them.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,27 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        // create a Truck object
+        Truck truck = new Truck();
+        truck.setName("DAF");
 
+        // create a Bulldozer object
+        Bulldozer bulldozer = new Bulldozer();
+        bulldozer.setName("CAT");
+
+        // create a Excavator object
+        Excavator excavator = new Excavator();
+        excavator.setName("ЄКГ");
+
+        //machines started working
+        truck.doWork();
+        bulldozer.doWork();
+        excavator.doWork();
+
+        // machines stopped working
+        truck.stopWork();
+        bulldozer.stopWork();
+        excavator.stopWork();
+    }
 }
