022f8432761fc3b68004a090bd5ffd4fee242611
Serhii Petrenko <Poshtasp76@gmail.com>
2021-09-02 18:06:41 +0300

    - created an array of machines - adding a loop for doWork and stopWork

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,25 +3,21 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         // create a Truck object
-        Truck truck = new Truck();
+        Machine truck = new Truck();
         truck.setName("DAF");
 
         // create a Bulldozer object
-        Bulldozer bulldozer = new Bulldozer();
+        Machine bulldozer = new Bulldozer();
         bulldozer.setName("CAT");
 
         // create a Excavator object
-        Excavator excavator = new Excavator();
+        Machine excavator = new Excavator();
         excavator.setName("ЄКГ");
 
-        //machines started working
-        truck.doWork();
-        bulldozer.doWork();
-        excavator.doWork();
+        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
 
-        // machines stopped working
-        truck.stopWork();
-        bulldozer.stopWork();
-        excavator.stopWork();
-    }
-}
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }}
