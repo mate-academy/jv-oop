17114e4d84e7a707c48d2a42aa3cad7cb67cbe6a
yaroslav <yarmoshyk96@gmail.com>
2021-09-07 16:12:40 +0300

    no static

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-    private static Machine truck = new Truck();
-    private static Machine bulldozer = new Bulldozer();
-    private static Machine excavator = new Excavator();
+    private Machine truck = new Truck();
+    private Machine bulldozer = new Bulldozer();
+    private Machine excavator = new Excavator();
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         truck.doWork();
         excavator.doWork();
         bulldozer.doWork();
