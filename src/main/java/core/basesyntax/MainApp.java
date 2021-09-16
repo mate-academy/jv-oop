0552cba7974d30c6d9dba6d1470b683b77d2fa56
agash <alexgash8@gmail.com>
2021-09-16 17:01:47 +0300

    Excercise is done. Build success. mvn clean package works fine!

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -14,44 +14,6 @@ public class MainApp {
     }
 }
 
-abstract class Machine {
-    public abstract void doWork();
-    public abstract void stopWork();
-}
 
-class Truck extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println("Truck started working");
-    }
 
-    @Override
-    public void stopWork() {
-        System.out.println("Truck stopped working");
-    }
-}
-
-class Bulldozer extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println("Bulldozer started working");
-    }
-
-    @Override
-    public void stopWork() {
-        System.out.println("Bulldozer stopped working");
-    }
-}
-
-class Excavator  extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println("Excavator  started working");
-    }
-
-    @Override
-    public void stopWork() {
-        System.out.println("Excavator  stopped working");
-    }
-}
 
