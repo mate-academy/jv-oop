7fc98ea5d86269e264e3adae5fed5588d4cf43f4
bukoz <bukoz@meta.ua>
2021-09-28 15:30:18 +0300

    Created classes Truck, Bulldozer, Excavator with two override methods from parent class Machine and created three object of each classes.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,16 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        truck.doWork ();
+        truck.stopWork ();
+        Machine bulldozer = new Bulldozer();
+        bulldozer.doWork ();
+        bulldozer.stopWork ();
+        Machine excavator = new Excavator();
+        excavator.doWork ();
+        excavator.stopWork ();
+    }
 }
+
