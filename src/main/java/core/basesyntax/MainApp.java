e888833492365cd980bfed7c2434f5f1c9dbf053
bukoz <bukoz@meta.ua>
2021-09-28 16:28:38 +0300

    clean text

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,14 +4,14 @@ public class MainApp {
 
     public static void main(String[] args) {
         Machine truck = new Truck();
-        truck.doWork ();
-        truck.stopWork ();
+        truck.doWork();
+        truck.stopWork();
         Machine bulldozer = new Bulldozer();
-        bulldozer.doWork ();
-        bulldozer.stopWork ();
+        bulldozer.doWork();
+        bulldozer.stopWork();
         Machine excavator = new Excavator();
-        excavator.doWork ();
-        excavator.stopWork ();
+        excavator.doWork();
+        excavator.stopWork();
     }
 }
 
