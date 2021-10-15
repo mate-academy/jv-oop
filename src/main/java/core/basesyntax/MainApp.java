e55ad6e374c2104721dfddcbd3582bb374922ac2
SonyaPakhomova <88778908+SonyaPakhomova@users.noreply.github.com>
2021-10-15 11:25:36 +0300

    Created methods doWork() and stopWork() for bulldozer, truck and excavator

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] arguments) {
+        Machine excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
+        Machine truck = new Truck();
+        truck.doWork();
+        truck.stopWork();
+        Machine bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+    }
 
 }
