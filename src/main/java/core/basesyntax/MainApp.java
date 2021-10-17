aa8ea03bcf962c2c5c7f99f5423940819e2c0071
SarumanIsAlive <90073141+SarumanIsAlive@users.noreply.github.com>
2021-10-17 15:45:42 +0300

    Everything is done, according to the list!

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
+import java.util.List;
+
 public class MainApp {
+    public static void main(String[] args) {
+        List<Machine> machines = List.of(new Excavator(), new Bulldozer(), new Truck());
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 
 }
