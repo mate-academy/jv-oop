5d09a66dd39af01ca3548acb8c1d16343dcbd445
Volodimir <vova.stepanenko2020@gmail.com>
2021-10-18 23:29:03 +0300

    create machine objects, and start it

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,12 @@
-package core.basesyntax;
+package main.java.core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        new Truck();
+        new Buldozer();
+        new Excavator();
 
+        Machine.startAllMachines();
+        Machine.stopAllMachines();
+    }
 }
