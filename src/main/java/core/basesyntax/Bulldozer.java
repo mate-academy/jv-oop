ee6637a40e0c43b77bf53c0cd68c6327c1322aa2
Yaroslav Kavun <yka40znp@gmail.com>
2021-10-18 19:53:43 +0300

    created an abstract blueprint Machine with two methods doWork and stopWork, overriding them in three child classes Truck, Bulldozer and Excavator then created three objects each of differrent class in main class using polimorphism principles and then thanks to polymorphism called methods of each class two times

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("The Bulldozer has started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The Bulldozer has stopped its work");
+    }
+
+}
