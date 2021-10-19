b51eb0f2992b0de8ee4e37a48d0bd6b8121c1d1b
Dzmitry Kozel <dmitrokoz88@gmail.com>
2021-10-19 16:13:26 +0300

    Fixed "cannot resolve symbol" mistake.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,14 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
-    Machine bulldozer = new Bulldozer();
-    Machine excavator = new Excavator();
-    Machine truck = new Truck();
+    public static void main(String[]args){
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
 
-    bulldozer.doWork();
-    excavator.doWork();
-    truck.doWork();
-    bulldozer.stopWork();
-    excavator.stopWork();
-    truck.stopWork();
+        bulldozer.doWork();
+        excavator.doWork();
+        truck.doWork();
+        bulldozer.stopWork();
+        excavator.stopWork();
+        truck.stopWork();
+    }
 }
