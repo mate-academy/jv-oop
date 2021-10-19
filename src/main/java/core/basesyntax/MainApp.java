53521bffc1e67baaef1947f449dee67b0fd43756
Roman Polulikh <polilikh.r@gmail.com>
2021-10-19 13:48:20 +0300

    optimized main method

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,16 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine truck = new Truck();
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
-
-        truck.doWork();
-        bulldozer.doWork();
-        excavator.doWork();
-        truck.stopWork();
-        bulldozer.stopWork();
-        excavator.stopWork();
+        Machine[] autoPark = {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : autoPark) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
-
 }
