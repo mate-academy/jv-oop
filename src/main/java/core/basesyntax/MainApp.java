35751ff5e494560dee372c3cf864d7d9612658db
Roman Polulikh <polilikh.r@gmail.com>
2021-10-19 13:25:48 +0300

    fixed code style issues

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,17 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
-  public static void main(String[] args) {
-    Machine truck = new Truck();
-    Machine bulldozer = new Bulldozer();
-    Machine excavator = new Excavator();
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
 
-    truck.doWork();
-    bulldozer.doWork();
-    excavator.doWork();
-    truck.stopWork();
-    bulldozer.stopWork();
-    excavator.stopWork();
-  }
+        truck.doWork();
+        bulldozer.doWork();
+        excavator.doWork();
+        truck.stopWork();
+        bulldozer.stopWork();
+        excavator.stopWork();
+    }
 
 }
