d3f05b82ae4b802063eacd64d68b6cb640f46cfc
Pavlo_Martsiniv <imitator2010@gmail.com>
2021-10-19 15:32:54 +0300

    fix mistakes , remove MachinesStatus class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,18 +4,16 @@ import core.basesyntax.machines.Bulldozer;
 import core.basesyntax.machines.Excavator;
 import core.basesyntax.machines.Truck;
 import core.basesyntax.workflow.Machine;
-import core.basesyntax.workflow.MachineWorkingStatus;
 import java.util.List;
 
 public class MainApp {
     public static void main(String[] arguments) {
         List<Machine> machines = List.of(new Excavator(), new Bulldozer(), new Truck());
         for (Machine machine : machines) {
-            MachineWorkingStatus.startWorkingDay(machine);
+            machine.doWork();
         }
         for (Machine machine : machines) {
-            MachineWorkingStatus.endWorkingDay(machine);
+            machine.stopWork();
         }
     }
-
 }
