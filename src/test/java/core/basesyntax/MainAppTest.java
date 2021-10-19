0a0814c6261c5c6312dbd2cd203db1dd190653b4
Anatolii Huryk <tolik.mario96@gmail.com>
2021-10-19 21:41:37 +0300

    creat masine

--- src/test/java/core/basesyntax/MainAppTest.java
+++ src/test/java/core/basesyntax/MainAppTest.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainAppTest {
-
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
+        Machine truck = new Truck();
+        truck.doWork();
+        truck.stopWork();
+        Machine bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+    }
 }
