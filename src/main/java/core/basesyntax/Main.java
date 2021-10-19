7b108c7b5b97dd2924615fedf6d4ca54c9544819
Sergii Svystun <sergiisvystun@gmail.com>
2021-10-19 16:04:54 +0300

    removed whitespaces in array in main method

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        Machine [] allKindMachines = new Machine [] {new Excavator(), new Bulldozer(), new Truck()};
+        Machine [] allKindMachines = new Machine[]{new Excavator(), new Bulldozer(), new Truck()};
         for (Machine eachMachineWork : allKindMachines) {
             eachMachineWork.doWork();
             eachMachineWork.stopWork();
