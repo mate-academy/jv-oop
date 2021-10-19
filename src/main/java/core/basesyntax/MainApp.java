4d698fd0e20a8f83c2a7b03bf6f7b9b3e9dea5e9
nishchenkokateryna <kateryna.nishchenko@gmail.com>
2021-10-19 14:21:16 +0300

    New foreachloop was edded.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,21 +1,13 @@
 package core.basesyntax;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine truck = new Truck();
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
-
-        ArrayList<Machine> machines = new ArrayList<>();
-        machines.add(truck);
-        machines.add(bulldozer);
-        machines.add(excavator);
-
-        for (Machine m : machines) {
-            m.doWork();
-            m.stopWork();
+        List<Machine> machines = List.of(new Excavator(), new Bulldozer(), new Truck());
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
