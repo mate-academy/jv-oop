f847b042e354d1d0c2168cd211fca13d6e44bcf5
farfardesu <savavlad003@gmail.com>
2021-10-19 17:05:47 +0300

    Unnecessary variables were removed.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,10 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine truck = new Truck();
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
-        Machine[] machines = {truck, bulldozer, excavator};
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
 
         for (Machine machine : machines) {
             machine.doWork();
