7f19c7bf43e47d4cdae9d9486c415aad8b9b03ce
Alexsis Moskovchuk <alekssiuss@gmail.com>
2021-10-19 18:37:45 +0300

    oop homework

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,20 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+
+        Machine[] machines = new Machine[]{bulldozer, excavator, truck};
+
+        for (Machine m : machines) {
+            m.doWork();
+        }
+
+        for (Machine m : machines) {
+            m.stopWork();
+        }
+    }
+
 }
