d93f79cf8521e4430a2a06bef2a1883f38f30f23
VitaliiMarchenko <vitalbka.01@gmail.com>
2021-10-20 12:01:31 +0300

    added classes and solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine[] techniques = new Machine[3];
+        techniques[0] = excavator;
+        techniques[1] = truck;
+        techniques[2] = bulldozer;
 
+        for (Machine machine : techniques) {
+            machine.doWork();
+        }
+        for (Machine machine : techniques) {
+            machine.stopWork();
+        }
+
+    }
 }
