b0154c0b69fb76ed116536758527d8e257b9c613
Mykhailo Tereshchenkov <mtwib.re@gmail.com>
2021-10-20 16:10:36 +0300

    foreach loop was added

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,14 +2,12 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine truck = new Truck();
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
-        truck.doWork();
-        bulldozer.doWork();
-        excavator.doWork();
-        truck.stopWork();
-        bulldozer.stopWork();
-        excavator.stopWork();
+        Machine[] machines = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine :machines) {
+            machine.doWork();
+        }
+        for (Machine machine :machines) {
+            machine.stopWork();
+        }
     }
 }
