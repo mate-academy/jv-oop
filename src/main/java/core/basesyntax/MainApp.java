8ab260e0c66fad163f7576733e3900fdd94396e6
Artik <Artik25942@gmail.com>
2021-10-20 21:09:20 +0300

    cleared by mvn

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,19 +1,19 @@
 package core.basesyntax;
 
 public class MainApp {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
 
-          Machine bulldozer = new Bulldozer();
+        Machine bulldozer = new Bulldozer();
 
-          Machine truck = new Truck();
+        Machine truck = new Truck();
 
-          Machine excavator = new Excavator();
+        Machine excavator = new Excavator();
 
-          bulldozer.doWork();
-          truck.doWork();
-          excavator.doWork();
-          bulldozer.stopWork();
-          truck.stopWork();
-          excavator.stopWork();
-     }
+        bulldozer.doWork();
+        truck.doWork();
+        excavator.doWork();
+        bulldozer.stopWork();
+        truck.stopWork();
+        excavator.stopWork();
+    }
 }
