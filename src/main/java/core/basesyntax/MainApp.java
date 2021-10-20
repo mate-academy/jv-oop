7af7e7d9011ae926ec39d0afc24240927f5f177e
Admin Oleksandr <maslo5@meta.ua>
2021-10-20 15:10:40 +0300

    changed the main method

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,14 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine excavator = new Excavator();
-        Machine bulldozer = new Bulldozer();
-        Machine truck = new Truck();
-        excavator.doWork();
-        bulldozer.doWork();
-        truck.doWork();
-        excavator.stopWork();
-        bulldozer.stopWork();
-        truck.stopWork();
+        Machine[] machines = {new Excavator(), new Bulldozer(), new Truck()};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
