044b1ce2cef8583a9f82f9b44dd967ed4563d9de
Vladyslav Chyver <lione2009lione@gmail.com>
2021-10-20 10:10:26 +0300

    arrays for-each was added

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,14 +2,15 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine machineInSector1 = new Bulldozer();
-        Machine machineInSector2 = new Excavator();
-        Machine machineInSector3 = new Truck();
-        machineInSector1.doWork();
-        machineInSector2.doWork();
-        machineInSector3.doWork();
-        machineInSector1.stopWork();
-        machineInSector2.stopWork();
-        machineInSector3.stopWork();
+        Machine[] machine = new Machine[3];
+        machine[0] = new Bulldozer();
+        machine[1] = new Excavator();
+        machine[2] = new Truck();
+        for (Machine machineX : machine) {
+            machineX.doWork();
+        }
+        for (Machine machineX : machine) {
+            machineX.stopWork();
+        }
     }
 }
