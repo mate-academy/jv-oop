d75caf786ebde7b6a1c7d821d5486c8232cda968
Kate Deshchinska <katerinka9636@gmail.com>
2021-10-20 13:42:12 +0300

    mvn clean package success

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,18 +1,18 @@
 package core.basesyntax;
 
 public class Main {
-        public static void main(String[] args) {
-            Machine exc1 = new Excavator();
-            Machine bull1 = new Bulldozer();
-            Machine truck1 = new Truck();
+    public static void main(String[] args) {
+        Machine exc1 = new Excavator();
+        Machine bull1 = new Bulldozer();
+        Machine truck1 = new Truck();
 
-            exc1.doWork();
-            bull1.doWork();
-            truck1.doWork();
+        exc1.doWork();
+        bull1.doWork();
+        truck1.doWork();
 
-            exc1.stopWork();
-            bull1.stopWork();
-            truck1.stopWork();
-        }
+        exc1.stopWork();
+        bull1.stopWork();
+        truck1.stopWork();
     }
+}
 
