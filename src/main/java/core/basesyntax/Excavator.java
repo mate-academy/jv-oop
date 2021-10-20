f3581a9e09bd90ccff89a5a37cdc88959650186a
Eugene-exe <zhekacauselove@gmail.com>
2021-10-20 18:39:23 +0300

    My solution

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Excavator start its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator stop its work");
+    }
+}
