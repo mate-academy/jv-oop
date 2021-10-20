00f430104c4fe94ecb5f8d0b546be7fcce08b6da
Kate Deshchinska <katerinka9636@gmail.com>
2021-10-20 17:48:27 +0300

    remove empty line and create machines array

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,17 +2,11 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        Machine exc1 = new Excavator();
-        Machine bull1 = new Bulldozer();
-        Machine truck1 = new Truck();
-
-        exc1.doWork();
-        bull1.doWork();
-        truck1.doWork();
-
-        exc1.stopWork();
-        bull1.stopWork();
-        truck1.stopWork();
+        Machine[] machines = new Machine[]{new Excavator(), new Bulldozer(), new Truck()};
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
 
