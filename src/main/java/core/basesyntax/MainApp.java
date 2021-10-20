e676751c775e6a0288c1f21d1823e25ae11e9b06
Vladyslav Chyver <lione2009lione@gmail.com>
2021-10-20 10:36:59 +0300

    objects and variables was renamed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,15 +2,15 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machine = new Machine[3];
-        machine[0] = new Bulldozer();
-        machine[1] = new Excavator();
-        machine[2] = new Truck();
-        for (Machine machineX : machine) {
-            machineX.doWork();
+        Machine[] machines = new Machine[3];
+        machines[0] = new Bulldozer();
+        machines[1] = new Excavator();
+        machines[2] = new Truck();
+        for (Machine machine : machines) {
+            machine.doWork();
         }
-        for (Machine machineX : machine) {
-            machineX.stopWork();
+        for (Machine machine : machines) {
+            machine.stopWork();
         }
     }
 }
