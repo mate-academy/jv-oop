18ee3db2abebed53078d9264c2f1d599f54a9aaa
Artik <Artik25942@gmail.com>
2021-10-20 21:01:25 +0300

    My home worck was done, wait to check

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
 
 public class MainApp {
+     public static void main(String[] args) {
 
+          Machine bulldozer = new Bulldozer();
+
+          Machine truck = new Truck();
+
+          Machine excavator = new Excavator();
+
+          bulldozer.doWork();
+          truck.doWork();
+          excavator.doWork();
+          bulldozer.stopWork();
+          truck.stopWork();
+          excavator.stopWork();
+     }
 }
