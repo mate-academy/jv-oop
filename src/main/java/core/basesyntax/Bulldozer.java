130d95952c29abfa456aae3ef071e8a40e86ab83
Artik <Artik25942@gmail.com>
2021-10-21 14:21:53 +0300

    dell all null Strings

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -4,6 +4,7 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Bulldozer started its work.");
     }
+
     public void stopWork() {
         System.out.println("Bulldozer stopped its work.");
     }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,6 +1,8 @@
 package core.basesyntax;
+
 public class Truck extends Machine {
     public void doWork() {
+
         System.out.println("Truck started its work.");
     }
 
