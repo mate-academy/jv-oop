e852a065fd78f4c92e57b2a7ddd2bbd8f901d870
Bohdan Hrytsak <2002lazofu@gmail.com>
2021-10-21 14:18:42 +0300

    Codeisworking

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
 
+        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
+
+        for (Machine m : machines) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
 }
