c6a8f06cea26c12edb506c2de9f3b650dd8c0869
Yevheniy Savenkov <yevheniy.savenkov@gmail.com>
2021-10-21 09:48:06 +0300

    second commit - loop added, empty lines removed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,17 +3,13 @@ package core.basesyntax;
 public class MainApp {
     public static void main() {
 
-        Machine truck = new Truck();
-        truck.doWork();
-        truck.stopWork();
-
-        Machine excavator = new Excavator();
-        excavator.doWork();
-        excavator.stopWork();
-
-        Machine bulldozer = new Bulldozer();
-        bulldozer.doWork();
-        bulldozer.stopWork();
-
+        Machine[] machines = new Machine[3];
+        machines[0] = new Truck();
+        machines[1] = new Excavator();
+        machines[2] = new Bulldozer();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
