ed831a078d4db5c491bc712cbf511d437926b53b
Artik <Artik25942@gmail.com>
2021-10-21 12:46:50 +0300

    the loop add

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,9 +1,7 @@
 package core.basesyntax;
 
 public class Excavator extends Machine {
-    public void doWork() {
-        System.out.println("Excavator started its work.");
-    }
+    public void doWork() { System.out.println("Excavator started its work."); }
 
     public void stopWork() {
         System.out.println("Excavator stopped its work.");
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,16 +4,16 @@ public class MainApp {
     public static void main(String[] args) {
 
         Machine bulldozer = new Bulldozer();
-
         Machine truck = new Truck();
-
         Machine excavator = new Excavator();
 
-        bulldozer.doWork();
-        truck.doWork();
-        excavator.doWork();
-        bulldozer.stopWork();
-        truck.stopWork();
-        excavator.stopWork();
+        Machine[] machines = new Machine[]{bulldozer, truck, excavator};
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+        for (Machine machine : machines) {
+            machine.stopWork();
+        }
+
     }
 }
