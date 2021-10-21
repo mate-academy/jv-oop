1316e6930fe24fc8ac0585b6915af474dd08e5b0
Artik <Artik25942@gmail.com>
2021-10-21 12:56:19 +0300

    clearder by mvn

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,7 +1,9 @@
 package core.basesyntax;
 
 public class Excavator extends Machine {
-    public void doWork() { System.out.println("Excavator started its work."); }
+    public void doWork() {
+        System.out.println("Excavator started its work.");
+    }
 
     public void stopWork() {
         System.out.println("Excavator stopped its work.");
