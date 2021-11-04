9fab11bfbbfd92c5c16f61f8d2f6e31a80aa1cde
Stanislav <stassonich@icloud.com>
2021-11-04 23:12:53 +0200

    fixed0

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,49 +1,6 @@
 package core.basesyntax;
 
-
 public abstract class Machine {
-    private String name;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getName() {
-        return name;
-    }
     public abstract void doWork();
     public abstract void stopWork();
-}
-class Excavator extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println(getName() + " start work!");
     }
-    @Override
-    public void stopWork() {
-        System.out.println(getName() + " stop work!");
-    }
-}
-class Bulldozer extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println(getName() + " start work!");
-    }
-    @Override
-    public void stopWork() {
-        System.out.println(getName() + " stop work!");
-    }
-}
-class Truck extends Machine{
-    @Override
-    public void doWork() {
-        System.out.println(getName() + " start work!");
-    }
-    @Override
-    public void stopWork() {
-        System.out.println(getName() + " stop work!");
-    }
-}
-
-
-
-
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,17 +4,14 @@ public class MainApp {
     public static void main(String []args){
 
     Machine truck = new Truck();
-    truck.setName("Volvo");
     truck.doWork();
     truck.stopWork();
 
     Machine excavator = new Excavator();
-    excavator.setName("Man");
     excavator.doWork();
     excavator.stopWork();
 
     Machine bulldozer = new Bulldozer();
-    bulldozer.setName("Audi");
     bulldozer.doWork();
     bulldozer.stopWork();
 }
