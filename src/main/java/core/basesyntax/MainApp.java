ee259bfe1a53a4a41527fad5310e37a7724f7b0a
Stanislav <stassonich@icloud.com>
2021-11-04 21:33:11 +0200

    Files changed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,21 +1,22 @@
 package core.basesyntax;
 
 public class MainApp {
-        public static void main(String []args){
+    public static void main(String []args){
+
     Machine truck = new Truck();
     truck.setName("Volvo");
     truck.doWork();
     truck.stopWork();
 
     Machine excavator = new Excavator();
-    truck.setName("Man");
-    truck.doWork();
-    truck.stopWork();
+    excavator.setName("Man");
+    excavator.doWork();
+    excavator.stopWork();
 
     Machine bulldozer = new Bulldozer();
-    truck.setName("Audi");
-    truck.doWork();
-    truck.stopWork();
+    bulldozer.setName("Audi");
+    bulldozer.doWork();
+    bulldozer.stopWork();
 }
     }
 
