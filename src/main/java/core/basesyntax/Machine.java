bd3acdd0d6299495e6014281292a199ed1b73aa0
Stanislav <stassonich@icloud.com>
2021-11-04 22:46:28 +0200

    fixed1

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,49 +1,49 @@
 package core.basesyntax;
 
-        public class Machine {
-        public String name;
 
-        public void setName(String name) {
-            this.name = name;
-        }
-        public String getName() {
-            return name;
-        }
-        public void doWork() {
-        }
-        public void stopWork() {
-        }
-        }
-       class Excavator extends Machine {
-        @Override
-        public void doWork() {
+public abstract class Machine {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public abstract void doWork();
+    public abstract void stopWork();
+}
+class Excavator extends Machine {
+    @Override
+    public void doWork() {
         System.out.println(getName() + " start work!");
-        }
-        @Override
-        public void stopWork() {
+    }
+    @Override
+    public void stopWork() {
         System.out.println(getName() + " stop work!");
-        }
-        }
-        class Bulldozer extends Machine {
-        @Override
-        public void doWork() {
+    }
+}
+class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
         System.out.println(getName() + " start work!");
-        }
-        @Override
-        public void stopWork() {
+    }
+    @Override
+    public void stopWork() {
         System.out.println(getName() + " stop work!");
-        }
-        }
-        class Truck extends Machine{
-       @Override
-       public void doWork() {
-       System.out.println(getName() + " start work!");
-       }
-       @Override
-       public void stopWork() {
-       System.out.println(getName() + " stop work!");
-       }
-       }
+    }
+}
+class Truck extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println(getName() + " start work!");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println(getName() + " stop work!");
+    }
+}
+
 
 
 
