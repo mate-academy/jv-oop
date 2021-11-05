f67a254846ab33d9fad97a0406688db51742b710
Stanislav <stassonich@icloud.com>
2021-11-05 17:09:21 +0200

    added changes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main(String []args){
+    public static void main(String []args) {
         Machine truck = new Truck();
      truck.setName("Volvo");
         Machine excavator = new Excavator();
