7e3323fdcaf3a1170501868fae624eb741b8451b
morozovyrv <72643876+morozovyrv@users.noreply.github.com>
2021-11-06 10:13:34 +0200

    Machines created. All machines started and stopped.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args){
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+        excavator.doWork();
+        excavator.stopWork();
+        truck.doWork();
+        truck.stopWork();
+    }
 }
