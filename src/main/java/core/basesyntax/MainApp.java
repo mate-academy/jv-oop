b73bb1e48b0a711633b83bf94c9e01997dd33f42
Evgeniy <kesh040593@ukr.net>
2021-11-09 12:38:32 +0200

    Solution to a homework assignment JAVA-OOP

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine machine = new Truck();
+        machine.doWork();
+        machine.stopWork();
+        machine.setName("Truck");
 
+        Machine bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+        bulldozer.setName("Bulldozer");
+
+
+        Machine excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
+        excavator.setName("Excavator");
+    }
 }
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("The Truck has started its work!");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The Truck has stopped working!");
+    }
+}
