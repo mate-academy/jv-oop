66dd6f98653445ebb25ef5e5de424bd573de41b8
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2021-11-12 14:21:07 +0200

    added classes : Machine , Buldozer , Truck , Excavator . added mathods : doWork and stopWork.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,15 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine truck1 = new Truck();
+        Machine buldozer1 = new Bulldozer();
+        Machine excavator1 = new Excavator();
+        truck1.doWork();
+        buldozer1.doWork();
+        excavator1.doWork();
+        truck1.stopWork();
+        buldozer1.stopWork();
+        excavator1.stopWork();
+    }
 }
