c8e9009d3dec5d038662d425c571727c8d6220a4
Stelmakh Yevhen <stelmakh.m.d@gmail.com>
2021-11-12 19:19:42 +0200

    Fixed checkstyle/ Added loop.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,13 +5,14 @@ public class MainApp {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
+        for (int i = 5; i >= 0; i--) {
+            truck.doWork();
+            bulldozer.doWork();
+            excavator.doWork();
 
-        truck.doWork();
-        bulldozer.doWork();
-        excavator.doWork();
-
-        truck.stopWork();
-        bulldozer.stopWork();
-        excavator.stopWork();
+            truck.stopWork();
+            bulldozer.stopWork();
+            excavator.stopWork();
+        }
     }
 }
