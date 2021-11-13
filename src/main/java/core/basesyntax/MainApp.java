756264b571ccb2c58ab865ee3aa842bb6d7c656c
Evgen Lanovenko <ev.lanovenko@gmail.com>
2021-11-13 20:21:41 +0200

    added array Machine and (for each)

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,12 +5,11 @@ public class MainApp {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-        truck.doWork();
-        bulldozer.doWork();
-        excavator.doWork();
-        truck.stopWork();
-        bulldozer.stopWork();
-        excavator.stopWork();
+        Machine[] trucks = {truck, bulldozer, excavator};
+        for (Machine arr : trucks) {
+            arr.doWork();
+            arr.stopWork();
+        }
     }
 
 }
