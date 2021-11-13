52bf5479849d77d18f20503f8cc686f176e09f12
Roman Okhrymovych <okhrymovych11@gmail.com>
2021-11-13 15:55:38 +0200

    Machine class

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer starts working!!!");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer stops working!!!");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,20 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String [] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        truck.doWork();
+        bulldozer.doWork();
+        excavator.doWork();
+        truck.stopWork();
+        bulldozer.stopWork();
+        excavator.stopWork();
+
+    }
 }
+
+
+
+
