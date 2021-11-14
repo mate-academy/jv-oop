0b7e4648fa073118afa850640686bf98ba68ee5e
Volodymyr Sibiruk <arthes46@gmail.com>
2021-11-14 13:37:04 +0200

    creating class "Machine"; creating classes "Truck", "Buldozer" and "Excavator" that inherit from "Machine"; methods "doWork" and "stopWork" added and overriding them; creating objects of each class; added loops to start work and to stop work of each machine.

---
+++ src/main/java/core/basesyntax/Buldozer.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Buldozer extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("Buldozer is working!");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Buldozer stop working!");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine buldozer = new Buldozer();
+        Machine excavator = new Excavator();
+        Machine[] machines = new Machine[] {truck, buldozer, excavator};
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+        for (Machine machine : machines) {
+            machine.stopWork();
+        }
+    }
 }
