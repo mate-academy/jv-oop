cc1ba383547a963c4e8ec1244eed0376653aa476
Roman Okhrymovych <okhrymovych11@gmail.com>
2021-11-15 21:37:58 +0200

    Machine class upDate

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -5,6 +5,7 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator starts working!!!");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Excavator stops working!!!");
