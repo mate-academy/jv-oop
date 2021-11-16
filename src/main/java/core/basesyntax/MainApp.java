f16a7dd1aa7c57d9bdd6c29d1756afbfbfb564bf
Vitaliy Bushovskiy <Bushovskiy@gmail.com>
2021-11-16 20:02:35 +0200

    changeBranch

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -11,5 +11,4 @@ public class MainApp {
             mashine.stopWork();
         }
     }
-
 }
--- src/main/java/core/basesyntax/Mashine.java
+++ src/main/java/core/basesyntax/Mashine.java
@@ -3,6 +3,5 @@ package core.basesyntax;
 public abstract class Mashine {
     public abstract void doWork();
     public abstract void stopWork();
-
 }
 
