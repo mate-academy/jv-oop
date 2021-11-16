e8ec248846833ece0860808369c690e2138bde80
Vitaliy Bushovskiy <Bushovskiy@gmail.com>
2021-11-16 19:31:04 +0200

    My first version

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Mashine truck = new Truck();
+        Mashine bulldozer = new Bulldozer();
+        Mashine excavator = new Excavator();
+        Mashine[] mashines = new Mashine[]{truck, bulldozer, excavator};
+        for (Mashine mashine : mashines) {
+            mashine.doWork();
+            mashine.stopWork();
+        }
+    }
 }
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Truck extends Mashine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Truck started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped its work");
+    }
+}
