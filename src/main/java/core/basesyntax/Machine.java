01e8ff90024c67b35d0ffc299bec70bcd8a5d06a
Nikita Melnikov <bam19921303@gmail.com>
2021-11-19 13:16:22 +0200

    Changes have been made to the class. Name objects in MainApp Fixed method body doWork, stopWork in Machine

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -2,9 +2,7 @@ package core.basesyntax;
 
 public abstract class Machine {
     public abstract void doWork(); {
-        System.out.println();
     }
     public abstract void stopWork(); {
-        System.out.println();
     }
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,14 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main (String[] args) {
-        Truck truck1 = new Truck();
-        Excavator excavator1 = new Excavator();
-        Bulldozer bulldozer = new Bulldozer();
-        truck1.doWork();
-        excavator1.doWork();
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        truck.doWork();
+        excavator.doWork();
         bulldozer.doWork();
-        truck1.stopWork();
-        excavator1.stopWork();
+        truck.stopWork();
+        excavator.stopWork();
         bulldozer.stopWork();
     }
 }
