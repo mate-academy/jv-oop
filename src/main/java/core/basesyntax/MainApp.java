e0b3fd3779f6a78803017a1627d098438a3387bb
Arina <crowlynx4@gmail.com>
2021-11-29 13:27:30 +0200

    array created, loops added.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,18 +2,15 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
-
-        excavator.doWork();
-        bulldozer.doWork();
-        truck.doWork();
-
-        excavator.stopWork();
-        bulldozer.stopWork();
-        truck.stopWork();
+        Machine[] machines = new Machine[] {excavator, bulldozer, truck};
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+        for (Machine machine : machines) {
+            machine.stopWork();
+        }
     }
-
 }
