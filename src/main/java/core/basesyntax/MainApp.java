57b1d654919b04ae3e108f82bcf6795d485662af
Oleh B <oleg.busygin@gmail.com>
2021-11-29 19:32:37 +0200

    in MainApp class used loops

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,11 +5,12 @@ public class MainApp {
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
         Machine truck = new Truck();
-        bulldozer.doWork();
-        bulldozer.stopWork();
-        excavator.doWork();
-        excavator.stopWork();
-        truck.doWork();
-        truck.stopWork();
+        Machine[] machines = {bulldozer, excavator,truck};
+        for (Machine machine: machines) {
+          machine.doWork();
+        }
+        for (Machine machine: machines) {
+            machine.stopWork();
+        }
     }
 }
