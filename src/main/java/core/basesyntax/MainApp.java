428ba643fbbfb2df3055fed8aed0eae477d2412f
Mariana Trinko <marjanavv@gmail.com>
2021-11-29 19:15:33 +0100

    1 first hw-2 + array

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,15 +2,15 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Excavator excavator = new Excavator();
-        Bulldozer bulldozer = new Bulldozer();
-        final Truck truck = new Truck();
-        excavator.doWork();
-        excavator.stopWork();
-        bulldozer.doWork();
-        bulldozer.stopWork();
-        truck.doWork();
-        truck.stopWork();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        Machine[] machins = {excavator,bulldozer,truck};
+        for (Machine m:machins) {
+            m.doWork();
+            m.stopWork();
+        }
     }
 }
+
 
