5125f15084ec2014838d1dc241309bd243e9053a
Borodai Oleg <olborol1993@gmail.com>
2021-11-30 09:19:58 +0200

    hw-1

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,19 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine machine1 = new Excavator();
+        Machine machine2 = new Bulldozer();
+        Machine machine3 = new Truck();
+
+        machine1.doWork();
+        machine2.doWork();
+        machine3.doWork();
+
+        machine1.stopWork();
+        machine2.stopWork();
+        machine3.stopWork();
+
+    }
+
 }
