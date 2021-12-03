7bbfb06b1c7ca489115621c43b0606b69224ea74
Felix <felixkolner@gmail.com>
2021-12-03 12:33:52 +0200

    Added jv-oop solution (after adjustment by the mentor). Thank you!

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,15 +1,26 @@
 package core.basesyntax;
 
 public class MainApp {
+
     public static void main(String[] args) {
-        Excavator excavator = new Excavator();
-        excavator.doWork();
-        excavator.stopWork();
-        Bulldozer bulldozer = new Bulldozer();
-        bulldozer.doWork();
-        bulldozer.stopWork();
-        Truck truck = new Truck();
-        truck.doWork();
-        truck.stopWork();
+
+        Machine[] machine = {new Excavator(), new Bulldozer(), new Truck()};
+        for (Machine value : machine) {
+            value.doWork();
+        }
+        Machine[] machine1 = {new Excavator(), new Bulldozer(), new Truck()};
+        for (Machine value : machine1) {
+            value.stopWork();
+        }
     }
 }
+
+   /*Machine excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
+        Machine bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+        Machine truck = new Truck();
+        truck.doWork();
+        truck.stopWork();*/
