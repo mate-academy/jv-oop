21ce11492b5231f793596fc572d6814456438dab
Borodai Oleg <olborol1993@gmail.com>
2021-12-03 12:52:04 +0200

    hw 1 fixes all review

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -6,15 +6,10 @@ public class MainApp {
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
-
-        excavator.doWork();
-        bulldozer.doWork();
-        truck.doWork();
-
-        excavator.stopWork();
-        bulldozer.stopWork();
-        truck.stopWork();
-
+        Machine[] machines = {bulldozer, excavator, truck};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
-
 }
