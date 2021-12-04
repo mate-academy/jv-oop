9f2d8f00dcfd315fa87584f7f5edd3b4b07c4bb1
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2021-12-04 12:12:06 +0200

    Created class Truck inherited from class Machine, overrided abstract methods doWork() and stopWork()

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
-public class Truck extends Machine{
+public class Truck extends Machine {
+
     @Override
     public void doWork() {
         System.out.println("Truck started working!");
