166ccf5f99345bbcf10329c570907838859a8edd
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2021-12-04 12:56:08 +0200

    Created class Excavator, inherited from Machine, overrided abstract classes

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -6,6 +6,7 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator started working!");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Excavator stopped working!");
