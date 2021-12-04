68fcbb2cf31a12194af89c7081f7e6f42b0b1180
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2021-12-04 12:56:29 +0200

    Created class Truck, inherited from Machine, overrided abstract classes

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -6,6 +6,7 @@ public class Truck extends Machine {
     public void doWork() {
         System.out.println("Truck started working!");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Truck stopped working!");
