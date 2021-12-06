5e9b8941be531d87c01f55974725e176870b3ac8
maksim <maksim.sokur90@gmail.com>
2021-12-06 10:22:22 +0300

    Added classes, method's, checking mvn test. Build success!

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    public Truck() {
+    }
+
+    @Override
+    public void doWork() {
+        System.out.println("Truck work!");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stop work");
+    }
+}
