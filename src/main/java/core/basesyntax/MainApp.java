c59785bb34c700a49058af2357e48ba67012b129
Vitaliy Zahrenko <vzagorenko@gmail.com>
2021-12-06 20:47:02 +0200

    MainApp main method has been reliased

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+
+        Machine[] machines = new Machine[3];
+        machines[0] = new Truck();
+        machines[1] = new Excavator();
+        machines[2] = new Bulldozer();
+
+        for (Machine m : machines) {
+            m.doWork();
+        }
+        for (Machine m : machines) {
+            m.stopWork();
+        }
+    }
+
 
 }
