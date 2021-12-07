61de005eb94f98de2863a4638b8ab3fc720c18ec
vlads <vladsysa@mail.ru>
2021-12-07 12:52:16 +0100

    Changes by Sysa

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,42 @@
 package core.basesyntax;
 
-public class MainApp {
+abstract class Machine {
 
+    public abstract void doWork();
+
+    public abstract void stopWork();
 }
+
+    class Truck extends Machine {
+        @Override
+        public void doWork() {
+            System.out.println("Machine started its work");
+        }
+        @Override
+        public void stopWork() {
+            System.out.println("Machine stopped its work");
+        }
+    }
+    class Bulldozer extends Machine {
+        @Override
+        public void doWork() {
+            System.out.println("Machine started its work");
+        }
+        @Override
+        public void stopWork() {
+            System.out.println("Machine stopped its work");
+        }
+    }
+    class Excavator extends Machine {
+        @Override
+        public void doWork() {
+            System.out.println("Machine started its work");
+        }
+        @Override
+        public void stopWork() {
+            System.out.println("Machine stopped its work");
+        }
+    }
+
+
+
--- src/test/java/core/basesyntax/MainAppTest.java
+++ src/test/java/core/basesyntax/MainAppTest.java
@@ -2,4 +2,18 @@ package core.basesyntax;
 
 public class MainAppTest {
 
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+
+        truck.doWork();
+        truck.stopWork();
+
+        bulldozer.doWork();
+        bulldozer.stopWork();
+
+        excavator.doWork();
+        excavator.stopWork();
+    }
 }
