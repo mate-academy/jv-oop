550dcc38ae763020304dce414f5dc53d0b5866ba
Natalia Kurhanska <kurhnatali@gmail.com>
2021-12-08 02:37:16 +0200

    created abstract class Machine with methods created inherited classes Truck, Bulldozer, Excavator, override methods added psvm method, create objects in MainApp

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        excavator.doWork();
+        Machine truck = new Truck();
+        truck.doWork();
+        Machine bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        excavator.stopWork();
+        truck.stopWork();
+        bulldozer.stopWork();
+
+    }
 
 }
