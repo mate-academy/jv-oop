3cc0ad63edd958c9f3a5605cb183a858849b5da0
Tsymbal Sergey <sfisher@ukr.net>
2021-12-09 19:41:33 +0200

    adding getClass().getSimpleName()

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,15 +1,14 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
-    private String name = "Truck";
 
     @Override
     public void doWork() {
-        System.out.println(name + " started its work.");
+        System.out.println(getClass().getSimpleName() + " started its work.");
     }
 
     @Override
     public void stopWork() {
-        System.out.println(name + " stopped its work");
+        System.out.println(getClass().getSimpleName() + " stopped its work");
     }
 }
