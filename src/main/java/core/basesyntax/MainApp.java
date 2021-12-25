c1f628633fca35e23e4017aaf7ff23bb3a4a2826
Ira Zatsnova <a.ir@outlook.com>
2021-12-25 19:46:37 +0200

    Implement methods doWorkAll and stopWorkAll for array of machines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
 
+        Machine[] machines = new Machine[] {truck, bulldozer, excavator};
+        for (Machine machine: machines) {
+            doWorkAll(machine);
+        }
+        for (Machine machine: machines) {
+            stopWorkAll(machine);
+        }
+    }
+    public static void doWorkAll(Machine machine) {
+        machine.doWork();
+    }
+
+    public static void stopWorkAll(Machine machine) {
+        machine.stopWork();
+    }
 }
