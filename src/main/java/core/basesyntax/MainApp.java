c9dc4f1893b24dcf2eb495f698249c9db73e2071
Viktoriia <viksonchik1996@ukr.net>
2021-12-28 20:11:19 +0200

    created class Machine added 2 methods created 3 classes that inherit Machine сreated objects of all classes and launched methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        truck.doWork();
+        bulldozer.doWork();
+        excavator.doWork();
+        truck.stopWork();
+        bulldozer.stopWork();
+        excavator.stopWork();
+    }
 }
