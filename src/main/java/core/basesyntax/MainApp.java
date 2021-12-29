8d10a1f49fde7500f2602690e85f6cd52978daf9
Kate Chernyavska <ccarts.kpi@gmail.com>
2021-12-29 13:44:23 +0200

    changed psvm method, created array that contain 3 machines and through cycle called 2 methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,17 +2,17 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
+        Machine[] machines = new Machine[3];
         Excavator excavator = new Excavator();
         Bulldozer bulldozer = new Bulldozer();
         Truck truck = new Truck();
-
-        excavator.doWork();
-        bulldozer.doWork();
-        truck.doWork();
-
-        excavator.stopWork();
-        bulldozer.stopWork();
-        truck.stopWork();
+        machines[0] = excavator;
+        machines[1] = bulldozer;
+        machines[2] = truck;
+        for (int i = 0; i < machines.length; i++){
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
     }
 
 }
