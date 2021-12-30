6e79528c10d1b490e151147d5cc81be44bc1b5a2
Kate Chernyavska <ccarts.kpi@gmail.com>
2021-12-30 22:22:13 +0200

    changed class in way to add whitespace after name inherited class and separeted two methods empty line

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
-public class Bulldozer extends Machine{
+public class Bulldozer extends Machine {
     @Override
     public void doWork() {
         System.out.println("Bulldozer starts doing its work!");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Bulldozer stops doing its work!");
