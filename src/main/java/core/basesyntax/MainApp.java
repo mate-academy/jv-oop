b837fcac08ff07d03882b5172a8a01fe03491741
dariiapikul <dariyapikul@gmail.com>
2022-01-03 18:21:53 +0100

    java classes Machine, Bulldozer, Excavator and Track were created, the MainApp updated

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
+import core.basesyntax.models.Bulldozer;
+import core.basesyntax.models.Excavator;
+import core.basesyntax.models.Machine;
+import core.basesyntax.models.Track;
+import java.util.List;
+
 /**
  * - Создать класс Machine с методами public abstract void doWork()
  * и public abstract void stopWork().
@@ -12,5 +18,13 @@ package core.basesyntax;
  * - Отправить все машини на работу и остановить работу всех машин.
  */
 public class MainApp {
+    public static void main(String[] args) {
+        Machine track = new Track();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
 
+        List<Machine> machineList = List.of(track, bulldozer, excavator);
+        machineList.forEach(Machine::doWork);
+        machineList.forEach(Machine::stopWork);
+    }
 }
