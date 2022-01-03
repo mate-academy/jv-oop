a42a453a0d6ad9f9513ef11456138809bf8ca971
Anton Hut <tonnyhut@icloud.com>
2022-01-03 22:46:11 +0200

    Created objects of each class with name and start working methods.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck("Truck");
+        Machine excavator = new Excavator("Excavator");
+        Machine bulldozer = new Bulldozer("Bulldozer");
 
+        truck.doWork();
+        excavator.doWork();
+        bulldozer.doWork();
+
+        truck.stopWork();
+        excavator.stopWork();
+        bulldozer.stopWork();
+
+    }
 }
+
+
