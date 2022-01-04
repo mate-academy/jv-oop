46b19a8f5c55cb6d7dc5b920f8b9581df95e7d2b
Anton Hut <tonnyhut@icloud.com>
2022-01-04 14:30:38 +0200

    deleted additional fields from class Machine and changed implementation for other methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,13 +3,8 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine truck = new Truck();
-        truck.setName("Truck");
-
         Machine excavator = new Excavator();
-        excavator.setName("Excavator");
-
         Machine bulldozer = new Bulldozer();
-        bulldozer.setName("Bulldozer");
 
         Machine[] machines = new Machine[]{truck, excavator, bulldozer};
 
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -4,11 +4,11 @@ public class Truck extends Machine {
 
     @Override
     public void doWork() {
-        System.out.println(getName() + " start working");
+        System.out.println(getClass().getSimpleName() + " start working");
     }
 
     @Override
     public void stopWork() {
-        System.out.println(getName() + " stop working");
+        System.out.println(getClass().getSimpleName() + " stop working");
     }
 }
