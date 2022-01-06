6efa90cf5cf585eb7b577daf9599b9b666f03f49
Iryna Khrustalova <blackkitty2204@gmail.com>
2022-01-06 14:58:59 +0100

    hw-1-jv-oop-complete

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,18 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main(String[]args){
+    public static void main(String[]args) {
         Machine excavator = new Excavator();
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine[] machines = new Machine[]{excavator,truck, bulldozer};
 
-        for (Machine machine: machines){
+        for (Machine machine: machines) {
             machine.doWork();
         }
-        for (Machine machine: machines){
+
+        for (Machine machine: machines) {
             machine.stopWork();
         }
     }
-
 }
