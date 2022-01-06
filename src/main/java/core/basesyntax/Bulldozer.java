9e796f72b82053b3904db86aac0e8fa9d7156d88
Iryna Khrustalova <blackkitty2204@gmail.com>
2022-01-06 11:47:03 +0100

    Jdk 11 was added

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 public class Bulldozer extends Machine{
     @Override
     public void doWork() {
+
         System.out.println("Bulldozer started its work.");
     }
     @Override
     public void stopWork() {
+
         System.out.println("Bulldozer stopped its work.");
     }
 
