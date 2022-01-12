4daf9b7bf15edf510105d9c669f85e7c6967fa92
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-01-12 12:40:11 +0200

    All problems are fixed. Let's try again!

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Bulldoser bulldoser = new Bulldoser();
+        Truck truck = new Truck();
+        Excavator excavator = new Excavator();
+        bulldoser.doWork();
+        truck.doWork();
+        excavator.doWork();
+        bulldoser.stopWork();
+        truck.stopWork();
+        excavator.stopWork();
+    }
 }
