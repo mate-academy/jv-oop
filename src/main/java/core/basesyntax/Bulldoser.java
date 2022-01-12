5e6166442d295eab1cb6879c7a7428fadedd7bb5
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-01-12 20:59:53 +0200

    Most problems are fixed. All classes are created in each single files. Now all vehicles are Machines) Let's try again!

---
+++ src/main/java/core/basesyntax/Bulldoser.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Bulldoser extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("Bulldoser start work!");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldoser stop work!");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine bulldoser = new Bulldoser();
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        bulldoser.doWork();
+        truck.doWork();
+        excavator.doWork();
+        bulldoser.stopWork();
+        truck.stopWork();
+        excavator.stopWork();
+    }
 
 }
