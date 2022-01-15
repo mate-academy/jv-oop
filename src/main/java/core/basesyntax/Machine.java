58b823cd5fab457a035a811ea512acf824f6b00a
Dima Semeniuk <semeniuk.dima@ukr.net>
2022-01-15 15:00:14 +0200

    Created class Machine and subclasses(Truck, Bulldozer and Excavator)

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,8 @@
+package core.basesyntax;
+
+public abstract class Machine {
+
+    public abstract void work();
+
+    public abstract void stopWork();
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        truck.work();
+        bulldozer.work();
+        excavator.work();
+        truck.stopWork();
+        bulldozer.stopWork();
+        excavator.stopWork();
+    }
 
 }
