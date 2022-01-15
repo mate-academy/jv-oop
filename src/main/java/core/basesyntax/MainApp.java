e9f71be45a88d6a576ce02f90f94c228c95ea9e7
LinkClink <shekaqwe@gmail.com>
2022-01-15 17:08:23 +0200

    create new classes/ solution problem (HW)
    Create new abstract class Machine
    Create new classes Excavator, Bulldozer

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine bulldozerOne = new Bulldozer();
+        Machine excavatorOne = new Excavator();
+        bulldozerOne.doWork();
+        bulldozerOne.stopWork();
+        excavatorOne.doWork();
+        excavatorOne.stopWork();
+    }
 }
