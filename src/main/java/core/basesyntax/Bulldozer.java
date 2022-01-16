11985278a81af8227497a6381a719fdeb4eceaae
Andriy_Poluhovic <chimbar@ukr.net>
2022-01-16 19:47:41 +0200

    All machines stopped work

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,4 +1,13 @@
 package core.basesyntax;
 
-public class Bulldozer {
+public class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer started work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer stopped work");
+    }
 }
