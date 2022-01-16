62079dd2c147998f6725833b310192e8b713994f
Oleh Mytiai <mytiai.ov@icloud.com>
2022-01-16 16:51:00 +0200

    in Main class created objects of Truck, Bulldozer, Excavator and ran each methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
+import core.basesyntax.machines.Bulldozer;
+import core.basesyntax.machines.Excavator;
+import core.basesyntax.machines.Truck;
+
 public class MainApp {
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+
+        excavator.doWork();
+        bulldozer.doWork();
+        truck.doWork();
+
+        excavator.stopWork();
+        bulldozer.stopWork();
+        truck.stopWork();
+    }
 
 }
