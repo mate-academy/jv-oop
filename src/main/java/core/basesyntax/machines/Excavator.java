aea3ade8e08c1177fb0f9e0164185c08f628ce33
Oleh Mytiai <mytiai.ov@icloud.com>
2022-01-16 16:39:13 +0200

    created classes Truck, Bulldozer, Excavator that inherit from Machine

---
+++ src/main/java/core/basesyntax/machines/Excavator.java
@@ -0,0 +1,16 @@
+package core.basesyntax.machines;
+
+import core.basesyntax.Machine;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+
+    }
+
+    @Override
+    public void stopWork() {
+
+    }
+}
---
+++ src/main/java/core/basesyntax/machines/Truck.java
@@ -0,0 +1,16 @@
+package core.basesyntax.machines;
+
+import core.basesyntax.Machine;
+
+public class Truck extends Machine {
+
+    @Override
+    public void doWork() {
+
+    }
+
+    @Override
+    public void stopWork() {
+
+    }
+}
