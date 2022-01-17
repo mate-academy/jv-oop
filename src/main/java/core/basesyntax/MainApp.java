3c3af6caa15b549b50f06e6d2f892ac56e6929dc
LinkClink <shekaqwe@gmail.com>
2022-01-17 09:36:47 +0200

    variable name fixes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,11 +2,11 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine bulldozerOne = new Bulldozer();
-        Machine excavatorOne = new Excavator();
-        bulldozerOne.doWork();
-        bulldozerOne.stopWork();
-        excavatorOne.doWork();
-        excavatorOne.stopWork();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+        excavator.doWork();
+        excavator.stopWork();
     }
 }
