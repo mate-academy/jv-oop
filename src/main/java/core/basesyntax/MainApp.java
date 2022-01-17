4c50800c4615e88c52d1b3d7410f0a7fa4d42d6a
sltnsrh <lifenjoy8888@gmail.com>
2022-01-17 16:46:26 +0200

    hw resolve

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Excavator excavator = new Excavator();
+        Bulldozer bulldozer = new Bulldozer();
+        Truck truck = new Truck();
 
+        excavator.doWork();
+        bulldozer.doWork();
+        truck.doWork();
+
+        excavator.stopWork();
+        bulldozer.stopWork();
+        truck.stopWork();
+    }
 }
