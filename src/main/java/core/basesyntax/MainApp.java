16cedc6d73af580f40ec9be965d3f6846b4048de
Andriy_Poluhovic <93582635+pikador10@users.noreply.github.com>
2022-01-17 09:21:30 +0200

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,11 +5,9 @@ public class MainApp {
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
-
         excavator.doWork();
         bulldozer.doWork();
         truck.doWork();
-
         excavator.stopWork();
         bulldozer.stopWork();
         truck.stopWork();
