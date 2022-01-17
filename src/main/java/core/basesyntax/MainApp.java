5479f9faba50312a0449a97a29239eb87a617c4c
Alex Drapikovsky <drapikovskyan@gmail.com>
2022-01-17 09:25:52 +0200

    OOP Basic. remove empty lines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,11 +5,9 @@ public class MainApp {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-
         truck.doWork();
         bulldozer.doWork();
         excavator.doWork();
-
         truck.stopWork();
         bulldozer.stopWork();
         excavator.stopWork();
