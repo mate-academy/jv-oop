0bad29f2716a884c60c12ac9f4349e4efeedf202
Sasha Bachynskyi <allfred12@gmail.com>
2022-01-17 09:09:12 +0200

    unnecessary empty lines were got rid of

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,9 +5,7 @@ public class MainApp {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-
         Machine[] machines = new Machine[] {truck, bulldozer, excavator};
-
         for (Machine machine : machines) {
             machine.doWork();
         }
