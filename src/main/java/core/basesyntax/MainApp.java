20b1acd626812ffd59c7b8a00420825862f05637
sltnsrh <lifenjoy8888@gmail.com>
2022-01-17 17:44:08 +0200

    fixed errors

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,14 +2,13 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Excavator excavator = new Excavator();
-        Bulldozer bulldozer = new Bulldozer();
-        Truck truck = new Truck();
-
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        
         excavator.doWork();
         bulldozer.doWork();
         truck.doWork();
-
         excavator.stopWork();
         bulldozer.stopWork();
         truck.stopWork();
