e7c1d0b39a2d6af476d550b7c4ab4e1b5f701104
sviat <svyatoslav.grinik@gmail.com>
2022-01-18 17:41:25 +0200

    all redundant empty lines was removed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,11 +5,9 @@ public class MainApp {
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
         Machine truck = new Truck();
-
         bulldozer.doWork();
         excavator.doWork();
         truck.doWork();
-
         bulldozer.stopWork();
         excavator.stopWork();
         truck.stopWork();
