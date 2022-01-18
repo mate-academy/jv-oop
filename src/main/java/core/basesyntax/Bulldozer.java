64beb8dbe207d97bd192659fbcc98a87d7491de8
Roman <dudanecr@gmail.com>
2022-01-18 13:37:25 +0200

    homevork OOP-Basic

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Bulldozer extends Macine {
+    public void doWork() {
+
+        System.out.println("Bulldozer start work");
+    }
+
+    @Override
+    public void stopWork() {
+
+        System.out.println("Bulldozer work stop");
+    }
+
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Macine truck = new Trukc();
+        Macine excavator = new Excavator();
+        Macine bulldozer = new Bulldozer();
+
+        truck.doWork();
+        excavator.doWork();
+        bulldozer.doWork();
+
+        truck.stopWork();
+        excavator.stopWork();
+        bulldozer.stopWork();
+    }
 
 }
