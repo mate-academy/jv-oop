1cc46d217787d32b936513c68bfdce3e2ee26b47
Ostap Danyliv <danyliv.ostap@gmail.com>
2022-01-19 18:55:11 +0200

    Tusk jv-oop complited

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck has began his work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck has stopped his work");
+    }
+
+}
+
+
