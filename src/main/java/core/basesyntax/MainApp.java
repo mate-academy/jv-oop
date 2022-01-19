d5e8c1fee4eb18e2eba2e38abbbdc1aae4f60dbb
Anton Ignatenko <87337009+antonignatenko@users.noreply.github.com>
2022-01-19 12:29:15 +0200

    implemented all methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
-
 public class MainApp {
     public static void main(String[] args) {
+
         Bulldozer bulldozer = new Bulldozer();
         Truck truck = new Truck();
         Excavator excavator = new Excavator();
