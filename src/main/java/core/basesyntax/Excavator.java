c869face9ab73848e0b7b61c83b80f6b8e03d3f4
Ivan Pavluchuk <java.python10@gmail.com>
2022-01-20 02:02:20 +0200

    added solution

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+    public void doWork() {
+        System.out.println("Excavator is starting.");
+    }
+
+    public void stopWork() {
+        System.out.println("Excavator is finishing.");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,15 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String [] arg) {
+        Truck tru = new Truck();
+        Bulldozer bul = new Bulldozer();
+        Excavator exc = new Excavator();
+        tru.doWork();
+        bul.doWork();
+        exc.doWork();
+        tru.stopWork();
+        bul.stopWork();
+        exc.stopWork();
+    }
 }
