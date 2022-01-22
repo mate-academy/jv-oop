5b58f9174a6b1cbf00588cb1e005dea162a2e17b
daviddohru@gmail.com <qwerty12AS12AS>
2022-01-22 21:26:06 +0300

    Save solutions

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,4 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+
+        truck.doWork();
+        bulldozer.doWork();
+
+        truck.stopWork();
+        bulldozer.stopWork();
+    }
 }
