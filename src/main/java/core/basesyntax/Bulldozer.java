07842d4c75848c5ffb55363efda6d64a266e4719
daviddohru@gmail.com <qwerty12AS12AS>
2022-01-22 22:43:24 +0300

    Save solutions

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -5,7 +5,6 @@ public class Bulldozer extends  Machine {
     public void doWork() {
         System.out.println("Bulldozer started its work.");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Bulldozer stopped its work.");
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -6,7 +6,6 @@ public class Truck extends Machine{
     public void doWork() {
         System.out.println("Truck started its work.");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Truck stopped its work.");
