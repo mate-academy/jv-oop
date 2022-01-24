259b5172df4595e52610a0c0bbf9d6a7a237e042
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-01-24 13:12:52 +0200

    changed cycle for to forEach

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,16 +2,16 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
         Machine[] machines = new Machine[3];
-        Excavator excavator = new Excavator();
-        Bulldozer bulldozer = new Bulldozer();
-        Truck truck = new Truck();
         machines[0] = excavator;
         machines[1] = bulldozer;
         machines[2] = truck;
-        for (int i = 0; i < machines.length; i++){
-            machines[i].doWork();
-            machines[i].stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
