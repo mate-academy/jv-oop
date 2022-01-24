4f8f1da1244d2d55b752f95a7a2fc920d7a77a1c
daviddohru@gmail.com <qwerty12AS12AS>
2022-01-24 23:46:55 +0300

    Add class Excavator

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,10 +4,13 @@ public class MainApp {
     public static void main(String[] args) {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
 
+        excavator.doWork();
         truck.doWork();
         bulldozer.doWork();
 
+        excavator.stopWork();
         truck.stopWork();
         bulldozer.stopWork();
     }
