cb8fd4ba88753e2829cceb1c83774caa1247d15a
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-01-25 16:19:41 +0200

    Create class Excavator

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+    public void doWork() {
+        System.out.println("The excavator started its work");
+    }
+    public void stopWork() {
+        System.out.println("The excavator stopped its work");
+    }
+}
