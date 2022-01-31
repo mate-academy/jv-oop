064e01261baf50f7384a4f9a5b55d97e9cc5898e
Misha Polunin <polunin1986@gmail.com>
2022-01-31 18:58:47 +0200

    - Created class Machine with doWork()` and stopWork()`. - Created classes Truck, Bulldozer, Excavator inherit from Machine. - In those classes override doWork(),stopWork() It print messages that certain machine started and stopped working. - In `Main` class created an object of each class.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
+	public static void main(String [] args)
+	{
+		Bulldozer bulldozer = new Bulldozer();
+		bulldozer.doWork();
+		bulldozer.stopWork();
 
+		Excavator excavator = new Excavator();
+		excavator.doWork();
+		excavator.stopWork();
+
+		Truck truck = new Truck();
+		truck.doWork();
+		truck.stopWork();
+	}
 }
