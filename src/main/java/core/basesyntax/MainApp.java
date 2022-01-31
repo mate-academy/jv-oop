2dc04c48cd03942a25501146d32a7ed4e89f7aea
Misha Polunin <polunin1986@gmail.com>
2022-01-31 19:06:56 +0200

    - fix

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,8 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
-	public static void main(String [] args)
-	{
+	public static void main(String [] args) {
 		Bulldozer bulldozer = new Bulldozer();
 		bulldozer.doWork();
 		bulldozer.stopWork();
