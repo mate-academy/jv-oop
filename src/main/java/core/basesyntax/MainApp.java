5ec8fcb01be3db1395386cf96af5256b0a14ed71
Dima Davydenko <dmytro.davydenko89@gmail.com>
2022-02-02 21:46:37 +0200

    Solution via loop created. Code style fixed.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,16 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine truck = new Truck();
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
-
-        truck.doWork();
-        bulldozer.doWork();
-        excavator.doWork();
-
-        truck.stopWork();
-        bulldozer.stopWork();
-        excavator.stopWork();
+        Machine[] machine = {new Truck(), new Bulldozer(), new Excavator()};
+        for (int i = 0; i < machine.length; i++) {
+            machine[i].doWork();
+            machine[i].stopWork();
+        }
     }
 }
