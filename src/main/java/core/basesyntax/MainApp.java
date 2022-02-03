76028780695b8e1cae38640db1639eeb973afdb2
B-i-g-D <ulinmob@gmail.com>
2022-02-03 14:55:47 +0200

    implemented array of machines that are run and stopped in a loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
-        for (Machine m : machines){
+        for (Machine m : machines) {
             m.doWork();
             m.stopWork();
         }
