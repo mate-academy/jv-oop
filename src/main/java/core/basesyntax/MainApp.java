57e22569bcae0a6b5ab5261ff7821cc0f9dc9880
Dima Pilipenko <dimadenver3@gmail.com>
2022-02-07 15:49:34 +0200

    task compleated

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,51 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        truck.doWork();
+        bulldozer.doWork();
+        excavator.doWork();
+        truck.stopWork();
+        bulldozer.stopWork();
+        excavator.stopWork();
+    }
 
+    public class Machine {
+        public abstract void doWork();
+        public abstract void stopWork();
+
+    }
+    public class Truck extends Machine {
+        @java.lang.Override
+        public void doWork() {
+            System.out.println("Truck was started to work");
+        }
+        @java.lang.Override
+        public void stopWork() {
+            System.out.println("Truck was stopped to work");
+        }
+    }
+    public class Bulldozer extends Machine {
+        @java.lang.Override
+        public void doWork() {
+            System.out.println("Bulldozer was started to work");
+        }
+        @java.lang.Override
+        public void stopWork() {
+            System.out.println("Bulldozer was stopped to work");
+        }
+    }
+    public class Excavator extends Machine {
+        @java.lang.Override
+        public void doWork() {
+            System.out.println("Excavator was started to work");
+        }
+        @java.lang.Override
+        public void stopWork() {
+            System.out.println("Excavator was stopped to work");
+        }
+    }
 }
--- src/test/java/core/basesyntax/MainAppTest.java
+++ src/test/java/core/basesyntax/MainAppTest.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
 public class MainAppTest {
-
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
