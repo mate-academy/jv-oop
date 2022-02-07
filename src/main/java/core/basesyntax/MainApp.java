f996a7112140e932bbae2397e436bf83c9908d42
vlad <vladdzyadevich@gmail.com>
2022-02-07 12:37:31 +0200

    Refactor code by comments (revert README.md file and add loops to MainApp)

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,16 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
-        Truck truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
 
-        bulldozer.doWork();
-        excavator.doWork();
-        truck.doWork();
-        bulldozer.stopWork();
-        excavator.stopWork();
-        truck.stopWork();
+        Machine[] machines = new Machine[]{bulldozer, excavator, truck};
+        for(int i = 0; i < machines.length; i++){
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
     }
-
 }
