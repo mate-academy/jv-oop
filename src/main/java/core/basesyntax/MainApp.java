44714e2a86e1e1c1dfb5c8939a6c97baedd358f1
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-02-13 21:49:54 +0200

    Change  override methods and create all objects

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,3 +1,13 @@
 package core.basesyntax;
 public class MainApp {
+    public static void main(String[] args){
+        Machine cyberTruck = new Truck();
+        cyberTruck.setTypeMachine("Cyber Truck");
+        Machine BuldCatD3 = new Bulldozer();
+        BuldCatD3.setTypeMachine("Buldozer Cat D3");
+        Machine ExcCat320D = new Excavator();
+        ExcCat320D.setTypeMachine("Excavator Cat 320 D");
+
+    }
+
 }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 public class Truck extends Machine{
     @Override
     public void doWork() {
-        System.out.println("Truck started its work !");
+        System.out.println(getTypeMachine() + " started its work !");
     }
     @Override
     public void stopWork() {
-        System.out.println("Truck stopped its work.");
+        System.out.println(getTypeMachine() + " stopped its work.");
     }
 }
