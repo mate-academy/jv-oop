c28f8a451fa6134c3e38dff00065a12282faffb5
VladimirShtukar <99186204+VladimirShtukar@users.noreply.github.com>
2022-02-17 14:24:02 +0200

    Update CI config

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Bulldozer extends Machine{
     @Override
     public void doWork() {
-        System.out.println("Bulldozer is working !");
+        System.out.println("Bulldozer is working ! ");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Bulldozer stopped working !");
+        System.out.println("Bulldozer stopped working ! ");
     }
 }
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -4,11 +4,11 @@ public class Excavator extends Machine {
 
     @Override
     public void doWork() {
-        System.out.println("Excavator is working !");
+        System.out.println("Excavator is working ! ");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Excavator stopped working !");
+        System.out.println("Excavator stopped working ! ");
     }
 }
