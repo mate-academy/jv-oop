4d33f425abab911e9d0aeb94bd0d173999ef0267
Angelina Roveska <lino4ka1396@gmail.com>
2022-02-23 17:18:19 +0200

    fixed

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Truck extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Truck start working");
+        System.out.println("Truck is working");
     }
 
     @Override
