f899580e2c64cfc0ffe246fa7c15058ac6c6217c
Pavlo Mitru <pavelpm1971@gmail.com>
2022-03-06 22:33:07 +0100

    added 4 classec whith overridet metods, create 3 objects in MainApp and and gave a command to execute for each object two overridden methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,9 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
 
+        truck.doWork();
+        truck.stopWork();
+        excavator.doWork();
+        excavator.stopWork();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+
+    }
 }
