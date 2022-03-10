1226072c0798096ac1e13884cf12b77733b52524
Pavlo Mitru <pavelpm1971@gmail.com>
2022-03-10 18:41:03 +0100

    rebuild MainApp's logic

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,15 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine truck = new Truck();
-        truck.doWork();
-        truck.stopWork();
-        Machine excavator = new Excavator();
-        excavator.doWork();
-        excavator.stopWork();
-        Machine bulldozer = new Bulldozer();
-        bulldozer.doWork();
-        bulldozer.stopWork();
+        Machine[] machines = new Machine[3];
+        machines[0] = new Truck();
+        machines[1] = new Bulldozer();
+        machines[2] = new Excavator();
 
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
