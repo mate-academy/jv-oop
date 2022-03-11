c305b202c080511a76c92be5c2058b141c206c8f
Denys.Sh <7860denis@gmail.com>
2022-03-11 19:36:46 +0200

    refactored

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,20 +2,19 @@ package core.basesyntax;
 
 import core.basesyntax.model.Bulldozer;
 import core.basesyntax.model.Excavator;
+import core.basesyntax.model.Machine;
 import core.basesyntax.model.Truck;
 
 public class MainApp {
     public static void main(String[] args) {
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
-        Truck truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine[] machines = {bulldozer,excavator,truck};
 
-        bulldozer.doWork();
-        excavator.doWork();
-        truck.doWork();
-
-        bulldozer.stopWork();
-        excavator.stopWork();
-        truck.stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
