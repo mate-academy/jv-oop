b9d19afe066f26e4a19986221731d4b2fd55d80f
sviatoslav <www.t.svyatoslav55@gmail.com>
2022-03-13 10:14:25 +0100

    Add abstract class and abstract methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Bulldozer bl = new Bulldozer();
-        bl.doWork();
-        bl.stopWork();
         Truck tr = new Truck();
-        tr.doWork();
-        tr.stopWork();
         Excavator ex = new Excavator();
-        ex.doWork();
-        ex.stopWork();
+     Machine[] machines = new Machine[] { bl,tr,ex};
+        for (Machine machin:machines
+             ) {
+            machin.doWork();
+            machin.stopWork();
+        }
     }
 }
