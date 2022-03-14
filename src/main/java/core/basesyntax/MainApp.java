b3e5855f5ac231df993872a281a876675435c1ed
Roma Yarishkin <romarijkov@gmail.com>
2022-03-14 14:58:59 +0200

    Task completed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        truck.doWork();
+        truck.stopWork();
+        Bulldozer bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+        Excavator excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
+    }
 }
