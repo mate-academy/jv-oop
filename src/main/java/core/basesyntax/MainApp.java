8ebb32fb495e11885acde49ad742e36c3678e55c
Andrew <tolstuy1996@gmail.com>
2022-03-14 16:36:12 +0200

    Solution HWOop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,16 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        truck.doWork();
+        truck.stopWork();
+        Bulldozer bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+        Excavator excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
+    }
+
 }
