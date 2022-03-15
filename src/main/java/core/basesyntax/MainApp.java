e56724be1e5b50326d86d83270db58e8380139b0
Dmytro Koliada <dmitrikstar@gmail.com>
2022-03-15 14:11:35 +0200

    remove empty line and rename

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,24 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
         Machine excavation = new Excavator();
-        /*
-        bulldozer.doWork();
-        truck.doWork();
-        excavation.doWork();
-
-        bulldozer.stopWork();
-        truck.stopWork();
-        excavation.stopWork();
-        */
-        Machine[] machine1 = new Machine[]{bulldozer, truck, excavation};
-        for (Machine machine : machine1) {
+        Machine[] machines = new Machine[]{bulldozer, truck, excavation};
+        for (Machine machine : machines) {
             machine.doWork();
         }
-        for (Machine machine : machine1) {
+        for (Machine machine : machines) {
             machine.stopWork();
         }
     }
