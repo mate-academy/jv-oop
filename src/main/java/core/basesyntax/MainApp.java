babb32496c263dd2310ac216c4e6a5ed489ef618
DanyloTiazhev <finckreg@gmail.com>
2022-03-17 17:33:48 +0200

    Vehicle Classes and Vehicles added

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args){
+        Truck ford = new Truck();
+        Bulldozer volvo = new Bulldozer();
+        Excavator alsoCat = new Excavator();
+        // Starting
+        ford.doWork();
+        volvo.doWork();
+        alsoCat.doWork();
+        // Stopping
+        ford.stopWork();
+        volvo.stopWork();
+        alsoCat.stopWork();
+    }
 
 }
