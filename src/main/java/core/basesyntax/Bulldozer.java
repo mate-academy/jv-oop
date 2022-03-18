fc1976b793528a4f59bdf9b0cfd16967975571ee
Sergey Pshenichnyh <serg.pshenichnyh@gmail.com>
2022-03-18 17:37:15 +0200

    fixed checkstyle

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -8,6 +8,6 @@ public class Bulldozer extends Machine {
 
     @Override
     public void stopWork() {
-        System.out.println("Bulldozer stopped its work"); ;'m'
+        System.out.println("Bulldozer stopped its work");
     }
 }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -11,4 +11,3 @@ public class Truck extends Machine {
         System.out.println("Truck stopped its work");
     }
 }
-','
