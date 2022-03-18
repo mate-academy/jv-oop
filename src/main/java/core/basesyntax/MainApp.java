1e3a171289117a4d19bd98bab8bf625dc26052fc
Sergey Pshenichnyh <serg.pshenichnyh@gmail.com>
2022-03-18 17:20:15 +0200

    fixed checkstyle.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Machine statusBulldozer = new Bulldozer();
         statusBulldozer.doWork();
         statusBulldozer.stopWork();
