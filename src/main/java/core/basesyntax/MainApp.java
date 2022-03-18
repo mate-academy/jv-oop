fd16eacc84dc9a485bad7a85265bb608a43c5bb7
Lyudmila D <dunda94lyudmila@gmail.com>
2022-03-18 15:47:02 +0200

    created 4 classes = 4 files: abstract Machine; Excavator, Bulldozer, Truck extend Machine created private String name, public abstract void (pav) doWork and stopWork in Machine created 3 objects: Volkswagen, Caterpillar, Hyundai in MainApp.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main() {
+        Machine volkswagen = new Truck();
+        volkswagen.setName("Volkswagen");
+        volkswagen.doWork();
+        volkswagen.stopWork();
 
+        Machine caterpillar = new Bulldozer();
+        caterpillar.setName("Caterpillar");
+        caterpillar.doWork();
+        caterpillar.stopWork();
+
+        Machine hyundai = new Excavator();
+        hyundai.setName("Hyundai");
+        hyundai.doWork();
+        hyundai.stopWork();
+    }
 }
