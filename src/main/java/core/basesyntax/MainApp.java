86f2cbe3718287cc8361a0244dff6fe401978d20
Nesstor Fedak <nesstortor@gmail.com>
2022-03-18 17:25:33 +0200

    created 3 classes inherited from class Machine with overriding abstract methods + created 3 elements each of created classes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
 
+        Machine comatsu = new Bulldozer();
+        Machine liebherr = new Excavator();
+        Machine caterpillar = new Truck();
+
+        comatsu.doWork();
+        liebherr.doWork();
+        caterpillar.doWork();
+
+        comatsu.stopWork();
+        liebherr.stopWork();
+        caterpillar.stopWork();
+    }
 }
