23de4939c349f958f23a4c886bc1b9c8ff1455b2
l_osyash <los.nazar24@gmail.com>
2022-03-19 15:36:56 +0200

    Task 1 is done.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
-public class MainApp {
+import javax.crypto.Mac;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        truck.doWork();
+        excavator.doWork();
+        bulldozer.doWork();
+        truck.stopWork();
+        excavator.stopWork();
+        bulldozer.stopWork();
+    }
 }
