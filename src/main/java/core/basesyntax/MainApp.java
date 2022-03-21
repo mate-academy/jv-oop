eac337aaab49be92cfd6ddc5ebadf8e3ee04a674
Sergey Pshenichnyh <serg.pshenichnyh@gmail.com>
2022-03-21 21:01:21 +0200

    added abstract type for variable names

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
-        Truck truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
         Machine[] machines = {bulldozer, excavator, truck};
 
         for (int i = 0; i < machines.length; i++) {
