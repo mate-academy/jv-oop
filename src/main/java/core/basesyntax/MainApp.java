9821c320f647466541deffd98133e0f7f31c9113
DanyloTiazhev <finckreg@gmail.com>
2022-03-21 19:00:59 +0200

    changes applied

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,17 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Truck ford = new Truck();
-        Bulldozer volvo = new Bulldozer();
-        Excavator alsoCat = new Excavator();
-        // Starting
-        ford.doWork();
-        volvo.doWork();
-        alsoCat.doWork();
-        // Stopping
-        ford.stopWork();
-        volvo.stopWork();
-        alsoCat.stopWork();
+        Machine ford = new Truck();
+        Machine volvo = new Bulldozer();
+        Machine cat = new Excavator();
+        Machine [] garage = {ford, volvo, cat};
+        for (Machine machine : garage) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 
 }
