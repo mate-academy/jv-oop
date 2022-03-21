b3a1bf8778cf8a2ad1d6ad1c22637cac24cd2e83
yuriikozar <yuriikozar@gmail.com>
2022-03-21 09:31:11 +0200

    Commit 4 Main Class modified

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,15 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Truck truck = new Truck();
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
-        truck.doWork();
-        bulldozer.doWork();
-        excavator.doWork();
-        truck.stopWork();
-        bulldozer.stopWork();
-        excavator.stopWork();
+        Machine [] machines = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine a : machines) {
+            a.doWork();
+            a.stopWork();
+        }
     }
-
 }
