911333b8d2ee5bc72a8c4ee9be9afc05afda444f
DanyloTiazhev <finckreg@gmail.com>
2022-03-22 12:02:04 +0200

    final changes applied

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,7 +5,7 @@ public class MainApp {
         Machine ford = new Truck();
         Machine volvo = new Bulldozer();
         Machine cat = new Excavator();
-        Machine [] garage = {ford, volvo, cat};
+        Machine[] garage = {ford, volvo, cat};
         for (Machine machine : garage) {
             machine.doWork();
             machine.stopWork();
