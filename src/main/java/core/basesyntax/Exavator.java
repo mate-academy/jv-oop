bc3e46fc81956bbd40bc9e3f22cd8ad8b058f892
Gigawatt94 <101740223+Gigawatt94@users.noreply.github.com>
2022-03-23 18:06:41 +0200

    first try to work machine

---
+++ src/main/java/core/basesyntax/Exavator.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Exavator extends Machine {
+    @Override
+    public void doWork(){
+        System.out.println("Exavator start working");
+    }
+    @Override
+    public void stopWork(){
+        System.out.println("Exavator stop working");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,22 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+
+        Machine banderomobil = new Truck();
+        Machine zil = new Bulldozer();
+        Machine gazon = new Exavator();
+
+        banderomobil.doWork();
+        zil.doWork();
+        gazon.doWork();
+
+        banderomobil.stopWork();
+        zil.stopWork();
+        gazon.stopWork();
+
+
+
+    }
+
 }
