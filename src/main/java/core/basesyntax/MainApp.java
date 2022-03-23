764b6c9bc793e5d07a473cef41bc13e5e8bd83b6
Vladyslav Liakhin <97559001+VladyslavLn@users.noreply.github.com>
2022-03-23 12:34:57 +0200

    Update src/main/java/core/basesyntax/MainApp.java
    Changed work -> machine
    
    Co-authored-by: Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] garage = new Machine[] {new Bulldozer(),new Excavator(),new Truck()};
-        for (Machine work :garage) {
+        for (Machine machine :garage) {
             work.doWork();
             work.stopWork();
         }
