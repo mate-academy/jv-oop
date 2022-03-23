6ce306dd4ba41af459938a362c148173a33052bb
Fantom5511 <mironovanton5511@gmail.com>
2022-03-24 00:07:10 +0200

    implemented the chain of inheritance

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("doWorkExc");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("stopWorkExc");
+    }
+}
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("doWorkTruck");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("stopWorkTruck");
+    }
+}
