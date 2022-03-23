d7417784f17271208626db7a7a688d0d38280d09
Vladyslav Liakhin <97559001+VladyslavLn@users.noreply.github.com>
2022-03-23 12:35:56 +0200

    Update src/main/java/core/basesyntax/MainApp.java
    Added whitespace
    
    Co-authored-by: Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] garage = new Machine[] {new Bulldozer(),new Excavator(),new Truck()};
+        Machine[] garage = new Machine[] {new Bulldozer(), new Excavator(), new Truck()};
         for (Machine machine :garage) {
             work.doWork();
             work.stopWork();
