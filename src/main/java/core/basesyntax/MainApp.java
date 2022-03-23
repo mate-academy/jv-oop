40dd7e88dbc63cdd7b7b44eed1f2b9fe56a42118
Vladyslav Lyakhin <kapuuustin@gmail.com>
2022-03-23 12:40:37 +0200

    Added whitespace and changed variable :)

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] garage = new Machine[] {new Bulldozer(),new Excavator(),new Truck()};
-        for (Machine work :garage) {
-            work.doWork();
-            work.stopWork();
+        Machine[] garage = new Machine[] {new Bulldozer(), new Excavator(), new Truck()};
+        for (Machine machine :garage) {
+            machine.doWork();
+            machine.stopWork();
         }
 
     }
