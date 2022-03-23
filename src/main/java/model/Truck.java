41c84f51a5cd09525597d6c88a052ee58480267b
GerasimovTA-92 <olexandr.gerasimov.02@gmail.com>
2022-03-23 18:31:14 +0200

    hw solution

---
+++ src/main/java/model/Truck.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Truck extends Machine {
+    public void doWork() {
+        System.out.println("Truck started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped its work");
+    }
+}
