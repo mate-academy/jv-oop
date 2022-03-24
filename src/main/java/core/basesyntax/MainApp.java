e17c21cafc05c660e1e0a4becccefd385005eef5
Gigawatt94 <101740223+Gigawatt94@users.noreply.github.com>
2022-03-24 13:16:52 +0200

    array Machine was added; 2 for was added; BUILD SUCCES;

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,20 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
-
     public static void main(String[] args) {
+        Machine[] auto = new Machine[3];
+        auto[0] = new Exavator();
+        auto[1] = new Bulldozer();
+        auto[2] = new Truck();
 
-        Machine banderomobil = new Truck();
-        Machine zil = new Bulldozer();
-        Machine gazon = new Exavator();
+        for (int i = 0; i < auto.length; i++) {
+            auto[i].doWork();
+        }
 
-        banderomobil.doWork();
-        zil.doWork();
-        gazon.doWork();
-
-        banderomobil.stopWork();
-        zil.stopWork();
-        gazon.stopWork();
-
+        for (int i = 0; i < auto.length; i++) {
+            auto[i].stopWork();
+        }
     }
 }
