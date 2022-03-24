eb98cf71d27658b27697868bd4e02f43b0b40b9c
Oleksander <ghjwtyrj25@gmail.com>
2022-03-24 16:56:42 +0200

    final. after mvn clean

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,11 +1,9 @@
 package core.basesyntax;
 
-import javax.crypto.Mac;
-
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Excavator(), new Truck(), new Bulldozer()};
-        for(Machine machine : machines) {
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
