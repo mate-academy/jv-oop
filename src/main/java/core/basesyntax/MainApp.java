24c807403a1df0427f072a24d7f8753dcfaf3952
mordast7 <mordast7@gmail.com>
2022-03-24 15:49:57 +0200

    fixed checkstyle mistakes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Machine[] machines = new Machine[] {new Excavator(), new Bulldozer(), new Truck() };
         for (Machine machine : machines) {
             machine.doWork();
