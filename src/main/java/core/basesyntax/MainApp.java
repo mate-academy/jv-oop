83d785e198717a6f1525038cce882853ba8604b5
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2022-03-24 14:33:48 +0200

    Add foreach loop into `main()` method

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,12 +5,10 @@ public class MainApp {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-
-        truck.doWork();
-        bulldozer.doWork();
-        excavator.doWork();
-        truck.stopWork();
-        bulldozer.stopWork();
-        excavator.stopWork();
+        Machine[] machines = {truck, bulldozer, excavator};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
