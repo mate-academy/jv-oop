d796e6bb0543c478f25f6263cfe3df8d814ccd9e
sofiadiakonova <sofiya.dyakonova@ukr.net>
2022-03-25 09:28:06 +0100

    hw 3 was done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,17 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine[] machines = {bulldozer, truck, excavator};
+
+        doWork(machines);
+    }
+    public static void doWork(Machine[] machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 }
