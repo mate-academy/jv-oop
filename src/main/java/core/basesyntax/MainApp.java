0df27843554ebf04322b08908610dfa258f9a044
Roman Omeliukh <romik3513@gmail.com>
2022-03-25 10:45:55 +0200

    homework jv-oop updated

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,18 +2,13 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String [] args) {
-
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
-
-        excavator.doWork();
-        bulldozer.doWork();
-        truck.doWork();
-
-        excavator.stopWork();
-        bulldozer.stopWork();
-        truck.stopWork();
+        Machine [] machine = new Machine[]{excavator,bulldozer,truck};
+        for (Machine machine1:machine) {
+            machine1.doWork();
+            machine1.stopWork();
+        }
     }
-
 }
