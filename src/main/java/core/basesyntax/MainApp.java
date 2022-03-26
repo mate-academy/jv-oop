d49f74b82c3d136f731bf5c612bb4ceac7c52c66
Mykola Dubich <nikolay19888@hotmail.com>
2022-03-26 17:44:15 +0200

    Creat classes: Machine, Truck, Bulldozer, Excavator.Ñ Implement methods: doWork and stopWork

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.model.Bulldozer;
+import core.basesyntax.model.Excavator;
+import core.basesyntax.model.Machine;
+import core.basesyntax.model.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        Machine[] machines = new Machine[]{truck,bulldozer,excavator};
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+        for (Machine machine : machines) {
+            machine.stopWork();
+        }
+    }
 }
