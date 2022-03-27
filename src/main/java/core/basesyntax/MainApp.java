6b555e4180bcf391155451acd33743472a3a2886
Voinkov Roman <gredisroman@gmail.com>
2022-03-27 10:59:33 +0200

    27.03.2022 java oop RomanGredis

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Excavator one = new Excavator();
+        Bulldozer two = new Bulldozer();
+        Truck three = new Truck();
+
+        one.doWork();
+        one.stopWork();
+        two.doWork();
+        two.stopWork();
+        three.doWork();
+        three.stopWork();
+    }
 
 }
