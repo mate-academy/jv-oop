4e55f229e7760ba9aff1785521106d5b309b2abb
Mark Shapka <mark.shapka1@gmail.com>
2022-03-28 12:33:00 +0300

    was removed empty lines and created array of machines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,16 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine bulldozer = new Bulldozer();
-        Machine truck = new Truck();
-        Machine excavator = new Excavator();
-
-        bulldozer.doWork();
-        truck.doWork();
-        excavator.doWork();
-
-        bulldozer.stopWork();
-        truck.stopWork();
-        excavator.stopWork();
+        Machine[] machines = {new Bulldozer(), new Truck(), new Excavator()};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
