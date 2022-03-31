4036d40bdc82472d3a866a9f01622859312542d8
Artem Bordichuk <artyombordichuk@gmail.com>
2022-03-31 17:40:30 +0300

    created new clases. added methods in MainApp class. implement psvm in MaimApp.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,15 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Machine newBulldozer = new Bulldozer();
+        Machine newExcavator = new Excavator();
+        Machine newTruck = new Truck();
+        Machine[] machines = new Machine[]{newBulldozer, newExcavator, newTruck};
+
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 
 }
