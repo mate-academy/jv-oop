91b9c06afb660277a8d5aa6f6f1b32777b48264c
Voinkov Roman <gredisroman@gmail.com>
2022-03-31 18:20:15 +0200

    final/GredisVoinkov/java_oop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Truck truck = new Truck();
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
 
         truck.doWork();
         bulldozer.doWork();
