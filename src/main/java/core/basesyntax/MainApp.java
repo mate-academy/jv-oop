fa206c50ae5cda2c2ce955f82cc2d112e9eb93e7
Maksym <Maksym_Personal@Dibrovs-MacBook-Pro.local>
2022-04-02 22:17:13 +0300

    First attempt. All files in src module

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,9 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+    for(Machine machine: machines){
+        machine.doWork();
+        machine.stopWork();
+    }
 }
