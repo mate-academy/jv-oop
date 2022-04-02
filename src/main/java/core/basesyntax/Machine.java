21bce8b958a16fbc4171bbc5ce454a473701cdd1
Maksym <Maksym_Personal@Dibrovs-MacBook-Pro.local>
2022-04-02 23:03:27 +0300

    Third attempt - fixed indentation

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,39 +1,39 @@
 package core.basesyntax;
 
 public abstract class Machine {
-        public abstract void doWork();
-        public abstract void stopWork();
+    public abstract void doWork();
+    public abstract void stopWork();
     }
 
-    class Truck extends Machine {
-        @Override
-        public void doWork(){
-            System.out.println("Truck started working.");
-        }
-        @Override
-        public void stopWork(){
-            System.out.println("Truck stopped working.");
-        }
+class Truck extends Machine {
+    @Override
+    public void doWork(){
+        System.out.println("Truck started working.");
     }
+    @Override
+    public void stopWork(){
+        System.out.println("Truck stopped working.");
+    }
+}
 
-    class Bulldozer extends Machine {
-        @Override
-        public void doWork(){
-            System.out.println("Bulldozer started working.");
-        }
-        @Override
-        public void stopWork(){
-            System.out.println("Bulldozer stopped working.");
-        }
+class Bulldozer extends Machine {
+    @Override
+    public void doWork(){
+        System.out.println("Bulldozer started working.");
     }
+    @Override
+    public void stopWork(){
+        System.out.println("Bulldozer stopped working.");
+    }
+}
 
-    class Excavator extends Machine {
-        @Override
-        public void doWork(){
-            System.out.println("Excavator started working.");
-        }
-        @Override
-        public void stopWork(){
-            System.out.println("Excavator stopped working.");
-        }
+class Excavator extends Machine {
+    @Override
+    public void doWork(){
+        System.out.println("Excavator started working.");
     }
+    @Override
+    public void stopWork(){
+        System.out.println("Excavator stopped working.");
+    }
+}
