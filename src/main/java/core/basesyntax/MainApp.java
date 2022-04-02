d454a80bba45ac0b0348d8056f909fce2c019608
Evhenii Vaskiv <evgenijvaskiv@gmail.com>
2022-04-02 19:54:43 +0300

    new classes, methods and array were added

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+     Machine[] machines = new Machine[3];
+     machines[0] = new Excavator();
+     machines[1] = new Truck();
+     machines[2] = new Bulldozer();
 
+     for (int i = 0; i < machines.length; i++) {
+         machines[i].doWork();
+         machines[i].stopWork();
+     }
+    }
 }
