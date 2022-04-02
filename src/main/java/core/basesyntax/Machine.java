ba25146229a2aa95aa18422af64ecee8f43616a0
Maksym <Maksym_Personal@Dibrovs-MacBook-Pro.local>
2022-04-02 23:09:07 +0300

    4th attempt - code style improvements

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -2,38 +2,42 @@ package core.basesyntax;
 
 public abstract class Machine {
     public abstract void doWork();
+
     public abstract void stopWork();
-    }
+}
 
 class Truck extends Machine {
     @Override
-    public void doWork(){
+    public void doWork() {
         System.out.println("Truck started working.");
     }
+
     @Override
-    public void stopWork(){
+    public void stopWork() {
         System.out.println("Truck stopped working.");
     }
 }
 
 class Bulldozer extends Machine {
     @Override
-    public void doWork(){
+    public void doWork() {
         System.out.println("Bulldozer started working.");
     }
+
     @Override
-    public void stopWork(){
+    public void stopWork() {
         System.out.println("Bulldozer stopped working.");
     }
 }
 
 class Excavator extends Machine {
     @Override
-    public void doWork(){
+    public void doWork() {
         System.out.println("Excavator started working.");
     }
+
     @Override
-    public void stopWork(){
+    public void stopWork() {
         System.out.println("Excavator stopped working.");
     }
 }
