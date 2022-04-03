5f951a386a3ef1b010f6848cdeed0cf56c327baf
Alex <alexoriobel@gmail.com>
2022-04-03 18:50:21 +0300

    Add Machine.java array;
    Fill it Machine.java array with Bulldozer.java, Excavator.java, Truck.java;
    
    Was Created foreach loop to print out Machine.doWork and Machine.stopWork methods for
    specific vehicle;

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.vehicle.Bulldozer;
+import core.basesyntax.vehicle.Excavator;
+import core.basesyntax.vehicle.Machine;
+import core.basesyntax.vehicle.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+
+        for (Machine m : machines) {
+            m.doWork();
+            m.stopWork();
+            System.out.println();
+        }
+    }
 }
