938c208d4daa3daf2c3aa437d67b83a0c3c05955
Aleksandr Nesterov <sailor2016@gmail.com>
2022-04-07 20:29:44 +0300

    moved to separeted files

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+class Excavator extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Excavator start work");
+
+    }
+
+    @Override
+    public void stopWork() {
+
+        System.out.println("Excavator stop work");
+    }
+}
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck start work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stop work");
+    }
+}
