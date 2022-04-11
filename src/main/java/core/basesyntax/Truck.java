87574063cc5bbdd2184c8d0fabc4bdd5b87da4d7
Misha Varenycia <mihvaren@gmail.com>
2022-04-11 20:48:16 +0300

    added new fixes

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,13 +1,13 @@
 package core.basesyntax;
 
-public class Truck  extends  Machine{
+public class Truck extends Machine {
     @Override
-    public void doWork(){
+    public void doWork() {
         System.out.println("Truck has started his work");
     }
 
     @Override
-    public void stopWork(){
+    public void stopWork() {
         System.out.println("Truck has stopped his work");
     }
 }
