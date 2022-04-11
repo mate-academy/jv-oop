41d84125af9048a40025b8862fd4d8cdf95664eb
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2022-04-11 14:25:26 +0300

    added fixes after review

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
-
 public class Excavator extends Machine {
-
     @Override
     public void doWork() {
         System.out.println("Excavator started its work");
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,16 +1,15 @@
 package core.basesyntax;
-
 public class MainApp {
     public static void main(String[] args) {
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
         Machine truck = new Truck();
 
-        Machine[] array = new Machine[]{bulldozer,excavator,truck};
+        Machine[] machines = new Machine[]{bulldozer,excavator,truck};
 
-        for (int i = 0; i < array.length; i++) {
-            array[i].doWork();
-            array[i].stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
