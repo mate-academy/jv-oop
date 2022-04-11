a74eb39060b509a30235a113ea992fb0009718a6
Aleksandr Nesterov <sailor2016@gmail.com>
2022-04-11 22:17:47 +0300

    edited some errors

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -4,12 +4,10 @@ class Excavator extends Machine {
     @Override
     public void doWork() {
         System.out.println("Excavator start work");
-
     }
 
     @Override
     public void stopWork() {
-
         System.out.println("Excavator stop work");
     }
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine [] technics = new Machine [] {new Truck(), new Bulldozer(), new Excavator()};
-        for (int i = 0; i < technics.length; i++) {
-            technics[i].doWork();
-            technics[i].stopWork();
+        Machine [] machines = new Machine [] {new Truck(), new Bulldozer(), new Excavator()};
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
         }
     }
 }
