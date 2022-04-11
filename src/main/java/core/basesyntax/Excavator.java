f1437c1ccf933ddaa53747ccbfb380ac5346533b
Oleksandr Petrunovskyi <o.petrunovskyi@gmail.com>
2022-04-11 09:39:51 +0300

    removed empty lines

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,12 +1,9 @@
 package core.basesyntax;
-
 public class Excavator extends Machine {
-
     @Override
     public void doWork() {
         System.out.println("Excavator started working");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Excavator has completed");
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,15 +1,11 @@
 package core.basesyntax;
-
 public class Truck extends Machine {
-
     @Override
     public void doWork() {
         System.out.println("Truck started working");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Truck has completed");
     }
 }
-
