028b7efb4a811f37dc4e5abc64dac1dd9e81a7a9
Natalya Shcheglova <uralgnome@gmail.com>
2022-04-16 12:19:01 +0200

    BUILD FAILURE was fixed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,14 +1,14 @@
 package core.basesyntax;
 
 public class Main {
-    public static void main() {
+    public static void main(String[] args) {
         Truck truck = new Truck();
         Buldozer buldozer = new Buldozer();
         Excavator excavator = new Excavator();
         Machine[] machines = {truck, buldozer, excavator};
-        for (Machine machine : machines){
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
     }
-       }
+}
