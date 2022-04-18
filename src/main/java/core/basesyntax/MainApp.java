1c6b070c17fb412f45f8fefb7d5c0061f86851ad
Maksym Karamavrov <makcumkaka@gmail.com>
2022-04-18 17:10:46 +0300

    mvn clean package was applied. Mistekes were fixed.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,17 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine excavator = new Excavator();
-        Machine truck = new Truck();
-        Machine bulldozer = new Bulldozer();
-        Machine [] machines = new Machine[]{excavator, truck, bulldozer};
-        for (Machine machine: machines){
+        Machine[] machines = new Machine[] {new Excavator(), new Truck(), new Bulldozer()};
+        for (Machine machine : machines) {
             printInfo(machine);
         }
     }
-    public static void printInfo(Machine machine ){
+
+    public static void printInfo(Machine machine) {
         machine.doWork();
         machine.stopWork();
     }
-
 }
