9e5f58a6ca2683520ebb3ffa3f1de547588a7cf6
Mark Godnyi <libertyinprogresslife@gmail.com>
2022-04-23 19:44:12 +0300

    Solution to jv-oop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        excavator.setName("Ave");
 
+        Machine truck = new Truck();
+        truck.setName("John");
+
+        Machine bulldozer = new Bulldozer();
+        bulldozer.setName("David");
+
+        Machine[] machines = new Machine[]{excavator, truck, bulldozer};
+
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
