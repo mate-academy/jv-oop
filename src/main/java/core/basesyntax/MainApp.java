216737d841e63479b69ea8acdfdd683697069262
Oleksii Mospan <alexey.mospan@gmail.com>
2022-04-28 14:36:31 +0300

    Fixed code-style issues

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machines = new Machine[] {new Bulldozer(), new Excavator(), new Truck()};
+        Machine[] machines = new Machine[]{new Bulldozer(), new Excavator(), new Truck()};
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
