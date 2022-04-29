c77abc350807d2fff25d90f6dfa14710eb7dbba5
Igor <ZIM20093@gmail.com>
2022-04-30 00:16:23 +0300

    implemented doWork(), stopWork()

--- src/main/java/core/basesyntax/machines/Excavator.java
+++ src/main/java/core/basesyntax/machines/Excavator.java
@@ -1,4 +1,13 @@
 package core.basesyntax.machines;
 
 public class Excavator extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Excavator started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator stopped its work");
+    }
 }
