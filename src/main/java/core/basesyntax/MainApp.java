66448c211046ace164c3dca361c696efb83dbe43
svyatoslav <sviatoslav55@icloud.com>
2022-04-30 18:18:37 +0200

    I deleted the empty deadlines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,17 +4,12 @@ public class MainApp {
     public static void main(String[] args) {
 
         Machine bulldozer = new Bulldozer();
-
         Machine excavator = new Excavator();
-
         Machine truck = new Truck();
-
         Machine[] machines = new Machine[] {bulldozer, excavator, truck};
-
         for (Machine machine : machines) {
             machine.doWork();
         }
-
         for (Machine machine : machines) {
             machine.stopWork();
         }
