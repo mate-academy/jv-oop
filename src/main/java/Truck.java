364d396dc9d4bf7c5e88f3730b9932764ac6b614
Zinchuk-Yevhenii-Mate <zenya87@gmail.com>
2022-05-01 17:51:14 +0300

    Createdcreated Machine class and doWork, stopWork methods,creted Truck, Bulldozer, Excavator classes inherited from Machine, created in main class machine array with truck bulldozer excavator

---
+++ src/main/java/Truck.java
@@ -0,0 +1,13 @@
+import core.basesyntax.Machine;
+
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck started work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped work");
+    }
+}
