f0a1446ae64f7151d272533239625930ae8d396e
VladimirShtukar <99186204+VladimirShtukar@users.noreply.github.com>
2022-05-02 16:14:53 +0300

    Add files via upload

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -5,7 +5,6 @@ public class Bulldozer extends Machine{
     public void doWork() {
         System.out.println("Bulldozer is working ! ");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Bulldozer stopped working ! ");
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -6,7 +6,6 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator is working ! ");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Excavator stopped working ! ");
