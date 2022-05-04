8774c2a0529e6da41c29a79bf8a7a3dadb03065e
Benatti <ssunica0@gmail.com>
2022-05-05 00:52:59 +0300

    fix syntax

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] cars = {new Truck(), new Bulldozer(), new Excavator()};
-        for(Machine car: cars) {
+        for (Machine car: cars) {
             car.doWork();
             car.stopWork();
         }
     }
-
 }
