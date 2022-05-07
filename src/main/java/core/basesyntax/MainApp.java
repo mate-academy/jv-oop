6f226fc9e5d2df497af8137bd780827a8a46bada
Dima Povkh <dimapovkh@gmail.com>
2022-05-07 22:59:09 +0200

    empty lines were removed. mvn clean package result is NO errors.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,11 +1,9 @@
 package core.basesyntax;
-
 public class MainApp {
     public static void main(String[] args) {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-
         Machine[] machines = new Machine[] {truck, bulldozer, excavator};
         for (Machine machine: machines) {
             machine.doWork();
@@ -14,5 +12,4 @@ public class MainApp {
             machine.stopWork();
         }
     }
-
 }
