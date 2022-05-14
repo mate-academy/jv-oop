c2f29fd64c7cafb3133151965199b18413f654a4
Konstantin Miroshnichenko <bigfishnotbad@gmail.com>
2022-05-14 22:19:22 +0300

    finished implementation class Main / BUILD SUCCESS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
 public class Main {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        Machine[] machineArray = new Machine[3];
+        machineArray[0] = new Truck();
+        machineArray[1] = new Bulldozer();
+        machineArray[2] = new Excavator();
+        for (Machine machine : machineArray) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
-
 }
