4dc6a124d9d1dd36221573de758207ad99bb2c5c
nazarbaklanov <nazarbaklanov@gmail.com>
2022-05-15 15:30:58 +0300

    Added class main. Added Machine array with name of machines.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machinesArray = {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : machinesArray) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
