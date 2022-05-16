99f82ed1f79c01aba24aad07f5c32db353ab9543
Ivan Yuriv <yuriv1024@gmail.com>
2022-05-16 09:37:22 +0300

    redundant empty lines was removed.

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -4,12 +4,10 @@ public class Truck extends Machine {
     @Override
     public void doWork() {
         System.out.println("Truck starts working");
-
     }
 
     @Override
     public void stopWork() {
         System.out.println("Truck stops working");
-
     }
 }
