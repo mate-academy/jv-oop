767f70718d5bf915acbc03f1352b8e6bf70d4df8
Anton Karpov <antonkarpov547@gmail.com>
2022-05-16 23:10:35 +0200

    Machines implemented

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machinesArray = {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine m : machinesArray) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
 }
