eedf91172dee1e8ecb166749ac2756165cf541cd
nazarbaklanov <nazarbaklanov@gmail.com>
2022-05-16 09:44:40 +0300

    The redundant empty lines are removed.

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
+
 public class Excavator extends Machine {
     @Override
     public void doWork() {
         System.out.println("Excavator is working.");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Excavator is stop working.");
