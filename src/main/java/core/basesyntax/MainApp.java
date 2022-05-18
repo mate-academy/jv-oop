a7b13f288083d704a67da2929462ffb66edc83b5
kqlynero <kqlynero@gmail.com>
2022-05-18 11:01:23 +0300

    -created abstract class Machine -created classes Truck, Bulldozer, Excavator which extends Machine -implements methods from Machine -create 3 object of Truck, Bulldozer, Excavator -in the loop run methods doWork() and stopWork() -fixed mistake in MainApp(current)

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,9 +4,9 @@ public class MainApp {
     public static void main(String[] args) {
         Machine [] machines = {new Truck(), new Excavator(), new Bulldozer()};
 
-        for (Machine current : machines) {
-            current.doWork();
-            current.stopWork();
+        for (Machine currentMachine : machines) {
+            currentMachine.doWork();
+            currentMachine.stopWork();
         }
     }
 }
