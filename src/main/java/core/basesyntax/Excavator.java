c724c30165aafdee508ee28045b2040ab4eb9166
Denys <drtymkivstain@gmail.com>
2022-05-18 15:59:21 +0300

    Implemented abstract class Machine, his successor classes Truck, Bulldozer, Excavator, methods doWork() and stopWork

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("The excavator started working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The excavator stopped working");
+    }
+}
