6a1a4ca7e63f0af3101f97dd1ed0ff41837a0d44
Vasylyi Bevz <bevzvasya@gmail.com>
2022-05-18 17:01:46 +0300

    Was added 4 class whith base functional

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+
+    public void doWork() {
+        System.out.println("Track start work");
+    }
+
+    public void stopWork() {
+        System.out.println("Track stop work");
+    }
+}
