785747a86ecd3862434de313b691eb8be305bb61
Nata <natserbo@gmail.com>
2022-05-18 15:49:05 +0300

    add text printing in doWork & stopWork in all slases

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Excavator extends Machine{
     @Override
     public void doWork() {
-
+        System.out.println("Excavator start working");
     }
 
     @Override
     public void stopWork() {
-
+        System.out.println("Excavator stop working");
     }
 }
