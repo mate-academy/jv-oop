706bf2d9a501ee95abf1d36a35a548f0e30c40ef
Yulii Lukhanin <julius.lukhanin@outlook.com>
2022-05-19 01:06:45 +0300

    Implemented classes Truck, Bulldozer and Excavator that extends the abstract class machine

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) { 
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machinesArray = new Machine[]{truck, bulldozer, excavator};
+        for (Machine machine : machinesArray) { 
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
