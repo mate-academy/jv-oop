5719277ebaa47ad19f98d1e9f473b120f0a51bec
Denys <drtymkivstain@gmail.com>
2022-05-18 16:40:16 +0300

    Implemented abstract class Machine, his successor classes Truck, Bulldozer, Excavator, methods doWork() and stopWork, removed redundant empty lines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -7,11 +7,9 @@ public class MainApp {
         Machine truck = new Truck();
         Machine excavator = new Excavator();
         Machine[] machines = {bulldozer, truck, excavator};
-
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
     }
-
 }
