ba1a109b113abd7f8727a18393d14a73fa747281
Karina Tanasiichuk <k.tanasiichuk18@gmail.com>
2022-05-18 22:12:09 +0300

    machine work - fixed

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,14 +1,13 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
-
     @Override
     public void doWork() {
-        System.out.println("core.basesyntax.Truck started its work!");
+        System.out.println("Truck started its work!");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("core.basesyntax.Truck stopped its work!");
+        System.out.println("Truck stopped its work!");
     }
 }
