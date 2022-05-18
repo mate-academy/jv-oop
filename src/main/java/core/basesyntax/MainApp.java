9daf74b3ba16e86ebf4940e2bf04247c3306dd6c
Nata <natserbo@gmail.com>
2022-05-18 17:24:17 +0300

    add main mathod, Track,Bulldoser, Excavator Objects and loop for doWork & stopWork realisation

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldoser = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machines = new Machine[]{truck,bulldoser,excavator};
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
 
 }
