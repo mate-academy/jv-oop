92debcf332e63731aad04af777f221bb487d84b8
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-05-20 16:39:16 +0300

    Was create arrays autoParck and cycle

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    Truck truck = new Truck();
+    Excavator excavator = new Excavator();
+    Bulldozer bulldozer = new Bulldozer();
 
+    Machine[] autoParck = {truck, excavator, bulldozer};
+    for (Machine machine : autoParck) {
+      machine.doWork();
+      machine.stopWork();
     }
+  }
 }
