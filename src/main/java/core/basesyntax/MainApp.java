71d70cc9872d03aec36e1b309a6122bc0201a0ce
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-05-20 19:04:59 +0300

    change format

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,15 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-  public static void main(String[] args) {
-    Truck truck = new Truck();
-    Excavator excavator = new Excavator();
-    Bulldozer bulldozer = new Bulldozer();
+    public static void main(String[] args) {
+	Machine truck = new Truck();
+	Machine excavator = new Excavator();
+	Machine bulldozer = new Bulldozer();
 
-    Machine[] autoParck = {truck, excavator, bulldozer};
-    for (Machine machine : autoParck) {
-      machine.doWork();
-      machine.stopWork();
+	Machine[] autoParck = { truck, excavator, bulldozer };
+	for (Machine machine : autoParck) {
+	    machine.doWork();
+	    machine.stopWork();
+	}
     }
-  }
 }
