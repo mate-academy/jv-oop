b8303260e928628b9b4d92b6dc3d0494c7d09e29
Denis Kravziuk <deniskravziuk@gmail.com>
2022-05-23 14:01:12 +0300

    Merge remote-tracking branch 'origin/Develop' into Develop. Changes were made accordingly.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,13 +5,10 @@ public class MainApp {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-
         Machine[] machines = {truck, bulldozer, excavator};
-
-        for (Machine item : machines) {
-            item.doWork();
-            item.stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
-
     }
 }
