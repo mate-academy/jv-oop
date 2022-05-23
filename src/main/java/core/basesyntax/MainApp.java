1e88ef55d4c25f09a07f20b6b57b8f659fba16e8
Roman Pylypchuk <roman.pylypchuk@yahoo.com>
2022-05-23 16:36:22 +0300

    changed for each (loop)

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,23 +4,19 @@ public class MainApp {
     public static void main(String[] args) {
         Machine Truck = new Truck();
         Truck.setName("Truck");
-        Truck.doWork();
-        Truck.stopWork();
 
         Machine Bulldozer = new Bulldozer();
         Bulldozer.setName("Bulldozer");
-        Bulldozer.doWork();
-        Bulldozer.stopWork();
 
         Machine Excavator = new Excavator();
         Excavator.setName("Excavator");
-        Excavator.doWork();
-        Excavator.stopWork();
 
-
-        Machine[] array = {Truck, Bulldozer, Excavator};
+        Machine[] array = new Machine[] {Truck, Bulldozer, Excavator};
         for (Machine machine : array) {
-            System.out.println(machine);
+            machine.doWork();
+        }
+        for (Machine machine : array) {
+            machine.stopWork();
         }
     }
 }
