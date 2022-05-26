0dd17e98d8a516360c75be7608c34c08fb1c756e
Sergii Uglovyi <s.uglovyi@gmail.com>
2022-05-26 16:03:03 +0300

    fix from maven-checkstyle

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
-        for (int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             machines[i].doWork();
             machines[i].stopWork();
         }
