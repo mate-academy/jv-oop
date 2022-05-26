dc007ae76295526b21cab3722fc03db8acd73e17
Sergii Uglovyi <s.uglovyi@gmail.com>
2022-05-26 15:35:00 +0300

    add solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+        for (int i = 0; i < 3; i++){
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
 }
