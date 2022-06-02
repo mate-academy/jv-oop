8a6186a2392d1fbcd6303c77ab49ae3f7775c452
Nastia Pyshak <nastapisak618@gmail.com>
2022-06-02 20:44:23 +0200

    Task done2

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
+
     public static void main(String[] args) {
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
