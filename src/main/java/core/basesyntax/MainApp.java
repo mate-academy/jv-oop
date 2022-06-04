c91020018cdf255bae11a0d62af7696e637c43fd
Vasyl Piznak <piznak3145@gmail.com>
2022-06-04 20:14:15 +0300

    The first time I reset the test.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine Bulldozer = new Bulldozer();
+        Machine Truck = new Truck();
+        Machine Excavator = new Excavator();
+    Machine [] cor = new Machine[] {Bulldozer,Truck,Excavator};
+       for(Machine machine: cor){
+           machine.doWork();
+           machine.stopWork();
+       }
+    }
 }
