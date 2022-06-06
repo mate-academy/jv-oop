3ca22ab180458a7100a70df1ec93b6cff3276628
Vasyl Piznak <piznak3145@gmail.com>
2022-06-06 12:52:14 +0300

    Check Style.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,13 +2,13 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine Bulldozer = new Bulldozer();
-        Machine Truck = new Truck();
-        Machine Excavator = new Excavator();
-    Machine [] cor = new Machine[] {Bulldozer,Truck,Excavator};
-       for(Machine machine: cor){
-           machine.doWork();
-           machine.stopWork();
-       }
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine [] cor = new Machine[] {bulldozer,truck,excavator};
+        for (Machine machine: cor) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
