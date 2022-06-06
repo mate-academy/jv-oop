95e8715bd33597fdec45caeb4c2bef4e7a4bdca6
Serhii Makarov <alsem70@gmail.com>
2022-06-06 19:23:02 +0300

    task oop basic

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -4,6 +4,7 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator started work");
     }
+
     public void stopWork() {
         System.out.println("Excavator stopped work");
     }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -4,6 +4,7 @@ public class Truck extends Machine {
     public void doWork() {
         System.out.println("Truck started work");
     }
+
     public void stopWork() {
         System.out.println("Truck stopped work");
     }
