0e01e5460a8ccd93de43dd7f6ec1ecf54205af9c
X3NON-135 <immort4ld3momo@gmail.com>
2022-06-08 15:36:49 +0300

    Realizing OOP paradigms

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,7 +4,7 @@ public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = new Machine[]{ new Truck(),
                                             new Bulldozer(),
-                                            new Excavator() };
+                                            new Excavator()};
 
         for (Machine machine : machines) {
             machine.doWork();
