6bee9307a6b7a00bf579de9eda3489270ddfcfb1
SergiiUstinchenko <s.ustinchenko@gmail.com>
2022-06-08 12:41:18 +0300

    First commit: was created classes Main, Machine Excavator, Bulldozer,Truck. In main class was created Machine array and implemented methodes  doWork, stopWork.

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        Excavator excavator = new Excavator();
+        Bulldozer bulldozer = new Bulldozer();
+        Truck truck = new Truck();
+        Machine [] machines = new Machine[]{excavator,bulldozer,truck};
+        for (Machine result: machines){
+            result.doWork();
+            result.stopWork();
+        }
+
+    }
+}
