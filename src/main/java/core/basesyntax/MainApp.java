92886292f87338cc1d977301df726dc312fcfc4f
Ivan Aldokhin <105324572+vanoald2601@users.noreply.github.com>
2022-06-09 12:45:28 +0300

    Added 4 Class: Machine,Truck, Bulldozer, Excavator. Create Machine array in Main. Change loop and I think correct misstake.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -11,7 +11,7 @@ public class MainApp {
         machines[0] = new Excavator();
         machines[1] = new Bulldozer();
         machines[2] = new Truck();
-        for (int i = 0; i < machines.length; i++) {
+        for (int i = 0; i <= machines.length-1; i++) {
             machines[i].doWork();
             machines[i].stopWork();
         }
