3b9ddc793df70003bd7549170ac82fb049da0910
SergiiUstinchenko <s.ustinchenko@gmail.com>
2022-06-09 11:58:51 +0300

    Second commit: added fixes errors in checkstyle

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,11 +1,9 @@
 package core.basesyntax;
-
-public class Bulldozer extends Machine{
+public class Bulldozer extends Machine {
     @Override
     public void doWork() {
         System.out.println("Bulldozer machine started its work");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Bulldozer machine stopped its work");
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,15 +1,13 @@
 package core.basesyntax;
-
 public class Main {
     public static void main(String[] args) {
         Excavator excavator = new Excavator();
         Bulldozer bulldozer = new Bulldozer();
         Truck truck = new Truck();
         Machine [] machines = new Machine[]{excavator,bulldozer,truck};
-        for (Machine result: machines){
+        for (Machine result: machines) {
             result.doWork();
             result.stopWork();
         }
-
     }
 }
