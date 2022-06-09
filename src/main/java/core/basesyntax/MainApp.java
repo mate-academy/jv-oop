469eeb686305be8c0e7c3321dce9c6cc09a61790
Ivan Aldokhin <105324572+vanoald2601@users.noreply.github.com>
2022-06-09 03:24:00 +0300

    Added 4 Class: Machine,Truck, Bulldozer, Excavator. Create Machine array in Main.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,9 +1,19 @@
 package core.basesyntax;
 
+import core.basesyntax.machine.Bulldozer;
+import core.basesyntax.machine.Excavator;
+import core.basesyntax.machine.Truck;
+
 public class MainApp {
+
     public static void main(String[] args) {
-        System.out.println("Hey");
-
+        Machine[] machines = new Machine[3];
+        machines[0] = new Excavator();
+        machines[1] = new Bulldozer();
+        machines[2] = new Truck();
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
     }
-
 }
