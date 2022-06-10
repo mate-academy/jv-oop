76d421f364d2ca35c10ca085a0c301ecd2eecd49
Ivan Aldokhin <105324572+vanoald2601@users.noreply.github.com>
2022-06-10 13:27:43 +0300

    Change Loop codition. Delete empty lines. Added 4 Class: Machine,Truck, Bulldozer, Excavator. Create Machine array in Main. Change loop and I think correct misstake. Add whitespace.and delete whitespace.

--- src/main/java/core/basesyntax/machine/Bulldozer.java
+++ src/main/java/core/basesyntax/machine/Bulldozer.java
@@ -6,12 +6,10 @@ public class Bulldozer extends Machine {
     @Override
     public void doWork() {
         System.out.println("Bulldozer machine started its work");
-
     }
 
     @Override
     public void stopWork() {
         System.out.println("Bulldozer machine stopped its work");
-
     }
 }
--- src/main/java/core/basesyntax/machine/Truck.java
+++ src/main/java/core/basesyntax/machine/Truck.java
@@ -6,12 +6,10 @@ public class Truck extends Machine {
     @Override
     public void doWork() {
         System.out.println("Truck machine started its work");
-
     }
 
     @Override
     public void stopWork() {
         System.out.println("Truck machine stopped its work");
-
     }
 }
