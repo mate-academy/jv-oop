fc4f788ed20229318e55e10597545db7ce4d2305
Mykola Kutsyi <kytsij@gmail.com>
2022-06-10 17:39:22 +0300

    I fix text stile

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -6,7 +6,7 @@ public class MainApp {
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
         Machine[] machines = new Machine[] {truck, bulldozer, excavator};
-        for(Machine machine : machines) {
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
             System.out.println("\n");
