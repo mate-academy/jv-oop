e07b7796223d7273a2c5732090b9c1bee906e254
Serhii Nazarenko <nasarenko82@gmail.com>
2022-06-16 09:13:58 +0300

    task completed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,25 @@
 package core.basesyntax;
 
-public class MainApp {
-
+public class  MainApp {
+    public static void main(String[] args){
+    Machine excavator = new Excavator();
+    excavator.nameMachine = "Excavator";
+    Machine bulldozer = new Bulldozer();
+    bulldozer.nameMachine = "Bulldozer";
+    Machine truck = new Truck();
+    truck.nameMachine = "Truck";
+    String[] Machine = new String[]{"Truck", "Bulldozer", "Excavator"};
+        for (String machineNow : Machine) {
+            if(machineNow.equals(truck.nameMachine)){
+                truck.doWork();
+                truck.stopWork();
+            } else if(machineNow.equals(bulldozer.nameMachine)) {
+                bulldozer.doWork();
+                bulldozer.stopWork();
+            }else if(machineNow.equals(excavator.nameMachine)){
+                excavator.doWork();
+                excavator.stopWork();
+            }
+        }
+}
 }
