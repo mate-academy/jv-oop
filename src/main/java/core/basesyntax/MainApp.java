02dcfb092ad115d40c68a3e2e5db96d42b675469
Vitaliy Ozhelivskiy <ozelivskijvitalij@gmail.com>
2022-06-19 20:49:03 +0300

    Create classes and add resolve

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,13 @@
 package core.basesyntax;
-
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[3];
+        machines[0] = new Truck();
+        machines[1] = new Bulldozer();
+        machines[2] = new Excavator();
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
 }
