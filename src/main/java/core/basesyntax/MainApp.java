4e95ed116db9ea59b7214ecb152925d294ff86d6
Vitaliy Ozhelivskiy <ozelivskijvitalij@gmail.com>
2022-06-19 21:15:34 +0300

    *added solution Refactor loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,9 +5,9 @@ public class MainApp {
         machines[0] = new Truck();
         machines[1] = new Bulldozer();
         machines[2] = new Excavator();
-        for (int i = 0; i < machines.length; i++) {
-            machines[i].doWork();
-            machines[i].stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
