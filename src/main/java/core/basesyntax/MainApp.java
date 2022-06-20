eb741becf4820eda8db3ccb0c8f834f323f6de2c
Oleksandr <dovy_al@ukr.net>
2022-06-20 09:03:36 +0300

    fix maven error

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
 
-import java.util.EmptyStackException;
-
 public class MainApp {
     public static void main(String[] args) {
         Machine excavator = new Excavator();
