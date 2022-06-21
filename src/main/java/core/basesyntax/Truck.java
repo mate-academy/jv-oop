0e5efa7233332792b8ac266a3c1686eba700e0f7
Vlad Deneha <vladcst26@gmail.com>
2022-06-21 20:57:10 +0300

    Created classes and array with objects of these classes. Called methods in Main App

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Truck extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("Certain machine started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Certain machine stopped its work");
+    }
+}
