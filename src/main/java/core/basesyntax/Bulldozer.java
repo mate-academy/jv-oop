7cac5c01d60a4819908d7031d704b50fa9f3438d
Vlad Deneha <vladcst26@gmail.com>
2022-06-21 20:56:57 +0300

    Created classes and array with objects of these classes. Called methods in Main App

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("Certain machine started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Certain machine stopped its work");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
 public class MainApp {
+    Truck truck = new Truck();
+    Bulldozer bulldozer = new Bulldozer();
+    Excavator excavator = new Excavator();
 
+    Machine[] machines = new Machine[] {truck, bulldozer, excavator};
+
+    public void main(Machine[] machines) {
+        truck.doWork();
+        truck.stopWork();
+
+        bulldozer.doWork();
+        bulldozer.stopWork();
+
+        excavator.doWork();
+        excavator.stopWork();
+    }
 }
+
