04ef4f002392ef292bb77a5a77ee5b845405dfb4
Andrii Kovalskyi <kovalskyi.andrii90@gmail.com>
2022-06-21 17:07:25 +0200

    class Machine was created with abstracts methods; classes Truck, Bulldozer, Excavator was created; overriding metods doWork and stopWork; calling overrided methods in Main class with loop;

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machine = new Machine[]{truck, bulldozer, excavator};
+        for(Machine transport : machine) {
+            transport.doWork();
+            transport.stopWork();
+        }
+
+    }
 
 }
