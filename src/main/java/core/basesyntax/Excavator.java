8332e9df7b1a89c739ac3bd8f18820aaee31adad
Andrii Kovalskyi <kovalskyi.andrii90@gmail.com>
2022-06-22 15:47:26 +0200

    fixes added after review v1-2

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -5,6 +5,7 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator is working...");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Excavator is stops...");
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -5,6 +5,7 @@ public class Truck extends Machine {
     public void doWork() {
         System.out.println("Truck is working...");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Truck is stops...");
