53eeed7e217188d53d8492cfd53860fbbd6cf2ae
Serhii Nazarenko <nasarenko82@gmail.com>
2022-06-22 15:55:28 +0300

    corrected mistakes

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,17 +1,8 @@
 package core.basesyntax;
 
 public abstract class Machine {
-    private String nameMachine;
-
     public abstract void doWork();
 
     public abstract void stopWork();
 
-    public String getNemeMachine() {
-        return nameMachine;
-    }
-
-    public void setNameMachine(String nameMachine) {
-        this.nameMachine = nameMachine;
-    }
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,24 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine excavator = new Excavator();
-        excavator.setNameMachine("Excavator");
-        Machine bulldozer = new Bulldozer();
-        bulldozer.setNameMachine("Bulldozer");
-        Machine truck = new Truck();
-        truck.setNameMachine("Truck");
-        String[] machine = new String[] {"Truck", "Bulldozer", "Excavator"};
-        for (String machineNow : machine) {
-            if (machineNow.equals(truck.getNemeMachine())) {
-                truck.doWork();
-                truck.stopWork();
-            } else if (machineNow.equals(bulldozer.getNemeMachine())) {
-                bulldozer.doWork();
-                bulldozer.stopWork();
-            } else if (machineNow.equals(excavator.getNemeMachine())) {
-                excavator.doWork();
-                excavator.stopWork();
-            }
+        Machine[] machine = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machineNow : machine) {
+            machineNow.doWork();
+            machineNow.stopWork();
         }
     }
 }
