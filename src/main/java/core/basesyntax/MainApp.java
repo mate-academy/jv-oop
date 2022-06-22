3dc98a9cc033f33bd640a973cc7a269396af2b99
Andrii First <andre_first@ukr.net>
2022-06-22 21:30:05 +0100

    First homework using Git. My solution. Created  abstract class Machine and 3 classes, which extend's it. As per task in main  were created array.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machine1 = new Machine[3];
+        Machine truck = new Truck();
+        machine1[0] = truck;
+        Machine bulldozer = new Bulldozer();
+        machine1[1] = bulldozer;
+        Machine excavator = new Excavator();
+        machine1[2] = excavator;
+
+        for (int i = 0; i < 3; i++) {
+            machine1[i].doWork();
+        }
+        for (int i = 0; i < 3; i++) {
+            machine1[i].stopWork();
+
+        }
+    }
+
 
 }
