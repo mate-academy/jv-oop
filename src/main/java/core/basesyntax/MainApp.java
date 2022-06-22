16a9a27592959be76d2b531f4e062227e2befe75
Azarov Artur <azarovartyr@gmail.com>
2022-06-23 00:59:03 +0300

    task solution is done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine [] array = {new Truck(),new Bulldozer(), new Excavator()};
+        for (Machine m : array) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
 }
