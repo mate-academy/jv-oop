ca741b636c0e350d65bda44913fd96a844bbc1df
Andrii Zakhaarchenko <C:\Progs\BatBase>
2022-06-26 13:32:40 +0300

    jv-oop-hw1-1st-test

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,5 @@
+package core.basesyntax;
+public abstract class Machine {
+    public abstract void doWork();      //abstract action start work
+    public abstract void stopWork();    //abstract action stop work
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine buildozer = new Bulldozer();    //Create object Bulldozer
+        Machine truck = new Truck();            //Create object Truck
+        Machine excavator = new Excavator();    //Create object Excavator
+        Machine[] machines = new Machine[] {buildozer, truck, excavator};   //Create Array
+        for (Machine eachMachine: machines) { // loop for each item in the array
+            eachMachine.doWork();             // action - doWork for each item in the array
+            eachMachine.stopWork();           //action 0 stopWork for each item in the array
+        }
+    }
 }
