b416352c0e16fa1a4e84c886a0417b48f7f7886e
Andrii Zakhaarchenko <C:\Progs\BatBase>
2022-06-26 13:38:04 +0300

    jv-oop-hw1-1st-done

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
+
         System.out.println("Excavator start work...");
     }
     @Override
     public void stopWork() {
+
         System.out.println("Excavator stop work");
     }
 }
