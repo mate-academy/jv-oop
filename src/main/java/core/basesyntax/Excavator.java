bb3df7918def87dc57f6a6de93aadd8a48285f1d
Vasyl Moshun <vasylmoshun@gmail.com>
2022-06-29 15:39:10 +0300

    Update Excavator.java

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -5,7 +5,6 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator start work");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Excavator stop work");
