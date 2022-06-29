08b7da63ba4cded982c6b3b6d85d726c8e9b966a
Vasyl Moshun <vasylmoshun@gmail.com>
2022-06-29 15:43:04 +0300

    Update Bulldozer.java

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -5,6 +5,7 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Bulldozer start work");
     }
+    
     @Override
     public void stopWork() {
         System.out.println("Bulldozer stop work");
