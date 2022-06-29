2454fab005f3c3d7920cc2f329aa66fae0569590
Vasyl Moshun <vasylmoshun@gmail.com>
2022-06-29 15:43:26 +0300

    Update Truck.java

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -5,6 +5,7 @@ public class Truck extends Machine {
     public void doWork() {
         System.out.println("Truck start work");
     }
+    
     @Override
     public void stopWork() {
         System.out.println("Truck stop work");
