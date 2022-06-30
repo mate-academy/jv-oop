80f98ce9242c79f7a2acae98ffa8afd2824c51da
Yura Koval <koval.yuriy.v@gmail.com>
2022-06-30 16:53:04 +0300

    correct errors of checkstyle

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
-       System.out.println("Excavator starts its work");
+        System.out.println("Excavator starts its work");
     }
+
     @Override
     public void stopWork() {
-       System.out.println("Excavator stopped its work");
+        System.out.println("Excavator stopped its work");
     }
 }
