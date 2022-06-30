35739fbfc8edde64941b3364c47b8f0005ff6dcf
Vlad Deneha <vladcst26@gmail.com>
2022-06-30 19:52:14 +0300

    now for sure deleted all empty lines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,13 +5,10 @@ public class MainApp {
         Truck truck = new Truck();
         Bulldozer bulldozer = new Bulldozer();
         Excavator excavator = new Excavator();
-
         Machine[] machines = new Machine[] {truck, bulldozer, excavator};
-
         for (Machine machine : machines) {
             machine.doWork();
         }
-
         for (Machine machine : machines) {
             machine.stopWork();
         }
