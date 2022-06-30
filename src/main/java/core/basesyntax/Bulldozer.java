44ef0a87eb54f9714a02807ed30e6a7463d6cf1b
Alina Nikiforova <0500302717a@gmail.com>
2022-06-30 15:32:07 +0200

    added fixes after review

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Bulldozer extends Machine {
     @Override
     public void doWork() {
-        System.out.println("core.basesyntax.Bulldozer started work");
+        System.out.println("Bulldozer started work");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("core.basesyntax.Bulldozer stopped work");
+        System.out.println("Bulldozer stopped work");
     }
 }
