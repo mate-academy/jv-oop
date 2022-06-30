748ea86e8fd306d35583adec3327161d9e0eb99f
Yura Koval <koval.yuriy.v@gmail.com>
2022-06-30 15:33:27 +0300

    Added psvm method. Created three objects a Machine class. Created array with these objects. Calls methods doWork() and stopWork() in a loop.

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -2,10 +2,12 @@ package core.basesyntax;
 
 public class Bulldozer extends Machine {
     @Override
-    public void doWork() { System.out.println("Bulldozer starts its work"); }
+    public void doWork() {
+       System.out.println("Bulldozer starts its work");
+    }
 
     @Override
-       public void stopWork() {
-     System.out.println("Bulldozer stopped its work");
+    public void stopWork() {
+       System.out.println("Bulldozer stopped its work");
     }
 }
