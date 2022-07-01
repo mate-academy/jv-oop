ce6f6464ccf2947d8f0e947cc4a2ca0edac6b030
Andrii Pylypenko <pilipenko1975@gmail.com>
2022-07-01 22:12:44 +0300

    Abstract reference is used instead of concrete and deleted empty lines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
-
 public class MainApp {
-
     public static void main(String[] args) {
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
