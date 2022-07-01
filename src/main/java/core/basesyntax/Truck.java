dda8d0d37eee949f1e7282f1bad84e3a576a5206
Andrii Pylypenko <pilipenko1975@gmail.com>
2022-07-01 21:47:12 +0300

    Abstract reference is used instead of concrete

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Truck extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Truck started working");
+        System.out.println("Truck started its work");
     }
 
     @Override
