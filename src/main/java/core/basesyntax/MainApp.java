6eeb1eca4c04fb54dba37171dbd91021ac2945c6
Andrii Pylypenko <pilipenko1975@gmail.com>
2022-07-01 18:15:32 +0300

    Task completed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Excavator excavator = new Excavator();
+        Bulldozer bulldozer = new Bulldozer();
+        Truck truck = new Truck();
+        Machine [] machines = new Machine[] {excavator, bulldozer, truck};
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
