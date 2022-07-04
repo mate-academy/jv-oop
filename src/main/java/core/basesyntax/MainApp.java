890100d9b6da7310feaa655ae8afa087381c6f01
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2022-07-04 20:53:59 +0300

    fixed common mistakes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,11 +1,7 @@
 package core.basesyntax;
-
-import java.util.Arrays;
-
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machine = {new Truck(), new Excavator(), new Bulldozer()};
-
         for (int i = 0; i < machine.length; i++) {
             machine[i].doWork();
             machine[i].stopWork();
