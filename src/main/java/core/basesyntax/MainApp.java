d1d0db8cc0e5c1c177df9647662f79cd18e19ede
Dmytro Vyshnivskyi <108304958+dmytrovb02@users.noreply.github.com>
2022-07-04 23:45:47 +0300

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
-
 public class MainApp {
-
     public static void main(String[] args) {
         Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
         for (Machine machine : machines) {
