9f3f6ca0928dc78cca06ba30502235324539f398
Oleksandr Gotin <alekander.gotin@gmail.com>
2022-07-05 17:15:56 +0300

    revised code

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("The bulldozer dug a trench. Prepare the truck.");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The soil is submerged. Break.");
+    }
+}
