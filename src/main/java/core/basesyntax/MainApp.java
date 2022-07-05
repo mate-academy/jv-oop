dcd845eaf1b5ce63dae0f4c9f7ba97996af99d6f
Dmytro Stakhiv <dstahiv@gmail.com>
2022-07-05 22:41:27 +0300

    Solution jv-oop Machine and MainApp changes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,15 +3,15 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine excavator = new Excavator();
-
         Machine truck = new Truck();
-
         Machine bulldozer = new Bulldozer();
 
         Machine[] machines = new Machine[] {excavator, truck, bulldozer};
-        for (Machine machin : machines) {
-            machin.doWork();
-            machin.stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+        for (Machine machine : machines) {
+            machine.stopWork();
         }
     }
 }
