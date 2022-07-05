058a04c298a481def9fad74d1f9a2d2e1ddd5387
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2022-07-05 08:33:30 +0300

    renamed array of Machine[]

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machine = {new Truck(), new Excavator(), new Bulldozer()};
-        for (int i = 0; i < machine.length; i++) {
-            machine[i].doWork();
-            machine[i].stopWork();
+        Machine[] machines = {new Truck(), new Excavator(), new Bulldozer()};
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
         }
     }
 }
