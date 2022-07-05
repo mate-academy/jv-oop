36e259ca1ea8f8d6cbcbea3e7a9e6aea46ee91f5
Maksym Bakunov <mpbakunov.it@gmail.com>
2022-07-05 20:53:43 +0200

    version #1 (for check)

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine [] machines = {new Excavator(), new Truck(), new Bulldozer()};
+        for (Machine m : machines) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
 
 }
