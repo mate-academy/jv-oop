74c8fd9726c080a0a7c72c6109570e01a5809ae2
Valerii <kulibchenkovalera@gmail.com>
2022-07-05 11:37:14 +0300

    completed task

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -5,7 +5,6 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Bulldozer started work");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Bulldozer stopped work");
