cf0b589f078ca90b3e1224344f95b1b51f3922e1
golov <holovkodv12@gmail.com>
2022-07-12 23:52:56 +0300

    created an abstract Machine class with two abstract methods. created three new required classes and overridden abstract methods. In a MainApp class added the psvm method, generated a new object for each classes, in the end, made an array of new objects and ran it

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
+import java.util.Arrays;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine $truck = new Truck();
+        Machine $excavator = new Excavator();
+        Machine $bulldozer = new Bulldozer();
+
+        Machine[] does = {$truck, $excavator, $bulldozer};
+        for(int i = 0; i < 3; i++){
+            does[i].doWork();
+            does[i].stopWork();
+        }
+        System.out.println(Arrays.toString(does));
+
+    }
 
 }
