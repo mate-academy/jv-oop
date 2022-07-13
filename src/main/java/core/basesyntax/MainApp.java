56998a584e948085a6bca2f732502b2fce7b2e63
Alexander <liero2D@gmail.com>
2022-07-14 00:10:01 +0300

    added classes /methods/loop. Please check task. Thanks.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
 
+        Machine[] industrialTransport = {truck, bulldozer, excavator};
+        for (Machine heavyMachine : industrialTransport) {
+            heavyMachine.doWork();
+            heavyMachine.stopWork();
+        }
+    }
 }
