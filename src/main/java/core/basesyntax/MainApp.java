3fe44023a656b0d67e2bc7bb5ae0f99403a3c72f
Igor Petrichenko <i.petrichenko@gmail.com>
2022-07-15 15:12:05 +0300

    Create class Machine containing methods public abstract void doWork() and public abstract void stopWork(). Create classes Truck, Bulldozer, Excavator that will inherit from Machine. In Main class create Machine array with Truck, Bulldozer and Excavator and call methods doWork() and stopWork() in a loop. Print message when machine start and stop working.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
+  public static void main(String[] args) {
+    Machine[] workMachine = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
+    for(Machine machine : workMachine) {
+      machine.doWork();
+      machine.stopWork();
+    }
+  }
 
 }
