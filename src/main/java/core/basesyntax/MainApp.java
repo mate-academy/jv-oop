4ddc4b290bcfe7a1d586f84380341ff8b4671dcb
Igor Petrichenko <i.petrichenko@gmail.com>
2022-07-15 18:58:28 +0300

    correct whitespace

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,17 +1,19 @@
 package core.basesyntax;
 
 public class MainApp {
-  public static void main(String[] args) {
-    Machine truck = new Truck();
-    Machine bulldozer = new Bulldozer();
-    Machine excavator = new Excavator();
-    Machine[] workMachine = new Machine[] {truck, bulldozer, excavator};
-    for(Machine machine : workMachine) {
-      machine.doWork();
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] workMachine = new Machine[] {truck, bulldozer, excavator};
+
+        for (Machine machine : workMachine) {
+            machine.doWork();
+        }
+
+        for (Machine machine : workMachine) {
+            machine.stopWork();
+        }
     }
-    for(Machine machine : workMachine) {
-      machine.stopWork();
-    }
-  }
 
 }
