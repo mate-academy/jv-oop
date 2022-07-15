174ec65d02e43a5633e8b2069312e39a86d98c09
Dmytro Holovko <103456963+dima-holovko@users.noreply.github.com>
2022-07-15 11:48:55 +0300

    Update src/main/java/core/basesyntax/MainApp.java
    Co-authored-by: Dasha <maliuk.dasha@gmail.com>

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -8,7 +8,7 @@ public class MainApp {
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
 
-        Machine[] does = {truck, excavator, bulldozer};
+        Machine[] machines = {truck, excavator, bulldozer};
         for (int i = 0; i < 3; i++) {
             does[i].doWork();
             does[i].stopWork();
