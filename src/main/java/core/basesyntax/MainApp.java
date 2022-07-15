ff2a4aa14cc978a24ac310c77467a1a7bef705ed
Igor Petrichenko <i.petrichenko@gmail.com>
2022-07-15 15:17:56 +0300

    In `Main` class created an object of each class. Start work of all machines and then stop them.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,9 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
   public static void main(String[] args) {
-    Machine[] workMachine = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
+    Machine truck = new Truck();
+    Machine bulldozer = new Bulldozer();
+    Machine excavator = new Excavator();
+    Machine[] workMachine = new Machine[] {truck, bulldozer, excavator};
     for(Machine machine : workMachine) {
       machine.doWork();
+    }
+    for(Machine machine : workMachine) {
       machine.stopWork();
     }
   }
