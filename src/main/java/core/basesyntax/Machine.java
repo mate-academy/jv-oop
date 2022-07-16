fefcbe717b93f5ce7487994a26ea8c4a1c0ffc13
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-16 19:15:35 +0200

    Machine2

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,5 +1,5 @@
 package main.java.core.basesyntax;
 public abstract class Machine {
-    public abstract void doWork ();
-    public abstract void stopWork ();
+    public abstract void doWork();
+    public abstract void stopWork();
 }
