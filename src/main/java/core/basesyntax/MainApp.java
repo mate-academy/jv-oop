af56763733a78bf0d901069f7686cb8936b2d921
Anatolii Bespalko <tolyan9901@gmail.com>
2022-07-16 21:58:03 +0300

    Does it work?

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+       Excavator excavator = new Excavator();
+       Bulldozer bulldozer = new Bulldozer();
+       Truck truck = new Truck();
+        Machine [] machines = new Machine[]{excavator,bulldozer,truck};
+        for(Machine itWork: machines){
+            itWork.stopWork();
+            itWork.doWork();
+        }
+    }
 }
