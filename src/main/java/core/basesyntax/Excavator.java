b62f0afeb68b8a779f68fab757fb4b217f78676c
Yaroslav Vashchenko <yaroslav.vashchenko@gmail.com>
2022-07-17 15:33:43 +0300

    added 3 machine classes and realised abstract

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+    @Override
+    public String doWork() {
+        return "Excavator started his work";
+    }
+    @Override
+    public String stopWork() {
+        return "Excavator stopped his work";
+    }
+}
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    @Override
+    public String doWork() {
+        return "Truck started his work";
+    }
+    @Override
+    public String stopWork() {
+        return "Truck stopped his work";
+    }
+}
