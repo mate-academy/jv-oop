7eea42b26971e9bed4740eb4b653781bc43e3174
Igor Petrichenko <i.petrichenko@gmail.com>
2022-07-19 14:47:08 +0300

    changed name array machines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,11 +5,11 @@ public class MainApp {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-        Machine[] workMachine = new Machine[] {truck, bulldozer, excavator};
-        for (Machine machine : workMachine) {
+        Machine[] machines = new Machine[] {truck, bulldozer, excavator};
+        for (Machine machine : machines) {
             machine.doWork();
         }
-        for (Machine machine : workMachine) {
+        for (Machine machine : machines) {
             machine.stopWork();
         }
     }
