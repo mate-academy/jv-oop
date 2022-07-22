0abca3a69f073970fa4d0c8886daf92bb600651f
Andrii Shevchuk <zovten156@gmail.com>
2022-07-22 16:27:38 +0300

    I made a new Classes, with methods. In a  MainApp I made a arrey, and call methods. I think I did something wrong, maybe i didn't understand a condition.

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -2,11 +2,11 @@ package core.basesyntax;
 
 public class Truck extends Machine {
     @Override
-    public void doWork() {
-        System.out.println("The truck started its work");
+  public void doWork() {
+    System.out.println("The truck started its work");
     }
     @Override
     public void stopWork() {
-        System.out.println("The truck stoped its work");
+    System.out.println("The truck stoped its work");
     }
 }
