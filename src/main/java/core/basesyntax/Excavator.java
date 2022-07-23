3c1096c207f1e7c745528e3fe216626ec2cc063b
Andrii Shevchuk <zovten156@gmail.com>
2022-07-23 10:33:18 +0300

    How i can check checkstyle in a IDEA?  I made a new Classes, with methods. In a  MainApp I made a arrey, and call methods. I think I did something wrong, maybe i didn't understand a condition.

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -2,11 +2,12 @@ package core.basesyntax;
 
 public class Excavator extends Machine {
     @Override
-  public void doWork() {
-    System.out.println("The excavator started its work");
+    public void doWork() {
+        System.out.println("The excavator started its work");
     }
+
     @Override
-  public void stopWork() {
-    System.out.println("The excavator stoped its work!");
+    public void stopWork() {
+        System.out.println("The excavator stoped its work!");
     }
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,14 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-  public static void main(String[] arg) {
-    Machine excavator = new Excavator();
-    Machine bulldozer = new Bulldozer();
-    Machine truck = new Truck();
-    Machine[] machines = {excavator, bulldozer, truck};
-      for (Machine machine : machines) {
-        machine.doWork();
-        machine.stopWork();
-      }
-  }
+    public static void main(String[] arg) {
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        Machine[] machines = {excavator, bulldozer, truck};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
