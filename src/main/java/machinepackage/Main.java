1ad34037200ed841ffe76ea889c311cd24b394d3
Oksana Chaban <97809697+OksanaC@users.noreply.github.com>
2022-07-24 15:21:07 +0200

    new solution

--- src/main/java/machinepackage/Main.java
+++ src/main/java/machinepackage/Main.java
@@ -2,14 +2,10 @@ package machinepackage;
 
 public class Main {
     public static void main(String[] args) {
-        Machine truck = new Truck();
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
-        truck.doWork();
-        truck.stopWork();
-        bulldozer.doWork();
-        bulldozer.stopWork();
-        excavator.doWork();
-        excavator.stopWork();
-    }
+        Machine[] machine = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machines : machine) {
+            machines.doWork();
+            machines.stopWork();
+        }
 }
+    }
--- src/main/java/machinepackage/Truck.java
+++ src/main/java/machinepackage/Truck.java
@@ -1,9 +1,11 @@
 package machinepackage;
 
 public class Truck extends Machine{
+    @Override
     public void doWork(){
         System.out.println("Truck started its work");
     }
+    @Override
     public void stopWork(){
         System.out.println("Truck stopped its work");
     }
