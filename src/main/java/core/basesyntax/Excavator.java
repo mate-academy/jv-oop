2e453c1fe257bb47a2cc8fec721a7ae91c1293cc
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-07-25 16:39:13 +0300

    empty lines were deleted, new name of variable - machine for MainApp

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public class Excavator extends Machine {
-
     @Override
     public void doWork() {
         System.out.println("Excavator started its work");
@@ -11,5 +10,4 @@ public class Excavator extends Machine {
     public void stopWork() {
         System.out.println("Excavator stopped its work");
     }
-
 }
