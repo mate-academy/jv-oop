de9fac330821a5d734b95ff1f3e0d8c4f15f03b9
Sofiia <sofiakazzz@gmail.com>
2022-07-25 13:30:26 +0300

    Add public abstract void doWork() and public abstract void stopWork().

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,7 +5,6 @@ public class MainApp {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-
         Machine[] machines = {truck, bulldozer, excavator };
         for (Machine machine : machines) {
             machine.doWork();
