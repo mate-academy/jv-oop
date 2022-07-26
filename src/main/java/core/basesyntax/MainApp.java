0180b9233676dff5a7cde37530c60150b8caed6f
Veers <raadch3011@gmail.com>
2022-07-26 15:33:33 +0300

    Added source files to classes
    created classe Machine(abstract), Truck, Buldoser and Excavator(inherited from Machine), override methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,41 +1,6 @@
 package core.basesyntax;
 
-abstract class Machine {
-    public abstract void doWork();
-    public abstract void stopWork();
-}
-class Truck extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println("Truck is working");
-    }
-    @Override
-    public void stopWork() {
-        System.out.println("Truck stopped working");
-    }
-}
 
-class Bulldozer extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println("Bulldozer is working");
-    }
-    @Override
-    public void stopWork() {
-        System.out.println("Bulldozer stopped working");
-    }
-}
-
-class Excavator extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println("Excavator is working");
-    }
-    @Override
-    public void stopWork() {
-        System.out.println("Excavator stopped working");
-    }
-}
 
 public class MainApp {
     public static void main(String[] args){
@@ -46,7 +11,9 @@ public class MainApp {
         };
 
         for (Machine machine : machines) {
+
             machine.doWork();
+
             machine.stopWork();
         }
     }
