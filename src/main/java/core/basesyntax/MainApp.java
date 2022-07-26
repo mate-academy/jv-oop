589bbf3fa0abcb5314627236768ca2e454728288
Volodymyr Kachan <vladimirkachan1996@gmail.com>
2022-07-26 22:29:13 +0300

    My hw jv-oop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
+import Machines.Bulldozer;
+import Machines.Excavator;
+import Machines.Machine;
+import Machines.Truck;
+
 public class MainApp {
+
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        Machine[] machine = {truck, bulldozer, excavator};
+        for (int i = 0; i < machine.length; i++){
+            machine[i].doWork();
+            machine[i].stopWork();
+        }
+    }
 
 }
