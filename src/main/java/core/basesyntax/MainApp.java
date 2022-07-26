4ab54d56876523fdcd5bdef94222aec9c3f8d7e2
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-07-26 19:17:10 +0200

    fix mvn

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-
         Machine[] machine = {new Truck(), new Bulldozer(), new Excavator()};
         for (Machine callAllMethods : machine) {
             callAllMethods.doWork();
