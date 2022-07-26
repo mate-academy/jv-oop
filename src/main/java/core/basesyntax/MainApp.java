39bbf0becbf6832607ed5d01b1cfbb2dd5141518
Nazar <litvik.12@gmail.com>
2022-07-26 08:47:19 +0300

    created several types of Machine

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        Machine[] machine = {excavator,bulldozer,truck};
+        for (Machine m:machine) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
 
 }
