66701911ba2f38a9ec7b1541f3d84e29979c1667
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-07-26 14:56:09 +0300

    create class Machine and his inheritances

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,13 +1,13 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
-  @Override
-  public void doWork() {
-    System.out.println("Truck started");
-  }
+    @Override
+    public void doWork() {
+        System.out.println("Truck started");
+    }
 
-  @Override
-  public void stopWork() {
-    System.out.println("Truck stopped");
-  }
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped");
+    }
 }
