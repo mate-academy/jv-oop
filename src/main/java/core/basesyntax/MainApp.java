d39a9db45c8be3ef4349b8d697d37bb181f5cfb2
Veers <raadch3011@gmail.com>
2022-07-26 15:13:35 +0300

    created classe Machine(abstract), Truck, Buldoser and Excavator(inherited from Machine), override methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,61 @@
 package core.basesyntax;
 
-public class MainApp {
+abstract class Machine {
+    public abstract void doWork();
+    public abstract void stopWork();
+}
 
+class Truck extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Truck is working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped working");
+    }
+}
+
+class Bulldozer extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer is working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer stopped working");
+    }
+}
+
+class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Excavator is working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator stopped working");
+    }
+}
+
+
+public class MainApp {
+    public static void main(String[] args){
+        Machine [] machines = new Machine[] {
+                new Bulldozer(),
+                new Truck(),
+                new Excavator()
+        };
+
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
