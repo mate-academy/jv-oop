c545d4b0b42d2e48df832c92c8b9851f7ddba111
Veers <raadch3011@gmail.com>
2022-07-26 15:35:18 +0300

    Added source files to classes
    created classe Machine(abstract), Truck, Buldoser and Excavator(inherited from Machine), override methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,9 +1,7 @@
 package core.basesyntax;
 
-
-
 public class MainApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Machine [] machines = new Machine[] {
                 new Bulldozer(),
                 new Truck(),
