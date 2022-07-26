8aa6b365894e0b308ac3defae12c4d4393593337
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-07-26 16:28:46 +0300

    The classes Machine and MainApp was finished

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+/**
+ * The class describes the technique with which we can work.
+ */
+public abstract class Machine {
+    public abstract void doWork();
+    public abstract void stopWork();
+}
