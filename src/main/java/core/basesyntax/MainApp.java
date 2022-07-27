70e32d24d30d42c7a99c6f17135cf703ed9f9022
Volodymyr Kachan <vladimirkachan1996@gmail.com>
2022-07-27 17:14:03 +0300

    Me home work java oop v03

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -11,10 +11,10 @@ public class MainApp {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-        Machine[] machine = {truck, bulldozer, excavator};
-        for (Machine machines : machine) {
-            machines.doWork();
-            machines.stopWork();
+        Machine[] machines = {truck, bulldozer, excavator};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 
