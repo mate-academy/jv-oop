44a0c4cfc703fa9fadad1775bf607a7c992c210d
fliarkovskyi <fliarkovskyi@gmail.com>
2022-07-27 18:26:56 +0300

    fix checkstyle

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,11 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine[] arrayMachine = new Machine[] {new Excavator(), new Bulldozer(), new Excavator()};
+        for (Machine counterMachine : arrayMachine) {
+            counterMachine.doWork();
+            counterMachine.stopWork();
+        }
+    }
 }
