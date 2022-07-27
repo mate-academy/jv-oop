30c280b8cd35fb1b250456ecf70268fea4fb5f53
Vasyl Pankiv <wasilius2@gmail.com>
2022-07-27 16:24:07 +0300

    fix foreach code-style

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,8 +4,7 @@ public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Bulldozer(), new Truck(), new Excavator()};
 
-        for (Machine machine :
-                machines) {
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
