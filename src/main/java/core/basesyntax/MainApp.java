0745af9826d8bf72a8cde50fc187e0209f5470e5
Vasyl Shevtsiv <vasyl.shevtsiv@gmail.com>
2022-07-27 11:59:06 +0300

    removing empty lines and changing varaible name

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = new Machine[] {new Truck(), new Excavator(),new Bulldozer()};
-        for (Machine m : machines) {
-            m.doWork();
-            m.stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
