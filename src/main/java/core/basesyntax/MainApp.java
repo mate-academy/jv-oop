f734ab87eac7afc737a33786c33b713fa49f4271
Aptypio13 <aptypio668@gmail.com>
2022-07-27 16:06:52 +0300

    Machine abstract class created

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine Excavator = new Excavator();
+        Machine Truck = new Truck();
+        Machine Bulldozer = new Bulldozer();
 
+        Machine[] ListOfMachines = new Machine[] {Excavator, Truck, Bulldozer};
+        for (Machine machine : ListOfMachines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Truck extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("The Truck started working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The Truck stopped working");
+    }
+
+}
