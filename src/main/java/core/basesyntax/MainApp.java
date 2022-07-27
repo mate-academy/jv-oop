163c2c742996e85eabf797bc3e4251f1fba14a70
mr-Claster <67782747+mr-Claster@users.noreply.github.com>
2022-07-27 21:46:43 +0300

    my homework done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
-    public void main() {
+    public static void main(String[] args) {
         Machine[] machines = new Machine[] {new Bulldozer(), new Excavator(), new Truck()};
         for (Machine machine: machines) {
             machine.doWork();
