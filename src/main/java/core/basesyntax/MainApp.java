dc7767deede06eebd6d9d4cf4a98857ace07ed02
LitvinOleksandr <sanek.litvin@gmail.com>
2022-07-27 16:15:43 +0300

    Called all classes and can look how its work. mvn clean package has not mistakes deleted empty line chenge call class machine to machines/

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machine = {new Bulldozer(),new Excavator(),new Truck()};
-        for (Machine machines: machine) {
-            machines.doWork();
-            machines.stopWork();
+        Machine[] machines = {new Bulldozer(),new Excavator(),new Truck()};
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
