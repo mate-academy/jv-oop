78cae63fe7ad71810da8bce54f4221087f4defaf
fliarkovskyi <fliarkovskyi@gmail.com>
2022-07-27 13:48:45 +0300

    my solution for realise Machine

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+  public static void main(String[] args) {
+    Machine[] arrayMachine = new Machine[] {new Excavator(), new Bulldozer(), new Excavator()};
+    for (Machine counterMachine : arrayMachine) {
+      counterMachine.doWork();
+      counterMachine.stopWork();
+    }
+  }
 }
