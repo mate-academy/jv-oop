a0d1bf9049b5f159e979b5dabc2f5c58b58fc087
Alexhqolma <66127994+Alexhqolma@users.noreply.github.com>
2022-07-27 15:56:00 +0300

    Create 4 classes and sout 3 messages about starting the work and 3 messages about ending the work by all 3 machines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine[] machineWork = new Machine[] {bulldozer, excavator, truck};
+        for (int i = 0; i < machineWork.length; i++) {
+            machineWork[i].doWork();
+            machineWork[i].stopWork();
+        }
+    }
 }
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    public void doWork() {
+        System.out.println("Truck started it's work");
+    }
+
+    public void stopWork() {
+        System.out.println("Truck stopped it's work");
+    }
+}
