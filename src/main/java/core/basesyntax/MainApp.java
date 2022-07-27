05aef51c3488956ed447a0ead493ec74df12504a
LitvinOleksandr <sanek.litvin@gmail.com>
2022-07-27 15:12:12 +0300

    Called all classes and can look how its work. mvn clean package has not mistakes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machine = {new Bulldozer(),new Excavator(),new Truck()};
+        for (Machine machines: machine) {
+            machines.doWork();
+            machines.stopWork();
+        }
+    }
 }
