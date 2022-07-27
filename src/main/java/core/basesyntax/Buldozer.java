d8f4b9d983717f2c0f6584f70d9fd6b6c2e87877
Sergei Matiko <adgodst@gmail.com>
2022-07-28 00:05:31 +0300

    Resolved task

---
+++ src/main/java/core/basesyntax/Buldozer.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Buldozer extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Buldozer started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Buldozer stopped its work");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,11 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine[] arrayOfMachine = new Machine[]{new Buldozer(), new Excavator(), new Truck()};
+        for (Machine machines : arrayOfMachine) {
+            machines.doWork();
+            machines.stopWork();
+        }
+    }
 }
