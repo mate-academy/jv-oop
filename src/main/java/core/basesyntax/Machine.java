ae18cc8fd66cda79e87bdab0da26bf123854f476
X3NON-135 <immort4ld3momo@gmail.com>
2022-07-27 09:19:35 +0100

    fixed and optimised code/codestyle

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public abstract class Machine {
-
     public abstract void stopWork();
 
     public abstract void doWork();
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
-
     @Override
     public void stopWork() {
         System.out.println("Truck has stopped a work");
