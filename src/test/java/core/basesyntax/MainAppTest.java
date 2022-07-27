b1256bfac3d95f1bd1f498c27c8016cb95f7f240
Thousandfold <you@example.com>
2022-07-27 11:25:40 +0300

    oop_basic_jv_core

--- src/test/java/core/basesyntax/MainAppTest.java
+++ src/test/java/core/basesyntax/MainAppTest.java
@@ -1,14 +1,5 @@
 package core.basesyntax;
 
 public class MainAppTest {
-    public static void main(String[] args) {
-        Machine truck = new Truck();
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
-        Machine[] machines = new Machine[] {truck, bulldozer, excavator};
-        for (int i = 0; i < machines.length; i++) {
-            machines[i].doWork();
-            machines[i].stopWork();
-        }
-    }
+
 }
