2083b739e4695052fd0cbbc12874bdff7e2ebce1
fliarkovskyi <fliarkovskyi@gmail.com>
2022-07-28 13:55:24 +0300

    fix name counterMachine for machine

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,9 +4,9 @@ public class MainApp {
 
     public static void main(String[] args) {
         Machine[] machines = new Machine[] {new Excavator(), new Bulldozer(), new Excavator()};
-        for (Machine counterMachine : machines) {
-            counterMachine.doWork();
-            counterMachine.stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
