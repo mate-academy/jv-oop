302562eb920c4098f41da186f095e79c371f0302
Yevhen Kupriiuk <kupriuk@gmail.com>
2022-07-28 13:41:22 +0300

    all empty limes are removed of all methods

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -4,12 +4,10 @@ public class Bulldozer extends Machine {
     @Override
     public void doWork() {
         System.out.println("Bulldozer started its work");
-
     }
 
     @Override
     public void stopWork() {
         System.out.println("Bulldozer stopped its work");
-
     }
 }
