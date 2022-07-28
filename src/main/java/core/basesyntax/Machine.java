8a05cb02fe0af5b104e4d57e110db986c9fb6bce
Sergei Matiko <adgodst@gmail.com>
2022-07-28 13:28:13 +0300

    edited

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -3,6 +3,6 @@ package core.basesyntax;
 public abstract class Machine {
 
     public abstract void doWork();
-    public abstract void stopWork();
 
+    public abstract void stopWork();
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
+
         Machine[] machines = new Machine[]{new Buldozer(), new Excavator(), new Truck()};
         for (Machine machine : machines) {
             machine.doWork();
