88319d7a704109eb87f9e348891f277f73475c6d
fliarkovskyi <fliarkovskyi@gmail.com>
2022-07-28 13:53:06 +0300

    fix name arrayMachine for machines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
-        Machine[] arrayMachine = new Machine[] {new Excavator(), new Bulldozer(), new Excavator()};
-        for (Machine counterMachine : arrayMachine) {
+        Machine[] machines = new Machine[] {new Excavator(), new Bulldozer(), new Excavator()};
+        for (Machine counterMachine : machines) {
             counterMachine.doWork();
             counterMachine.stopWork();
         }
