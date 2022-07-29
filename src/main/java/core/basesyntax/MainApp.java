8811aeed5b0a936a243e5e6ea7e00811f34a490b
victoriadobryden <vikidobryden@gmail.com>
2022-07-29 08:21:22 +0300

    added machines class hierarchy and methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    //In Main class create Machine array with Truck, Bulldozer and Excavator and call methods doWork() and stopWork() in a loop.
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[3];
+        machines[0] = new Truck();
+        machines[1] = new Bulldozer();
+        machines[2] = new Excavator();
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
+
 
 }
