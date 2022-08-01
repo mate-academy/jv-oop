ff0adeb065475ebac5050417a51e693ba4bd5818
mariatkachuk <mariia.tkachuk@icloud.com>
2022-08-01 17:40:38 +0300

    hw-1 jv-oop

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Truck worked");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stop");
+    }
+}
