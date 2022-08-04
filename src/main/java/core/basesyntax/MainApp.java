826b7eec1e3bac53c0e130684fb16c2b43fe2c3b
MAxim Sychinskiy <max.sychinskiy@gmail.com>
2022-08-04 21:56:14 +0300

    My first homework task on GitHub

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void mainApp (String[] args) {
 
+        Machine truck = new Truck() ;
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+
+        Machine [] machines = new Machine[] { truck, bulldozer, excavator};
+
+        for (Machine allMachines : machines){
+            allMachines.doWork();
+        }
+
+        for (Machine allMachines : machines){
+            allMachines.stopWork();
+        }
+    }
 }
