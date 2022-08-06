78fd1bf28e49b98817e1f27cb8260930b5e1a85e
Andrii Petruk <andrzej.petruk@bk.ru>
2022-08-06 22:02:55 +0200

    task solved

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -5,7 +5,6 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator started its work");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Excavator stopped its work");
