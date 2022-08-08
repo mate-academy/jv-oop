55834c06f03329632ca99afefe808866623ba375
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2022-08-08 13:01:09 +0200

    Added Solution. Create three objects, array, and two methods which call messages.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine [] machines = new Machine[]{truck, bulldozer, excavator};
+        printStartMessages(machines);
+        printStopMessages(machines);
+    }
+    public static void printStartMessages (Machine[] machines){
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
+    public static void printStopMessages (Machine[] machines){
+        for (Machine machine : machines) {
+            machine.stopWork();
+        }
+    }
 }
