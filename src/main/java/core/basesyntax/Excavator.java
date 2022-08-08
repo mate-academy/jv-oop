62ef461b5e889babe267670a70a7aa301a3feec7
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2022-08-08 12:58:55 +0200

    Added Excavator Class

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Excavator extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("The Excavator started work");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("The Excavator stopped work");
+    }
+}
