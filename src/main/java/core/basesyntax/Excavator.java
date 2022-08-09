ddd44e400562a6f906d9169021fb6eb4c906cf87
Gulbanu Bibicheva <bibicheva@gmail.com>
2022-08-09 12:13:45 +0200

    OOP task was solved

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+    public void doWork() {
+        System.out.println ("Excavator starts working");
+    }
+    public void stopWork() {
+        System.out.println ("Excavator stops working") ;
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine Truck = new Truck() ;
+        Machine Bulldozer = new Bulldozer() ;
+        Machine Excavator = new Excavator() ;
+        Machine [] cars = new Machine[] {Truck, Bulldozer, Excavator} ;
+        for (Machine car : cars) {
+            if (car != null) {
+                car.doWork();
+                car.stopWork();
+            }
+        }
+    }
 }
