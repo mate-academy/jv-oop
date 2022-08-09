efdf896a1a1b85e162f268ba31cb90ce00d1f83b
Gulbanu Bibicheva <bibicheva@gmail.com>
2022-08-09 12:13:45 +0200

    OOP task was solved

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,16 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine Truck = new Truck();
+        Machine Bulldozer = new Bulldozer();
+        Machine Excavator = new Excavator();
+        Machine [] cars = new Machine[] {Truck, Bulldozer, Excavator};
+        for (Machine car : cars) {
+            if (car != null) {
+                car.doWork();
+                car.stopWork();
+            }
+        }
+    }
 }
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+
+    @Override
+    public void doWork() {
+        boolean work = true;
+        System.out.println("Truck starts working");
+        }
+
+    @Override
+    public void stopWork() {
+        boolean stop = true;
+        System.out.println("Truck stops working");
+    }
+}
