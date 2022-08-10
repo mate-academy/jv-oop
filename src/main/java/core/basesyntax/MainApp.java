64343a0df3e14c0ed32a7236ae6a8c909ba7f010
mister.Sokor <102083412+GranulaGrace@users.noreply.github.com>
2022-08-09 23:51:50 -0500

    -was created new classes 'Truck, Machine, Buildoser, Excavator' -created two methods for new classes and implemented using Polymorphism

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
 
+        Machine [] machines = new Machine [] {truck, bulldozer, excavator };
+
+        for (Machine cars : machines) {
+            cars.doWork();
+        }
+        for (Machine cars : machines) {
+            cars.stopWork();
+        }
+    }
 }
