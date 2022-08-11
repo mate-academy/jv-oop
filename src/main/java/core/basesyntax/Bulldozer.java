d770123eace8e42cdff042138965da970f62fd67
Oleksandr Klymenko <klim777angel@gmail.com>
2022-08-11 19:38:28 +0300

    Added 4 classes, changed Main class, deleted empty lines

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,14 +1,11 @@
 package core.basesyntax;
-
 public class Bulldozer extends Machine {
     public Bulldozer() {
-
     }
     @Override
     public void doWork() {
         System.out.println("Bulldozer started it`s work");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Bulldozer stopped it`s work");
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,14 +1,11 @@
 package core.basesyntax;
-
 public class Excavator extends Machine {
     public Excavator() {
-
     }
     @Override
     public void doWork() {
         System.out.println("Excavator started it`s work");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Excavator stopped it`s work");
