7b160f697bc2af76d19a664ec4f67f0e59818313
Maks Hrytsyna <hrytsynamax@gmail.com>
2022-08-12 17:50:35 +0300

    escape in line 5 fixed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
