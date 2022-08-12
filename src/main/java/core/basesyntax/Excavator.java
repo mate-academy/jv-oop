8cfad498ba4aca72ad434aa7b52da616b6314758
Maks Hrytsyna <hrytsynamax@gmail.com>
2022-08-12 13:18:59 +0300

    added class overrided method

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Excavator started its work.");
+    }
+    @Override
+    public void stopWork() {
+        super.stopWork();
+        System.out.println("Excavator stopped its work.");
+
+    }
+}
