ca14a8ba32e32f417b93ec1ae5cbaa377e022d98
Maks Hrytsyna <hrytsynamax@gmail.com>
2022-08-12 14:01:53 +0300

    added fixes after review

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -5,6 +5,7 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator started its work.");
     }
+
     @Override
     public void stopWork() {
         super.stopWork();
--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 public class Machine {
     public void doWork() {
     }
+
     public void stopWork() {
     }
 }
