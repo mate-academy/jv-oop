522c9149e3ea5ccd7afefdf4d9d557683897af78
Andrii_Pikhur <andrii.pikhur@gmail.com>
2022-08-15 17:12:36 +0300

    Name of variable changed to "machines"

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] fleet1 = {new Truck(), new Buldozer(), new Excavator()};
-        for (Machine unit : fleet1) {
+        Machine[] machines = {new Truck(), new Buldozer(), new Excavator()};
+        for (Machine unit : machines) {
             unit.doWork();
             unit.stopWork();
         }
