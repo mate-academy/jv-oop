0e2cfb2c2f2039114aa31dd2784161f7b4b8d3ff
AndriiP <andrey.pslk@gmail.com>
2022-08-17 12:59:30 +0300

    Empty lines were removed. Fori replaced with for each

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,9 +5,9 @@ public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
 
-        for (int i = 0; i < machines.length; i++) {
-            machines[i].doWork();
-            machines[i].stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 
