f77eccf1a597eae40f0a9d79c2d93ee4ed218338
sasha <radovietsoleksandr@gmail.com>
2022-08-18 14:17:56 +0300

    machines1

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,19 +2,17 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-        Machine [] machines = {truck , bulldozer , excavator };
-        for (int i = 0 ; i < machines.length ; i ++){
+        Machine [] machines = {truck,bulldozer,excavator};
+        for (int i = 0; i < machines.length; i++){
             truck.doWork();
             truck.stopWork();
             bulldozer.doWork();
             bulldozer.stopWork();
             excavator.doWork();
             excavator.stopWork();
-
         }
     }
 }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -3,11 +3,10 @@ package core.basesyntax;
 public class Truck extends Machine{
     @Override
     public void doWork() {
-        System.out.println(" run Truck ");
+        System.out.println("run Truck");
     }
-
     @Override
     public void stopWork() {
-        System.out.println(" stop Truck ");
+        System.out.println("stop Truck");
     }
 }
