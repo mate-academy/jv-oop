6e50fb7a593bdb6fa5713ea49d91b3d7f05188c6
Alkayde <morozovnikita5899@gmail.com>
2022-08-27 01:56:27 +0300

    Created Machine,Bulldozer,Excavator,Truck classes and their methods. Added psvm with machines array and a loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machinesArray = new Machine[3];
+        machinesArray[0] = new Bulldozer();
+        machinesArray[1] = new Excavator();
+        machinesArray[2] = new Truck();
 
+        for (Machine machine : machinesArray){
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
