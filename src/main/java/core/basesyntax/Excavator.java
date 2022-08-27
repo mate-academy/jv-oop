ec3a2f4b57f01bebda9806bd507a1aad20489484
Nikita Kolesnikov <kit1datasvit@gmail.com>
2022-08-27 16:22:58 +0300

    Compl task

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Excavator extends Machine{
+
+
+    @Override
+    public void doWork() {
+        System.out.println("Excavator work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator stop work");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,18 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+
+        Machine[] machines = new Machine[] {truck, bulldozer, excavator};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+    }
+
+
+    }
+
 }
