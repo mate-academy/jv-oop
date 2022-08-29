016c7f711a1bcabf00f2fd834851e7419809d73c
Nikita Kolesnikov <kit1datasvit@gmail.com>
2022-08-29 20:01:48 +0300

    fix mistakes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,8 +3,13 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
-        Truck truck = new Truck();
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machines = {bulldozer, excavator, truck};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
