ddd34f124aba84a7ec140d08d21e801f62351d3b
Nataliia Batiukova <batiukova.nataliia@gmail.com>
2022-09-02 16:08:24 +0100

    -created new classes -implemented methods -override methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[]args) {
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine buldozer = new Buldozer();
+        Machine[] allMachines = new Machine[] {excavator, truck, buldozer};
+        for (Machine machine: allMachines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
