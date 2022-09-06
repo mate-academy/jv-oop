8612ae12f49ea49628033bcd7054f3057388b7f9
Yurii Trokhymenko <uta1965_@ukr.net>
2022-09-06 18:08:49 +0200

    added psvm printInfo methods added classes machine truck bulldozer excavator 6

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -6,9 +6,7 @@ public class MainApp {
         Machine truct = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-
         Machine[] machines = new  Machine[] {truct,bulldozer,excavator};
-
         for (Machine machine:machines) {
             printInfo(machine);
         }
