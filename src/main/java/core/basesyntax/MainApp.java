12a6db3c7061dca6ee15b9b45caed3121244930a
Yurii Trokhymenko <uta1965_@ukr.net>
2022-09-06 21:03:48 +0200

    added fixed review

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,7 +5,9 @@ public class MainApp {
         Machine truct = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
+
         Machine[] machines = new Machine[] {truct,bulldozer,excavator};
+
         for (Machine machine:machines) {
             machine.doWork();
             machine.stopWork();
