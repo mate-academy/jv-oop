78d9173d7437b303e67b7437edea6c8df0592ff6
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2022-09-07 23:01:59 +0300

    mvn clean package approved this code

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
-	
-	public static void main(String[] args) {
-		Machine[] machineArray = { new Truck(), new Bulldozer(), new Excavator() };
-		for (Machine m : machineArray) {
-			m.doWork();
-			m.stopWork();
-		}
-	}
+
+    public static void main(String[] args) {
+        Machine[] machineArray = { new Truck(), new Bulldozer(), new Excavator() };
+        for (Machine m : machineArray) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
 }
