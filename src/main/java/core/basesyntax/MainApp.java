504f1077d2556836e7b449aee6b8ea8ac458f9de
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2022-09-07 22:15:05 +0300

    empty line before main method was added

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
+	
 	public static void main(String[] args) {
 		Machine[] machineArray = { new Truck(), new Bulldozer(), new Excavator() };
 		for (Machine m : machineArray) {
