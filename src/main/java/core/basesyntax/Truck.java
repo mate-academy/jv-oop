4dcdc996a2258958e6b1f0b2149002a60e6d91cf
Mykhailo Dolia <111272141+mndolia@users.noreply.github.com>
2022-09-14 14:45:11 +0100

    Update Truck.java
    Add whitespace

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
-public class Truck extends Machine{
+public class Truck extends Machine {
     @Override
     public void stopWork() {
         System.out.println("Truck started its work.");
     }
+    
     @Override
     public void doWork() {
         System.out.println("Truck stopped its work.");
