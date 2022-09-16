4d627cb23c197726bb754cbbafef4c55b6dc9f14
Vitalina Volkivska <volkivskav@gmail.com>
2022-09-16 23:46:32 +0300

    was created supperclass Machine with abstract methods doWork() and stopWork();  all methods were overloaded in created  subclasses; in Main method was created Machine array; methods were called in loop;

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Excavator extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("The excavator has started working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The excavator has stoped working");
+    }
+}
