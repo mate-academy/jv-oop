5f2d2d7a069c7c2bc018e3fea611b698bdffe4c1
Oleksandr Dikhtyarenko <oleksandrdihtarenko11@gmail.com>
2022-09-16 11:24:50 +0300

    fixed problems with blanc spaces

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -5,12 +5,10 @@ public class Bulldozer extends Machine {
     @Override
     public void doWork() {
         System.out.println("Bulldozer is working");
-
     }
 
     @Override
     public void stopWork() {
         System.out.println("Bulldozer stopped working");
-
     }
 }
