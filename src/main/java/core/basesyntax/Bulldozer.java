731d48632eb4e2a7a43814509dd165b79b784f66
DY-JS <delo1604@ukr.net>
2022-09-17 18:11:12 +0300

    Add classes

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine{
+    public void doWork() {
+        System.out.println("Bulldozer started work");
+    };
+    public void stopWork() {
+        System.out.println("Bulldozer stopped work");
+    }
+}
