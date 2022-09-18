bf9a6f89bb8a3edfc9efc762fece95984d312312
alexeyukrainets <qaaq.com.ua@gmail.com>
2022-09-18 10:54:11 +0200

    Final version.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,18 +1,19 @@
 package core.basesyntax;
 
 public class MainApp {
-
     public static void main(String[] args) {
-        String[] machine = {"Truck", "Bulldozer", "Excavator"};
-        for (int i = 0; i < machine.length; i++) {
-            Machine work = new Machine() {
-                public void doWork() {
-                    System.out.println("Start work");
-                }
-                public void stopWork() {
-                    System.out.println("Stop work");
-                }
-            };
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+
+        Machine[] machines= new Machine[] {truck, bulldozer, excavator};
+
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+
+        for (Machine machine : machines) {
+            machine.stopWork();
         }
     }
 }
