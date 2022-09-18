fe56db7fa67c5f9d338f612a23ef0a3406ae5520
alexeyukrainets <qaaq.com.ua@gmail.com>
2022-09-18 11:16:23 +0200

    Final version

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Start work truck");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Stop work truck");
+    }
+}
