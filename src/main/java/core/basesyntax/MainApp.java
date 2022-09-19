2f3e86c95b85c23844a2eb01277bed04c937e489
Roman Mariushkin <ramzzzesik99@gmail.com>
2022-09-19 13:18:49 +0300

    added machines to project

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args){
+        Truck truck = new Truck();
+        Excavator excavator = new Excavator();
+        Bulldozer bulldozer = new Bulldozer();
+        Machine[] machines = {truck, bulldozer, excavator};
+        for (int i = 0; i< machines.length; i++){
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
 }
