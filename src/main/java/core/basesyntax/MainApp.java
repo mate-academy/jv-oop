7895344d58dd67806bbf75550536b60c1c21ffec
Viktoriia Maherko <victoriazahurska1999@gmail.com>
2022-09-19 15:04:35 +0300

    Fixeddd

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,14 +1,14 @@
 package core.basesyntax;
 
-  public class MainApp {
-  public static void main(String[] args) {
+public class MainApp {
+    public static void main(String[] args) {
         Machine bulldozer1 = new Bulldozer("650M");
         Machine truck1 = new Truck("TR260");
         Machine excavator1 = new Excavator("CX130D");
         Machine[] machines = new Machine[] {bulldozer1, truck1, excavator1};
         for (Machine machine : machines) {
-          machine.doWork();
-          machine.stopWork();
+            machine.doWork();
+            machine.stopWork();
+        }
     }
-  }
 }
