7c921b91a49d22529b8bf7d8a6270550f475f983
Viktoriia Maherko <victoriazahurska1999@gmail.com>
2022-09-19 15:07:15 +0300

    Fixeddd

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -10,10 +10,13 @@ public abstract class Machine {
     public void setModel(String model) {
         this.model = model;
     }
+
     public Machine(String model) {
         this.model = model;
     }
+
     public abstract void doWork();
+
     public abstract void stopWork();
 
 }
