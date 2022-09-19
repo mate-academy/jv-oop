2efe24cb1529be9159aa75ac044795525c671dd7
Viktoriia Maherko <victoriazahurska1999@gmail.com>
2022-09-19 14:50:46 +0300

    Fixed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,13 +1,13 @@
 package core.basesyntax;
 public class MainApp {
-  public static void main (String[] args ) {
-    Machine bulldozer1 = new Bulldozer("650M");
-    Machine truck1 = new Truck("TR260");
-    Machine excavator1 = new Excavator("CX130D");
-    Machine[] machines = new Machine[] {bulldozer1, truck1, excavator1};
-      for (Machine machine : machines) {
-        machine.doWork();
-        machine.stopWork();
+  public static void main (String[] args) {
+     Machine bulldozer1 = new Bulldozer("650M");
+     Machine truck1 = new Truck("TR260");
+     Machine excavator1 = new Excavator("CX130D");
+     Machine[] machines = new Machine[] {bulldozer1, truck1, excavator1};
+        for (Machine machine : machines) {
+          machine.doWork();
+          machine.stopWork();
     }
   }
 }
