b87486c9ddd04ab258d1b06e599e4605fa69d397
Oleksandr Shyshkan <o.shyshkan@gmail.com>
2022-09-19 10:58:49 +0300

    Done home work task - jv-oop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,10 @@
 package core.basesyntax;
-
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] arrayMachine = {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine oneMachine : arrayMachine) {
+            oneMachine.doWork();
+            oneMachine.stopWork();
+        }
+    }
 }
