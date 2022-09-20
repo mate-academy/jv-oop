595939682ceaa7842dede8e7d1b4a3c493032b28
Yulia Tsariv <purplesquirrel68@gmail.com>
2022-09-20 12:20:39 +0300

    methods are implemented

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,13 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine[] allMachine = new Machine[]{new Excavator(),new Bulldozer(),new Truck()};
+
+        for (int a = 0;a < allMachine.length;a++) {
+            allMachine[a].doWork();
+            allMachine[a].stopWork();
+        }
+
+    }
 }
