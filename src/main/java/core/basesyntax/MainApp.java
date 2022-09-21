521c2b42a5eeb95fbd41020777845beb5bb4abaf
Dmitri Kolos <skil@bigmir.net>
2022-09-22 01:44:16 +0300

    task solved

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[3];
+        machines[0] = new Truck();
+        machines[1] = new Bulldozer();
+        machines[2] = new Excavator();
 
+        for (Machine element : machines) {
+            element.doWork();
+            element.stopWork();
+        }
+    }
 }
+
+
