8010ee7ae8599dd69d10addf0040b06627bc004f
kolia <kolian201497@gmail.com>
2022-09-21 14:03:52 +0300

    ///

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -8,8 +8,8 @@ public class MainApp {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-        //List<Machine> machines = Arrays.asList(truck, bulldozer, excavator);
-        //machines.stream().forEach(a-> a.doWork());
-      //  machines.stream().forEach(a-> a.stopWork());
+        List<Machine> machines = Arrays.asList(truck, bulldozer, excavator);
+        machines.stream().forEach(a-> a.doWork());
+        machines.stream().forEach(a-> a.stopWork());
     }
 }
