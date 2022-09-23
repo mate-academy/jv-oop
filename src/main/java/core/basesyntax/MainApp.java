cf618598a758abac0912c834a56fa9d8ace3ba61
Dmitriy Kolos <106387200+DmitriyK86@users.noreply.github.com>
2022-09-23 03:01:35 +0300

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -7,9 +7,9 @@ public class MainApp {
         machines[1] = new Bulldozer();
         machines[2] = new Excavator();
 
-        for (Machine element : machines) {
-            element.doWork();
-            element.stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
