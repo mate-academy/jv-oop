d09c402a8074e1a033e39a4deab67842b620f24b
sergiejkoczergin <kacherkpi@gmail.com>
2022-09-24 23:18:41 +0200

    done

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[3];
+        machines[0] = new Bulldozer();
+        machines[1] = new Truck();
+        machines[2] = new Excavator();
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+
+
+    }
+}
