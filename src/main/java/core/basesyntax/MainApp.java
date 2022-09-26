b7fe2e5826f29d73179804254c91bb080493a017
GrishaSeniv <seniv.grisha@gmail.com>
2022-09-26 19:36:34 +0300

    Created Machine, Excavator, Truck and  Bulldozer classes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machinesArray = new Machine[3];
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        machinesArray[0] = excavator;
+        machinesArray[1] = truck;
+        machinesArray[2] = bulldozer;
+        for (Machine machine : machinesArray) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
