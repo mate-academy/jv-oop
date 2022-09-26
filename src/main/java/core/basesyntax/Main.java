a06863dbedf027a034a7ecefe56112108328ea58
Oleksii Bilko <lucheus@gmail.com>
2022-09-26 16:52:22 +0300

    resolved task

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        Machine[] machines = new Machine[] {excavator, bulldozer, truck};
+        for (Machine machine : machines){
+            machine.doWork();
+            machine.stopWork();
+        }
+
+    }
+
+}
