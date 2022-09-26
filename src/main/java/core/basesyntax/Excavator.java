38c11fdd72d2462cbe58cd3a66ee33add40830f4
Yulia Tsariv <purplesquirrel68@gmail.com>
2022-09-26 16:54:34 +0300

    mistakes fixed

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Excavator  started its work");
+        System.out.println("Excavator started its work");
     }
 
     @Override
