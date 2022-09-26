43ef2dc14fc090c748c2a61775c490b1eb84e81d
drKacher <kacherkpi@gmail.com>
2022-09-26 23:51:25 +0200

    done hw after code review

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -6,11 +6,9 @@ public class Main {
         machines[0] = new Bulldozer();
         machines[1] = new Truck();
         machines[2] = new Excavator();
-        for (int i = 0; i < machines.length; i++) {
-            machines[i].doWork();
-            machines[i].stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
-
-
     }
 }
