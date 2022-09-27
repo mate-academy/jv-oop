581ecebb73f9354f9ca2148ff1d1534889200694
Anastasia Vasylyk <anastasiavasylykv@gmail.com>
2022-09-27 18:20:17 +0300

    Was realized Truck, Bulldozer, Excavator and Machine classes

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("certain machine started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("certain machine stopped its work");
+    }
+}
