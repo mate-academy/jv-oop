2cd98992fe5c8f2600eb37363982b0b9a00126b5
Luka Lozovyk <lukalozovyk@gmail.com>
2022-09-27 15:08:09 +0300

    Created classes Machine, Truck, Bulldozer, Excavator. Implemented methods doWork() and stopWork().

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Truck t1 = new Truck();
+        Bulldozer b1 = new Bulldozer();
+        Excavator e1 = new Excavator();
+
+        Machine[] machines = {t1, b1, e1};
+        for (Machine m : machines) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
 
 }
