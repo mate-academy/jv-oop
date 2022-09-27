c9b068564410a15c3f93b4ea9dcaeef0e3932910
Luka Lozovyk <lukalozovyk@gmail.com>
2022-09-27 15:18:57 +0300

    Used abstract reference instead of specific ones after reviewing common mistakes file.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Truck t1 = new Truck();
-        Bulldozer b1 = new Bulldozer();
-        Excavator e1 = new Excavator();
+        Machine t1 = new Truck();
+        Machine b1 = new Bulldozer();
+        Machine e1 = new Excavator();
 
         Machine[] machines = {t1, b1, e1};
         for (Machine m : machines) {
