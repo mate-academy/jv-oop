60abdf545079fec6cb0e2f4734f86c98417a70a0
Oleksandr Drobot <drob4@ukr.net>
2022-09-27 22:06:23 +0300

    added new classes and methods according to the task

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] garage = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
 
+        for (Machine car : garage) {
+            car.doWork();
+            car.stopWork();
+        }
+    }
 }
