638d4ff8f56c4034691c8477e3b163eb1092d62e
Andrii Matveev <matveev.tesla@gmail.com>
2022-09-28 16:39:30 +0300

    methods for Excavator was added

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,7 +4,7 @@ public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
 
-        for (int i = 0; i < machines.length - 1; i++) {
+        for (int i = 0; i < machines.length; i++) {
             machines[i].doWork();
             machines[i].stopWork();
         }
