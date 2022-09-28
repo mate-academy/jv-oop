414f99cb0a1173603bbde8af70cc9a989fd10037
Oleg Marchuk <mar4uk14@gmail.com>
2022-09-28 17:50:26 +0300

    Create others classes and override our methods

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Excavator extends Machine{
+
+    @Override
+    public void doWork() {
+        System.out.println("Excavator start to work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator stop to work");
+    }
+
+
+}
