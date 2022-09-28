eb385f170d363deb444e0f8d9474d9a7806677cf
thebitler <yurchyknikita@gmail.com>
2022-09-28 10:22:37 +0200

    Fixed code style, moved inner classes to new files

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,51 +2,11 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        for (Machine machine : new Machine[]{ new Truck(), new Bulldozer(), new Excavator() }) {
+        Machine[] machines = { new Truck(), new Bulldozer(), new Excavator() };
+
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
-        }
-    }
-
-    public abstract static class Machine {
-        public abstract void doWork();
-
-        public abstract void stopWork();
-    }
-
-    public static class Truck extends Machine {
-        @Override
-        public void doWork() {
-            System.out.println("Machine Truck has started");
-        }
-
-        @Override
-        public void stopWork() {
-            System.out.println("Machine Truck has stopped it's work");
-        }
-    }
-
-    public static class Bulldozer extends Machine {
-        @Override
-        public void doWork() {
-            System.out.println("Machine Bulldozer has started");
-        }
-
-        @Override
-        public void stopWork() {
-            System.out.println("Machine Bulldozer has stopped it's work");
-        }
-    }
-
-    public static class Excavator extends Machine {
-        @Override
-        public void doWork() {
-            System.out.println("Machine Excavator has started");
-        }
-
-        @Override
-        public void stopWork() {
-            System.out.println("Machine Excavator has stopped it's work");
         }
     }
 }
