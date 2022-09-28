93fcd08b8c427ffa5b95a1821773ce23c759aeae
Nazarii Pertsak <pertsak98@gmail.com>
2022-09-28 11:12:08 +0300

    changed to abstract reference where it possible

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Truck truck = new Truck();
-        Excavator excavator = new Excavator();
-        Bulldozer bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
         Machine[] array = {truck, excavator, bulldozer};
         for (Machine machine: array) {
             machine.doWork();
