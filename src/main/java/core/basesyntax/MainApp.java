7b71790cf089cb27adebdf549481e3ca6ea73749
Andrii Matveev <matveev.tesla@gmail.com>
2022-09-28 16:27:06 +0300

    Create class Machin,Truck,Buldozer,Excavator.Overrided methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,13 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+
+        for (int i = 0; i < machines.length - 1; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
 
 }
