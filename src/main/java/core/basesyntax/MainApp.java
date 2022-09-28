28a42b3a5bc5da1799fbbdd00c02562e767f6141
Danyl Kniaziev <kdanil0401@gmail.com>
2022-09-28 14:48:46 +0300

    added and implemented classes Machine, Truck, Bulldozer, Excavator

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] arr = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : arr) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
