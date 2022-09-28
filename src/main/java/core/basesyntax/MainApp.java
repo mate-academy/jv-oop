dc4cdf89837d8c3b762afd0d2a8d1a43e9bcd96e
Oleg Marchuk <mar4uk14@gmail.com>
2022-09-28 17:52:49 +0300

    Create Machine array and call methods in a loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine Excavator = new Excavator();
+        Machine Truck = new Truck();
+        Machine Bulldozer = new Bulldozer();
+        Machine[] machines = {Excavator,Truck,Bulldozer};
+        for(Machine working : machines){
+            working.doWork();
+            working.stopWork();
+        }
+        System.out.println();
+    }
 }
