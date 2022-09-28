f006e22677366f2c09fe65d5df1ac160d279e43c
Anton Pushkar <tosha.pushkar@gmail.com>
2022-09-28 13:55:54 +0300

    My first homework

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,9 @@
 package core.basesyntax;
 
 public class MainApp {
-
+     Machine machines[] = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
+     for(Machine mashine : machines){
+         mashine.doWork();
+         mashine.stopWork();
+    }
 }
