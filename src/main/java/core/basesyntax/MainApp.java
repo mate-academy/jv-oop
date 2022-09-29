46b2f50e976f760e7fc05c1ae84c1384b7511e3b
Ivan Drebot <drebotsvan9@gmail.com>
2022-09-29 16:39:45 +0300

    Finished homework task

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] arr = {new Truck(), new Bulldozer(), new Excavator()};
 
+        for (Machine machine : arr) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
