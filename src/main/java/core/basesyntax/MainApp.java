823404c160a9f3840f498ccb5da72805564fa23a
Nazar Hnatyuk <stim1.one@gmail.com>
2022-09-29 13:40:15 +0300

    1. Create class Machine 2. create three classes Excavator , Truck and Bulldozer which extends from machine. 3. Create array machineIsWork which include truck, bulldozer, excavator . 4. Call methods machine.doWork() and machine.stopWork() in a for each loop.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
 
-}
+    Machine truck = new Truck();
+    Machine bulldozer = new Bulldozer();
+    Machine excavator = new Excavator();
+
+    Machine[] machinesIsWork = new Machine[] {truck, bulldozer, excavator};
+
+    for ( Machine machine : machinesIsWork) {
+        machine.doWork();
+    }
+    for( Machine machine : machinesIsWork) {
+        machine.stopWork();
+     }
+    }
+    }
