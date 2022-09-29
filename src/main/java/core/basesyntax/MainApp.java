bc63c0a6773ccef809b4cee039d2c1f905424511
Krupii Oleh <oleg2003andriy@gmail.com>
2022-09-29 15:04:09 +0300

    complited

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Excavator excavator = new Excavator();
+        Bulldozer bulldozer = new Bulldozer();
+        Machine [] machineArray = new Machine[] {truck, bulldozer, excavator};
+        for (Machine machine: machineArray) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
