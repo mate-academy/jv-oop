faea0d85bd2c7d75e43730f66069ef17ab4b373b
Sergienko Anton <sergienko.anton.v@gmail.com>
2022-09-29 15:30:48 +0300

    All requirements are fulfilled

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine buldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine [] arr = {buldozer, excavator, truck};
+        for (Machine machine : arr) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 
 }
