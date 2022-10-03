f19b52458a37da84167299ece8fb13a7a73fe1f1
Ilya Samilyak <samilyakilya@gmail.com>
2022-10-03 20:52:56 +0300

    Removed empty line

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,18 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
 
         Machine[] array = {truck, bulldozer, excavator};
-
         for (Machine element : array) {
             element.doWork();
             element.stopWork();
         }
-
     }
-
 }
