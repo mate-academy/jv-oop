d541368dc8557c6fc150c915f4bc5172d53b97f5
Volodymyr Romanyshyn <Romanvlad000@gmail.com>
2022-10-05 22:10:12 +0300

    added fixes after review

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,6 +2,6 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Valid solution");
+        System.out.println("Valid_ solution");
     }
 }
