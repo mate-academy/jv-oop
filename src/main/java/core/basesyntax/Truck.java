d62fe4a2e9e15a30b422edb3e4e2238eda865766
AndriiBohun <poikka69@gmail.com>
2022-10-06 14:14:17 +0300

    commit002

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -4,12 +4,10 @@ public class Truck extends Machine{
     @Override
     public void doWork() {
         System.out.println("Truck started its work");
-
     }
 
     @Override
     public void stopWork() {
         System.out.println("Truck stopped its work.");
-
     }
 }
