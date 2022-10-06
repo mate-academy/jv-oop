4a4306cd6caceb72975a4fb7baac3ab8802da664
AndriiBohun <poikka69@gmail.com>
2022-10-06 14:39:46 +0300

    commit003

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,6 +5,7 @@ public class MainApp {
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
+
         Machine[] arrayMachines = new Machine[]{excavator, bulldozer, truck};
         for (Machine c : arrayMachines) {
             c.doWork();
