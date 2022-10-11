350bfcac72c151e81ccb1a45d51234d315ca31ac
Tetiana B <tbabenko0909@gmail.com>
2022-10-11 15:06:48 +0200

    hope that is true :)

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class MainApp {
 
+    public static void main(String[] args) {
+
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+
+        Machine[] all = new Machine[]{excavator,bulldozer,truck};
+        for (Machine a : all) {
+            a.doWork();
+            a.stopWork();
+        }
+    }
 }
