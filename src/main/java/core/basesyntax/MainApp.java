12b2745a2a1d04d4ab7ab9ba97cb9316368cc99b
Bohdan Halitskyi <b.a.galitskiy@gmail.com>
2022-10-12 16:45:51 +0300

    Created abstract class Machine with inheriting subclasses Excavator, Bulldozer, Truck, Edited main method with creation of machines array and execution loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+public static void main (String[] args) {
+    Machine[] machines = new Machine[3];
+    machines[0] = new Truck();
+    machines[1] = new Bulldozer();
+    machines[2] = new Excavator();
+    for (Machine machine : machines) {
+        System.out.println(machine.doWork());
+        System.out.println(machine.stopWork());
+    }
+}
 }
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Truck extends Machine{
+    @Override
+    public String doWork() {
+        return "Truck started working";
+    }
+
+    @Override
+    public String stopWork() {
+        return "Truck stopped work";
+    }
+}
