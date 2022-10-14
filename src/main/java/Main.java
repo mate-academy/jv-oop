6a0b3062919082b06eb1d6c4a0d509e636c7bf77
Bohdan Shevtsov <shevtsov.b@gmail.com>
2022-10-14 11:51:33 +0300

    cleaned by maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Machine [] machine;
         machine = new Machine[3];
         machine[0] = new Bulldozer();
         machine[1] = new Excavator();
         machine[2] = new Truck();
-        for (int i = 0; i < machine.length; i++ ) {
+        for (int i = 0; i < machine.length; i++) {
             machine[i].doWork();
             machine[i].stopWork();
         }
