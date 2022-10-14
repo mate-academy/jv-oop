7b7757a603a4dab9ebfb7750f4f25af13e1ec25e
Malinskyi Daniil <danmalina2707@gmail.com>
2022-10-14 21:56:22 +0300

    name of the main class changed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-class MainApp {
+class Main {
     public static void main(String[]args) {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
