035900cd3f4772cb760f9915cb347003bd542471
Andriy <Andriy@email.com>
2022-10-17 15:30:04 +0300

    adding changes

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -5,6 +5,7 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator starts work");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Excavator stops work");
