84aa2307be7cbfc0271ab5f6bd988d0e92d1d310
Tetiana B <tbabenko0909@gmail.com>
2022-10-17 15:32:23 +0200

    Machine implementation.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
 
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        Machine[] allMachines = new Machine[]{excavator, bulldozer, truck};
+        for (Machine a : allMachines) {
+            a.doWork();
+            a.stopWork();
+        }
+    }
 }
