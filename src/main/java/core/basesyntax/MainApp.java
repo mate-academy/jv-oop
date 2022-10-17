bfd15323183ab02375090491f1847fcbdef25090
Andriy <89684976+SichniyAndriy@users.noreply.github.com>
2022-10-17 15:46:40 +0300

    Update src/main/java/core/basesyntax/MainApp.java
    Co-authored-by: Anastasia Len <81781479+aaananas@users.noreply.github.com>

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,9 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] buildingTechniqs = { new Truck(), new Excavator(), new Bulldozer(),
-                                       new Truck(), new Excavator(), new Bulldozer(),
-                                       new Truck(), new Excavator(), new Bulldozer() };
+        Machine[] buildingTechniqs = {new Truck(), new Excavator(), new Bulldozer()};
         for (Machine machine: buildingTechniqs) {
             machine.doWork();
             machine.stopWork();
