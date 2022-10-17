81ca63583dbcf1635493b8594edf18d63b9970d3
Andriy <Andriy@email.com>
2022-10-17 15:25:27 +0300

    remove separated lines

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -5,7 +5,6 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator starts work");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Excavator stops work");
