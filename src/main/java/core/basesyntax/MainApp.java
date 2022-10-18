fba529110f42f63a622e919cc802f01d217f0a92
Yaroslav Liashchenko <liaroswin@gmail.com>
2022-10-18 20:36:36 +0300

    edited checkstyle

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
+
         Excavator excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
