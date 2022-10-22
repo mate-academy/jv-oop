4bb3180377855f548062457ce305d01b1fe0ae2c
Dmytro Koval <dimonkoval@gmail.com>
2022-10-22 20:10:05 +0300

    Second commit

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,14 +3,13 @@ package core.basesyntax;
 public class Main {
 
     public static void main(String[] args) {
-    Truck t = new Truck();
-    Bulldozer b = new Bulldozer();
-    Excavator e = new Excavator();
-    Machine[] array = {t, b, e}; //"Truck", "Bulldozer", "Excavator"};
+        Truck t = new Truck();
+        Bulldozer b = new Bulldozer();
+        Excavator e = new Excavator();
+        Machine[] array = {t, b, e};
         for (int i = 0; i < array.length; i++) {
             array[i].doWork();
             array[i].stopWork();
         }
-
     }
 }
