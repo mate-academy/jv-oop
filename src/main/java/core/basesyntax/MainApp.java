7e93e06e93fc2454dca8f232012dbb9d48985757
Alexander Karpenko <karalexander@gmail.com>
2022-10-24 01:32:37 +0300

    Added 4 files as a java classes and Main class was modified

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
-
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] carPark = new Machine[3];
+        carPark[0] = new Truck ();
+        carPark[1] = new Bulldozer();
+        carPark[2] = new Excavator();
 
+        for (Machine workingMachine : carPark) {
+            workingMachine.doWork();
+            workingMachine.stopWork();
+            System.out.println();
+        }
+    }
 }
