20dde5c276328e821ec490f7a985ca4a377e103c
Alona Vyrodova <alenavirodova@gmail.com>
2022-10-23 18:57:29 +0300

    Home work

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        Machine[] array = new Machine[]{excavator, bulldozer, truck};
+        for (int i = 0; i < array.length; i++) {
+            array[i].doWork();
+            array[i].stopWork();
 
+        }
+    }
 }
