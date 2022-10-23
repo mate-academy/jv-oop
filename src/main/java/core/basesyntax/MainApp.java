77f5f02514dffa1c5ae578577218ec07e2ae0e4c
Yurii Storozhuk <yurii.storozhuk@gmail.com>
2022-10-23 22:44:37 +0300

    I am committing multiple files (create classes Machine  and subclasses Bulldozer, Excavator and Truck; create Machine[] array with all subclasses and start methods doWork and stopWork every subclass with loop)

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,11 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine[] array = {new Bulldozer(), new Excavator(), new Truck()};
+        for (Machine item: array) {
+            item.doWork();
+            item.stopWork();
+        }
+    }
 }
