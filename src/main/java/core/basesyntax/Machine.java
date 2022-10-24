a56a677963ef9d87210ae8c7922b0ccf4a0f6673
MykhailoMykhasov <105455921+MykhailoMykhasov@users.noreply.github.com>
2022-10-24 22:11:18 +0300

    Update src/main/java/core/basesyntax/Machine.java
    Co-authored-by: Leonid Sivko <rommel@ukr.net>

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,21 +1,8 @@
 package core.basesyntax;
 
 abstract class Machine {
-    private String name;
-
-    public Machine(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public abstract void doWork();
 
     public abstract void stopWork();
 }
+
