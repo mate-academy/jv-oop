da6fa21e4f9bf32bed45efe570e56664880903c2
MykhailoMykhasov <105455921+MykhailoMykhasov@users.noreply.github.com>
2022-10-24 22:19:37 +0300

    Update src/main/java/core/basesyntax/Bulldozer.java
    Co-authored-by: Leonid Sivko <rommel@ukr.net>

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,11 +1,6 @@
 package core.basesyntax;
 
 public class Bulldozer extends Machine {
-
-    public Bulldozer(String name) {
-        super(name);
-    }
-
     @Override
     public void doWork() {
         System.out.println(getName() + " Start");
