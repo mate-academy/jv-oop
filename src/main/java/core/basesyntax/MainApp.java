64805cd5196e2ff7c7ae1c5cec5de626c2fdcbc9
Ololenko Oleksandr <sahkaa2009@gmail.com>
2022-10-24 18:47:13 +0300

    fix

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,15 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
         Machine[] machines = new Machine[] {truck, bulldozer, excavator};
-        for (Machine machine: machines){
+        for (Machine machine: machines) {
             machine.doWork();
         }
-        for (Machine machine: machines){
+        for (Machine machine: machines) {
             machine.stopWork();
         }
     }
