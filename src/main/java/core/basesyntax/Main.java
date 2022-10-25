3320fe79fa2cf91cdd61e43a5cefa7fc3ce07fa4
Dmytro Koval <dimonkoval@gmail.com>
2022-10-25 09:48:32 +0300

    renamed classes of machine

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        Truck t = new Truck();
-        Bulldozer b = new Bulldozer();
-        Excavator e = new Excavator();
-        Machine[] array = {t, b, e};
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        Machine[] array = {truck, bulldozer, excavator};
         for (int i = 0; i < array.length; i++) {
             array[i].doWork();
             array[i].stopWork();
