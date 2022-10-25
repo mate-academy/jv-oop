2fff6072c9b8f0224537e77768ecbfddcc00c26c
Yurii Storozhuk <yurii.storozhuk@gmail.com>
2022-10-25 09:06:35 +0300

    Fix up redundant empty lines.

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,12 +1,11 @@
 package core.basesyntax;
-
 public class Bulldozer extends Machine {
-
     @Override
     public void doWork() {
         System.out.println("Bulldozer work!");
     }
 
+    @Override
     public void stopWork() {
         System.out.println("Bulldozer stop!");
     }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
-
 public class MainApp {
-
     public static void main(String[] args) {
         Machine[] array = {new Bulldozer(), new Excavator(), new Truck()};
         for (Machine item: array) {
