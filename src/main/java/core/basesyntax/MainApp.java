86b81ede70ca8a7b057bc91ff162c2a86de19161
DY-JS <delo1604@ukr.net>
2022-10-28 01:40:22 +0300

    Solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Machine truck = new Truck();
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
-public class Truck extends Machine{
+public class Truck extends Machine {
     public void doWork() {
         System.out.println("Truck started work");
-    };
+    }
+
     public void stopWork() {
         System.out.println("Truck stopped work");
     }
