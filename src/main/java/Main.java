e0f0518955e40a0f28f7c521143c94b7a0d27a47
Dima Byrka <fobossen@gmail.com>
2022-10-29 23:08:44 +0300

    Main file with execution

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Machine[] MachineArray = new Machine[3];
+        MachineArray[0] = new Excavator();
+        MachineArray[1] = new Bulldozer();
+        MachineArray[2] = new Truck();
+    for (int i = 0; i < MachineArray.length; i++) {
+        MachineArray[i].doWork();
+        MachineArray[i].stopWork();
+    }
+    }
+}
