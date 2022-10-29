1471de4c718fd73a4d2a43ccab8d0d686d670fc6
Vadym Valchak <vadym.valchak@gmail.com>
2022-10-29 09:25:26 +0300

    Change code style

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,15 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-  public static void main(String[] args) {
-    Machine bulldozer = new Bulldozer();
-    Machine excavator = new Excavator();
-    Machine truck = new Truck();
-    Machine[] machines = new Machine[] { bulldozer, excavator, truck };
+    public static void main(String[] args) {
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine[] machines = new Machine[] { bulldozer, excavator, truck };
 
-    for (Machine machine : machines) {
-      machine.doWork();
-      machine.stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
-  }
 }
