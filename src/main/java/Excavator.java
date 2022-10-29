4d7e1091cadf6c641157c583d939f6aa2ed9906c
Dima Byrka <fobossen@gmail.com>
2022-10-29 23:07:02 +0300

    hw1-version01 Created parent class, few subclasses, overwrited methods for subclasses

---
+++ src/main/java/Excavator.java
@@ -0,0 +1,10 @@
+public class Excavator extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Excavator is working now!");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator stops working!");
+    }
+}
---
+++ src/main/java/Truck.java
@@ -0,0 +1,11 @@
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck is working now!");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stops working!");
+    }
+
+}
