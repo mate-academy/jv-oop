e0c45119736e25627a39bfa83bc180c9c07023ed
Vadym Valchak <vadym.valchak@gmail.com>
2022-10-29 08:20:02 +0300

    Created Bulldozer, Excavator, Machine classes. Run methods of each classes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-
+	public static void main(String[] args) {
+		Machine bulldozer = new Bulldozer();
+		Machine excavator = new Excavator();
+		Machine truck = new Truck();
+		Machine[] machines = new Machine[] {bulldozer, excavator, truck};
+		
+		for (Machine machine : machines) {
+			machine.doWork();
+			machine.stopWork();
+		}
+	}
 }
