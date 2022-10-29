94ffe6879d315c17b77f7b2c1cbcbfeb4c3e0b1c
Dima Byrka <fobossen@gmail.com>
2022-10-29 23:13:24 +0300

    hw1-version01 Created parent class, few subclasses, overwrited methods for subclasses

---
+++ src/main/java/Bulldozer.java
@@ -0,0 +1,11 @@
+public class Bulldozer extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer is working now!");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer stops working");
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-package core.basesyntax;
-
 public class Main {
     public static void main(String[] args) {
         Machine[] MachineArray = new Machine[3];
