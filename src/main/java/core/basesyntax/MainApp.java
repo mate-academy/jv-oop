58fd28ddc9de0bf3b65b00bec1ae8bfb10c881fe
Olga Kolosova <olgakolosova0214@gmail.com>
2022-10-30 12:45:06 +0200

    Create "Machine" array in class MainApp and call methods doWork()` and `stopWork()` in a loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,17 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main(String[] args) {
+    public static void main(String[]args) {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
+
         Machine[] machine = new Machine[]{truck,bulldozer,excavator};
-for(Machine machines : machine) {
-    machines.doWork();
-}
-for(Machine machines : machine) {
-    machines.stopWork();
-}
-}
+        for (Machine machines : machine) {
+            machines.doWork();
+        }
+        for (Machine machines : machine) {
+            machines.stopWork();
+        }
+    }
 }
 
