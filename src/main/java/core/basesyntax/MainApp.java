ce848dae375b25978736db36f69436e4bd5aa592
ArtemMakovskyy <photomak79@gmail.com>
2022-10-30 20:22:55 +0200

    1) Create abstract class Machine with two abstract methods: doWork, stopWork. 2) Create three classes: Truck, Bulldozer, Excavator which extends from Machine and override it methods.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,13 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machines = {truck, bulldozer, excavator};
+        for (Machine machine:machines){
+            machine.doWork();
+            machine.stopWork();
+        }
     }
-
 }
