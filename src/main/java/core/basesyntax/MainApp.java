144dd20265badc2721c2f92ae1385430d98fee1d
ArtemMakovskyy <photomak79@gmail.com>
2022-10-30 20:46:53 +0200

    In an existed class MainApp, in the main method created code with Machine array with `Truck`, `Bulldozer` and `Excavator` and call methods `doWork()` and `stopWork()` in a loop.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine truck = new Truck();
-        Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
         Machine[] machines = {truck, bulldozer, excavator};
         for (Machine machine:machines){
             machine.doWork();
