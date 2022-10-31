4d973a2207d3e6256415330f35b43d258f8d910a
Danylo Borysenko <borysenkodanyl@gmail.com>
2022-10-31 20:52:58 +0200

    errors mvn have been corrected

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Machine Truck = new Truck();
-        Machine Bulldozer = new Bulldozer();
-        Machine Excavator = new Excavator();
-        Machine[] machines = new Machine[]{Truck,Bulldozer,Excavator};
-        for (int i = 0;i < machines.length;i++){
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machines = new Machine[] {truck,bulldozer,excavator};
+        for (int i = 0; i < machines.length; i++) {
             machines[i].doWork();
             machines[i].stopWork();
         }
