5a48fa14c762c95d60e88dc02301d14d892a61d7
Dima Byrka <fobossen@gmail.com>
2022-10-31 19:42:27 +0200

    fixed compile error, should be fine

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         Machine[] machineArray = new Machine[3];
-        MachineArray[0] = new Excavator();
-        MachineArray[1] = new Bulldozer();
-        MachineArray[2] = new Truck();
+        machineArray[0] = new Excavator();
+        machineArray[1] = new Bulldozer();
+        machineArray[2] = new Truck();
         for (int i = 0; i < machineArray.length; i++) {
-            MachineArray[i].doWork();
-            MachineArray[i].stopWork();
+            machineArray[i].doWork();
+            machineArray[i].stopWork();
         }
     }
 }
