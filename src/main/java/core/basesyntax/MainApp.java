6a8f9d015699fe134f49825e667738df719f6f7b
Igor Puhach <igorpuhach21@gmail.com>
2022-10-31 16:10:44 +0200

    added psvm method

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -6,7 +6,6 @@ public class MainApp {
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
 
-
         Machine[] machines = new Machine[]{truck, bulldozer, excavator};
 
         for (Machine machine : machines) {
