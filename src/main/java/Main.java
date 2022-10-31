33bb0ebbe91652ea1ac7f4b618cd23e203bc9d06
Danylo Borysenko <borysenkodanyl@gmail.com>
2022-10-31 19:00:27 +0200

    main method was added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        Machine Truck = new Truck();
+        Machine Bulldozer = new Bulldozer();
+        Machine Excavator = new Excavator();
+        Machine[] machines = new Machine[]{Truck,Bulldozer,Excavator};
+        for (int i = 0;i < machines.length;i++){
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
     }
 }
