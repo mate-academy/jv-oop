ab2b40c30b01e1c4acb33a5562b717d4aaae5190
Danylo Borysenko <borysenkodanyl@gmail.com>
2022-11-01 19:48:42 +0200

    added fixes after review

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,10 +2,7 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        Machine truck = new Truck();
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
-        Machine[] machines = new Machine[] {truck,bulldozer,excavator};
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
         for (int i = 0; i < machines.length; i++) {
             machines[i].doWork();
             machines[i].stopWork();
