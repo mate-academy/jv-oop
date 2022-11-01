757685928de0bbac5e2a70c84759fe4f615bc98c
Denys Yastrebov <programmer_p@outlook.com>
2022-11-01 21:16:31 +0200

    task solved

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public abstract class Machine {
+
+    public Machine() {
+    }
+    public abstract String doWork();
+    public abstract String stopWork();
+}
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    @Override
+   public String doWork() {
+       return "Truck started its work.";
+
+   }
+
+    @Override
+    public String stopWork() {
+        return "Truck stopped its work.";
+    }
+}
