36fb0b2757844acab7be9b461881c4f3fa13f2b7
Volodymyr Romanyshyn <Romanvlad000@gmail.com>
2022-11-08 00:08:06 +0200

    Homework was successfully built

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -4,11 +4,13 @@ public class Excavator extends Machine {
 
     @Override
     public void doWork() {
+
         System.out.println("Excavator started work");
     }
 
     @Override
     public void stopWork() {
+
         System.out.println("Excavator stops work");
     }
 }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -3,11 +3,13 @@ package core.basesyntax;
 public class Truck extends Machine {
 
     @Override
-    public void doWork() { System.out.println("Truck started work");
+    public void doWork() {
+        System.out.println("Truck started work");
     }
 
     @Override
     public void stopWork() {
+
         System.out.println("Truck stops work");
     }
 }
