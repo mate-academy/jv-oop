179e7b9b99e15e2525b2aa3ba212fdbf3ba17a88
Anastasiia Metelska <metelska.anastasiia@gmail.com>
2022-11-08 12:05:22 +0200

    classes and methods were added methods were called in main

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Machine {
+
+
+    public String doWork(){
+        return "I'm doing my work";
+    }
+
+    public String stopWork(){
+        return "I have stopped my work";
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+
+        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
+
+        for (Machine machine : machines){
+            System.out.println(machine.doWork());
+            System.out.println(machine.stopWork());
+
+        }
+    }
 
 }
