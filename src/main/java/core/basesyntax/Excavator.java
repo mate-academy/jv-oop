fdc70dbe4a8b2890b00008357cd569c204e7458c
Oleksandr <xtremesasha@outlook.com>
2022-11-09 18:54:00 +0200

    fixed massage

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Excavator: vvjj");
+        System.out.println("Excavator: Do work");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Excavator stop");
+        System.out.println("Excavator: Stop work");
     }
 }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Truck extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Track: rrrr");
+        System.out.println("Track: Do work");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Track stop");
+        System.out.println("Track: Stop work");
     }
 }
