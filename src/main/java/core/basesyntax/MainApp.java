dd5a10b7be0536f83f74b0c01533b2774da7f6f8
Melnychuk <melnychukmyhailo@gmail.com>
2022-11-11 12:42:00 +0200

    machine task solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,15 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator  = new Excavator ();
 
+       Machine[] machines = new Machine[]{truck, bulldozer, excavator};
+       for(Machine machine : machines) {
+           machine.doWork();
+           machine.stopWork();
+       }
     }
 
 }
