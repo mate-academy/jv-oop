270ac845c77b52a45efdd0493caa7d1e6ae04757
Stepan Peknyi <stepan_peknyy@ukr.net>
2022-11-13 19:44:27 +0200

    Solution of jv-oop task

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        Machine myTruck = new Truck();
+        Machine myBulldozer = new Bulldozer();
+        Machine myExcavator = new Excavator();
+        Machine[] machines = new Machine[] {myTruck, myBulldozer, myExcavator};
+        for (int i = 0; i < machines.length; i++ ) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
+}
