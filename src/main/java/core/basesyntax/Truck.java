92c9b065954c587975b257c714dce1fa36cfe142
Oleksii Parkhomenko <kcas.pilot@gmail.com>
2022-11-14 16:04:51 +0200

    removed empty lines

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
+
 public class Truck extends Machine {
     @Override
     public void doWork() {
         System.out.println("Truck started, it works");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Truck stopped");
