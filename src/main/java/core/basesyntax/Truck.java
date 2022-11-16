a5c47b089e748dffe6468117ccf791e26a9cf911
Алексей <m.leshka955@gmail.com>
2022-11-16 17:36:49 +0200

    creating 3 classes, 1 abstract class, 2 methods in abstract class, override 2 method and 1 loop how to call two methods in main class

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Truck extends Machine{
+
+    @Override
+    public void doWork() {
+        System.out.println("Truck do work!");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stop work!");
+    }
+}
