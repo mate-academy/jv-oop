5b833e14130e7363c1b7d5d3480724f798626059
Nikita Akosnov <nikitaua4226@gmail.com>
2022-11-16 18:48:08 +0200

    added methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Machine[] machines = new Machine[]{new Truck(), new Excavator(), new Bulldozer()};
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
     }
 }
