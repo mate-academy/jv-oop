d2b8994336e9967bef28324a4a0c648533658bbb
Yevhenii Kilovyi <jenya.kilevoy@gmail.com>
2022-11-17 21:48:48 +0200

    OOP Basic fixed mistakes

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Bulldozer extends Machine {
     @Override
     public void doWork() {
-        System.out.println("core.basesyntax.Bulldozer started its work");
+        System.out.println("Bulldozer started its work");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("core.basesyntax.Bulldozer stopped its work");
+        System.out.println("Bulldozer stopped its work");
     }
 }
