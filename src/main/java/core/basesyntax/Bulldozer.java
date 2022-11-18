45d410bb2a114f3e27f309b2e24296044006b4f7
Alina Smirnova <0970352020a@gmail.com>
2022-11-18 19:59:07 +0300

    after fixing 2

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -5,9 +5,9 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Bulldozer started to work!");
     }
+
     @Override
     public void stopWork() {
-
         System.out.println("Bulldozer stopped to work!");
     }
 }
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,12 +3,11 @@ package core.basesyntax;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
-
         System.out.println("Excavator started to work!");
     }
+
     @Override
     public void stopWork() {
-
         System.out.println("Excavator stopped to work!");
     }
 }
