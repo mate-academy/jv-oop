cde7cc2104407fcde564ca201bd2ad6169cc094c
Alina Smirnova <0970352020a@gmail.com>
2022-11-18 19:42:36 +0300

    hw done, waiting for reply if everything ok )

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck started to work!");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped to work!");
+    }
+}
