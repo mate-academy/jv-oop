f0dc50b0fdd0b950a1e144946bcd78780e6ad95d
Ivan Vovnyanko <moonkit.dior@gmail.com>
2022-11-21 11:57:34 +0300

    corrected grammatical errors

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
+
 public class MainApp {
+
     public static void mainApp(String[] args) {
         System.out.println("example");
-        Machine[] machineArray = {new Truck("truck"), new Bulldozer("bulldozer"), new Excavator("excavator")};
+        Machine[] machineArray = { new Truck("truck"), new Bulldozer("bulldozer"), new Excavator("excavator")};
         for (Machine element: machineArray) {
             element.doWork();
             element.stopWork();
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class Truck extends Machine{
+public class Truck extends Machine {
     private String truckName;
 
     public String getTruckName() {
