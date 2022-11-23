f85a7242ea4bb16cbe203907804561f201e6f2bc
Vladyslav Fesenko <vlad.11.fesenko@gmail.com>
2022-11-23 13:23:12 +0200

    main method implemented

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        Truck truck = new Truck();
 
+        Machine[] machines = new Machine[] {bulldozer, excavator, truck};
+
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
