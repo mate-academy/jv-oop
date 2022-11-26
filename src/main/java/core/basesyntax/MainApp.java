4f4239746a9b1d88cce9d5b200ca1bdaa59126a8
Maksym Tymoshchenko <timoschenkomaks123@gmail.com>
2022-11-26 18:44:48 +0200

    new commit

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,9 +1,9 @@
 package core.basesyntax;
 
-public class Main {
+public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Bulldozer(), new Excavator(), new Truck()};
-        for (Machine machine: machines) {
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
