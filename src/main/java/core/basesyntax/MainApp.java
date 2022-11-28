16caf287ff54c098e954551fc860303563b50831
Serhii Kopervas <sergkopervas@gmail.com>
2022-11-28 21:48:04 +0100

    It was performed by Serhii Kopervas on 11/28/22.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,12 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine[] machinesArray = {new Truck(), new Bulldozer(), new Excavator()};
+
+        for(Machine machine : machinesArray){
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
