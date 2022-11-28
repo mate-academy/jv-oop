b5bc6cb522f08af115a86a8c91f3913231feb9bb
Gleb Shevchenko <glieb.shevchenko@gmail.com>
2022-11-28 14:14:15 +0200

    Loop created

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,5 +2,15 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine machinery[] = new Machine[]{truck, excavator, bulldozer};
+        int count = 0;
+        while (count < machinery.length) {
+            machinery[count].doWork();
+            machinery[count].stopWork();
+            count++;
+        }
     }
 }
