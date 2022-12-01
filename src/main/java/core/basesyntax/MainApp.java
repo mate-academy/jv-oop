2b050bff6813e7a3e66821f0f725fb95d976651e
Serhii Kopervas <sergkopervas@gmail.com>
2022-12-01 18:27:48 +0100

    The third commit from Serhii Kopervas dated 12/01/22. Syntax error were fixed.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,7 +5,7 @@ public class MainApp {
     public static void main(String[] args) {
         Machine[] machinesArray = {new Truck(), new Bulldozer(), new Excavator()};
 
-        for(Machine machine : machinesArray){
+        for (Machine machine : machinesArray) {
             machine.doWork();
             machine.stopWork();
         }
