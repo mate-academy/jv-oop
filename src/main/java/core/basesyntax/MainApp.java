dafab0dc743262a4bac01c68b9826e4039abedcb
mykoladymchenko <mykola.dymchenko@insightsoftware.com>
2022-12-02 21:03:34 +0200

    Added: - abstract class Machine (abstract methods: doWork(), stopWork()) - classes Truck, Bulldozer, Excavator (every class overrides abstract methods from parent class Machine) - psvm to the Main class and call methods from the Machine array in loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,13 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machinesArray = {new Truck(), new Bulldozer(), new Excavator()};
 
+        for (Machine kindOfMachine: machinesArray
+             ) {
+            kindOfMachine.doWork();
+            kindOfMachine.stopWork();
+        }
+    }
 }
