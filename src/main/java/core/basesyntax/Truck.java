7dd4bd9341a64163182bc1a56677e52f428e936b
Volodymyr Gaievsky <mr.gvvd@gmail.com>
2022-12-03 13:06:16 +0200

    class Machine was created. Excavator, Bulldozer, Truck classes were created as extended from Machine class. doWork() & stopWork methods were overrvide in extended classes. In MainApp were created Machine[] & realized loops for doWork and stopWork().

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("A long road is waiting");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("I'm staying in a middle of nowhere, cause navigator is crashed");
+    }
+}
