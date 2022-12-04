a659ebef9424cf350ebe3d45fb7aa68b82a263d4
Ihor Vynogradskyi <np.logist@ukr.net>
2022-12-04 20:03:11 +0200

    add abstract kinds of Machine

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[] {
+            new Truck(), new Excavator(), new Buldozer()
+        };
+
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 
 }
