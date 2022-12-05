9c22e416eec572d9085e10e9e6f18f2d55cc8021
Gleb Shevchenko <glieb.shevchenko@gmail.com>
2022-12-05 14:56:11 +0200

    ÐFor loop instead of while

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,12 +5,10 @@ public class MainApp {
         Machine truck = new Truck();
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
-        Machine[] machinery = new Machine[]{truck, excavator, bulldozer};
-        int count = 0;
-        while (count < machinery.length) {
-            machinery[count].doWork();
-            machinery[count].stopWork();
-            count++;
+        Machine[] machines = new Machine[]{truck, excavator, bulldozer};
+        for (int i = 0; i < 3; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
         }
     }
 }
