46ab1fe893139cb11fda572f8aa68f4ed8a12f1f
Ihor Vynogradskyi <109805553+Avassor3000@users.noreply.github.com>
2022-12-05 13:08:51 +0200

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machines = new Machine[] {
+        Machine[] machines = {
             new Truck(), new Excavator(), new Buldozer()
         };
 
