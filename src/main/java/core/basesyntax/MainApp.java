a9b98215b6d2964a255b93cf677943319168da51
Oleksandr Kostiukevych <kaasnake2@gmail.com>
2022-12-06 20:50:53 +0200

    OOP Basic task Machines done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machineArray = new Machine[3];
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        machineArray[0] = truck;
+        machineArray[1] = excavator;
+        machineArray[2] = bulldozer;
+        for (Machine currentMachine : machineArray) {
+            currentMachine.doWork();
+            currentMachine.stopWork();
+        }
+    }
 }
