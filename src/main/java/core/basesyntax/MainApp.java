61c40d58c7a55e86ada8638d54c7ed5eab0cea2e
Denis Pugovkin <den.pugovkin@gmail.com>
2022-12-06 18:35:18 +0200

    First commit. All points of tasks have been done. Please check it.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
 public class MainApp {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 
+    public static void main(String[] args) {
+        Machine machine1 = new Truck();
+        Machine machine2 = new Excavator();
+        Machine machine3 = new Bulldozer();
+
+        Machine[] machines = new Machine[]{machine1, machine2, machine3};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
+
