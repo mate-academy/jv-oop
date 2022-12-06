a24300cd4910c6a11d13c733172b1a0dbf225764
Oleksandr Karpiuk <karpo99old@gmail.com>
2022-12-06 14:53:33 +0200

    Finish solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
+
         Machine[] array = new Machine[3];
         array[0] = new Truck();
         array[1] = new Bulldozer();
