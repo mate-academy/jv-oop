639c0bf040fece4d76b722995d49bb795de21373
Dmitriy Krasnopir <dmitriy.krasnopir@gmail.com>
2022-12-07 22:52:00 +0200

    app was changed

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,30 +1,7 @@
 package core.basesyntax;
 
 public abstract class Machine {
-    private int tankSize;
-    private String model;
+    public abstract void doWork();
 
-    public String doWork() {
-        return null;
-    }
-
-    public String stopWork() {
-        return null;
-    }
-
-    public int getTankSize() {
-        return tankSize;
-    }
-
-    public void setTankSize(int tankSize) {
-        this.tankSize = tankSize;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
+    public abstract void stopWork();
 }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -2,12 +2,12 @@ package core.basesyntax;
 
 public class Truck extends Machine {
     @Override
-    public String doWork() {
-        return "Truck started its work";
+    public void doWork() {
+        System.out.println("Truck started its work");
     }
 
     @Override
-    public String stopWork() {
-        return "Truck stopped its work";
+    public void stopWork() {
+        System.out.println("Truck stopped its work");
     }
 }
