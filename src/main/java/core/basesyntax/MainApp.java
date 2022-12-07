d7a44898a9aa86fd4572fa0acfa7b4e30d589002
Polina Lysytska <nikolavna100500@gmail.com>
2022-12-07 19:32:44 +0200

    tasks were implemented

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] arrayOfMachines = {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : arrayOfMachines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
