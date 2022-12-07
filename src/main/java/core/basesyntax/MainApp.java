a5ff38f470926dd8c549e7129e2198dc4db5882a
Дмитро <dima.smyk99.19@gmail.com>
2022-12-07 12:32:03 +0200

    Create abstract parent class with two methods. Created 3 heirs that implement its methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    private static Truck truck = new Truck();
+    private static Bulldozer bulldozer = new Bulldozer();
+    private static Excavator excavator = new Excavator();
+    private static Machine [] machines = {truck,bulldozer,excavator};
 
+    public static void main(String[] args) {
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
