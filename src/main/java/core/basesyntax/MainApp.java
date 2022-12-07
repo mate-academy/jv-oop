61734d2355106ffae725eb10ffbfa0dd403878f6
Oleksandr Kostiukevych <kaasnake2@gmail.com>
2022-12-07 09:09:35 +0200

    OOP Basic task Machines - Naming was fixed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,16 +2,16 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machineArray = new Machine[3];
+        Machine[] machines = new Machine[3];
         Machine truck = new Truck();
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
-        machineArray[0] = truck;
-        machineArray[1] = excavator;
-        machineArray[2] = bulldozer;
-        for (Machine currentMachine : machineArray) {
-            currentMachine.doWork();
-            currentMachine.stopWork();
+        machines[0] = truck;
+        machines[1] = excavator;
+        machines[2] = bulldozer;
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
