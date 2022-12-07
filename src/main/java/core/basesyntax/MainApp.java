4a21bbccd9088977af804ce97493a427919badc1
Your NameMykola <mykola.osolinskyi@gmail.com>
2022-12-07 16:33:55 +0100

    corrected machines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] mashines = {new Truck(), new Bulldozer(), new Excavator()};
-        for (Machine mashine : mashines) {
-            mashine.doWork();
-            mashine.stopWork();
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 
