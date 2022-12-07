a42183cc2161f36a1f316a6249bd98f4058ed975
Maksim Surzhik <kabaneri15@gmail.com>
2022-12-07 14:19:12 +0200

    Readme task done, 4 classes created, main class edited

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        Machine[] machines = {truck,bulldozer,excavator};
 
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
+
