b3f167e5e0f1a7bf9a885b18627a895a90f6334b
Pandoryms <andrei.hlebov21@gmail.com>
2022-12-08 15:15:39 +0200

    Task was complited

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Machine[] machines = new Machine[3];
+        machines[0] = new Truck();
+        machines[1] = new Bulldozer();
+        machines[2] = new Excavator();
+
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
+}
