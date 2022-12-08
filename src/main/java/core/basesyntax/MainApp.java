78390c3c507585694271d0423ce3ba078a6867b5
Artem Voitiuk <artemon2504@gmail.com>
2022-12-08 13:44:28 +0200

    homework oop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-
         Truck truck = new Truck();
         Bulldozer bulldozer = new Bulldozer();
         Excavator excavator = new Excavator();
