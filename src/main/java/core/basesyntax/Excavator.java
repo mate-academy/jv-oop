0e1453dd56b9d928ebf8af24160d6e6a58b04ca5
Andrii Yakhimets <andriy.yahimets@gmail.com>
2022-12-09 00:20:12 +0200

    Classes and Methods were created and implemented

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("The Excavator goes TRRRRR");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The Excavator stopped its work");
+    }
+}
