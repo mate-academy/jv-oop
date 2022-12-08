47927556988c3baeddb3fb3570c71db791ab3b9a
Olexiy <korm2909@gmail.com>
2022-12-08 23:26:48 +0200

    Working MainApp

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Excavator is working");
+        return;
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator has stopped");
+        return;
+    }
+}
