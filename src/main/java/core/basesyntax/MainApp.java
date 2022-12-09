346a7b15e885c76dfb3d9914b70bf1b147690b60
Oleg <96373763+Olegator191@users.noreply.github.com>
2022-12-09 12:00:37 +0200

    implemented task4

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,20 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-
     public static void main(String[] args) {
-
         Truck truck = new Truck();
         Bulldozer bulldozer = new Bulldozer();
         Excavator excavator = new Excavator();
 
         Machine[] machine = new Machine[]{truck, bulldozer, excavator};
-
         for (Machine machine1 : machine) {
-
             machine1.doWork();
             machine1.stopWork();
-
         }
     }
 }
