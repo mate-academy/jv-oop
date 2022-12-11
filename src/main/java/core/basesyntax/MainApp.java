1fce8cef66140522870dda6087bb0662d0429576
Denis Pugovkin <den.pugovkin@gmail.com>
2022-12-11 09:40:19 +0200

    Maven stylecheck, fixed.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,12 +5,10 @@ public class MainApp {
     public String toString() {
         return super.toString();
     }
-
     public static void main(String[] args) {
         Machine machine1 = new Truck();
         Machine machine2 = new Excavator();
         Machine machine3 = new Bulldozer();
-
         Machine[] machines = new Machine[]{machine1, machine2, machine3};
         for (Machine machine : machines) {
             machine.doWork();
@@ -18,4 +16,3 @@ public class MainApp {
         }
     }
 }
-
