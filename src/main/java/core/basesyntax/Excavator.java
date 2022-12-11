9da8b8f9d5bfbc8878472be49a931b086ab81989
vojtcekhovski <111798968+vojtcekhovski@users.noreply.github.com>
2022-12-11 09:40:56 +0000

    OOP Basic HomeWork.2

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
+
 public class Excavator extends Machine {
+    
     @Override
     public void doWork() {
         System.out.println("Excavator started work!");
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
+
 public class MainApp {
+    
     public static void main(String[] args) {
         Machine[] machines = {new Bulldozer(), new Excavator(), new Truck()};
         for (Machine machine : machines) {
