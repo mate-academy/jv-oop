d8a710dd9ea093c13ff377a9092fab17eee42423
Mik911 <911Sokol@gmail.com>
2022-12-12 17:49:21 +0200

    JC-OPP-opp

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Truck extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("truck started its work");
+    }
+
+    @Override
+    public void stopWork() {
+System.out.println("truck stopped its work");
+    }
+}
