1dae25d6036d8318d512f25a908cdd4eaf87e805
Sergii Gainovskyi <pandastars535gmail.com>
2022-12-12 19:38:52 +0200

    Class was created method was called

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 public abstract class Machine {
 
     public abstract void doWork();
+
     public abstract void stopWork();
 
 }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,15 +1,15 @@
 package core.basesyntax;
 
 public class Main {
-        public static void main(String[] args) {
-            Machine truck = new Truck();
-            Machine bulldozer = new Bulldozer();
-            Machine excavator = new Excavator();
-            Machine[] machines = new Machine[]{truck,bulldozer,excavator};
-            for (Machine m : machines){
-                m.doWork();
-                m.stopWork();
-            }
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machines = new Machine[]{truck,bulldozer,excavator};
+        for (Machine m : machines) {
+            m.doWork();
+            m.stopWork();
         }
     }
+}
 
