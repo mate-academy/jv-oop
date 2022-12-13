bb00551711693bc533f4a1e7c3e6073d62891982
Dmytro Voronin <d.voronin.owen@gmail.com>
2022-12-13 17:07:11 +0200

    comment on line 5

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,8 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machines = new Machine[]{new Excavator(), new Truck(), new Bulldozer()}; //create Machine array with Truck, Bulldozer and Excavator
+        //create Machine array with Truck, Bulldozer and Excavator
+        Machine[] machines = new Machine[]{new Excavator(), new Truck(), new Bulldozer()};
         for (int i = 0; i < machines.length; i++) {
             machines[i].doWork();
         }
