d87ac18812ec1a0b3e952ba0654c09ce5e8950ac
Dmytro Voronin <d.voronin.owen@gmail.com>
2022-12-13 15:36:25 +0200

    added ArrayTypeStyle

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine machine[] = new Machine[3];
+        Machine machine [] = new Machine [3];
         machine[0] = new Excavator();
         machine[1] = new Truck();
         machine[2] = new Bulldozer();
