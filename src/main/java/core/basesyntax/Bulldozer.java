62c98ec4542b458c15fd30f0a471a37fd02c58fa
Anatolii Nazarov <anatoly_n@ukr.net>
2022-12-15 13:13:05 +0200

    Created classes and methods for them

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+
+    public void doWork() {
+        System.out.println("Bulldozer machine started its work");
+    }
+
+    public void stopWork(){
+        System.out.println("Bulldozer machine stopped its work");
+    }
+}
