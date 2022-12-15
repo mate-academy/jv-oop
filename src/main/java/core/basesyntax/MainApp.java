90c8eee23b72e54443e6a65779cb3e225e3f21d8
Vlad Polikarpov <vladpolikarpovvp@gmail.com>
2022-12-15 10:23:18 +0200

    fixed tabulation problems

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,13 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
+
         Machine[] machineArray = new Machine[3];
 
         machineArray[0] = new Truck();
         machineArray[1] = new Buldozer();
         machineArray[2] = new Excavator();
 
-        for(Machine m : machineArray){
+        for (Machine m : machineArray) {
             m.doWork();
             m.stopWork();
         }
