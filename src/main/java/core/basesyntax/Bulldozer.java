66a42035a23797d0cfa778e15351b5d479eac1a5
Hryhorii Chernolutskyi <iamchgi1976@gmail.com>
2022-12-16 11:26:52 +0200

    Second try

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,4 +1,13 @@
 package core.basesyntax;
 
-public class Bulldozer {
+public class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println(this.getClass().getSimpleName() + " started its work.");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println(this.getClass().getSimpleName() + " stopped its work.");
+    }
 }
