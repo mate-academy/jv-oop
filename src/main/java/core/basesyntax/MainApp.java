15415a277af925fd75cbdfa8b64379ec69b5b7ee
Vlad Polikarpov <vladpolikarpovvp@gmail.com>
2022-12-16 13:29:19 +0200

    fixed name and tabulation problems

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,14 +4,12 @@ public class MainApp {
     public static void main(String[] args) {
 
         Machine[] machineArray = new Machine[3];
-
         machineArray[0] = new Truck();
         machineArray[1] = new Buldozer();
         machineArray[2] = new Excavator();
-
-        for (Machine m : machineArray) {
-            m.doWork();
-            m.stopWork();
+        for (Machine machine : machineArray) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
