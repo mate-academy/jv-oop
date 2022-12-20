76969eb4cf99eae449f68af63403ade581b009b0
Vadym Khodorovskyi <vadimhodor@gmail.com>
2022-12-20 17:01:19 +0200

    added fixes check list

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
-public class Bulldozer extends Machine{
+public class Bulldozer extends Machine {
     @Override
     public void doWork() {
         System.out.println("Bulldozer start work");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Bulldozer stop work");
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -5,6 +5,7 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator start work");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Excavato stop work");
