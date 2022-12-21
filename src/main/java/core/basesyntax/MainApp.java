6617144e5b3064dac6e89833daf2d13ac5d6a44b
Anna Melnychuk <annamelnichuk2512@gmail.com>
2022-12-21 13:55:47 +0200

    New solution, changed location of files

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
-
 public class MainApp {
+    public static void main(String[] args) {
+        Machine Truck = new Truck();
+        Machine Bulldozer = new Bulldozer();
+        Machine Excavator = new Excavator();
+        Machine[] machines = new Machine[]{Truck, Bulldozer, Excavator};
 
+        for (Machine value : machines) {
+            value.doWork();
+            value.stopWork();
+        }
+    }
 }
