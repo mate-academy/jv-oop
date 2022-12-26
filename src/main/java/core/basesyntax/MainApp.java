e431fcd4c7f6ea77abcd9d66cd6069a7f3d1e145
Igor Parovenko <igorparovenko@yahoo.com>
2022-12-26 21:21:46 +0100

    addedChangesAfterMentorSuggestions

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,11 +1,10 @@
 package core.basesyntax;
-
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machines = {new Bulldozer(),new Truck(),new Excavator()};
-        for (Machine list:machines) {
-            list.doWork();
-            list.stopWork();
+        Machine[] machines = {new Bulldozer(), new Truck(), new Excavator()};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
