3e0176b692e682e6b4848cca4175cd711a28a275
Igor Parovenko <igorparovenko@yahoo.com>
2022-12-26 19:17:30 +0100

    homeTaskForGitHub1

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machines = {new Bulldozer(),new Truck(),new Excavator()};
+        for (Machine list:machines) {
+            list.doWork();
+            list.stopWork();
+        }
+    }
 }
