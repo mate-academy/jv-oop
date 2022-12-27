0bd501e995d8056c2e725ef64666fc91ad3516aa
Artem Zaiarnyi <artem_zaiarnyi@epam.com>
2022-12-27 12:48:05 +0200

    Added classes and override methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,55 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
-    Truck truck = new Truck();
-    Bulldozer bulldozer = new Bulldozer();
-    Excavator excavator = new Excavator();
-    Machine [] machines = {truck,bulldozer,excavator};
 
-    for(Machine machine: machines) {
-        System.out.println(machine.stopWork());
-        System.out.println(machine.doWork());
-    }
+    public static void main(String[] args) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
 
-    public class Machine {
-        public abstract void doWork() {
-
-        }
-
-        public abstract void stopWork() {
-
+        for(Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 
-    public class Truck extends Machine {
-        @Override
-        public abstract void doWork() {
-            return "Truck started its work";
-        }
-        @Override
-        public abstract void stopWork() {
-            return "BulTruckldozer stoped its work";
-        }
-    }
-    public class Bulldozer extends Machine {
-        @Override
-        public abstract void doWork() {
-            return "Bulldozer started its work";
-        }
-        @Override
-        public abstract void stopWork() {
-            return "Bulldozer stoped its work";
-        }
-    }
 
-    public class Excavator extends Machine {
-        @Override
-        public abstract void doWork() {
-            return "Excavator started its work";
-        }
-        @Override
-        public abstract void stopWork() {
-            return "Excavator stoped its work";
-        }
-    }
+
+
 }
