83c7d6e0301bee1d4c5794f03c50a0ef19e1b764
gordgelin <gordgelin@s69a.su>
2022-12-27 03:20:40 +0200

    Initial commit.
    Created parent class Machine and all the child classes - Truck, Bulldozer, Excavator.
    
    Child classes' methods have been implemented
    
    psvm logic written and tested

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) throws InterruptedException {
+        Machine[] machines = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
+
+        for (Machine machine : machines) {
+            machine.doWork();
+
+            Thread.sleep(1000);
+
+            machine.stopWork();
+
+            Thread.sleep(1000);
+        }
+
+    }
 
 }
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Truck  extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Track is working!");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped working!");
+    }
+}
