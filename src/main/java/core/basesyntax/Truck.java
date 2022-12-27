c1d071645d87ab74c1f1c100aeca8d1f9cfc667a
Dmytro Nikolaienko <dmytronikolaienko@cloud.upwork.com>
2022-12-27 23:27:55 +0200

    jv_oop: fixing according to review

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
-    @java.lang.Override
+    @Override
     public void doWork() {
         System.out.println("Truck started it's work");
     }
 
-    @java.lang.Override
+    @Override
     public void stopWork() {
         System.out.println("Truck stopped it's work");
     }
