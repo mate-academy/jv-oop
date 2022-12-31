10a3f7d2d4d0a2091cfe2b31f26b8bb326371ff8
Denys Dominskyi <dominskyj@gmail.com>
2022-12-31 20:49:13 +0200

    solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Mashine[] arr = {new Excavator(), new Bulldozer(), new Truck()};
+        for (Mashine mashine : arr) {
+            mashine.doWork();
+            mashine.stopWork();
+        }
+    }
 }
---
+++ src/main/java/core/basesyntax/Mashine.java
@@ -0,0 +1,7 @@
+package core.basesyntax;
+
+public abstract class Mashine {
+    public abstract void doWork();
+
+    public abstract void stopWork();
+}
