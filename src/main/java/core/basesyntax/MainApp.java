11b8db3c7bd4e8a6ad55dc30c7274da45297f0e9
Yaroslav Ometiukh <yaroslav.ometiukh@outlook.com>
2022-12-31 13:06:19 +0200

    Created class Machine and clases that extend it. Implement methods on those clases;

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine volvoT = new Truck();
+        Machine volvoB = new Bulldozer();
+        Machine volvoE = new Excavator();
+        Machine[] technics = {volvoT, volvoB, volvoE};
 
+        for (Machine tech : technics
+        ) {
+            tech.doWork();
+            tech.stopWork();
+        }
+    }
 }
