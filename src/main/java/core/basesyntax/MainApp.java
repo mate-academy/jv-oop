6ea4a235b43a6aed74bc7da3cd4008b1f6c6f3fa
yermilov.m <yermilov.m@hotmail.com>
2023-01-02 02:12:05 +0100

    created Machine array and methods doWork aand stopWork called in a loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] workGroup = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+        for (int i = 0; i < workGroup.length * 2; i++) {
+            if(i < workGroup.length) workGroup[i].doWork();
+            else workGroup[i - workGroup.length].stopWork();
+        }
+    }
 }
