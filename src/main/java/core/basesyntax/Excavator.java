3824c36c1573beb5ecea59de93628b1dd2744619
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-01-03 00:30:15 +0200

    created classes Truck,Bulldozer, Excavator and methods, fixed

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,5 +1,6 @@
 package core.basesyntax;
-public class Excavator extends Machine{
+
+public class Excavator extends Machine {
     @Override
     public void doWork() {
         System.out.println("The excavator started its work");
