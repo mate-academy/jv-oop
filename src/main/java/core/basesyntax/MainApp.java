afa113200e672e8be7a7ea7844c89220a4f9d5e9
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-01-02 23:42:20 +0200

    created classes Truck,Bulldozer, Excavator and methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machine = new Machine[3];
+        machine[0] = new Truck();
+        machine[1] = new Bulldozer();
+        machine[2] = new Excavator();
+
+        for (int i = 0; i < machine.length; i++) {
+            machine[i].doWork();
+            machine[i].stopWork();
+        }
+    }
 
 }
