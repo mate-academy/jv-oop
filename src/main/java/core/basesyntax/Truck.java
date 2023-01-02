e35b756acdc90446a08084f53355fba60f959820
Vladyslav-Voronkov <vladyslavvoronkov@gmail.com>
2023-01-02 17:39:17 +0100

    - Created class Machine.java - Created classes: Truck.java, Bulldozer.java, Excavator.java that`s inherited from Machine.java - Overrided methods doWork() and stopWork() and they will print messages. - In MainApp.java created Machine, Truck, Bulldozer and Excavator and called methods doWork() and stopWork() in a loop;

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Truck extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("Truck is working now");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck was stopped");
+    }
+}
