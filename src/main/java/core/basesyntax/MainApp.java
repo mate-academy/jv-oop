f77a43415ce607c5f9b53a6f028c3b100e24b645
yermilov.m <yermilov.m@hotmail.com>
2023-01-02 19:51:47 +0100

    mvn clean package command done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,8 +4,11 @@ public class MainApp {
     public static void main(String[] args) {
         Machine[] workGroup = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
         for (int i = 0; i < workGroup.length * 2; i++) {
-            if(i < workGroup.length) workGroup[i].doWork();
-            else workGroup[i - workGroup.length].stopWork();
+            if (i < workGroup.length) {
+                workGroup[i].doWork();
+            } else {
+                workGroup[i - workGroup.length].stopWork();
+            }
         }
         System.out.println("Work is done");
     }
