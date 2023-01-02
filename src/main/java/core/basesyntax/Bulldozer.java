77aa640e2984f9826f6e075e9d554718a90ce908
yermilov.m <yermilov.m@hotmail.com>
2023-01-02 01:35:25 +0100

    overrided doWork() in Machine child classes

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,4 +1,9 @@
 package core.basesyntax;
 
 public class Bulldozer extends Machine{
+
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer " + this + " started");
+    }
 }
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,4 +1,8 @@
 package core.basesyntax;
 
 public class Excavator extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("Excavator " + this + " started");
+    }
 }
