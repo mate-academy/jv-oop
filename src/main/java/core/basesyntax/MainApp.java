d7e7af83e9c31a211ddb191435a16c56d0259090
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-01-03 22:10:47 +0200

    created classes Truck,Bulldozer, Excavator and methods, fixed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,14 +2,11 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machine = new Machine[3];
-        machine[0] = new Truck();
-        machine[1] = new Bulldozer();
-        machine[2] = new Excavator();
+        Machine[] arrayMachines = {new Bulldozer(), new Truck(), new Excavator()};
 
-        for (int i = 0; i < machine.length; i++) {
-            machine[i].doWork();
-            machine[i].stopWork();
+        for (Machine machine : arrayMachines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 
