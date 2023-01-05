5296225bc8c609a80de92ae5d05e22ec5c9d19e2
kromch <94765048+kromch@users.noreply.github.com>
2023-01-05 19:13:13 +0200

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,12 +1,50 @@
 package core.basesyntax;
 //abstract class
-public abstract class Machine {
-	
+abstract class Machine {
 	//abstract methods
 	public abstract void doWork();
-	public abstract void doWork();
+	public abstract void stopWork();
+}
+class Truck extends Machine {
+	@Override
+	public void doWork() {
+	    System.out.println(Truck.class.getSimpleName()+" now working");
+	}
 	
+	@Override
+	public void stopWork() {
+	    System.out.println(Truck.class.getSimpleName()+" now stopped");
+	}
+}
+class Bulldozer extends Machine {
+	@Override
+	public void doWork() {
+	    System.out.println(Bulldozer.class.getSimpleName()+" now working");
+	}
+	
+	@Override
+	public void stopWork() {
+	    System.out.println(Bulldozer.class.getSimpleName()+" now stopped");
+	}
+}
+class Excavator extends Machine {
+	@Override
+	public void doWork() {
+	    System.out.println(Excavator.class.getSimpleName()+" now working");
+	}
+	
+	@Override
+	public void stopWork() {
+	    System.out.println(Excavator.class.getSimpleName()+" now stopped");
+	}
 }
 public class MainApp {
+    public static void main(String args[]) {
+        Machine []machs = {new Truck(),new Bulldozer(),new Excavator()};
+        for(Machine m:machs) {
+            m.doWork();
+            m.stopWork();
+        }
+   }
 
 }
