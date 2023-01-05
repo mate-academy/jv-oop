9eb603417562b902c48792c2e5a258cb567d2a0b
kromch <94765048+kromch@users.noreply.github.com>
2023-01-05 12:47:00 +0200

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
-
+//abstract class
+public abstract class Machine {
+	
+	//abstract methods
+	public abstract void doWork();
+	public abstract void doWork();
+	
+}
 public class MainApp {
 
 }
