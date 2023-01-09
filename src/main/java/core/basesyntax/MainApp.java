46b113f2037755bd081eebc34eca8eb25cabb849
Oleksii Klymenko <alexeyklimenkowork@gmail.com>
2023-01-09 17:58:54 +0200

    Created Excavator, Bulldozer, Truck and Machine. All functions work in MainApp.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,17 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+
+        Machine[] machines = new Machine[3];
+        machines[0] = new Bulldozer();
+        machines[1] = new Excavator();
+        machines[2] = new Truck();
+
+        for (Machine each : machines) {
+            each.doWork();
+            each.stopWork();
+        }
+
+    }
 }
