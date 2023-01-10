0dc79dfc7b7f1617134af42a4b0de81c64ead6eb
kromch <94765048+kromch@users.noreply.github.com>
2023-01-10 13:22:18 +0200

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,64 +1,57 @@
-package core.basesyntax;
-
 // abstract class
-
-abstract class Machine {
-
-    // abstract methods
-    public abstract void doWork();
-    
-    public abstract void stopWork();
-
-}
-
-class Truck extends Machine {
-    
-    @Override
-    public void doWork() {
-        System.out.println(Truck.class.getSimpleName() + " now working");
-    }
-
-    @Override
-    public void stopWork() {
-        System.out.println(Truck.class.getSimpleName() + " now stopped");
-    }
-}
-
-class Bulldozer extends Machine {
-    
-    @Override
-    public void doWork() {
-        System.out.println(Bulldozer.class.getSimpleName() + " now working");
-    }
-
-    @Override
-    public void stopWork() {
-        System.out.println(Bulldozer.class.getSimpleName() + " now stopped");
-    }
-}
-
-class Excavator extends Machine {
-    
-    @Override
-    public void doWork() {
-        System.out.println(Excavator.class.getSimpleName() + " now working");
-    }
-
-    @Override
-    public void stopWork() {
-        System.out.println(Excavator.class.getSimpleName() + " now stopped");
-    }
-}
-
 public class MainApp {
 
-    public static void main(String []args) {
-        Machine []machs = {
-            new Truck(),
-            new Bulldozer(),
-            new Excavator()
-        };
-        for (Machine m: machs) {
+    abstract class Machine {
+        // abstract methods
+        public abstract void doWork();
+        public abstract void stopWork();
+    }
+
+    class Truck extends Machine {
+        @Override
+        public void doWork() {
+            System.out.println(Truck.class.getSimpleName() + " now working");
+        }
+
+        @Override
+        public void stopWork() {
+            System.out.println(Truck.class.getSimpleName() + " now stopped");
+        }
+    }
+
+    class Bulldozer extends Machine {
+        @Override
+        public void doWork() {
+            System.out.println(Bulldozer.class.getSimpleName() + " now working");
+        }
+
+        @Override
+        public void stopWork() {
+            System.out.println(Bulldozer.class.getSimpleName() + " now stopped");
+        }
+    }
+
+    class Excavator extends Machine {
+        @Override
+        public void doWork() {
+            System.out.println(Excavator.class.getSimpleName() + " now working");
+        }
+
+        @Override
+        public void stopWork() {
+            System.out.println(Excavator.class.getSimpleName() + " now stopped");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        MainApp mo = new MainApp();
+        MainApp.Machine[] machs = new MainApp.Machine[3];
+
+        machs[0] = mo.new Truck();
+        machs[1] = mo.new Bulldozer();
+        machs[2] = mo.new Excavator();
+        for (MainApp.Machine m: machs) {
             m.doWork();
             m.stopWork();
         }
