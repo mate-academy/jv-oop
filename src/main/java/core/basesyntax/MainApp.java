410ec2e16d25a09c19dda940c98a10926b38bdb9
Maxim Skory <devisrush@gmail.com>
2023-01-12 01:54:32 +0200

    psvm was added where created Machine array which contains one instance each of Bulldozer, Excavator and Truck.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine [] machinery = {new Bulldozer(), new Excavator(), new Truck()};
+    }
 }
