3547be7287f41d5bdee0c2846dcdb3aa3e2b178b
Maxim Skory <devisrush@gmail.com>
2023-01-12 02:08:28 +0200

    Machines working logic was added

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,5 +3,9 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine [] machinery = {new Bulldozer(), new Excavator(), new Truck()};
+        for (Machine machine : machinery){
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
