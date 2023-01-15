861b27108c85dbdc983424973534eaee925b2a90
IhorPetrytskyi333 <mygit333@gmail.com>
2023-01-15 12:52:42 -0800

    update2

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,12 +1,13 @@
 package core.basesyntax;
+
 public class MainApp {
     public static void main(String[] args) {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-        Machine[] machines = new Machine[] {truck, bulldozer, excavator};
+        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
 
-        for (Machine mach : machines ) {
+        for (Machine mach : machines) {
             mach.doWork();
             mach.stopWork();
         }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -5,7 +5,6 @@ public class Truck extends Machine {
     public void doWork() {
         System.out.println("Truck started its work");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Truck stopped work");
