99bc9dfa4e85c6356ff2b035089ae507f0c0dd3e
Nataliia <ladna1984@gmail.com>
2023-01-21 21:22:47 +0200

    In MainApp.java added psvm and added array machine. In Machine.java added two abstract methods. In Truck.java, Excavator.java and in Bulldozer.java added sout.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Excavator excavator = new Excavator();
+        Bulldozer bulldozer = new Bulldozer();
 
+        Machine [] machine = new Machine[] { truck, bulldozer, excavator};
+        for (int i = 0; i < machine.length; i++) {
+            machine[i].doWork();
+            machine[i].stopWork();
+
+        }
+    }
 }
