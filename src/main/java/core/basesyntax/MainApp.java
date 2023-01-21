cde2ad9f614f828e0835a6764f60f25187ee0394
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-21 15:23:36 +0200

    Added psvm method and created Machine array

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] array = new Machine[3];
+        array[0] = new Bulldozer();
+        array[1] = new Truck();
+        array[2] = new Excavator();
+    }
 
 }
