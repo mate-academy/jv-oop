4c5531129f60fa23d7d75f5a957bd64451c597e8
Roman Morozov <morozov.ctk@gmail.com>
2023-01-26 20:16:01 +0200

    Solution

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -6,7 +6,6 @@ public class Truck extends Machine {
     public void doWork() {
         System.out.println("Truck start working");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Truck stop working");
