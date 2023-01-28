235e04f014e7264a636519acba5a6af242e684f2
Сергей <serhii.serdiuk@gmail.com>
2023-01-28 22:10:21 +0200

    solution fo jv-oop task

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,42 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine[] machines = {excavator, truck, bulldozer};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
+}
+    abstract class Machine {
+        public abstract void doWork();
+        public abstract void stopWork();
+    }
+    class Excavator extends Machine{
+        @Override
+        public void doWork() {
+    }
+        @Override
+        public void stopWork() {
+    }
+}
+    class Truck extends Machine{
+        @Override
+        public void doWork() {
+    }
+        @Override
+        public void stopWork() {
+    }
+}
+    class Bulldozer extends Machine{
+        @Override
+        public void doWork() {
+    }
+        @Override
+        public void stopWork() {
+    }
 }
