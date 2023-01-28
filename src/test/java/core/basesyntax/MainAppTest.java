5b467e52718e6bbca8bf96a97ec0784118934dd3
potrap <potrapmax@gmail.com>
2023-01-28 22:56:01 +0200

    added 4 classes

--- src/test/java/core/basesyntax/MainAppTest.java
+++ src/test/java/core/basesyntax/MainAppTest.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainAppTest {
-
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[]{new Bulldozer(), new Excavator(), new Truck()};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
