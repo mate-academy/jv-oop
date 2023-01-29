6f61cdf2301271d976bb9cdd8a8ce84f6b510c44
Сергей <serhii.serdiuk@gmail.com>
2023-01-29 14:34:54 +0200

    Delete unnecessary lines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public class MainApp {
-
     public static void main(String[] args) {
         Machine[] machines = {new Excavator(), new Truck(), new Bulldozer()};
         for (Machine machine : machines) {
