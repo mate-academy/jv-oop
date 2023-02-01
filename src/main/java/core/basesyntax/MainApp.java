eda512b71ceaf3ace3bd61294b49d19a829a584c
Vasyl Sagaidak <saikopixie@gmail.com>
2023-02-01 12:12:11 +0200

    All necessary classes been created

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,10 @@
 package core.basesyntax;
 
+import core.basesyntax.machines.Bulldozer;
+import core.basesyntax.machines.Excavator;
+import core.basesyntax.machines.Machine;
+import core.basesyntax.machines.Truck;
+
 public class MainApp {
 
 }
