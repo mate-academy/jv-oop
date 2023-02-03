299d2bf9c65dc3fc32b06a037c7d08bee3d33228
WallRun <dimonty2003@gmail.com>
2023-02-03 20:53:51 +0200

    Fixed indentation

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,16 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
-  public static void main(String[] args) {
-    Truck truck = new Truck();
-    Excavator excavator = new Excavator();
-    Bulldozer bulldozer = new Bulldozer();
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Excavator excavator = new Excavator();
+        Bulldozer bulldozer = new Bulldozer();
 
-    Machine[] machines = new Machine[]{truck, bulldozer, excavator};
+        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
 
-    for (Machine machine : machines) {
-      machine.doWork();
-      machine.stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
-  }
 }
