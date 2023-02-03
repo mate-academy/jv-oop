6369576f2b6afcf3817941944e13998f54e25583
WallRun <dimonty2003@gmail.com>
2023-02-03 20:42:48 +0200

    Initial commit

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+  public static void main(String[] args) {
+    Truck truck = new Truck();
+    Excavator excavator = new Excavator();
+    Bulldozer bulldozer = new Bulldozer();
 
+    Machine[] machines = new Machine[]{truck, bulldozer, excavator};
+
+    for (Machine machine : machines) {
+      machine.doWork();
+      machine.stopWork();
+    }
+  }
 }
