26b98c424db2290a1947761e63d2e9908be370a4
kromch <94765048+kromch@users.noreply.github.com>
2023-02-06 22:15:46 +0200

    Update Excavator.java

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,16 +1,4 @@
-package core.vaseclasses; 
-
-public class Bulldozer extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println(Bulldozer.class.getSimpleName() + " now working");
-    }
-
-    @Override
-    public void stopWork() {
-        System.out.println(Bulldozer.class.getSimpleName() + " now stopped");
-    }
-}
+package core.baseclasses; 
 
 class Excavator extends Machine {
     @Override
