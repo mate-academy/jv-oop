d3b269170f1a3a06fe133af8f2acf377d03d0e7a
Sergiy Golubchenko <galuba1@gmail.com>
2023-02-06 16:43:02 +0200

    2 method was added to all classes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machinesArray = new Machine[3];
+        machinesArray[0] = new Bulldozer();
+        machinesArray[1] = new Excavator();
+        machinesArray[2] = new Truck();
+        for (Machine m : machinesArray) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
 }
