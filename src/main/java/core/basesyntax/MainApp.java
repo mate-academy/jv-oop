18460339fa888258a8ad936b14ff1789b3e43b32
kyrylh <kyryl.huliuk@gmail.com>
2023-02-06 16:13:36 +0200

    Fixed checkstyle

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,11 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machine = {new Truck(), new Bulldozer(), new Excavator()};
-
-        for (Machine m: machine) {
-            m.doWork();
-            m.stopWork();
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
