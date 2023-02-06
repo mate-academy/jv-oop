2224426826a22b1ec88d301effcc80175e2d7257
Denys Dominskyi <108018143+Dominskyi93@users.noreply.github.com>
2023-02-06 17:25:33 +0200

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Mashine[] arr = {new Excavator(), new Bulldozer(), new Truck()};
-        for (Mashine mashine : arr) {
+        Mashine[] machines = {new Excavator(), new Bulldozer(), new Truck()};
+        for (Mashine mashine : machines) {
             mashine.doWork();
             mashine.stopWork();
         }
