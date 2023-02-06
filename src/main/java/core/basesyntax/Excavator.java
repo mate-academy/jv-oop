266a4047abe260901b91b85bdb34efc6b85a8e9b
Ivan Bakhmet <bahmetvanya1290@gmail.com>
2023-02-06 17:26:08 +0200

    completed task

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Excavator starting it's work");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator ended it's work");
+    }
+}
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck starting it's work");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Truck ended it's work");
+    }
+}
