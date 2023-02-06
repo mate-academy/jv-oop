a6a47c1dd7077483c60199a0fe8623eb58f52f44
kromch <94765048+kromch@users.noreply.github.com>
2023-02-06 22:06:27 +0200

    Create Machine.java

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,9 @@
+package core.baseclasses;
+
+// using abstract class
+public abstract class Machine {
+    // abstract methods
+    public abstract void doWork();
+
+    public abstract void stopWork();
+}
