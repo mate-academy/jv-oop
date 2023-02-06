4bed7324369276d67576f2b7df14ebe007f19b04
Sergiy Golubchenko <galuba1@gmail.com>
2023-02-06 17:28:48 +0200

    fix program build on mentor revue

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,13 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machinesArray = new Machine[3];
-        machinesArray[0] = new Bulldozer();
-        machinesArray[1] = new Excavator();
-        machinesArray[2] = new Truck();
-        for (Machine m : machinesArray) {
-            m.doWork();
-            m.stopWork();
+        Machine[] machines = {new Bulldozer(), new Excavator(), new Truck()};
+        for (Machine machin : machines) {
+            machin.doWork();
+            machin.stopWork();
         }
     }
 }
