285d608f6c1d0a4423cd8f56ea267d66af89a778
Skrudj <ssinilov@gmail.com>
2023-02-06 12:07:32 +0200

    fixed iteration var naming in main

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Bulldozer(), new Excavator(), new Truck()};
-        for (Machine i: machines) {
-            i.doWork();
-            i.stopWork();
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
