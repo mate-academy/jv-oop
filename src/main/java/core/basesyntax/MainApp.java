338f9f57cbdead8a39cc8fc3b1be1d2f7a362430
kromch <94765048+kromch@users.noreply.github.com>
2023-02-06 22:18:09 +0200

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,48 +1,9 @@
 package core.basesyntax;
 
-// using abstract class
-abstract class Machine {
-    // abstract methods
-    public abstract void doWork();
-
-    public abstract void stopWork();
-}
-
-class Truck extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println(Truck.class.getSimpleName() + " now working");
-    }
-
-    @Override
-    public void stopWork() {
-        System.out.println(Truck.class.getSimpleName() + " now stopped");
-    }
-}
-
-class Bulldozer extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println(Bulldozer.class.getSimpleName() + " now working");
-    }
-
-    @Override
-    public void stopWork() {
-        System.out.println(Bulldozer.class.getSimpleName() + " now stopped");
-    }
-}
-
-class Excavator extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println(Excavator.class.getSimpleName() + " now working");
-    }
-
-    @Override
-    public void stopWork() {
-        System.out.println(Excavator.class.getSimpleName() + " now stopped");
-    }
-}
+import core.baseclasses.Bulldozer;
+import core.baseclasses.Excavator;
+import core.baseclasses.Machine;
+import core.baseclasses.Truck;
 
 public class MainApp {
     public static void main(String[] args) {
