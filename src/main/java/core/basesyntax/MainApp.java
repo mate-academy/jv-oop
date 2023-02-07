5365d4e0ada5fdf9165cded5d626946bd608ef19
“Ymishok321 <kkoiku3@gmail.com>
2023-02-08 00:04:13 +0200

    Necessary classes are created and used in MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.classes.Bulldozer;
+import core.basesyntax.classes.Excavator;
+import core.basesyntax.classes.Machine;
+import core.basesyntax.classes.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machines = new Machine[] {truck, bulldozer, excavator};
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
