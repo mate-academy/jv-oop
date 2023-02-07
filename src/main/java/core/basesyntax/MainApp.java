8100a12bbba4cc66a0938c6f72895d9831269b32
kromch <94765048+kromch@users.noreply.github.com>
2023-02-07 13:05:39 +0200

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -7,8 +7,7 @@ import core.baseclasses.Truck;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machs = {new Truck(), new Bulldozer(), new Excavator()};
-        
+        Machine[] machs = {new Truck(), new Bulldozer(), new Excavator()};   
         for (Machine m: machs) {
             m.doWork();
             m.stopWork();
