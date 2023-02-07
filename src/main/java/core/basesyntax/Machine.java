4a8c67066628ea9c6a096ff7738dee7e77098796
kromch <94765048+kromch@users.noreply.github.com>
2023-02-07 13:03:55 +0200

    Update Machine.java

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -2,7 +2,6 @@ package core.baseclasses;
 
 // using abstract class
 public abstract class Machine {
-    // abstract methods
     public abstract void doWork();
 
     public abstract void stopWork();
