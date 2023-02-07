b38236679654dea8dbeb19c645e580fe6aaee5e3
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-02-08 00:47:46 +0200

    fixed codestyle

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,9 +4,9 @@ public class MainApp {
 
     public static void main(String[] args) {
         Machine[] heavyMachinesArray = {new Excavator(), new Bulldozer(), new Truck()};
-        for(Machine machine : heavyMachinesArray){
-        machine.doWork();
-        machine.stopWork();
+        for (Machine machine : heavyMachinesArray) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class Truck extends Machine{
+public class Truck extends Machine {
     @Override
     public void doWork() {
         System.out.printf("%s is starting!\n", getClass().getSimpleName());
