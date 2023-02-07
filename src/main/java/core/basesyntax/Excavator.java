5a7c8c7a2468c6b5bde538c9a0bb4d8c3bdf6109
Artem <artempharm83@gmail.com>
2023-02-07 18:35:34 +0200

    A Machine class is created containing the public abstract void doWork() and public abstract void stopWork() methods. The classes Truck, Bulldozer, Excavator are created, which are inherited from Machine. These classes override doWork() so that it prints a message that the specified machine has started its work. The stopWork() function has also been redefined. It prints messages that a certain machine has stopped its work. In the MainApp class, I created an array of cars with a truck, a bulldozer, and an excavator, and called the doWork() and stopWork() methods in a loop.

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+    @Override
+    public  void doWork(){
+        System.out.println("Excavator: Started its work.");
+    }
+    @Override
+    public  void stopWork(){
+        System.out.println("Excavator: Stopped its work.");
+    }
+}
