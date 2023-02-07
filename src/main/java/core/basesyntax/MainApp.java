adff1c91dbcaaff0ba2dd73b8cec5a3759b04386
Yaroslav Chornyi <chorniy.j@gmail.com>
2023-02-07 18:43:09 +0200

    Added psvm, create 3 objects of classes Bulldozer, Excavator, Truck then create array of this objects and implemented loop with methods doWork and stopWork.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,16 +3,13 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
 
-
         Machine truck = new Truck();
-        Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-        Machine[] machines = new Machine[]{excavator, bulldozer, truck};
+        Machine bulldozer = new Bulldozer();
+        Machine[] machines = new Machine[]{excavator, truck, bulldozer};
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
-
-
         }
     }
 }
