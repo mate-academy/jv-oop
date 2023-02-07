5928cd926d637fa7b0beae7fb5a2c8fea93edbb2
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2023-02-07 09:53:59 +0200

    created classes Bulldozer, Excavator, Truck, Machine; implemedted methods doWork, stopWork

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machinePark = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : machinePark) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
