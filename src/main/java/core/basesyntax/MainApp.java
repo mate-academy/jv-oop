6c437ee5fb8b021dbc965367a87c8220875f307e
De1eF <jatfun58@gmail.com>
2023-02-07 17:57:59 +0200

    Added Bulldozer, Excavator and Truck classes which inherit Machine class. Abstract methods from Machine class doWork(), stopWork() are called from MainApp.main.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
+    private static final Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
 
+    public static void main(String[] args) {
+        for (Machine m : machines) {
+            m.doWork();
+        }
+        for (Machine m : machines) {
+            m.stopWork();
+        }
+    }
 }
