43084a980432d0a62cf949adfcd39684a32e4a2d
Maksym <maxim.yashyn@gmail.com>
2023-02-08 19:34:13 +0200

    override and implement a stopWork abstract method in Truck, Bulldozer, Excavator classes

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,6 +3,11 @@ package core.basesyntax;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Bulldozer is working");
+        System.out.println("Excavator is working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator is stop working");
     }
 }
