f5091be582d1211fd70f279dc44d49b94dbac56f
yuriy49 <yuriy121991@gmail.com>
2023-02-08 21:37:01 +0200

    Try to avoid common mistakes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,9 +5,9 @@ public class MainApp {
 
   public static void Main() {
 
-    Excavator excavator = new Excavator();
-    Bulldozer bulldozer = new Bulldozer();
-    Truck truck = new Truck();
+    Machine excavator = new Excavator();
+    Machine bulldozer = new Bulldozer();
+    Machine truck = new Truck();
 
     Machine [] machines = new Machine[] {truck, bulldozer, excavator};
 
