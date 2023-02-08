764c0ed834e0cfce2b93dc73dbaa3d4ffaad233c
Mykyta <nikita.nikonets@gmail.com>
2023-02-08 14:14:54 +0200

    style fix

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -6,7 +6,6 @@ public class MainApp {
         machines[0] = new Truck();
         machines[1] = new Bulldozer();
         machines[2] = new Excavator();
-
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
