fa6bda667d8126c312943ec9d5f9e0999b3c24e1
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-02-08 19:49:14 +0200

    Implemented solution to homework

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machine = {(new Excavator()), (new Truck()), (new Bulldozer())};
 
+        for (Machine machineCheck : machine) {
+            machineCheck.doWork();
+        }
+        for (Machine machineCheck : machine) {
+            machineCheck.stopWork();
+        }
+    }
 }
