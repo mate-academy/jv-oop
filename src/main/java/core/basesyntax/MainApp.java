df6a6fced00d90c017fd60e29b28d222bb4e3e61
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-02-08 15:22:56 +0200

    create Machine array and call overrides methods in loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine [] machinesArray = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : machinesArray) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 
 }
