1dfaefb70a4d74ce2ac8dbd2e22693943237a82b
yuriy49 <yuriy121991@gmail.com>
2023-02-08 23:15:51 +0200

    incorrect indentation level

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
-      System.out.println("Excavator start work");
+        System.out.println("Excavator start work");
     }
 
     @Override
     public void stopWork() {
-      System.out.println("Excavator stop work");
+        System.out.println("Excavator stop work");
     }
 }
