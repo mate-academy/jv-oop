60280ebcb528a9520154d86b6d63a24f14c6faec
Yaroslav Chornyi <123074518+jaroslavcyk@users.noreply.github.com>
2023-02-08 12:20:09 +0200

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-
         Machine truck = new Truck();
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
