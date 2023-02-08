8a79b2e8dae3886922ed6595c993e95421fd4dbe
Sanyk Volodymyr <sanikvladimir@gmail.com>
2023-02-08 16:54:45 +0200

    added solution: Machine, Excavator, Bulldozer, Truck classes implemented

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        Machine[] arrayOfMachines = {excavator, bulldozer, truck};
+        for (Machine machine : arrayOfMachines) {
+            machine.doWork();
+            machine.stopWork();
+        }
 
+    }
 }
