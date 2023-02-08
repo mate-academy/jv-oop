85e2d360e19d55d781e0200fd5245555937851f7
Nazar Zvarych <zvarych45@gmail.com>
2023-02-08 12:05:01 +0200

    I fixed, all mistakes

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 public class Bulldozer extends Machine {
-    @Override public void doWork() {
+    @Override
+    public void doWork() {
         System.out.println("Bulldozer started its work");
     }
 
+    @Override
     public void stopWork() {
         System.out.println("Truck stopped its work");
     }
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 public class Excavator extends Machine {
-    @Override public void doWork() {
+    @Override
+    public void doWork() {
         System.out.println("Excavator started its work");
     }
 
+    @Override
     public void stopWork() {
         System.out.println("Truck stopped its work");
     }
