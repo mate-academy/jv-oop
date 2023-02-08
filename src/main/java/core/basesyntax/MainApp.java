6582e09888171cdf857f667566b93e8e45170f8c
Oleksandr Krasnov <redman2775@gmail.com>
2023-02-08 21:07:07 +0200

    4 classes added; 2 methods added to class Machine; Methods overriding in inheriting classes; Array in MainApp created; Methods called in a loop; "mvn clean package" return "BUILD SUCCES"

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
-public class MainApp {
+import machines.Bulldozer;
+import machines.Excavator;
+import machines.Machine;
+import machines.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[] {new Excavator(), new Bulldozer(), new Truck()};
+
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
