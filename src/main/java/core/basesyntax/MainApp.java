781d8e74cfc06851533201b24e78a720c49b6645
Nazar Zvarych <zvarych45@gmail.com>
2023-02-08 15:29:56 +0200

    I fixed, all mistakes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main() {
-        Machine[] machines = new Machine[]{new Bulldozer(),new Truck(),new Excavator()};
+        Machine[] machines = new Machine[] {new Bulldozer(), new Truck(), new Excavator()};
         for (int i = 0; i < machines.length; i++) {
             machines[i].doWork();
             machines[i].stopWork();
