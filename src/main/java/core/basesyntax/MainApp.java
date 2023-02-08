38de288ec4475e67307fdf4527701d4e151fb180
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2023-02-09 00:47:34 +0200

    aded solution for task and passed check style

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
 
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
 }
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck is working right now!");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck is stopped work right now!");
+    }
+}
