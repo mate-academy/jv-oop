bffde2e9faf5b686c7f14ce8073d949c582e9f0e
Sanyk Volodymyr <sanikvladimir@gmail.com>
2023-02-09 12:45:29 +0200

    rewrited and readded all classes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,8 +5,8 @@ public class MainApp {
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
-        Machine[] arrayOfMachines = {excavator, bulldozer, truck};
-        for (Machine machine : arrayOfMachines) {
+        Machine[] machines = new Machine[]{excavator, bulldozer, truck};
+        for (Machine machine:machines) {
             machine.doWork();
             machine.stopWork();
         }
