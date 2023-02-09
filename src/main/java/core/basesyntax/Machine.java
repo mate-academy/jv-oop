c385e8b24e3d0bc7a2da5934c4d8c82ed4ec76f4
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2023-02-09 12:01:51 +0200

    fixed methods doWork(), stopWork()

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,9 +1,7 @@
 package core.basesyntax;
 
 public abstract class Machine {
-    public void doWork() {
-    }
+    public abstract void doWork();
 
-    public void stopWork() {
-    }
+    public abstract void stopWork();
 }
