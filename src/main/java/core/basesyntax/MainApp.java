8e2032f3769027bdc9c5d67f3d21c9e1c5b6aae0
Vitalii Fedychkanych <fraugerschoh@gmail.com>
2023-02-09 16:28:26 +0200

    add 1 abstract class and 3 children where override methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+  public static void main(String[] args) {
+    Machine truck = new Truck();
+    Machine bulldozer = new Bulldozer();
+    Machine excavator = new Excavator();
+    Machine[] machine = new Machine[]{truck, bulldozer, excavator};
+    for (int i = 0; i < machine.length; i++) {
+      machine[i].doWork();
+      machine[i].stopWork();
+    }
+  }
 }
