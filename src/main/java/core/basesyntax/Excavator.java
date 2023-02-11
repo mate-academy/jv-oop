785863fc5cccaba9499d3a2fea20c24b221be20d
Olga M <axiomkaonline@gmail.com>
2023-02-11 20:36:00 +0200

    add objects, add array, add loop

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,4 +1,11 @@
 package core.basesyntax;
 
 public class Excavator extends Machine {
+    @Override
+    public void  doWork() {
+        System.out.println("Excavator started working");
+    }
+    public void stopWork() {
+        System.out.println("Excavator started working");
+    }
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args){
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        Object machine [] = {truck, bulldozer, excavator};
+
+        for (int i = 0; i < machine.length; i++) {
+
+        }
+    }
 }
