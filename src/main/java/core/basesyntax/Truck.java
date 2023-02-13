39e85ad0c8285155aaa4ea7c4ffb0d840b1fc54c
Ivan Mosiur <mosiurivsn@gmail.com>
2023-02-13 20:45:51 +0200

    The Machine class created two methods and overridden them in the descendant classes.

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public  class Truck extends Machine {
+    @Override
+    public  void doWork(){
+        System.out.println("Truck do work");
+    }
+    @Override
+    public  void stopWork(){
+        System.out.println("Truck stop work");
+    }
+}
