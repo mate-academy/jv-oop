4525bacf9daa9da84258d6f9eacfc4d063c740af
Oleksandr Rolik <rolik.alexx@gmail.com>
2023-02-13 22:32:41 +0200

    Implement all classes in main method

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Excavator(), new Truck(), new Bulldozer()};
-        for(Machine machine : machines){
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
