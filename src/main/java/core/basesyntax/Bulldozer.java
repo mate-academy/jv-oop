27c2bad9d78a95338fa83744210fb35dc5405b62
Ivan Mosiur <mosiurivsn@gmail.com>
2023-02-13 21:46:57 +0200

    removed  spaces.

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -4,8 +4,7 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Bulldozer do work");
     }
-    public void stopWork(){
-
+    public void stopWork() {
         System.out.println("Bulldozer stop work");
     }
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,7 +5,7 @@ public class MainApp {
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
         Machine excavator = new Excavator();
-        Machine[] machines = new Machine[] { bulldozer, truck, excavator } ;
+        Machine[] machines = new Machine[] { bulldozer, truck, excavator };
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
