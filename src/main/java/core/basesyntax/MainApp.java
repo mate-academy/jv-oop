403b522621d7a506b9927f092e763b9e1203709f
dreo <mishkin878@gmail.com>
2023-02-17 19:37:17 +0200

    add Suggested change from mentor

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,15 +2,15 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Truck truck = new Truck();
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
 
-        Machine[] machine = {truck, bulldozer, excavator};
+        Machine[] machines = {truck, bulldozer, excavator};
 
-        for (int i = 0; i < machine.length; i++) {
-            machine[i].doWork();
-            machine[i].stopWork();
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
         }
     }
 
