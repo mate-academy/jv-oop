3d4f3b9a16d21b51a4186d1a18d1ef441268151b
dreo <mishkin878@gmail.com>
2023-02-17 11:32:48 +0200

    finished task

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+
+        Machine[] machine = {truck, bulldozer, excavator};
+
+        for(int i = 0; i < machine.length; i++){
+            machine[i].doWork();
+            machine[i].stopWork();
+        }
+    }
 
 }
