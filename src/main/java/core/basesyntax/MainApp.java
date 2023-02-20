d83e5ec9e8fe80d94bca224db9ebb10b0db5d782
Lev Shtana <120460187+CapybaraFrog@users.noreply.github.com>
2023-02-20 13:44:39 +0300

    Created new class Machine  , added inheritance clasеss Truck , Bulldozer , Excavator to class Machine  ,addeed Override metod doWork and stopWork in a child clases. In the MainApp class, created a Machine array with a Truck, Bulldozer, and Excavator and called the doWork() and stopWork() methods in a loop.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
 public class MainApp {
+    Machine truck = new Truck();
+    Machine bulldozer = new Bulldozer();
+    Machine excavator = new Excavator();
+
+    Machine[] machines = {truck,bulldozer, excavator} ;
+
+    {
+
+        for (Machine machine : machines) {
+
+            machine.doWork();
+        }
+
+        for (Machine machine : machines) {
+
+            machine.stopWork();
+        }
+    }
 
 }
+
