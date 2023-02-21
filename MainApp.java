cdc0b9cf580973eb40c213bcc8f2b5fc99bd35ea
Mutaborr <123407682+Mutaborr@users.noreply.github.com>
2023-02-21 10:01:17 +0200

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -3,9 +3,8 @@ public class MainApp {
   public static void main(String[] args) {
       Machine [] machines = { new Truck(), new Bulldozer(), new Excavator()};
         for(int i = 0; i < machines.length; i++){
-            Machine machine = machines[i];
-            machine.doWork();
-            machine.stopWork();
+            machines[i].doWork();
+            machines[i].stopWork();
         }
     }
 }
