380035ac3da1d27b53878ccdde696f4cd5cc6851
Mutaborr <123407682+Mutaborr@users.noreply.github.com>
2023-02-21 09:53:12 +0200

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -1,17 +1,12 @@
 package org.example;
-
 public class  Truck extends Machine {
-
     @Override
     public void doWork() {
-
-            System.out.println("Truck started its work.");
-
+       System.out.println("Truck started its work.");
     }
 
     @Override
     public void stopWork() {
-
-            System.out.println("Truck stopped its work.");
+       System.out.println("Truck stopped its work.");
         }
     }
