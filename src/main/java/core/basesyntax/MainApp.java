dbd01fc19444df9bf235f4a18a7b6ff3303029ba
Alex-Solod <solodyhin_2@icloud.com>
2023-02-22 01:32:31 +0200

    Created class Machine containing methods public abstract void doWork() and public abstract void stopWork(). Also created classes Truck, Bulldozer, Excavator that inherit from Machine. In those classes Truck, Bulldozer, Excavator overrided doWork() and Override stopWork() as well. In MainApp class created Machine array with Truck, Bulldozer and Excavator and called methods doWork() and stopWork() in a loop.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,8 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+        Machine[] machines;
+        machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
 
         for (Machine machine : machines) {
             machine.doWork();
