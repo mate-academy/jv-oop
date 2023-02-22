398a459357e25d95401b79e1c66c10fce4890726
Lev Shtana <120460187+CapybaraFrog@users.noreply.github.com>
2023-02-22 10:15:07 +0300

    I just wanted to let you know that I took your advice and removed the unnecessary spaces from my code. However, I must admit that I am still unsure about the task you asked me to complete, specifically regarding moving a certain piece of code to the main method. Could you please provide me with further clarification or guidance on how to accomplish this task?
    Thank you for your help.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,21 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
-
     private static Machine[] machines = {new Truck(),new Bulldozer(),new Excavator()};
-    public static Machine[] getMachines() {
 
+    public static Machine[] getMachines() {
         return machines;
     }
 
     public static void setMachines(Machine[] machines) {
-
         MainApp.machines = machines;
-
         for (Machine machine : getMachines()) {
-
             machine.stopWork();
-
             machine.doWork();
 
         }
