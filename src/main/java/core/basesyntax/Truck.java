f640b76654499a798406ed42c011e2cc0d134cb8
Alex-Solod <solodyhin_2@icloud.com>
2023-02-22 16:17:26 +0200

    Delete space between commands

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -5,7 +5,6 @@ public class Truck extends Machine {
     public void doWork() {
         System.out.println("Truck is start working.");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Truck stop working.");
