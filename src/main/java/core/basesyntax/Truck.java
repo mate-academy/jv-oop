08e566c4141f43d67171d65c6630226ad4f90bb1
Oleksandr Solodukhin <78688752+Alex-Solod@users.noreply.github.com>
2023-02-22 17:22:55 +0200

    Update Truck.java

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
+
     @Override
     public void doWork() {
         System.out.println("Truck is start working.");
     }
+    
     @Override
     public void stopWork() {
         System.out.println("Truck stop working.");
