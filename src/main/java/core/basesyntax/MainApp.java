3e9af17137199142d9639a59c1dde0829e3f79d7
Lev Shtana <120460187+CapybaraFrog@users.noreply.github.com>
2023-02-23 10:05:22 +0300

    Fix syntax errors in code. Thank you for your help.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,19 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
-    private static Machine[] machines = {new Truck(),new Bulldozer(),new Excavator()};
+    public static void main(String[] args) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
 
-    public static Machine[] getMachines() {
-        return machines;
-    }
-
-    public static void setMachines(Machine[] machines) {
-        MainApp.machines = machines;
-        for (Machine machine : getMachines()) {
-            machine.stopWork();
+        for (Machine machine:machines) {
             machine.doWork();
-
+            machine.stopWork();
         }
     }
 }
-
