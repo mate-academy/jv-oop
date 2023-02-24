f2fb0bddc7ddf6e9c75b9570bb4e9d287e3e1ac4
Victor Rudkovsky <vpcomtek@gmail.com>
2023-02-25 00:49:24 +0200

    -- OOP basic fixed v1.3

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,11 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine excavator = new Excavator();
-        Machine bulldozer = new Bulldozer();
-        Machine truck = new Truck();
-
-        Machine[] machines = new Machine[] {excavator, bulldozer, truck};
+        Machine[] machines = new Machine[] {new Excavator(), new Bulldozer(), new Truck()};
 
         for (Machine machine: machines) {
             machine.doWork();
