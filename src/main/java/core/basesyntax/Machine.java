887d5c60851c767f9626d4641297c7c5faf0f1e1
Volodymyr Petsukh <v.i.petsukh@gmail.com>
2023-02-28 14:11:37 +0200

    finish tasks from readme file

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public class  Machine {
+    public void doWork(){
+        System.out.println("Machine start work");
+    }
+
+    public void stopWork(){
+        System.out.println("Machine stop work");
+    }
+}
