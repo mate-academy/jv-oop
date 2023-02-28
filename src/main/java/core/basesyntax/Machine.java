f346c011f575d1fa28e6440038413659389f4302
Volodymyr Petsukh <v.i.petsukh@gmail.com>
2023-02-28 17:26:39 +0200

    the abstract method in Machine class has been corrected

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,11 +1,7 @@
 package core.basesyntax;
 
-public class Machine {
-    public void doWork() {
-        System.out.println("Machine start work");
-    }
+public abstract class Machine {
+    public abstract void doWork();
 
-    public void stopWork() {
-        System.out.println("Machine stop work");
-    }
+    public abstract void stopWork();
 }
