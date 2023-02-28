03edd33342ce0f28ec6a73f8c71f13917ef7754e
Sieryi D <hunterst@ukr.net>
2023-02-28 20:28:07 +0200

    check-checkstile-errors

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,15 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main (String[] args) {
-        Machine Truck = new Truck();
-        Machine Excavator = new Excavator();
-        Machine Bulldozer = new Bulldozer();
-        Machine[] Machines = {Truck, Excavator, Bulldozer};
-        for (Machine machine : Machines) {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine[] machines;
+        machines = new Machine[]{truck, excavator, bulldozer};
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
     }
-
 }
