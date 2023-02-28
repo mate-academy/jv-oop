ef7b27dae91c97978c23e3fdef374187cd302628
Sieryi D <hunterst@ukr.net>
2023-02-28 18:29:50 +0200

    add-foreach

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main (String[] args) {
+        Machine[] Machine = new Machine[3];
+        Machine Truck = new Truck();
+        Machine Excavator = new Excavator();
+        Machine Bulldozer = new Bulldozer();
+        Machine[0] = Truck;
+        Machine[1] = Excavator;
+        Machine[2] = Bulldozer;
+        for (Machine machine : Machine) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 
 }
