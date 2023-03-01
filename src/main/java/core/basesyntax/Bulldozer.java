23500eef23b5e97a84f48fb7c539756ed39d05c3
okorsak <125895224+okorsak@users.noreply.github.com>
2023-03-01 18:17:31 +0000

    add 4 classes and main box

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("The Bulldozer do work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The Bulldozer stop work");
+    }
+}
