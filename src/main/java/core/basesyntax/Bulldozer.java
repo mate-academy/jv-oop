15d9b39ce39c684dfc4b37280fe9ea7140bbb13d
Oleks Korsak <korsak.ua@icloud.com>
2023-03-02 10:44:56 +0200

    add code style fixes

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -5,7 +5,7 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("The Bulldozer do work");
     }
-
+    
     @Override
     public void stopWork() {
         System.out.println("The Bulldozer stop work");
