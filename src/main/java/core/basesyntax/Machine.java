db9972ad288379e1876532371c92ae37c22db2de
karvatskyi <bogdankarvatskiy14@gmail.com>
2023-03-03 19:48:35 +0200

    formatted code

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,4 +1,5 @@
 package core.basesyntax;
+
 public abstract class Machine {
     public abstract void doWork();
 
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
+
 public class MainApp {
-public void showWork() {
-    Machine[] machineArray = {new Excavator(),new Truck(),new Bulldozer()};
-    for (Machine numberOfArray : machineArray) {
-        numberOfArray.doWork();
-        numberOfArray.stopWork();
+    public void showWork() {
+        Machine[] machineArray = {new Excavator(), new Truck(), new Bulldozer()};
+        for (Machine numberOfArray : machineArray) {
+            numberOfArray.doWork();
+            numberOfArray.stopWork();
+        }
     }
-}
 }
