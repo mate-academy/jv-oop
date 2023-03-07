84f26b160a2a163a7dc2d794d7b681aebb8134d4
Sergey Zavalniuk <sergzavalnyuk@gmail.com>
2023-03-08 01:51:42 +0300

    Added psvm method, 3 classes with overriding methods and one abstract class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-
         Machine[] machine = new Machine[]{new Excavator(), new Bulldozer(), new Truck()};
         for (Machine machine1: machine) {
             machine1.doWork();
