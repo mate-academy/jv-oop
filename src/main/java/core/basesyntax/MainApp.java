b968320162a459208932259f3a390eef99bbcbe8
Vladyslav Denysiuk <denysiuk.vladyslav@gmail.com>
2023-03-07 20:32:04 +0200

    Solution of the first Git problem

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,44 @@
 package core.basesyntax;
 
-public class MainApp {
+abstract class Machine {
+    public abstract void doWork();
+    public abstract void stopWork();
+}
 
+class Truck extends Machine {
+    public void doWork() {
+        System.out.println("Truck started working.");
+    }
+    public void stopWork() {
+        System.out.println("Truck stopped working.");
+    }
+}
+
+class Bulldozer extends Machine {
+    public void doWork() {
+        System.out.println("Bulldozer started working.");
+    }
+    public void stopWork() {
+        System.out.println("Bulldozer stopped working.");
+    }
+}
+class Excavator extends Machine {
+    public void doWork() {
+        System.out.println("Excavator started working.");
+    }
+    public void stopWork() {
+        System.out.println("Excavator stopped working.");
+    }
+}
+
+public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+
+        for (int i = 0; i < machines.length; i++) {
+            Machine machine = machines[i];
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
