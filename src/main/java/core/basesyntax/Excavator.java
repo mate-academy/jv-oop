92cef3b8a568a00facec7413b1a3b183af4d5e87
Taras Machyta <taras.machyta.sa@gmail.com>
2023-03-11 23:32:40 +0200

    Update failures

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println(getName() + " started its work..");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println(getName() + " stopped its work..");
+
+    }
+}
