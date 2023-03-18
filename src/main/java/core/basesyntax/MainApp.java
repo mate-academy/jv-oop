e9419b1075d446e0bcb8545da7267eb7f3ccdb6a
Yevhen Tolkunov <yevhentolkunov@gmail.com>
2023-03-18 17:04:55 +0200

    Add all class and make loop start/stop work

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
-public class MainApp {
+public class MainApp{
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machina = new Machine[] {truck, bulldozer, excavator};
+
+        for (Machine ma: machina) {
+            ma.doWork();
+            ma.stopWork();
+        }
+    }
+
 
 }
