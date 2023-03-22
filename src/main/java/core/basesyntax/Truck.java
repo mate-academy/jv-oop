e3f356c0dccf36550c5215dc56d2bb7d6fade1f9
Vova Maryniak <volodimirmarinak27@gmail.com>
2023-03-22 15:59:27 +0200

    added an annotation

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,12 +1,15 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
-   @Override
+
+    @Override
+
     public void doWork() {
         System.out.println("Truck started its work");
     }
 
     @Override
+
     public void stopWork() {
         System.out.println("Truck stopped its work");
     }
