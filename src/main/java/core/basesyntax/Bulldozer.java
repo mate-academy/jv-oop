ffcd7fcacb8f7f95819d101306578ca3ade4d913
Anna Poplavska <semachka100201@gmail.com>
2023-03-24 10:48:29 +0100

    added new classes, change MainApp.java

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+    public Bulldozer() {
+    }
+    public void doWork() {
+        System.out.println("Bulldozer started its work.");
+    }
+    public void stopWork() {
+        System.out.println("Bulldozer stopped its work.");
+    }
+}
