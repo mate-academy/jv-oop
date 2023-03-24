06989e6f8d090366f57c090e5ed1fed42f6a646a
z1row <pmeysarosh2004@gmail.com>
2023-03-24 19:52:52 +0100

    Used new class's methods in MainApp

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Excavator excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
+
+        Bulldozer bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+
+        Truck truck = new Truck();
+        truck.doWork();
+        truck.stopWork();
+    }
 
 }
