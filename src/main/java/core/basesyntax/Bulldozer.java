fe96724cc592abb816bf3c65d0ca579ca038725d
Andrii Khomenko <andpey.homenko@gmail.com>
2023-04-01 00:02:43 +0300

    hw-1-jv-oop-final-solution

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+public class Bulldozer extends Machine {
+    public void doWork() {
+        System.out.println("bulldozer has started");
+    }
+    public void stopWork() {
+        System.out.println("bulldozer finished work");
+    }
+}
---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+public class Excavator extends Machine{
+    public void doWork() {
+        System.out.println("excavator has started");
+    }
+    public void stopWork() {
+        System.out.println("excavator finished work");
+    }
+}
