bf7ace0ddf730515ca1e40a2643f5427e9dc113f
Andriy Karpa <McMerfee@i.ua>
2023-04-05 21:14:37 +0300

    added psvm method and other method

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args){
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machine = new Machine[] {truck, bulldozer, excavator};
+                for (Machine mashuna:machine){
+                    mashuna.doWork();
+                    mashuna.stopWork();
+                }
+    }
+
+
+
+
+
+
 
 }
