9d8a2008e16b8fafc7aff8b9d3d0ca2a3344dde8
Pavlo Polezhay <pasha200417@gmail.com>
2023-04-06 15:56:06 +0300

    Implemented

---
+++ src/main/java/core/basesyntax/Buldozer.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer started its work.");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer stopped its work.");
+    }
+}
+
+
