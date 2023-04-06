970148a6ef0d373b6f1b7fa855a321e95a4c4c19
Yurii Ivanov <ivanovyura76@gmail.com>
2023-04-06 17:35:49 +0300

    Fixes: add spaces in MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public abstract class MainApp {
-    public static void main(String [] args) {
-        Machine [] machines = {new Truck(),new Bulldozer(),new Excavator()};
+    public static void main(String[] args) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
         for (Machine currentMachine : machines) {
             currentMachine.doWork();
             currentMachine.stopWork();
