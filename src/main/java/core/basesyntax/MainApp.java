f2ca7a5660b508e5ae02b332e274e049a80aeb69
Vasya <xaski514@gmail.com>
2023-04-08 17:38:36 +0300

    jv-oop HW

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,12 +4,8 @@ public class MainApp {
     public static void main(String[] args) {
         Machine [] machines = {new Truck(), new Bulldozer(), new Excavator()};
         for (Machine machine: machines) {
-            for (int i = 0; i < 1; i++) {
-                machine.doWork();
-            }
-            for (int i = 0; i < 1; i++) {
-                machine.stopWork();
-            }
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
