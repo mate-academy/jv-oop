d8b4c171d12ac79633657beaecdfac8783936398
VladSchweps <vladschweps1@gmail.com>
2023-04-10 21:38:49 +0300

    Add classes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+
+        Machine[] mArray = new Machine[3];
+        mArray[0] = new Truck();
+        mArray[1] = new Buldozer();
+        mArray[2] = new Excavator();
+
+        for (Machine m: mArray) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
 
 }
