5f055bc5eaa0e71f86d48a3ecb88c51a7d75457b
Yana Bilyk <bilyk.yn@gmail.com>
2023-04-11 09:59:25 -0400

    one more class and created an array in MaineApp

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,9 @@
 package core.basesyntax;
 
 public class MainApp {
-
+ Machine [] machines = { new Truck(), new Bulldozer(), new Excavator()};
+ for(int i = 0; i < machines.length; i++ ){
+ Machine machine = machines[i];
+  System.out.println(machine.doWork() + machine.stopWork(););
+ }
 }
