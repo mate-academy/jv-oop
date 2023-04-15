9ba0e1965df217592faff6069ce62cdfd8f34d58
Olha <olga.olegovna2802@online.ua>
2023-04-15 19:05:35 +0100

    Create an abstract class Machine. Add 2 abstract methods: doWork and stopWork. Create 3 classes that extends an abstract class Machine. Override in these classes all methods from class Machine. In main method create 3 new objects: truck, bulldozer, excavator. Create an array with all these objects. Create a loop, which calls all methods from these objects.

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
-
 public class Truck extends Machine {
     @Override
     public void doWork() {
         System.out.println("Truck has started its work");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Truck has stopped its work");
