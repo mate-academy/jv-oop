65e07b015dfd772c7b524c4691651d1e367f3dd4
Artem Fomenko <u2glbro@gmail.com>
2023-04-16 18:02:36 +0300

    Create Machine array objects Truck, Bulldozer and Excavator and call methods doWork() and stopWork() in a loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public void makeMachins() {
+        Machin[] machins = {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machin machin : machins) {
+            machin.doWork();
+            machin.stopWork();
+        }
+    }
 }
