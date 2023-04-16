9e6dd460880b7cd71e07182c58208eab30ed8bd6
Artem Fomenko <u2glbro@gmail.com>
2023-04-16 18:00:39 +0300

    Created classes Truck, Bulldozer, Excavator (super class is Machine)

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Excavator extends Machin{
+    @Override
+    public void doWork() {
+        System.out.println("Excavator started work");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator stop work");
+    }
+}
