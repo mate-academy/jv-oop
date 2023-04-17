ebc3eb1cbb55ce7af3f660ba904d5081425ac859
Bohdan Vorobiei <vbs@ukr.net>
2023-04-17 14:22:34 +0300

    Homework correction of mistakes

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class Machine {
     public void doWork() {
-        System.out.println("Machine started it is work");
+        System.out.println("Machine started its work");
     }
     public void stopWork() {
-        System.out.println("Machine stopped it is work");
+        System.out.println("Machine Stopped its work");
     }
 }
