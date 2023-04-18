477edc5a6ded363798a57800316744a04a37c6f3
Gamemod13 <igorfetov1997@gmail.com>
2023-04-18 13:50:16 +0300

    Corrected object declaration in MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,10 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
-        Machine truck = new Truck();
-        Machine[] machines = {bulldozer, excavator, truck};
+        Machine[] machines = {new Bulldozer(), new Excavator(), new Truck()};
         for (Machine machine : machines) {
             machine.doWork();
         }
