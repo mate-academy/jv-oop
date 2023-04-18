54d398aa68ee61611cc916abc05d3f66234f47b8
Chris <tynkastya@gmail.com>
2023-04-18 15:58:54 +0300

    implemented task oop basic

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = {new Excavator(), new Truck(), new Bulldozer()};
 
+        for (Machine elem: machines) {
+            elem.doWork();
+            elem.stopWork();
+        }
+    }
 }
