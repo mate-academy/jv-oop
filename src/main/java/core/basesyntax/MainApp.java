281ab439e1f90f1bf3436f9c39a04c9dd926aaac
VladSchweps <vladschweps1@gmail.com>
2023-04-19 10:51:52 +0300

    Added fixed to maven checkstyle error

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
+
     public static void main(String[] args) {
         Machine[] machines = {new Truck(), new Buldozer(), new Excavator()};
         for (Machine machine : machines) {
