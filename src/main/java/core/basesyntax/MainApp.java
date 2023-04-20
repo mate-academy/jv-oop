8defba38964aa6bacc109e5a43d1a3b6fa58c0d3
Viktoriia Semenchenko <vickysemenchenko@gmail.com>
2023-04-20 15:12:03 +0300

    added classes Machine, Truck, Bulldozer and Excavator added and implemented methods doWork() and stopWork() for all classes added Machine array with Truck, Bulldozer and Excavator and implemented loop for calling methods doWork() and stopWork()

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = {new Bulldozer(), new Truck(), new Excavator()};
 
+        for (Machine m: machines) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
 }
