5d868ec18b551ed595adfbb1f09920205d4df10f
viprko <pr.and.vi@gmail.com>
2023-04-20 13:15:00 +0300

    fixed style, remove redundant empty lines

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,12 +1,10 @@
 package core.basesyntax;
 
 public class Excavator extends Machine {
-    @Override
     public void doWork() {
         System.out.println("Excavator started working");
     }
 
-    @Override
     public void stopWork() {
         System.out.println("Excavator stopped working");
     }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -9,8 +9,3 @@ public class MainApp {
         }
     }
 }
-
-
-
-
-
