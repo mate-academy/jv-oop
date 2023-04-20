998b6d4400a55a26926f6407697d99f0f58d6949
Yaroslav Khmaruk <yarik9656422@gmail.com>
2023-04-20 20:38:20 +0300

    added Machine class and Excavator, Bulldozer,Truck which extends Machine. Add and override methods doWork and stopWork. Add a loop.

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+class Truck extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Truck started work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stoped work");
+    }
+}
