ee7d435583323d2de97ec334c7673b17f14fb082
Volodymyr Rusyn <volodymyrruso@gmail.com>
2023-04-20 13:39:06 +0200

    deleted extra free lines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,11 +1,14 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
+
         }
     }
 }
