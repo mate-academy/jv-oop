be1363121d3f7d556cd9cceaeffcd72c87a5469d
Oleh Zhabrovets <zh_oleg1997@icloud.com>
2023-04-20 14:30:22 +0300

    check on mavik is successful

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -4,6 +4,7 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Bulldozer started work");
     }
+
     public void stopWork() {
         System.out.println("Bulldozer stopped work");
     }
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
-public class Excavator extends Machine{
+public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator started work");
     }
+
     public void stopWork() {
         System.out.println("Excavator stopped work");
     }
