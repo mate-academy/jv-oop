45a7ec5ff45faae7bcaa4301f7f6b53b189e2ee9
Volodymyr Rusyn <volodymyrruso@gmail.com>
2023-04-20 15:55:52 +0200

    added empty lines at the end and remove empty line in main method. added empty line in Machine class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,11 +1,9 @@
 package core.basesyntax;
 
-import java.util.Arrays;
-
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
-        for (Machine machine : machines) {
+        for (Machine machine : machines){
             machine.doWork();
             machine.stopWork();
         }
