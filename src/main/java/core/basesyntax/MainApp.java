ea7060a4319f164579731dfa46e7b9a5194af1ac
Viktoriia Semenchenko <vickysemenchenko@gmail.com>
2023-04-20 15:50:33 +0300

    removed redundant empty line

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Bulldozer(), new Truck(), new Excavator()};
-
         for (Machine m: machines) {
             m.doWork();
             m.stopWork();
