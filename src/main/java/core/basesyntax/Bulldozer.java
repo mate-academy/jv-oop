3af7df877d835090568cd2bb3a175314e9ffffaf
Bohdan Vorobiei <vbs@ukr.net>
2023-04-20 14:37:30 +0300

    Home work. Fixed bugswith empty lines.

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -4,8 +4,8 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Bulldozer has started working");
     }
+
     public void stopWork() {
         System.out.println("Bulldozer has stopped working");
     }
 }
-
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -4,8 +4,8 @@ public class Truck extends Machine {
     public void doWork() {
         System.out.println("Truck has started working");
     }
+
     public void stopWork() {
         System.out.println("Truck has stopped working");
     }
 }
-
