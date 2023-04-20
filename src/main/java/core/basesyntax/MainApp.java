f011b09a01916ab0807b69a30f85641e7b5f9c99
Arina Polna <polna.arina@gmail.com>
2023-04-20 23:04:24 +0300

    solve task by calling mathods in loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[]{new Bulldozer(), new Excavator(), new Truck()};
+        for (Machine mac : machines) {
+            mac.doWork();
+            mac.stopWork();
+        }
+    }
 }
