60b5249805f2ba5cb5403ca17ea3f5844e1f91e5
Stanislav Golubkov <115059948+stas83ggg@users.noreply.github.com>
2023-04-20 21:09:52 +0300

    Add files via upload

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,13 @@
+package JavaCoreOOPBasic;
+
+public class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer starts its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer stops its work");
+    }
+}
