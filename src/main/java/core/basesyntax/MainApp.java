bf5524c40b5a512aee5d09fc93b1ae2c26d02186
grete <grete9597@gmail.com>
2023-04-21 18:36:31 +0300

    simplifying Main method

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,13 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine truck = new Truck();
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
-        Machine[] machines = new Machine[3];
-        machines[0] = truck;
-        machines[1] = bulldozer;
-        machines[2] = excavator;
+        Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
