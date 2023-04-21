05f853a13698854aac3e6a0878cdd74914368dba
Mykyta Udovycehnko <nikitaudav123@gmail.com>
2023-04-21 12:30:27 +0300

    improve code and make it more concise

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,16 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
     public static void main(String[] args) {
-        Machine truck = new Truck();
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
-        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
+        Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
     }
-
 }
