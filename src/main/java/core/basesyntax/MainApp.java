cd92eaf1c0ceca839afad3ff4346eaaef4d93a1a
grete <grete9597@gmail.com>
2023-04-21 18:01:56 +0300

    Maven checkstyle fixed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,13 +5,11 @@ public class MainApp {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-
         Machine[] machines = new Machine[3];
         machines[0] = truck;
         machines[1] = bulldozer;
         machines[2] = excavator;
-
-        for(Machine machine : machines) {
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
