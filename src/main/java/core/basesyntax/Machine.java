805daec693b0597ca4f455dfb616e27acf6b3297
Arthur Dvinskykh <dv7art@gmail.com>
2023-04-21 15:28:13 +0300

    Solution

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public abstract class Machine {
+
+    public void doWork() {
+        System.out.println("Machine started working");
+    }
+
+    public void stopWork() {
+        System.out.println("Machine stopped working");
+    }
+}
