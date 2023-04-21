f7fae8adbbdabe7850dad3ae60366db3a52f2fb4
Viktoriia Semenchenko <vickysemenchenko@gmail.com>
2023-04-21 12:23:37 +0300

    renamed variable

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Bulldozer(), new Truck(), new Excavator()};
-        for (Machine m: machines) {
-            m.doWork();
-            m.stopWork();
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
