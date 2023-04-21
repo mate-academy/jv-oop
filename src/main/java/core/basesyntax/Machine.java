96828c9d9a5c06909b7a312b49ea02b845930b44
Arthur Dvinskykh <dv7art@gmail.com>
2023-04-21 15:39:12 +0300

    Create abstract methods

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -2,11 +2,7 @@ package core.basesyntax;
 
 public abstract class Machine {
 
-    public void doWork() {
-        System.out.println("Machine started working");
-    }
+    public abstract void doWork();
 
-    public void stopWork() {
-        System.out.println("Machine stopped working");
-    }
+    public abstract void stopWork();
 }
