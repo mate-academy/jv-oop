4523e84aa44d21989d34112dddc018713a959220
Denys Bondarenko <denysbondarenkoo@gmail.com>
2023-04-24 22:30:19 +0300

    denis fixed bugs

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -2,5 +2,6 @@ package core.basesyntax;
 
 public abstract class Machine {
     public abstract String doWork();
+
     public abstract String stopWork();
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main(String[] args){
-    Machine[] excavator = {new Excavator(),new Bulldozer(),new Truck()};
-    for (int i = 0; i < excavator.length; i++) {
-     excavator[i].doWork();
-     excavator[i].stopWork();
-     System.out.println(excavator[i].doWork() + "\n" + excavator[i].stopWork() + "\n");
-    }
+    public static void main(String[] args) {
+        Machine[] excavator = {new Excavator(),new Bulldozer(),new Truck()};
+        for (int i = 0; i < excavator.length; i++) {
+            excavator[i].doWork();
+            excavator[i].stopWork();
+            System.out.println(excavator[i].doWork() + "\n" + excavator[i].stopWork() + "\n");
+        }
     }
 }
