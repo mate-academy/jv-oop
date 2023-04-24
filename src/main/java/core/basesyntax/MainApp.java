eb2ad56500468d8c757bc118e5c49f451482a906
Victor Ruban <edmont373@gmail.com>
2023-04-24 09:48:57 +0300

    Fix mistakes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,13 +3,10 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
-
-        Machine[] cars = new Machine[] {new Excavator(), new Truck(), new Bulldozer()};
-
-        for (Machine car : cars) {
-            car.doWork();
-            car.stopWork();
+        Machine[] machines = new Machine[] {new Excavator(), new Truck(), new Bulldozer()};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
-
 }
