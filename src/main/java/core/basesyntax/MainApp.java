e5cff40eff8d17fb69878d2e9fe1a697dc61d7e2
Viktor Bykh <bykhviktor21@gmail.com>
2023-04-24 11:23:44 +0300

    removed unnecessary method

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,18 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-    private Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
-
-    public Machine[] getMachines() {
-        for (Machine machine: machines) {
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
-        return machines;
-    }
-
-    public static void main(String[] args) {
-        MainApp app = new MainApp();
-        Machine[] machines = app.getMachines();
     }
 }
