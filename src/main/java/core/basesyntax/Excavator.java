2912943f6aa84aaf4202e62dc5aaca56decd536f
OLYA <Ole4kastar79@gmail.com>
2023-04-25 23:28:49 +0300

    fixed checkstyle

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,9 +1,12 @@
 package core.basesyntax;
+
 public class Excavator extends Machine {
+
     @Override
     public void doWork() {
         System.out.println("Excavator starts working");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Excavator stops working");
--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,6 +1,8 @@
 package core.basesyntax;
-public abstract class Machine {
-   public Machine() {}
-   public abstract void doWork();
-   public abstract void stopWork();
-}
+
+   public abstract class Machine {
+
+      public abstract void doWork();
+
+      public abstract void stopWork();
+   }
