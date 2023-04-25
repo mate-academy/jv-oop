f52da90e78cba9a304556cf96f51ffcb09e51bcf
OLYA <Ole4kastar79@gmail.com>
2023-04-25 23:03:02 +0300

    create class Excavator

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+public class Excavator extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Excavator starts working");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator stops working");
+    }
+}
