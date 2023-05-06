d5570819d4e0a00c411e1357d6d851147aec8fc9
pochtalon <pochtalon1010@gmail.com>
2023-05-06 21:31:57 +0300

    Was created an array with Machine objects.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine [] array = new Machine[3];
+        array[0] = new Truck();
+        array[1] = new Bulldozer();
+        array[2] = new Excavator();
 
+        for (Machine machine : array){
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
