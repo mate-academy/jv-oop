a4af6bc32fee326d75b67a84caf539342550ed25
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-05-10 18:53:57 +0300

    task done

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+    public  void doWork() {
+        System.out.println("Bulldozer start work");
+    };
+    public  void stopWork() {
+        System.out.println("Bulldozer stop work");
+    };
+}
