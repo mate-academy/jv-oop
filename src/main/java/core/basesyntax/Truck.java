d775df749ee76efd6248b8904bf5c354e375c23d
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-05-10 20:11:59 +0300

    fix error from pull request3

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
-    public  void doWork(){
+    public void doWork() {
         System.out.println("Truck start work");
     }
 
-    public  void stopWork(){
+    public void stopWork() {
         System.out.println("Truck stop work");
     }
 }
