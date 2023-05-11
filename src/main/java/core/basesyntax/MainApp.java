cdafac8983a0cbc39133aac96a3b3feb749e0244
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-05-11 10:26:09 +0300

    code refactoring based on mentor feedback

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,17 +3,11 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
 
-        Machine bulldozer = new Bulldozer();
+        Machine[] machines = {new Bulldozer(), new Truck(), new Excavator()};
 
-        Machine truck = new Truck();
-
-        Machine excavator = new Excavator();
-
-        Machine[] arr = {bulldozer, truck, excavator};
-
-        for (Machine type:arr) {
-            type.doWork();
-            type.stopWork();
+        for (Machine machine:machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
