282fe06633a2bee389e3e859d8e98e381229c4e0
Kristina Shpilchyna <kriss.mva@gmail.com>
2023-05-12 13:25:17 +0200

    added new classes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,12 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine [] arrayMachines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : arrayMachines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
+
 }
