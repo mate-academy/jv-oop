828b5fa96edb08aab7a563e3570ddee9e18f96ed
Mykyta Oleksin <32339836+nacenik@users.noreply.github.com>
2023-05-12 10:15:08 +0300

    solution hw-1

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Machine started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Machine stopped its work");
+    }
+}
+
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine track = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine [] machines = new Machine [] {track,bulldozer,excavator};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
