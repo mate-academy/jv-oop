04141cee50e3e4af8a51c2443c996cb08a8c6c92
Vira <kramar5red@gmail.com>
2023-05-13 22:03:02 +0300

    Improves to code have been made

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,9 +5,13 @@ public class MainApp {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-        Machine[] machine = new Machine[]{truck, bulldozer, excavator};
+        Machine[] machine = new Machine[] {
+                truck,
+                bulldozer,
+                excavator
+        };
 
-        for(int i =0; i < machine.length; i++){
+        for (int i = 0; i < machine.length; i++) {
             machine[i].doWork();
             machine[i].stopWork();
         }
