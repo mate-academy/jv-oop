a966c5a22ab0a45dedad9e3c2653cdee5af25eb9
Bohdan Vorobiei <vbs@ukr.net>
2023-05-13 16:06:29 +0300

    Empty lines  again

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -4,6 +4,7 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Bulldozer has started working");
     }
+
     public void stopWork() {
         System.out.println("Bulldozer has stopped working");
     }
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -4,6 +4,7 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator has started working");
     }
+
     public void stopWork() {
         System.out.println("Excavator has stopped working");
     }
