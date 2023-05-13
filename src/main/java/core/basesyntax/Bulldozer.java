8c2310551311e7878e3e1b1af22ad5f8623de712
Vira <kramar5red@gmail.com>
2023-05-13 22:16:01 +0300

    More improves have been made

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -5,6 +5,7 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Bulldozer has started to work!");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Bulldozer has stopped to work!");
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -5,6 +5,7 @@ public class Truck extends Machine {
     public void doWork() {
         System.out.println("Truck has started to work!");
     }
+
     @Override
     public void stopWork() {
 
