7c0583545f949a8eeba987eba6174c20439ae7bd
Volodymyr Mandryka <volodymyr.m@mate.academy>
2023-05-19 10:18:27 +0300

    First sollution for hw

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
 
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+
+        Machine [] machines = new Machine [3];
+
+        machines[0] = truck;
+        machines[1] = bulldozer;
+        machines[2] = excavator;
+
+
+        for (Machine machine : machines) {
+            machine.stopWork();
+            machine.doWork();
+        }
+
+    }
 }
