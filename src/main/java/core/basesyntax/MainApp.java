e63821b41a8a8c338458fad60d8884e08c26ead2
Volodymyr Mandryka <volodymyr.m@mate.academy>
2023-05-19 10:29:00 +0300

    second solution for jv-oop hw

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,21 +2,18 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
 
-        Truck truck = new Truck();
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
+        Machine [] machines = new Machine [] {truck, bulldozer, excavator};
 
-        Machine [] machines = new Machine [3];
-
-        machines[0] = truck;
-        machines[1] = bulldozer;
-        machines[2] = excavator;
-
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
 
         for (Machine machine : machines) {
             machine.stopWork();
-            machine.doWork();
         }
 
     }
