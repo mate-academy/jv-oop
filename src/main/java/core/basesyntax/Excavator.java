689a99e6446914c97fad6b45a3736c611fe22ccc
Vlad Zinevych <vladisznv@gmail.com>
2023-05-23 12:58:19 +0300

    added whitespaces

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -5,6 +5,7 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator started its work!");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Excavator stopped its work!");
