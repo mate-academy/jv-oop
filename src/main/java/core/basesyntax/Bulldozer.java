892fb3da120b3d22d9e9f540c9c6bf031f57ccbd
Leshchenko <411100253g@gmail.com>
2023-05-30 12:08:53 +0300

    checkstyle fixed

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,13 +1,13 @@
 package core.basesyntax;
 
 public class Bulldozer extends Machine {
-  @Override
-  public void doWork() {
-    System.out.println("Bulldozer doWork");
-  }
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer doWork");
+    }
 
-  @Override
-  public void stopWork() {
-    System.out.println("Bulldozer stopWork");
-  }
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer stopWork");
+    }
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,16 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-  public static void main(String[] args) {
-    Machine[] machines = new Machine[]{
-        new Bulldozer(),
-        new Excavator(),
-        new Truck()
-    };
-    for (Machine machine : machines) {
-      machine.doWork();
-      machine.stopWork();
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[] {
+            new Bulldozer(),
+            new Excavator(),
+            new Truck()
+        };
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
-  }
-
 }
