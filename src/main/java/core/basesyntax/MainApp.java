c0ed3b37a3943d2c36e4bd2e0133ab3429ed1cec
Leshchenko <411100253g@gmail.com>
2023-06-01 14:41:23 +0300

    1. change output message in doWork and stopWork 2. change MainApp    array initialization    and loop in which machines work

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,13 +2,15 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machines = new Machine[] {
-            new Bulldozer(),
-            new Excavator(),
-            new Truck()
-        };
+        Machine[] machines = new Machine[3];
+        machines[0] = new Truck();
+        machines[1] = new Excavator();
+        machines[2] = new Bulldozer();
+
         for (Machine machine : machines) {
             machine.doWork();
+        }
+        for (Machine machine : machines) {
             machine.stopWork();
         }
     }
