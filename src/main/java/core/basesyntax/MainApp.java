00136ac75e6a64d87761de1a188809794255d0ae
Chorniypistolet <volkivskiy95@gmail.com>
2023-06-03 21:20:58 +0300

    corrected hw

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[3];
+        Excavator excavator = new Excavator();
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        machines[0] = excavator;
+        machines[1] = truck;
+        machines[2] = bulldozer;
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
