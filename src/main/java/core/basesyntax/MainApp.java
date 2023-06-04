92c9db71d17114ae92e58c0fd80f5950f65c0daf
Sergei Kudelin <chertterner686@gmail.com>
2023-06-04 13:30:25 +0300

    first commit

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machine = new Machine[]{new Excavator(), new Bulldozer(), new Truck()};
+        for (Machine vehicle : machine) {
+            vehicle.doWork();
+            vehicle.stopWork();
+        }
+    }
 }
