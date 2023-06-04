cb0783a5f498f6439a59ffa435604341bcbd006a
Vitalii <stelmaschyk.vg@gmail.com>
2023-06-04 12:52:54 +0300

    resolved task

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -8,7 +8,6 @@ public class MainApp {
         Excavator excavator = new Excavator();
         Machine[] machines = new Machine[]{truck, bulldozer, excavator};
        
->>>>>>> master
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
