a6ea0a89aafe305a03683adbb0b2c67440ceb257
Evhenii Polishuchenko <sds191sds@gmail.com>
2023-06-05 23:25:39 +0300

    created classes , implemented doWork() and stopWork() methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.machines.Bulldozer;
+import core.basesyntax.machines.Excavator;
+import core.basesyntax.machines.Machine;
+import core.basesyntax.machines.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        Machine[] machines = {truck,bulldozer,excavator};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
---
+++ src/main/java/core/basesyntax/machines/Bulldozer.java
@@ -0,0 +1,13 @@
+package core.basesyntax.machines;
+
+public class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("The bulldozer started working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The bulldozer stopped working");
+    }
+}
