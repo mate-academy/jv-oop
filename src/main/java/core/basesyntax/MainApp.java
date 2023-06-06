35645d628daa983b1cbae9e295abe52daad0f359
Sergei Kudelin <chertterner686@gmail.com>
2023-06-06 19:41:12 +0300

    fixed after check

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,9 @@
 package core.basesyntax;
 
 public class MainApp {
-    private static final Machine[] machine = new Machine[]{new Excavator(), new Bulldozer(), new Truck()};
+    private static final Machine[] machine = new Machine[]{
+            new Excavator(), new Bulldozer(), new Truck()
+    };
 
     public static void main(String[] args) {
         for (Machine vehicle : machine) {
