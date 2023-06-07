85173b9323768d8bd4314b839075b862571ce8c0
Mykola Horianin <anyavic2379@gmail.com>
2023-06-07 21:58:08 +0300

    Created machine classes and methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,28 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+
+        Machine[] machins = {truck, bulldozer, excavator};
+        for (Machine machine: machins) {
+            if (machine == truck) {
+                truck.doWork();
+                bulldozer.stopWork();
+                excavator.stopWork();
+            } else if (machine == bulldozer) {
+                bulldozer.doWork();
+                truck.stopWork();
+                excavator.stopWork();
+            } else {
+                excavator.doWork();
+                truck.stopWork();
+                bulldozer.stopWork();
+            }
+        }
+
+    }
 
 }
