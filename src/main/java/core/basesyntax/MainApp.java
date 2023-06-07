8810bc4fcd59ec6d823ae83db075c896023655d5
ANDREY <andrey.kiyas@gmail.com>
2023-06-07 23:48:54 +0300

    The decision is sent for review

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck("Truck");
+        Machine bulldozer = new Bulldozer("Bulldozer");
+        Machine excavator = new Excavator("Excavator");
 
+        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
+
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
 }
