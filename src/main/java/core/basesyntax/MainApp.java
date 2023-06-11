62b6093e973ce484b8c8668c828e5f558d384608
Vitalii Seleshchuk <seleschuk@gmail.com>
2023-06-11 23:13:12 +0300

    Syntax errors fixed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -9,35 +9,42 @@ public class MainApp {
         }
     }
 }
+
 abstract class Machine {
     public abstract void doWork();
     public abstract void stopWork();
 }
+
 class Truck extends Machine {
     @Override
     public void doWork() {
         System.out.println("Truck Start to Work");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Truck Stop Working");
     }
 }
+
 class Bulldozer extends Machine {
     @Override
     public void doWork() {
         System.out.println("Bulldozer Start to Work");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Bulldozer Stop Working");
     }
 }
+
 class Excavator extends Machine {
     @Override
     public void doWork() {
         System.out.println("Excavator Start to Work");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Excavator Stop Working");
