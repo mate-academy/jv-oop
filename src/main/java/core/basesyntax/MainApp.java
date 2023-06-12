6f4316bee4f404edd74baba90dd890fbb18e9739
Maksym Musiienko <maxymvm@gmail.com>
2023-06-13 01:14:56 +0300

    created Bulldozer, Truck, Excavator classes inheriting abstract Machine class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Excavator excavator = new Excavator();
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Machine[] machines = new Machine[]{excavator, truck, bulldozer};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
