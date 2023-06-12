05651da95b28d86a0a089226ea055d2bd0cea237
Anton Uzhva <anton.itec91@gmail.com>
2023-06-12 17:26:22 +0300

    Fix code style mistakes

--- src/main/java/core/basesyntax/machine/Truck.java
+++ src/main/java/core/basesyntax/machine/Truck.java
@@ -1,8 +1,8 @@
 package core.basesyntax.machine;
 
-import core.basesyntax.machine.Machine;
 
-public class Truck  extends Machine {
+
+public class Truck extends Machine {
     @Override
     public void doWork() {
         System.out.println("Truck has started work");
