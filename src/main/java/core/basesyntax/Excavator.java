24e8197980384337059e306c337836b684710b13
Liudmyla <mercedese.l@ukr.net>
2023-06-12 19:13:04 +0300

    created classes with their methods, created an object

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -4,8 +4,8 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator started its work.");
     }
-    public void stopWork() {
 
-         System.out.println("Excavator stopped its work.");
+    public void stopWork() {
+        System.out.println("Excavator stopped its work.");
     }
 }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -2,11 +2,10 @@ package core.basesyntax;
 
 public class Truck extends Machine {
     public void doWork() {
-
         System.out.println("Truck started its work.");
     }
-    public void stopWork() {
 
+    public void stopWork() {
         System.out.println("Truck stopped its work.");
     }
 }
