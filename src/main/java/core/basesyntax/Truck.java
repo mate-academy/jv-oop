0efde62a3a178166dced612fcf404db9050adecc
I <dovgopolkir@gmail.com>
2023-06-13 11:00:21 +0300

    added changes to class Truck

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,2 +1,14 @@
-package core.basesyntax;public class Truck {
+package core.basesyntax;
+
+public class Truck extends Machine{
+
+    @Override
+    public void doWork() {
+        System.out.println("Truck started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck ended its work");
+    }
 }
