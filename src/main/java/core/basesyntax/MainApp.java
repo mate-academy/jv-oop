1b10a7ffdb58105e839183d5845edf307deb9b70
I <dovgopolkir@gmail.com>
2023-06-13 11:46:42 +0300

    added style correction in Main

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class MainApp {
  public static void main(String[] args) {
-
   Machine[] machines = new Machine[] {new Truck(), new Excavator(), new Bulldozer()};
   for (Machine machine: machines){
    machine.doWork();
