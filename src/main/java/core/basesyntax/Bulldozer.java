151ddcc80d5a9646d60322a2505078171ffa15c7
I <dovgopolkir@gmail.com>
2023-06-13 10:56:08 +0300

    added changes to Bulldozer class

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,2 +1,13 @@
-package core.basesyntax;public class Bulldozer {
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer ended its work");
+    }
 }
