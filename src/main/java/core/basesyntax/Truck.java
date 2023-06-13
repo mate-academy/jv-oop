f87cbd59451093ed2af7696a61ab7f4edcbbd126
I <dovgopolkir@gmail.com>
2023-06-13 13:07:14 +0300

    added style changes to Class Truck

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,12 +1,10 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
-
     @Override
     public void doWork() {
         System.out.println("Truck started its work");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Truck ended its work");
