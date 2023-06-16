61726df2855312f0b4e8ba118ac6d5bc5fdc075f
Andrii Hryniuk <grinuk0706@gmail.com>
2023-06-17 01:04:00 +0300

    Was made first version my HW

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+       Machine [] mass = {new Bulldozer(), new Excavator(), new Truck()};
+        for (int i = 0; i < mass.length; i++) {
+            mass[i].doWork();
+            mass[i].stopWork();
+        }
+    }
 }
