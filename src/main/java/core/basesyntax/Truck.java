49125ce5c5e9f4ed9ee5f808f4834227f4a7a91d
Jean-Mark Kimbembe <marselium@gmail.com>
2023-06-17 16:46:44 +0300

    fixed with checkstyle

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
-public class Truck extends Machine{
+public class Truck extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Truck started its work");
+        System.out.println("Truck started its rk");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Truck stopped its work");
