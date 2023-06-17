8f0b457e17be8d147b7b2d56cd58462842238926
Andrii Hryniuk <grinuk0706@gmail.com>
2023-06-17 17:43:47 +0300

    verified checkstyle

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-       Machine [] machines = {new Bulldozer(), new Excavator(), new Truck()};
+        Machine [] machines = {new Bulldozer(), new Excavator(), new Truck()};
         for (int i = 0; i < machines.length; i++) {
             machines[i].doWork();
             machines[i].stopWork();
