b0e6974dd8a999c58f37a5e64ae4bc943e609324
Andrii Hryniuk <grinuk0706@gmail.com>
2023-06-17 16:28:06 +0300

    Deleted first empty lines in everythyng classes and renamed array

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-       Machine [] mass = {new Bulldozer(), new Excavator(), new Truck()};
-        for (int i = 0; i < mass.length; i++) {
-            mass[i].doWork();
-            mass[i].stopWork();
+       Machine [] machines = {new Bulldozer(), new Excavator(), new Truck()};
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
         }
     }
 }
