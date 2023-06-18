18ef1009d595d299adb2839b7faba4683a865f28
Denys-Maksym Kachalup <denys.k82@gmail.com>
2023-06-18 19:49:26 +0300

    add new classes and methods

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -9,6 +9,6 @@ public class Bulldozer extends Machine {
 
     @Override
     public void stopWork() {
-        System.out.println("Bulldozer stopped");
+        System.out.println("Bulldozer stopped!");
     }
 }
