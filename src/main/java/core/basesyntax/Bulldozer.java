f6c4d7a92165e8baa745e096d644f68538ded948
Denys-Maksym Kachalup <denys.k82@gmail.com>
2023-06-18 20:07:42 +0300

    add classes,methods and fix empty line

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -4,11 +4,11 @@ public class Bulldozer extends Machine {
 
     @Override
     public void doWork() {
-        System.out.println("Bulldozer works");
+        System.out.println("Bulldozer works!");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Bulldozer stopped");
+        System.out.println("Bulldozer stopped!");
     }
 }
