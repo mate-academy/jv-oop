95e89c7a3db33e52cd7b6db37a683195d8682685
Denys-Maksym Kachalup <Denys-Maksym.Kachalup.ITIS.2020@edu.lpnu.ua>
2023-06-18 19:18:41 +0300

    add some classes and methods

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Excavator extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("Excavator works!");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Excavators stopped");
+    }
+}
