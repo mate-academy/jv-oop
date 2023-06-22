c741254453f16a33b3d4a0cbe001f4bb37f505ca
anastezia12 <for.game.anaria@gmail.com>
2023-06-23 00:29:25 +0200

    1

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    Machine machine[] = {new Truck(), new Bulldozer(), new Excavator()};
+
+    public MainApp() {
+        for (Machine i : machine) {
+            i.doWork();
+        }
+        for (Machine i : machine) {
+            i.stopWork();
+        }
+    }
 
 }
