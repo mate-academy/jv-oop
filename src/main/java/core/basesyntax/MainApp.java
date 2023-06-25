e1c77f288dd12449302e15e6b21390b10a354a7e
Mykhailo Vorobets <misha.shark6969@gmail.com>
2023-06-25 23:13:57 +0300

    Created a Machine class that contains the abstract methods doWork() and stopWork(). Created the classes Truck, Bulldozer, Excavator, which were inherited from Machine. In these classes, I overrode the doWork() method so that it displays a message that a certain machine has started working. I also overrode stopWork(). It displays a message when a particular machine has stopped working. In the MainApp class, you created a Machine array with the Truck, Bulldozer, and Excavator machines and called the doWork() and stopWork() methods in a loop.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine machine [] = {new Bulldozer(), new Excavator(), new Truck()};
-        for (Machine machine1: machine) {
+        Machine machine[] = {new Bulldozer(), new Excavator(), new Truck()};
+        for (Machine machine1:machine){
             machine1.doWork();
             machine1.stopWork();
         }
