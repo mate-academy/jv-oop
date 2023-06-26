fe7b06dcb1d5b08b5c8d48e78e883a24e3e6cb36
Denis Balako <denisarius2000s@gmail.com>
2023-06-26 12:08:18 +0300

    fixed checkstyle mistakes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public class MainApp {
-
     public static void main(String[] args) {
         Machine[] machines = {new Excavator(), new Bulldozer(), new Truck()};
 
@@ -10,5 +9,4 @@ public class MainApp {
             machine.stopWork();
         }
     }
-
 }
