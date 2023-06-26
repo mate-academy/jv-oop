b54bac27decd2f895377ac8bbe01109da8722cf3
Evhenii Polishuchenko <sds191sds@gmail.com>
2023-06-26 12:47:51 +0300

    fixed mistakes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -7,9 +7,9 @@ import core.basesyntax.machines.Truck;
 
 public class MainApp {
     public static void main(String[] args) {
-        Truck truck = new Truck();
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
         Machine[] machines = {truck,bulldozer,excavator};
         for (Machine machine : machines) {
             machine.doWork();
