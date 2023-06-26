8df0e58aaadc498bd0abd8242f7cc28697bb040c
Vladyslav Keda <dakadofordisluv@gmail.com>
2023-06-26 17:59:19 +0300

    created method buildEach and doAndStopWorkForEachMachine

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.model.Bulldozer;
+import core.basesyntax.model.Excavator;
+import core.basesyntax.model.Machine;
+import core.basesyntax.model.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+        doAndStopWorkForEachMachine();
+    }
+
+    static void doAndStopWorkForEachMachine() {
+        for (Machine machine: buildEach()) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
+
+    static Machine[] buildEach() {
+        return new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+    }
 }
