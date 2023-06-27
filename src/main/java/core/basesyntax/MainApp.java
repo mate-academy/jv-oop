87160a45d31213f71ac198ce6f6fecb59ead0903
satori1111 <romakuchmii@gmail.com>
2023-06-27 12:51:13 +0300

    created machines classes and called methods in MainApp

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.machines.Bulldozer;
+import core.basesyntax.machines.Excavator;
+import core.basesyntax.machines.Machine;
+import core.basesyntax.machines.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machineArray = new Machine[]{ new Truck(),new Bulldozer(),new Excavator()};
+        for (Machine machine: machineArray) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
