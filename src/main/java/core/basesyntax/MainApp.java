0489e525bbad0f1b76c8bd53b38be2d63429e12b
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-06-27 15:25:09 +0300

    added homework solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
 }
