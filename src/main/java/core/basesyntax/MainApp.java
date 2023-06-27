1371890f67ca6a907e7e0b408cf53b4485fdd437
Olex <sanekparkhomovsky@gmail.com>
2023-06-27 16:47:18 +0300

    created machine class and three inherited classes (excavator,ruck,bulldozer) with overriding machine dowork() method + fixed style mistakes using mvn

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck1 = new Truck();
+        Machine buldozer1 = new Bulldozer();
+        Machine excavator1 = new Excavator();
 
+        Machine[] machines = {truck1,buldozer1,excavator1};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
