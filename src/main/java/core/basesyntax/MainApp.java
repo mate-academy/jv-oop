4ce1d5d5894682afa495ac19c3d29d3fa515bbb3
DaniilNepelev <kakashilvng@gmail.com>
2023-06-28 15:07:03 +0300

    uncomment 7 line in MainApp for succesfull work

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,7 +4,7 @@ public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = new Machine[3];
 
-        //machines[0] = new Truck();
+        machines[0] = new Truck();
         machines[1] = new Bulldozer();
         machines[2] = new Excavator();
 
