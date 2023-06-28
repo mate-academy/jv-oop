2e4c5f457f4749762ea14fb587515f905d1642a0
Vladyslav Keda <dakadofordisluv@gmail.com>
2023-06-28 22:15:23 +0300

    added fixes after review

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -11,13 +11,17 @@ public class MainApp {
     }
 
     private static void doAndStopWorkForEachMachine() {
-        for (Machine machine : buildMachines()) {
+        Machine[] machines = buildMachines();
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
     }
 
     private static Machine[] buildMachines() {
-        return new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        return new Machine[]{truck, bulldozer, excavator};
     }
 }
