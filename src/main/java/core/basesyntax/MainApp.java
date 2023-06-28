61834ba6600ba182363bb1781c640691c26b48c6
Dmytro Varukha <dmytro.varukha@gmail.com>
2023-06-28 22:34:21 +0300

    Revert "was created classes of machines and implemented methods 'doWork()' and 'stopWork()' for each of them"
    This reverts commit 689da128d229c2dedc85688fd0a7e5b60eeab274.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,15 +1,5 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main(String[] args) {
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
-        Machine truck = new Truck();
-        Machine[] machinesArray = new Machine[]{bulldozer, excavator, truck};
-        for (int i = 0; i < machinesArray.length; i++) {
-            Machine machine = machinesArray[i];
-            machine.doWork();
-            machine.stopWork();
-        }
-    }
+
 }
