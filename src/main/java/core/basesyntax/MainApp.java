43345fe41bc59b1701113c39fce5b87136cf4de5
Dmytro Varukha <dmytro.varukha@gmail.com>
2023-06-28 22:41:41 +0300

    was created classes of machines and implemented methods 'doWork()' and 'stopWork()' for each of them

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine[] machines = new Machine[] {bulldozer, excavator, truck};
+        for (int i = 0; i < machines.length; i++) {
+            Machine machine = machines[i];
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
