ce61b0753512e7b171591e90f65ef0b507d716e5
holsoni <soniakostashchuk@gmail,com>
2023-06-28 23:23:48 +0300

    added empty lines after methods

--- src/main/java/core/basesyntax/Buldozer.java
+++ src/main/java/core/basesyntax/Buldozer.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 public class Buldozer extends Machine {
     public Buldozer() {
     }
+
     @Override
     public void doWork() {
         System.out.println("Buldozer started work!");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Buldozer stopped its work!");
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 public class Truck extends Machine {
     public Truck() {
     }
+
     @Override
     public void doWork() {
         System.out.println("Truck started work!");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Truck stopped its work!");
