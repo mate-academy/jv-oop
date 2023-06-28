38b55fbe5df7b15002ef1138815594832128f718
Oleh <popovich158@gmail.com>
2023-06-28 23:04:07 +0300

    fixed test failure

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -7,14 +7,10 @@ import core.basesyntax.machinepack.Truck;
 
 public class MainApp {
     public static void main(String[] args) {
-
-
         Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
-
-
     }
 }
--- src/main/java/core/basesyntax/machinepack/Machine.java
+++ src/main/java/core/basesyntax/machinepack/Machine.java
@@ -2,6 +2,6 @@ package core.basesyntax.machinepack;
 
 public abstract class Machine {
     public abstract void doWork();
-    public abstract void stopWork();
 
+    public abstract void stopWork();
 }
