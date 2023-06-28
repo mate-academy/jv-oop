13328a95581d0a50eebf974b23cbbd2199cfe5f7
Oleh <popovich158@gmail.com>
2023-06-28 21:57:46 +0300

    Created class Machine containing methods public abstract void doWork() and public abstract void stopWork(). Created classes Truck, Bulldozer, Excavator that will inherit from Machine. In those classes overrided doWork(), so it will print message that certain machine started its work. Override stopWork() as well. It  prints messages that certain machine stopped its work. In MainApp class created Machine array with Truck, Bulldozer and Excavator and called methods doWork() and stopWork() in a loop.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.machinepack.Bulldozer;
+import core.basesyntax.machinepack.Excavator;
+import core.basesyntax.machinepack.Machine;
+import core.basesyntax.machinepack.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+
+
+        Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+
+
+    }
 }
