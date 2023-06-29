8afaa19010a6905d07b7d82a52d72040162c18bd
Zhdanov Anton <anton1992xxxx@gmail.com>
2023-06-29 18:22:50 +0300

    deleted empty lime in MainApp

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -7,9 +7,7 @@ import core.basesyntax.machines.Truck;
 
 public class MainApp {
     public static void main(String[] args) {
-
         Machine[] machines = new Machine[] {new Truck(), new Excavator(), new Bulldozer()};
-
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
