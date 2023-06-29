86c906785f8b196afe3fb294df706c45be666573
Ihor <kalaurdelta96@gmail.com>
2023-06-29 12:04:33 +0300

    Created new Classes Excavator, Bulldozer, Track which extends Machine. Implement methods doWork and stopWork.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
 
+        Excavator excavator = new Excavator();
+        Bulldozer bulldozer = new Bulldozer();
+        Truck truck = new Truck();
+
+        Machine[] machines = {excavator, bulldozer, truck};
+
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
