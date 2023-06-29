91ad2e990c953f77250f5ad9f7b4129b74bfa801
Ihor <kalaurdelta96@gmail.com>
2023-06-29 12:53:11 +0300

    changed reference from specific to abstract

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
 
-        Excavator excavator = new Excavator();
-        Bulldozer bulldozer = new Bulldozer();
-        Truck truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
 
         Machine[] machines = {excavator, bulldozer, truck};
 
