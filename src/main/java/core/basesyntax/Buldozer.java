51c441dc22f38c4dcfc0a026359935df3d49d5a7
Andrii Bozhenko <a.s.bozhenko@gmail.com>
2023-06-29 16:10:08 +0200

    created class Machine (containing methods public abstract void doWork() and public abstract void stopWork())  created clases Truck, Bulldozer, Excavator with doWork() and stopWork()  In MainApp class create Machine array with Truck, Bulldozer and Excavator and call methods doWork() and stopWork() in a loop.

---
+++ src/main/java/core/basesyntax/Buldozer.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Buldozer extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("Buldozer starts work!");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Buldozer stops work!");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Excavator excavator = new Excavator();
+        Buldozer buldozer = new Buldozer();
+        Truck truck = new Truck();
+        Machine[] machines = new Machine [] {excavator, buldozer, truck};
 
+        for (Machine equpment:machines) {
+            equpment.doWork();
+            equpment.stopWork();
+        }
+    }
 }
