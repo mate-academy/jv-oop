3a2f39698d871ac78ea421ebba31c0c170473f30
Andrii Bozhenko <a.s.bozhenko@gmail.com>
2023-06-29 16:26:02 +0200

    added spaces between methods

--- src/main/java/core/basesyntax/Buldozer.java
+++ src/main/java/core/basesyntax/Buldozer.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
-public class Buldozer extends Machine{
+public class Buldozer extends Machine {
     @Override
     public void doWork() {
         System.out.println("Buldozer starts work!");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Buldozer stops work!");
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -6,6 +6,7 @@ public class Truck extends Machine {
     public void doWork() {
         System.out.println("Truck starts work!");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Truck stops work!");
