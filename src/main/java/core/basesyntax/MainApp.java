4add49881d288a297b98d2d7dabf396c230ee87f
Oleg Stashkiv <stashkivolegg@gmail.com>
2023-06-29 21:21:58 +0300

    Add abstract class Machine and his descendants: Truck, Bulldozer, Excavator. Override methods doWork() and stopWork().

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.model.Bulldozer;
+import core.basesyntax.model.Excavator;
+import core.basesyntax.model.Machine;
+import core.basesyntax.model.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine [] machines = {new Excavator(), new Bulldozer(), new Truck()};
+        for (Machine m : machines) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
 }
