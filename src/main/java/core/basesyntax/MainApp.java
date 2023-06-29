0ed4ba9bff5e3aa7b5e466097fd518f385111271
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-06-29 22:56:53 +0300

    removed redundant empty lines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -6,12 +6,9 @@ public class MainApp {
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
         Machine[] machines = new Machine[] {truck, bulldozer, excavator};
-
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
     }
 }
-
-
