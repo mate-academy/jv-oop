0115afd573edcf1ebcec0e1ab751fa6fc3ee650c
Parabellum7 <109682043+Parabellum7@users.noreply.github.com>
2023-07-05 15:21:33 +0300

    Removed extra empty lines, make an array " machines" smaller in volume.

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
-
 abstract class Machine {
     public abstract void doWork();
-
     public abstract void stopWork();
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,12 +1,7 @@
 package core.basesyntax;
-
 public class MainApp {
-
     public static void main(String[] args) {
-        Machine[] machines = new Machine[3];
-        machines[0] = new Truck();
-        machines[1] = new Bulldozer();
-        machines[2] = new Excavator();
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
 
         for (Machine machine : machines) {
             machine.doWork();
