5a14a60a8bba24b571e6fd8906d0737f9b16bf60
Parabellum7 <109682043+Parabellum7@users.noreply.github.com>
2023-07-05 15:35:10 +0300

    Removed extra empty lines, make an array " machines" smaller in volume.

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
+
 public class Excavator extends Machine {
     public void doWork() {
         System.out.println("The Excavator started its work");
     }
+
     public void stopWork() {
         System.out.println("The Excavator has finished its work");
     }
