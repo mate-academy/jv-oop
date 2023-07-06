140d575ef01cfb5b05e910b0c21cd6df533c1cf0
Danial <danialbest55@gmail.com>
2023-07-06 18:16:46 +0300

    idk how i kant puss that

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,54 @@
 package core.basesyntax;
 
-public class MainApp {
-
+abstract class Machine {
+    public abstract void doWork();
+    public abstract void stopWork();
 }
+
+class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck has started working.");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck has stopped working.");
+    }
+}
+
+class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer has started working.");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer has stopped working.");
+    }
+}
+
+class Excavator extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Excavator has started working.");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator has stopped working.");
+    }
+}
+
+public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
+}
+
