7d4271939cadfb28f54f651d1e33d469487d0104
Nazario Lupen <nazario.lupen@gmail.com>
2023-07-09 21:57:43 +0300

    hw_java_oop_solutions

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,2 +1,14 @@
-package core.basesyntax;public class Excavator {
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Excavator started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator stopped its work");
+    }
 }
