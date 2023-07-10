b6c96ab563ba49f2106681ee4d44afe304ca8388
Nazario Lupen <nazario.lupen@gmail.com>
2023-07-10 17:51:52 +0300

    test_one

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Truck started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped its work");
+
+    }
+
+}
