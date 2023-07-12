cdfe748982a04817817f4cca186e64dcc001f736
Oleksii Borysenko <343502597s@gmail.com>
2023-07-12 15:11:33 +0200

    fertig

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class MainApp {
 
+    public static void main(String[] args) {
+        Bulldozer bulldozer = new Bulldozer();
+        Truck truck = new Truck();
+        Excavator excavator = new Excavator();
+        Machine [] machines = new Machine[] {bulldozer,truck,excavator};
+        bulldozer.doWork();
+        truck.doWork();
+        excavator.doWork();
+        bulldozer.stopWork();
+        truck.stopWork();
+        excavator.stopWork();
+    }
 }
