bc5afb00fc6641a83afc2fb7fad5bb8fe2eb1fb0
Artem Hlukhoman <giantking1231@gmail.com>
2023-07-14 22:32:53 +0300

    made cycle in main, added annotations

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 public class Excavator extends Machine {
+    @Override
     public void doWork() {
         System.out.println("Excavator starts doing work!");
     }
 
+    @Override
     public void stopWork() {
         System.out.println("Excavator stopped doing work :(");
     }
