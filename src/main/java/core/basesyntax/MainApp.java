93f8f0803bb35d6f95c61e71af2592e54c273ed4
Andrey Polezhaiev <polezhaievandrey28@gmail.com>
2023-07-19 00:26:39 +0300

    add objects of Machines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -7,9 +7,16 @@ import core.basesyntax.service.Machine;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machines = new Machine[]{new Bulldozer(), new Excavator(), new Truck()};
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        Truck truck = new Truck();
+        Machine[] machines = new Machine[]{bulldozer, excavator, truck};
+
         for (Machine machine: machines) {
             machine.doWork();
+        }
+
+        for (Machine machine: machines) {
             machine.stopWork();
         }
     }
