0b83bc249eb1002ef406d7c99201a3964ba9660f
Misha Kazarian <misha.kazaryan2@gmail.com>
2023-07-23 00:38:22 +0300

    add classes Machine, Truck, Bulldozer, Excavato and their implementation

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
 
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine buildozer = new Bulldozer();
+        Machine[] array = new Machine[]{truck, excavator, buildozer};
+
+        for (Machine x : array) {
+            x.doWork();
+            x.stopWork();
+        }
+    }
 }
