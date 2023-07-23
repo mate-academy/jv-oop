525b57f1917e4808be2891d207af36ded2bfea64
Oleksandr Pidhaiko <iviviv1999@gmail.com>
2023-07-24 02:10:50 +0300

    created machine classes with overrided methods. array with machines created in MainApp class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+  public static void main(String[] args) {
+    Machine[] arrayOfMachines = new Machine[]{new Bulldozer(),new Excavator(), new Truck()};
+    for (Machine machine : arrayOfMachines) {
+      machine.doWork();
+      machine.stopWork();
+    }
+  }
 }
