6136be62d753a9b79953c0de35043532b7fb27b8
Peresada Kostya <peresadakostya81@gmail.com>
2023-07-23 16:04:09 +0300

    Created class Machine containing methods public abstract void doWork() and public abstract void stopWork() Add classes Truck, Bulldozer, Excavator that will inherit from Machine and override doWork() and stopWork() methods In Main class was created Machine array with Truck, Buldozer and Excavator and call methods doWork() and stopWork() in a loop

--- src/main/java/core/basesyntax/Buldozer.java
+++ src/main/java/core/basesyntax/Buldozer.java
@@ -1,11 +1,9 @@
 package core.basesyntax;
-
 public class Buldozer extends  Machine{
     @Override
     public void doWork() {
         System.out.println("Buldozer started work");
     }
-
     @Override
     public void stopwork() {
         System.out.println("Buldozer stopped work");
