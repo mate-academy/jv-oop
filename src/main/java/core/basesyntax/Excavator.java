a6caecd9ae7717bd8b781aafd101ff39f2118fee
Vlad Rublenko <vladik.rar@ukr.net>
2023-07-23 18:38:46 +0300

    deleted whitespaces

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,14 +1,14 @@
 package core.basesyntax;
 
-public class Excavator extends Machine{
+public class Excavator extends Machine {
 
     @Override
-    public void doWork(){
+    public void doWork() {
         System.out.println("Excavator: start of work");
     }
 
     @Override
-    public void stopWork(){
+    public void stopWork() {
         System.out.println("Excavator: stop of work");
     }
 
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,14 +1,14 @@
 package core.basesyntax;
 
-public class Truck extends Machine{
+public class Truck extends Machine {
 
     @Override
-    public void doWork(){
+    public void doWork() {
         System.out.println("Truck: start of work");
     }
 
     @Override
-    public void stopWork(){
+    public void stopWork() {
         System.out.println("Truck: stop of work");
     }
 
