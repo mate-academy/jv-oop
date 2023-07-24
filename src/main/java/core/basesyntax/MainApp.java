3999a91d1157e464c2fbe873d6520252512c653a
ruslanPonomarev <brain@Brains-MacBook-Air.local>
2023-07-24 17:27:29 +0300

    Solved OOP Basic Task

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,16 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+
+        for (Machine machine : machines) {
+            machine.stopWork();
+        }
+    }
+
 }
