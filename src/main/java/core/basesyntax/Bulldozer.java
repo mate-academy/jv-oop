3b3b94d23e14f146962f37b77629dcafdd74d567
Oleksandr Pidhaiko <iviviv1999@gmail.com>
2023-07-24 03:02:19 +0300

    fixed checkstyle errors

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,12 +1,13 @@
 package core.basesyntax;
 
 public class Bulldozer extends Machine {
-  @Override
-  public void doWork() {
-    System.out.println("Bulldozer started its work.");
-  }
-  @Override
-  public void stopWork() {
-    System.out.println("Bulldozer stopped its work.");
-  }
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer started its work.");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer stopped its work.");
+    }
 }
