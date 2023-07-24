5c0f76562dea404dfbac96e971f69b018ba67da4
ruslanPonomarev <ruslan.ponomarev@wearebrain.com>
2023-07-24 18:25:21 +0300

    fix-remove_whitespaces

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,13 +1,11 @@
 package core.basesyntax;
 
-public class MainApp{
+public class MainApp {
 
-    public static void main(String[] args){
-        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
-        for (Machine machine : machines){
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : machines) {
             machine.doWork();
-        }
-        for (Machine machine : machines){
             machine.stopWork();
         }
     }
