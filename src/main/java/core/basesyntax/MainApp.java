f6cc09bc2bc9bc6ba2db549f49ec191142542862
Peresada Kostya <peresadakostya81@gmail.com>
2023-07-25 19:02:59 +0300

    Use a single space to separate non-whitespace characters

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -6,7 +6,6 @@ public class MainApp {
         Buldozer buldozer = new Buldozer();
         Excavator excavator = new Excavator();
         Machine [] machine = new Machine[] {truck, buldozer, excavator};
-
         for (int i = 0; i < machine.length; i++) {
             machine[i].doWork();
             machine[i].stopwork();
