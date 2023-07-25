fe6866515cd2a0951fe95109720ee362df323029
evgenkolner <evgen.kolner@gmail.com>
2023-07-25 15:44:31 +0300

    fixed the mistakes

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -2,12 +2,7 @@ package core.basesyntax;
 
 public abstract class Machine {
     public abstract void doWork();
-<<<<<<< HEAD
 
     public abstract void stopWork();
-=======
-    public abstract void stopWork();
-
-
->>>>>>> origin/hw-1
 }
+
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -3,14 +3,11 @@ package core.basesyntax;
 public class Truck extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Machine started its work");
+        System.out.println("Truck started its work");
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/hw-1
     @Override
     public void stopWork() {
-        System.out.println("Machine stopped its work");
+        System.out.println("Truck started its work");
     }
 }
