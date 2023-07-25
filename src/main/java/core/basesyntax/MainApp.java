faf850c6b8a2a6f64fd3108a11b8a24e0ecf0bb4
Peresada Kostya <peresadakostya81@gmail.com>
2023-07-25 18:34:58 +0300

    Classes Truck, Bulldozer, Excavator no public more.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
-    Truck truck = new Truck();
-    Buldozer buldozer = new Buldozer();
-    Excavator excavator = new Excavator();
-
-    public void work() {
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Buldozer buldozer = new Buldozer();
+        Excavator excavator = new Excavator();
         Machine [] machine = new Machine[] {truck, buldozer, excavator};
+
         for (int i = 0; i < machine.length; i++) {
             machine[i].doWork();
             machine[i].stopwork();
