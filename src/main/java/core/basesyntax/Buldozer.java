c4e51afc36db50266786370a6f6df89f7fe2fa9b
Peresada Kostya <peresadakostya81@gmail.com>
2023-07-25 20:05:20 +0300

    Use a single space to separate non-whitespace characters

--- src/main/java/core/basesyntax/Buldozer.java
+++ src/main/java/core/basesyntax/Buldozer.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-class Buldozer extends  Machine {
+class Buldozer extends Machine {
     @Override
     public void doWork() {
         System.out.println("Buldozer started work");
