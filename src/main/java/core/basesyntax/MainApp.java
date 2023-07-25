824cbab81e1f6986eaff21b101b93df7395e0cf9
Oleksandr Tymoshenko <aleksandrtimosenko089@gmail.com>
2023-07-25 23:00:56 +0300

    solved the task

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,15 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void machinesWork() {
+        Machine[] machines = new Machine[3];
+        machines[0] = new Truck();
+        machines[1] = new Excavator();
+        machines[2] = new Bulldozer();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
+
