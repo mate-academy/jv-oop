e1c8f82268fbd49047c0480a0a57501362341fcb
tbasy <t.basylkevych@gmail.com>
2023-07-27 22:57:30 +0300

    added class Machine, Truck, Bulldozer, Excavator and called methods doWork, stopWork

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
 public class MainApp {
+    public MainApp() {
+        String[] Machine;
+        String Truck = new String();
+        String Bulldozer = new String();
+        String Excavator = new String();
+        Machine = new String[]{Truck, Bulldozer, Excavator};
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
 
+        truck.doWork();
+        truck.stopWork();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+        excavator.doWork();
+        excavator.stopWork();
+    }
 }
