cab791c74c34450df5f7b48f85831feca669bcb3
Bohdan Maksymenko <bogdan.maksimenko.2002@gmail.com>
2023-07-28 23:10:52 +0300

    create abstract class Machine and some machines and override 2 methods of abstract classes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Excavator excavator = new Excavator();
+        Bulldozer bulldozer = new Bulldozer();
+        Truck truck = new Truck();
+        Machine[] machines = {excavator, truck, bulldozer};
+        for (Machine machine:machines){
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
