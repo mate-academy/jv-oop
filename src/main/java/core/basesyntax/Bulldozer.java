74d3f382daae598b34ba63a2bad82e0f6db9abb0
Oleksii Samoliuk <samoluk.aleksey@gmail.com>
2023-07-30 13:27:53 +0300

    Implemented task.

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine{
+
+    public Bulldozer(){
+
+    }
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer started working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer stopped working");
+    }
+}
