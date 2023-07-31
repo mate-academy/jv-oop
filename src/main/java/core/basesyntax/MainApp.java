62d1b4672ad21ff633c92b9a3370c61d158dcb91
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2023-07-31 17:36:54 +0300

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class MainApp {
     Machine[] machine = {new Bulldozer(), new Excavator(), new Truck()};
 
-    public void run() {
+        {
         for (int i=0; i <= machine.length; i++){
             machine[i].doWork();
             machine[i].stopWork();
