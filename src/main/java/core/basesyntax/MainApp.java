c7a50e451d14fd8af60d69eaa011bdfa28770e9a
tbasy <t.basylkevych@gmail.com>
2023-07-31 15:12:14 +0300

    fixed checkstyle mistakes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,15 +1,17 @@
 package core.basesyntax;
-public class MainApp {
-    public MainApp() {
-        String[] Machine = new String[]{"Truck", "Bulldozer", "Excavator"};
-        Machine truck = new Truck();
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+        Machine truck = new Truck();
         truck.doWork();
         truck.stopWork();
+
+        Machine bulldozer = new Bulldozer();
         bulldozer.doWork();
         bulldozer.stopWork();
+
+        Machine excavator = new Excavator();
         excavator.doWork();
         excavator.stopWork();
     }
