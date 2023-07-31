7479842ae085ce77909ab7d2c523dd2e3b0440a2
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2023-07-31 17:36:11 +0300

    Update Excavator.java

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -16,23 +16,4 @@ public class Excavator extends Machine {
         System.out.println("Excavator stop work");
     }
 }
-=======
-package core.basesyntax;
-
-public class Excavator extends Machine {
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public void doWork() {
-        System.out.println("Excavator start work");
-    }
-
-    @Override
-    public void stopWork() {
-        System.out.println("Excavator stop work");
-    }
-}
 
