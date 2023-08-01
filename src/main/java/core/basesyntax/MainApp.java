c8c158a24da38ee963c5b5e5d0a4f2b538d01a2f
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2023-08-01 09:12:22 +0300

    Update MainApp.java
    Change fori to foreach

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,9 +4,10 @@ public class MainApp {
     Machine[] machine = {new Bulldozer(), new Excavator(), new Truck()};
 
         {
-        for (int i=0; i <= machine.length; i++){
-            machine[i].doWork();
-            machine[i].stopWork();
+        for (Machine i : machine)
+        {
+            i.doWork();
+            i.stopWork();
         }
     }
 }
