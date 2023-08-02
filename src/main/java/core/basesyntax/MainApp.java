117ca3003bb43567c5d9f1876f9965d340167730
nazariykolesnikov000738 <140967381+nazariykolesnikov000738@users.noreply.github.com>
2023-08-03 00:34:22 +0300

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
+  Track newTrack = new Track();
+  Bulldozer newBulldozer = new Bulldozer() ;
+  Excavator newExcavator = new Excavator();
 
+  Machines[] machines = {  newTrack, newBulldozer, newExcavator };
+
+  for (Machines machine : machines) {
+    machine.doWork();
+    machine.stopWork();
+  }
 }
