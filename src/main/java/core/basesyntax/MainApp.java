2eb3b12750879276ae266da02d6be004dc60a6bd
anastezia12 <for.game.anaria@gmail.com>
2023-08-03 05:21:26 +0200

    2

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,9 +1,12 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class MainApp {
-    Machine machine[] = {new Truck(), new Bulldozer(), new Excavator()};
+    private Machine[] machine = {new Truck(), new Bulldozer(), new Excavator()};
 
     public MainApp() {
+
         for (Machine i : machine) {
             i.doWork();
         }
