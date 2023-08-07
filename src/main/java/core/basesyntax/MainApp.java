07a6c252b3074a3c2675ccc080f2741c57d80f49
Volodymyr Skvira <deruty13061999@gmail.com>
2023-08-07 12:42:23 +0300

    Fixed checkstyle mistakes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
-    public void doWork () {
+    public void doWork() {
         Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
         for (int i = 0; i < machines.length; i++) {
             machines[i].doWork();
