ff0e313d7a1de45dc89d81799be434b5169445da
Denis Gorda <dnsgorda@gmail.com>
2023-08-07 18:13:37 +0300

    Machines was created and methods was called.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public void main() {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
