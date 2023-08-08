1fd123188aca0d5908119e9831e9bdb27dd31251
silchenko-arsen <silchenkoarsen@gmail.com>
2023-08-08 18:53:31 +0300

    create exercise

---
+++ src/main/java/core/basesyntax/Buildozer.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Buildozer extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("Buildozer started work.");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Buildozer stoped work.");
+    }
+}
