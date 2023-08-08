e43ca3115a0e199762812a85cb388e8f57b8e12c
Oleh Oliiarnyk <oliver.aka.rid@gmail.com>
2023-08-08 15:43:16 +0300

    changed overrided methods

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
-
+        System.out.println("Machine started");
     }
 
     @Override
     public void stopWork() {
-
+        System.out.println("Machine stopped");
     }
 }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Truck extends Machine {
     @Override
     public void doWork() {
-
+        System.out.println("Machine started");
     }
 
     @Override
     public void stopWork() {
-
+        System.out.println("Machine stopped");
     }
 }
