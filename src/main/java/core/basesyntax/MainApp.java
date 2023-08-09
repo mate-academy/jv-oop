bfa7098857852a712947dc29153bf48deffd84c5
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-08-09 16:29:26 +0300

    added fixes after review

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
-        for (int i = 0; i < machines.length; i++) {
-            machines[i].doWork();
-            machines[i].stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
