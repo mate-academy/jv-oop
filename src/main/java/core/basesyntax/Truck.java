5b7a72bccda55e59a187b182088b07aa340e603d
Andrii Mikhaylovskiy <Andrew.chase999@gmail.com>
2023-08-11 21:04:58 +0300

    -remove all extra lines

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,12 +1,9 @@
 package core.basesyntax;
-
 public class Truck extends Machine {
-
     @Override
     public void doWork() {
         System.out.println("Truck started its work!");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Truck stopped its work!");
