c7d89c32c1c053ee3b60dd49204d3cf7f516c2b5
Anastasiia <anastasiiafi7@gmail.com>
2023-08-11 16:49:57 +0300

    added solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,16 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public void main(String[] args) {
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
+    private Machine Truck;
+    private Machine Bulldozer;
+    private Machine Excavator;
+    Machine[] machines = new Machine[]{Truck, Bulldozer, Excavator};
+
+
 }
