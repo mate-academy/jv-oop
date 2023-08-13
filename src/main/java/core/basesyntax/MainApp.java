a2cc0f46030081cca5d1291a110428e343b33fbd
IvanIKS <kryanus2005@gmail.com>
2023-08-13 18:59:43 +0300

    mvn clean package

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,18 +5,17 @@ In those classes override doWork(), so it will print message that certain machin
 Override stopWork() as well. It should print messages that certain machine stopped its work.
 In MainApp class create Machine array with Truck, Bulldozer and Excavator and call methods doWork() and stopWork() in a loop.*/
 public class MainApp {
-    public static void main (String[] args){
-    Machine[] machinesArray = new Machine[3];
-    machinesArray[0] = new Excavator();
-    machinesArray[1] = new Bulldozer();
-    machinesArray[2] = new Truck();
-
-    for (int i = 0; i < 3; ++i){
-        machinesArray[i].doWork();
-    }
-    for (int i = 0; i < 3; ++i){
-        machinesArray[i].stopWork();
-    }
+    public static void main (String[] args) {
+        Machine[] machinesArray = new Machine[3];
+        machinesArray[0] = new Excavator();
+        machinesArray[1] = new Bulldozer();
+        machinesArray[2] = new Truck();
+        for (int i = 0; i < 3; ++i) {
+            machinesArray[i].doWork();
+        }
+        for (int i = 0; i < 3; ++i) {
+            machinesArray[i].stopWork();
+        }
     }
 
 }
