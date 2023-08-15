8607ab7d5df5a785ba2d25e6fd141c4b336e13c5
Vlad Rublenko <vladik.rar@ukr.net>
2023-08-15 17:37:34 +0300

    deleted printl, renamed object to parent class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,16 +3,15 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
-        Truck truck = new Truck();
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
 
         Machine[] machines = {truck, bulldozer, excavator};
 
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
-            System.out.println("----------------");
         }
     }
 }
