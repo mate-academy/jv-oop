af3b80c13f79644be5a3196b66e13123f6006b23
tbasy <t.basylkevych@gmail.com>
2023-08-17 17:11:18 +0300

    Сreated loop forEach

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,16 +3,9 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
-        Machine truck = new Truck();
-        truck.doWork();
-        truck.stopWork();
-
-        Machine bulldozer = new Bulldozer();
-        bulldozer.doWork();
-        bulldozer.stopWork();
-
-        Machine excavator = new Excavator();
-        excavator.doWork();
-        excavator.stopWork();
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
