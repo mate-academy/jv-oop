d0243f4f6a1a2d811f16068eb1c7c52cefa61c7c
Oleksii Shpak <willwildred@gmail.com>
2023-08-20 12:05:59 +0200

    added psvm method with logic

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        Machine[] machines = {excavator, bulldozer, truck};
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
 }
