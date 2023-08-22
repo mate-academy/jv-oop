eddb154fea9aa6d566ed6b04ce10d6c4fa36dc97
TkachukTara <zaz42988@gmail.com>
2023-08-22 12:12:23 +0300

    Homework done!

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] typeOfMachine = new Machine[]{
+                new Truck(),
+                new Bulldozer(),
+                new Excavator()
+        };
 
+        for (Machine machine : typeOfMachine) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    @Override
+    public void  doWork(){
+        System.out.println("Truck Working");
+    }
+    @Override
+    public void  stopWork(){
+        System.out.println("Truck Stopped Working");
+    }
+}
