76e6bf58c00552d559a1ea4f721a7ac51d3cc862
Vladyslav Herasymenko <dalv23092005@gmail.com>
2023-08-26 21:20:36 +0200

    Created abstract class Machine with abstract methods doWork and stopWork and its consequences: Bulldozer, Excavator, Truck. In MainApp class created main method and realized it for using doWork and stopWork methods in every class.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,9 +1,7 @@
 package core.basesyntax;
 
-import core.basesyntax.machines.Bulldozer;
-import core.basesyntax.machines.Excavator;
-import core.basesyntax.machines.Machine;
-import core.basesyntax.machines.Truck;
+import core.basesyntax.machines.*;
+
 
 public class MainApp {
     public static void main(String[] args) {
