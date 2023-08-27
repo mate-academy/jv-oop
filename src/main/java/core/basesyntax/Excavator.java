1dc0b2cf93f5d654b3df3115f92aafa601f1febc
Illya <illios.piv@gmail.com>
2023-08-27 05:25:51 +0200

    Created classes `Bulldozer`, `Excavator`, `Truck` and `Machine`. Added abstract methods `doWork()`, `stopWork()` and override in those classes. Create `Machine` array with objects: `Truck`, `Bulldozer` and `Excavator`. Call methods `doWork()` and `stopWork()` in a loop.

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+public class Excavator extends Machine {
+    public void doWork() {
+        System.out.println("The Excavator started its work");
+    }
+    public void stopWork() {
+        System.out.println("The Excavator stopped its work");
+    }
+}
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+public class Truck extends Machine {
+    public void doWork() {
+        System.out.println("The Truck started its work");
+    }
+    public void stopWork() {
+        System.out.println("The Truck stopped its work");
+    }
+}
