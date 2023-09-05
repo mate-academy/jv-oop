f92721a478a558d2bd4e44fc2f93ddba6fde9d32
Kseniia Stepanchenko <red.xantoria@gmail.com>
2023-09-05 15:42:46 +0300

    [OOP-Basic]  initial solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
 
+        excavator.setMachineModel("Bob");
+        bulldozer.setMachineModel("Bultozavr");
+        truck.setMachineModel("KAMAZ");
+
+        Machine[] machines = new Machine[3];
+        machines[0] = excavator;
+        machines[1] = bulldozer;
+        machines[2] = truck;
+
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+
+    }
 }
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Truck " + getMachineModel() + " starts work!");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck " + getMachineModel() + " stopped work.");
+    }
+}
