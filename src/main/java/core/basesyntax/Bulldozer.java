bd660f8ab478ff50d12609b48ecf44b7a51b8a75
Vitalii Voitseshuk <voitseshuk.v@gmail.com>
2023-09-07 22:52:32 +0300

    overrided methods doWork and stopWork

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,2 +1,14 @@
-package core.basesyntax;public class Bulldozer {
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("The bulldozer starts working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The bulldozer stops working");
+    }
 }
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,2 +1,14 @@
-package core.basesyntax;public class Excavator {
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("The excavator starts working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The excavator stops working");
+    }
 }
