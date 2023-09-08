4ef4075ab8fc7ca3cf9ef3071d2a9171690dc183
Vladyslav Druhov <space.drugov@gmail.com>
2023-09-08 17:42:53 +0300

    Implemented the task on OOP

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck started work.");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped work.");
+    }
+}
