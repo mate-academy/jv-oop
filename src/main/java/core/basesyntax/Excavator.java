4f1230786822ee565b72ef85d7011715d97601d7
Serhii Bodnar <sergeybodnar34@gmail.com>
2023-09-10 19:45:58 +0300

    added solution

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+    public void doWork() {
+        System.out.println("The excavator began its work");
+    }
+
+    public void stopWork() {
+        System.out.println("The excavator stopped working");
+    }
+}
