47aec979366e983701b04dfaf31da87353ff09dc
Kostia Ibraiev <kostiantyn.ibraiev@gmail.com>
2023-09-12 22:01:33 -0600

    all classes are created, main class is completed

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Excavator extends Machine{
+
+  @Override
+  public void doWork() {
+    System.out.println("Excavator works");;
+  }
+
+  @Override
+  public void stopWork() {
+    System.out.println("Excavator stops");;
+  }
+  
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,8 +2,18 @@ package core.basesyntax;
 
 public class MainApp {
   public static void main(String[] args) {
-    System.out.println("test");
+    Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+
+    for (Machine machine : machines) {
+      machine.doWork();
+      machine.stopWork();
+    }
+
+
     
   }
 
 }
+
+//In MainApp class create Machine array with Truck, Bulldozer and Excavator 
+//and call methods doWork() and stopWork() in a loop.
