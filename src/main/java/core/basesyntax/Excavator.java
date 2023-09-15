9187955284e4e888e43eecd8e3785520520c96c7
Piotr Kaliszuk <p_kaliszuk@tlen.pl>
2023-09-15 22:01:19 +0200

    First Solution

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Excavator extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("Kopu-Kop");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("The Excavator stopped working");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machines = {new Bulldozer(), new Excavator(), new Truck()};
+        printMethodsInLoop(machines);
+    }
+    public static void printMethodsInLoop(Machine[] machines){
+        for (Machine machine : machines){
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
