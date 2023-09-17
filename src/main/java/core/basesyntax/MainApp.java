c2950bd53c2a42f1b0102f7842841f676a0d7f5c
Banderan <bartoszwojcik2004@gmail.com>
2023-09-17 11:01:27 +0200

    Commiting first try to compite Task & adding whitespace

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
-        for (Machine elementStart : machines){
+        for (Machine elementStart : machines) {
             elementStart.doWork();
         }
 
         System.out.println();
 
-        for (Machine elementStop : machines){
+        for (Machine elementStop : machines) {
             elementStop.stopWork();
         }
     }
