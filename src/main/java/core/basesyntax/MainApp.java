f6225edd83c3308757b5a203d2a28a0055cc1ffd
unknown <liubchenkalexander@gmail.com>
2023-09-19 21:25:45 +0300

    made classes and loop that use them

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] allMachine = new Machine[3];
+        allMachine[0] = truck;
+        allMachine[1] = bulldozer;
+        allMachine[2] = excavator;
+        for (Machine machine: allMachine) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
