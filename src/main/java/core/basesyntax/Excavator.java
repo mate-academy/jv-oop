d9b313dec1133b1509340299e0e5a835517aff45
Leonid Bondarenko <leonidbondarenko69@gmail.com>
2023-09-20 17:38:40 +0300

    the task is done 20_09_23.

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("This Excavator do work");
+
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("This Excavator stop work");
+    }
+}
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Truck extends Machine{
+
+    @Override
+    public void doWork() {
+        System.out.println("This Truck do work");
+
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("This Truck stop work");
+    }
+}
