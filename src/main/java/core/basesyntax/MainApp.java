a757ecc4feb648ed0f5d532a9bcf527bfa0b5060
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2023-09-20 19:30:21 +0300

    did the taskk

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -43,7 +43,7 @@ class Excavator extends Machine {
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machines = new Machine[] { new Truck(), new Bulldozer(), new Excavator() };
+        Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
 
         for (Machine machine : machines) {
             machine.doWork();
