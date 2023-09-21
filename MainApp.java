2c4c3a50ce54ae8c25c659f6dbcf9861599f5e78
Vadim Kadasiev <144882268+VadimKadasiev@users.noreply.github.com>
2023-09-21 20:28:43 +0300

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class MainApp {
+
+
+    public static void main(String[] args) {
+        Machine [] array = new Machine []{new Truck(), new Bulldozer(), new Excavator()};
+        for (int i = 0; i < array.length;i++){
+            array[i].doWork();
+            array[i].stopWork();
+        }
+    }
+}
