9e8fe62b8d2a395910125cb373b4c407bf503e86
AdamMudrak <mudrets2000@gmail.com>
2023-09-21 22:55:00 +0300

    Add a model of a truck with realization of the abstract machine methods

---
+++ src/main/java/core/basesyntax/Transport/Truck.java
@@ -0,0 +1,16 @@
+package core.basesyntax.Transport;
+
+import core.basesyntax.Machine;
+
+public class Truck extends Machine {
+
+  @Override
+  public void doWork() {
+    System.out.println("The truck started its work");
+  }
+
+  @Override
+  public void stopWork() {
+    System.out.println("The truck stopped its work");
+  }
+}
