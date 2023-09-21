6c096192b6a2648adbb01e675fe479ec6ad2145d
Danil Ponomarenko <danil.ponomvrenko@gmail.com>
2023-09-21 16:11:32 +0300

    Create abstract class Machine with methods doWork() & stopWork(), and their implementations in new classes Bulldozer, Excavator and Truck. In MainApp create an array with methods invocations in for each loop.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine[] machineArray = new Machine[] {truck, excavator, bulldozer};
 
+        for (Machine machine :
+                machineArray) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
