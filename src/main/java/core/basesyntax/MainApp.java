1662bd150bee51f69ce51da224ea10e10675a623
AdamMudrak <mudrets2000@gmail.com>
2023-09-21 23:04:35 +0300

    Add Machine array with Truck, Bulldozer and Excavator

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import core.basesyntax.Transport.Bulldozer;
+import core.basesyntax.Transport.Excavator;
+import core.basesyntax.Transport.Truck;
+
 public class MainApp {
   public static void main(String[] args) {
-
+    Machine truck = new Truck();
+    Machine excavator = new Excavator();
+    Machine bulldozer = new Bulldozer();
+    Machine[] machines = new Machine[] {truck, excavator, bulldozer};
   }
 }
