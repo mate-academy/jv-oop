83e39c78e0e9954fbdac1202f610a8bd3e4ca0f6
Vadim Kadasiev <144882268+VadimKadasiev@users.noreply.github.com>
2023-09-22 14:45:34 +0300

    Update Truck

--- Truck.java
+++ Truck.java
@@ -1,10 +1,10 @@
 package core.basesyntax;
 
 public class Truck extends Machine{
-
     public void doWork() {
         System.out.println("Truck started its work");
     }
+    
     public void stopWork(){
         System.out.println("Truck stopped its work");
     }
