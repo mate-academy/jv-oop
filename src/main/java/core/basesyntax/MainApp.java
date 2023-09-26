e4227023dcde4c2515d528beb91fcab21e1e92ea
Danil Ponomarenko <akame.dante13@gmail.com>
2023-09-26 11:24:18 +0300

    Simplified my code (removed unnecessary initialization in psvm)

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machineArray = new Machine[] {new Truck(), new Excavator(), new Bulldozer()};
-
         for (Machine machine :
                 machineArray) {
             machine.doWork();
