899a74626c9171b6cb0519e51d62417055033ad3
Nataliia Shtykh <forstudying93@gmail.com>
2023-09-27 14:38:15 +0300

    completed the task jv-oop-1

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck1 = new Truck();
+        Machine bulldozer1 = new Bulldozer();
+        Machine excavator1 = new Excavator();
+
+        Machine[] machines = new Machine[] {truck1, bulldozer1, excavator1};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 
 }
