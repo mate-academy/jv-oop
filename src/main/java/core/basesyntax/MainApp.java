d32c95324e95744bf76cbce38d13f78e7f5b52c7
White EdenI <danil.gayvoronsky2201@gmail.com>
2023-09-28 23:26:53 +0300

    Repaired all the things that needed to be repaired. Created the Main class and used runMachines method from MainApp

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,12 +1,8 @@
 package core.basesyntax;
 
 public class MainApp {
-    private Machine truck = new Truck();
-    private Machine bulldozer = new Bulldozer();
-    private Machine excavator = new Excavator();
-    private Machine[] machines = {truck, bulldozer, excavator};
 
-    public static void runMachines(Machine[] machines) {
+    public void runMachines(Machine[]machines) {
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
