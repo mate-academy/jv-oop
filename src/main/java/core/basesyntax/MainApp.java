d39294f72a34b7481f35fa9e178738cee1eb69c2
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-09-28 12:14:29 +0300

    added psvm and made some improvements after the code review

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -6,12 +6,11 @@ import core.basesyntax.model.Machine;
 import core.basesyntax.model.Truck;
 
 public class MainApp {
-    private Machine truck = new Truck();
-    private Machine bulldozer = new Bulldozer();
-    private Machine excavator = new Excavator();
-    private Machine[] machines = new Machine[] {truck, bulldozer, excavator};
-
-    public static void runMachines(Machine[] machines) {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machines = new Machine[] {truck, bulldozer, excavator};
         for (Machine machine: machines) {
             machine.doWork();
             machine.stopWork();
