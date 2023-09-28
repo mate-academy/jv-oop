ad379c1b0407edeef6db5c832d7a5d1fb1eb88b2
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2023-09-28 14:58:02 +0300

    Moved call new Excavator(), new Bulldozer(), new Truck() in array initialization Machine[] machines in class MainApp

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,10 +2,8 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Excavator excavator = new Excavator();
-        Bulldozer bulldozer = new Bulldozer();
-        Truck truck = new Truck();
-        Machine[] machines = new Machine[]{excavator, bulldozer, truck};
+
+        Machine[] machines = new Machine[]{new Excavator(), new Bulldozer(), new Truck()};
 
         for (Machine machine:machines) {
             machine.doWork();
