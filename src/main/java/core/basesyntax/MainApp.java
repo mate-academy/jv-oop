80cff6ad29445d26e77e20409303b2cb8acb83ad
Myself <wetball74@gmail.com>
2023-09-28 11:31:10 +0300

    Space removed, space added

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machineArr = new Machine[] {new Truck(),new Bulldozer(), new Excavator()};
+        Machine[] machineArr = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
         for (Machine machine : machineArr) {
             machine.doWork();
             machine.stopWork();
