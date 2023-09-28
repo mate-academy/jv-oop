304314b0e0a916ddbb401c1aca16bb1aae658b96
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2023-09-28 12:55:39 +0200

    solution jv-oop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
-public class MainApp {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class MainApp {
+  public static void main(String[] args) {
+	ArrayList<Machine> objectsOfMachine = new ArrayList<>();
+	objectsOfMachine.add(new Bulldozer());
+	objectsOfMachine.add(new Excavator());
+	objectsOfMachine.add(new Truck());
+	for (Machine el : objectsOfMachine) {
+	  el.doWork();
+	  el.stopWork();
+	}
+  }
 }
