6db84d1544137606cbefd346d6cf7e12f2376056
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2023-09-28 18:04:06 +0300

    created Truck, Bulldozer, Excavator and Machine classes; overrode doWork() and stopWork(); created Machine array and called methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
 
+        Machine[] machineArray = new Machine[] {truck, bulldozer, excavator};
+
+        for (Machine machine : machineArray) {
+            machine.doWork();
+            machine.stopWork();
+            System.out.println();
+        }
+    }
 }
