8f161a32e58e5919c14281c70e7b6f5ecbda8087
Nataliia Shtykh <forstudying93@gmail.com>
2023-09-28 09:04:52 +0300

    corrected mistake found during the review process

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,11 +2,8 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine truck1 = new Truck();
-        Machine bulldozer1 = new Bulldozer();
-        Machine excavator1 = new Excavator();
 
-        Machine[] machines = new Machine[] {truck1, bulldozer1, excavator1};
+        Machine[] machines = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
