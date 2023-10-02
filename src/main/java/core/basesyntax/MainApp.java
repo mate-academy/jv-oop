1ed778beb4feadd050aaf958f60513c6e9c75062
Kozharin-Vasul <kozharinvasyl@gmail.com>
2023-10-02 15:39:48 +0300

    checkstyle audit...

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void mainApp(String[] args) {
-        Machine[] machines  = new Machine[] { new Excavator(), new Bulldozer(), new Truck() };
+        Machine[] machines = new Machine[] { new Excavator(), new Bulldozer(), new Truck() } ;
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
