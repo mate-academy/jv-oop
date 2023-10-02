60b3e097763cd43c36a3b33062cf37b3ec997d55
Jakub <kuba.r.pawlowski@gmail.com>
2023-10-02 17:28:59 +0200

    I did it

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,10 @@
 package core.basesyntax;
 
 public class MainApp {
+    Machine excavator = new Excavator();
+    Machine bulldozer = new Bulldozer();
+    Machine truck = new Truck();
+    Machine[] machines = new Machine[] {excavator,bulldozer,truck};
+
 
 }
