d8172bdcf9a915bdb9014dcc8421bd2c9d234206
Vlad <vladivlev666@gmail.com>
2023-10-07 16:54:28 +0300

    the first result of doing homework on the oopÑÑ

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine{
+    @Override
+    public void doWork() {
+        System.out.println("The bulldozer has started work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer stops working");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Bulldozer();
+        Machine[] allMachine = {excavator, bulldozer, truck};
+        for (int i = 0; i < allMachine.length; i++) {
+            allMachine[i].doWork();
+            allMachine[i].stopWork();
+        }
+    }
 }
