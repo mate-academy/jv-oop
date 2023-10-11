79c8059e12d50c7c9d958253e1a3afab89acd9fd
Snizhana Sosiedova <snejik1111@gmail.com>
2023-10-11 11:55:05 +0300

    Added classes Truck, Bulldozer, Excavator and their methods.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,18 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+
+        Machine[] machineArray = new Machine[] {truck, bulldozer, excavator};
+
+        for (Machine machine : machineArray) {
+            machine.doWork();
+            machine.stopWork();
+        }
+
+    }
+
 }
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+public class Truck extends Machine{
+
+    public Truck(){
+
+    }
+
+    @Override
+    public void doWork() {
+        System.out.println("Truck started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped its work");
+    }
+}
