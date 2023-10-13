7917b8ee48e9cad226d2ccead1be06bf4dee6df8
Maksym Shulypa <maximussamsung1@gmail.com>
2023-10-13 18:43:58 +0300

    done with all checks

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[3];
+        machines[0] = new Bulldozer();
+        machines[1] = new Truck();
+        machines[2] = new Excavator();
+        for(Machine machine : machines) {
+         machine.doWork();
+         machine.stopWork();
+        }
+    }
 }
