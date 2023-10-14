0e9ce2bffd010627045a644ef8b81689d7ca9c4b
Misha <mishadyakivnich@gmail.com>
2023-10-14 16:10:18 +0300

    ffrom ide

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        final Machine[] machine = new Machine[3];
+        machine[0] = new Bulldozer();
+        machine[1] = new Excavator();
+        machine[2] = new Truck();
 
+        for (Machine value : machine) {
+            value.doWork();
+            value.stopWork();
+        }
+    }
 }
+
