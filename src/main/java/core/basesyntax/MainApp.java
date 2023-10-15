5818b895baf6f47a28bc9c698e1151b449bd98a4
Dominik Gągało <cliffcamer@gmail.com>
2023-10-15 11:37:08 +0200

    OOP basic program

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main (String[] args) {
+        Machine[] machines = {
+                new Machines.Truck(),
+                new Machines.Bulldozer(),
+                new Machines.Excavator()
+        };
+
+        for(Machine machine : machines) {
+            machine.doWork();
+        }
+
+        for(Machine machine : machines) {
+            machine.stopWork();
+        }
+    }
 
 }
