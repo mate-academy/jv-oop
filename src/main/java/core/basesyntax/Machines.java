334a54961b07c1a9a05dbea8bf61296616c3c790
Dominik Gągało <cliffcamer@gmail.com>
2023-10-15 12:01:19 +0200

    Fixed checkstyle errors v1

--- src/main/java/core/basesyntax/Machines.java
+++ src/main/java/core/basesyntax/Machines.java
@@ -1,40 +1,41 @@
 package core.basesyntax;
 
 public abstract class Machines {
-    public static class Truck extends Machine {
-        @Override
-        public void doWork() {
-            System.out.println("Truck started its work");
-    }
-        @Override
-        public void stopWork() {
-            System.out.println("Truck stopped working");
+        public static class Truck extends Machine {
+            @Override
+            public void doWork() {
+                System.out.println("Truck started its work");
         }
-    }
 
-    public static class Bulldozer extends Machine {
-        @Override
-        public void doWork() {
-            System.out.println("Bulldozer started its work");
+            @Override
+            public void stopWork() {
+                System.out.println("Truck stopped working");
+            }
         }
-        @Override
-        public void stopWork() {
+
+        public static class Bulldozer extends Machine {
+            @Override
+            public void doWork() {
+                System.out.println("Bulldozer started its work");
+            }
+
+            @Override
+            public void stopWork() {
             System.out.println("Bulldozer stopped working");
+            }
         }
-    }
 
-    public static class Excavator extends Machine {
-        @Override
-        public void doWork() {
-            System.out.println("Excavator started its work");
+        public static class Excavator extends Machine {
+            @Override
+            public void doWork() {
+                System.out.println("Excavator started its work");
+            }
+
+            @Override
+            public void stopWork() {
+                System.out.println("Excavator stopped working");
+            }
         }
-        @Override
-        public void stopWork() {
-            System.out.println("Excavator stopped working");
-        }
-    }
-
-
 }
 
 
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,18 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Machine[] machines = {
                 new Machines.Truck(),
                 new Machines.Bulldozer(),
                 new Machines.Excavator()
         };
 
-        for(Machine machine : machines) {
+        for (Machine machine : machines) {
             machine.doWork();
         }
 
-        for(Machine machine : machines) {
+        for (Machine machine : machines) {
             machine.stopWork();
         }
     }
