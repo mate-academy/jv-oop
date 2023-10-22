59769bb6805d348f90596f1590fe43813badfd36
Obusker <mr.kudryachenko@gmail.com>
2023-10-22 21:05:12 +0300

    Home Work

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,86 @@
 package core.basesyntax;
 
-public class MainApp {
+abstract class Machine {
+    public abstract void doWork();
 
+    public abstract void stopWork();
+}
+
+abstract class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped its work");
+    }
+}
+
+abstract class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer stopped its work");
+    }
+}
+
+abstract class Excavator extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Excavator started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator stopped its work");
+    }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+      Machine bulldozer = new Bulldozer() {
+          @Override
+          public void doWork() {
+              super.doWork();
+          }
+
+          @Override
+          public void stopWork() {
+              super.stopWork();
+          }
+      };
+      Machine truck = new Truck() {
+          @Override
+          public void doWork() {
+              super.doWork();
+          }
+
+          @Override
+          public void stopWork() {
+              super.stopWork();
+          }
+      };
+      Machine excavator = new Excavator() {
+          @Override
+          public void doWork() {
+              super.doWork();
+          }
+
+          @Override
+          public void stopWork() {
+              super.stopWork();
+          }
+      };
+      Machine[] machines = {bulldozer, truck, excavator};
+      for (Machine machine : machines) {
+          machine.doWork();
+          machine.stopWork();
+      }
+  }
 }
