68338bbf71e108ae3e691a60cfde517def6834a1
Bohdan <bohdan.g.54@gmail.com>
2023-10-24 22:17:29 +0300

    added need functionality

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,29 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.machine.Bulldozer;
+import core.basesyntax.machine.Excavator;
+import core.basesyntax.machine.Machine;
+import core.basesyntax.machine.Truck;
 
+public class MainApp {
+    // STUPID, USE MAIN METHOD
+    private static Machine[] machines = new Machine[3];
+    private static void initialize() {
+        machines[0] = new Excavator();
+        machines[1] = new Bulldozer();
+        machines[2] = new Truck();
+        
+    }
+
+    public static void execute() {
+        initialize();
+        for (Machine m:machines) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
+
+    public static void main(String[] args) {
+        execute();
+    }
 }
---
+++ src/main/java/core/basesyntax/machine/Excavator.java
@@ -0,0 +1,16 @@
+package core.basesyntax.machine;
+
+public class Excavator extends Machine{
+    public Excavator() {
+
+    }
+    @Override
+    public void doWork() {
+        System.out.println("Excavator started working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator stopped working");
+    }
+}
