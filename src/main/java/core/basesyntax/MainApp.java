5c376a1eefb3ad0cc05a2e8860cdbf509fc45275
bobik <oliferchukrivne@i.ua>
2023-10-25 15:00:16 +0300

    jv-oop task done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine[] m = new Machine[3];
+        m[0] = new Excavator();
+        m[1] = new Bulldozer();
+        m[2] = new Truck();
+        for (Machine machine : m) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
