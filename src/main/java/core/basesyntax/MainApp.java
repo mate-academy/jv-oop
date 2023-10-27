712fa78832e601a2b5b177d62f5c3f86cd7dbaf5
Sasha <sasha.panchyshun@gmail.com>
2023-10-27 15:15:04 +0300

    little changes in MainApp

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,9 +2,11 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machines = new Machine[]{new Excavator(),
+        Machine[] machines = {
+                new Excavator(),
                 new Bulldozer(),
-                new Truck()};
+                new Truck()
+                };
         for (Machine machine: machines) {
             machine.doWork();
             machine.stopWork();
