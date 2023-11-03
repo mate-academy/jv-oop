de8479ce5c2580b7850777799f671b1a10617fa1
Andrii <allias5000@gmail.com>
2023-11-03 22:52:34 +0200

    Added required classes and methods, added loop at MainApp class that is calling new methids for new objects

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Machine {
+
+    public void doWork() {
+        System.out.println("Machine started working");
+    }
+
+    public void stopWork() {
+        System.out.println("Machine stopped working");
+    }
+
+}
+
