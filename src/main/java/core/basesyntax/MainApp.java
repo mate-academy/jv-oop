29c7a1e1b4beda7029aeed8bdec7dce9ab3a763b
Soqiable <sooqiable@gmail.com>
2023-11-05 12:24:24 +0200

    ... 1. created abstract class - Machine 2. created other inherited classes 3. implemented solution in Main class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,7 +5,6 @@ public class MainApp {
         Machine[] machines = new Machine[] {
             new Truck(), new Bulldozer(), new Excavator()
         };
-
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
