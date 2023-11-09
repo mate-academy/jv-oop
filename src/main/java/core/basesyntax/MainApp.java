2889b5fc5574b0820b8206cc9e5626e1060ac540
Ciocio1987 <147817009+Ciocio1987@users.noreply.github.com>
2023-11-09 11:25:29 +0100

    Correction of empty lines in MainApp

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,8 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
-    
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Machine track = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
@@ -13,8 +12,3 @@ public class MainApp {
         }
     }
 }
-
-
-
-
-
