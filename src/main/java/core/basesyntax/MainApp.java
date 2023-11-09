3e823ec5c7364fcd2b5023d8239b2640f5e4f74a
Nikita <nik.pogrebnoy@gmail.com>
2023-11-09 23:32:41 +0200

    added classes, made some overrides.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,13 @@
 package core.basesyntax;
 
 public class MainApp {
+    Machine [] machines = new Machine[2];
 
+    for (int i = 0; i < 2; i++) {
+        machines[i].doWork();
+    }
+
+    for (int i = 0; i < 2; i++) {
+        machines[i].stopWork();
+    }
 }
