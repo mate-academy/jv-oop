9a26f26dd42a9d750f913bb8e630d1433e93e67a
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:02:43 +0200

    solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machine = {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine m1 : machine) {
+            m1.doWork();
+            m1.stopWork();
+        }
+    }
 }
+
