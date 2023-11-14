ad96204fc7545e99bc3738a66b19def82976903c
SerJey <haccent12@gmail.com>
2023-11-14 17:44:43 +0100

    made first realisation

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] array = new Machine[3];
+        array[0] = new Truck();
+        array[1] = new Bulldozer();
+        array[2] = new Excavator();
 
+        for (Machine machine : array) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
+//In the MainApp class create a Machine array with Truck, Bulldozer,and Excavator
+// and call methods doWork() and stopWork() in a loop.
