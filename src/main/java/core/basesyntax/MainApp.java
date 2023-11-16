49d51adc19e3d9ff78b27e3d9026b1240d420487
Oleh <mihailyakyb45@gmail.com>
2023-11-16 18:29:15 +0200

    homework done3

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Truck(), new Excavator(), new Bulldozer()};
+
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
