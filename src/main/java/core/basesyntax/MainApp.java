bd76b780c006b682d39b90e4e81499898d71b59f
Yan Tretiak <tretiak.199801@gmail.com>
2023-11-22 11:38:27 +0200

    Do home task

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] arrayMachine = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
 
+        for (Machine machine : arrayMachine) {
+            System.out.println("");
+            machine.doWork();
+            machine.stopWork();
+            System.out.println("");
+        }
+    }
 }
