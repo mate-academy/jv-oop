bc66d472e1719c3d0b3e647949e1f68eb537d032
Yarolsav <kiritok712@gmail.com>
2023-11-23 18:15:34 +0100

    HMjv-oop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,9 +2,8 @@ package core.basesyntax;
 
 abstract class Machine {
     public abstract void doWork();
-
     public abstract void stopWork();
-}
+    }
 
 class Truck extends Machine {
     @Override
