1b1e79ae9ced0d99cddeccbd09f84ab2995e940b
Roman Soproniuk <anteumbra@gmail.com>
2023-11-25 20:48:21 +0200

    task done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,13 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machine = { new Excavator(), new Bulldozer(), new Truck() };
+        int count = 0;
+        while (machine.length > count) {
+            machine[count].doWork();
+            machine[count].stopWork();
+            count++;
+        }
+    }
 }
