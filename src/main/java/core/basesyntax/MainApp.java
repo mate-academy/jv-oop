13022c944fea9cf7cdc9318e8a3f9c4530a62e05
Roman Soproniuk <anteumbra@gmail.com>
2023-11-26 12:15:05 +0200

    for instead while

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,11 +3,9 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machine = { new Excavator(), new Bulldozer(), new Truck() };
-        int count = 0;
-        while (machine.length > count) {
-            machine[count].doWork();
-            machine[count].stopWork();
-            count++;
+        for (int i = 0; i < machine.length; i++) {
+            machine[i].doWork();
+            machine[i].stopWork();
         }
     }
 }
