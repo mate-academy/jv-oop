09cce23ca640d2b379f3f559ceb6b4fa4e86fc74
Oleksandr Kalenyk <carbonmail88@gmail.com>
2023-11-29 21:14:23 +0200

    Added main method in MainApp class and fixed overrides in Excavator class

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
-        System.out.println("The bulldozer started it's work");
+        System.out.println("The excavator started it's work");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("The bulldozer has stopped it's work");
+        System.out.println("The excavator has stopped it's work");
     }
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,23 +1,20 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machineList = new Machine[]{
+                truck,
+                bulldozer,
+                excavator
+        };
 
-    private Machine truck = new Truck();
-    private Machine bulldozer = new Bulldozer();
-    private Machine excavator = new Excavator();
-    private Machine[] machineList = new Machine[] {
-            truck,
-            bulldozer,
-            excavator
-    };
-
-    public void eachWork() {
         for (Machine machine : machineList) {
             machine.doWork();
         }
-    }
 
-    public void eachStop() {
         for (Machine machine : machineList) {
             machine.stopWork();
         }
