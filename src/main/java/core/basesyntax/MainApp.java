68dff3272faad09c5ae1612c4ae227b27091836c
YEvhen Korobka <korobkaevgeny@gmail.com>
2023-11-30 08:32:35 +0200

    show-work

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,16 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine[] machine = new Machine[] {
+                new Truck(),
+                new Bulldozer(),
+                new Excavator()
+        };
+        for (Machine show: machine) {
+            show.doWork();
+            show.stopWork();
+        }
+    }
+
 }
