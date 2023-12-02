2d682b01d9a1677c7298d063c1f65710f6e66e81
Orest 40th <student2528@bmet.org.ua>
2023-12-02 14:11:33 +0200

    deleted redundant lines used method override where possible

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -6,8 +6,8 @@ public class Bulldozer extends Machine {
         System.out.println("Bulldozer started working");
     }
 
+    @Override
     public void stopWork() {
         System.out.println("Bulldozer stopped working");
-
     }
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -10,10 +10,6 @@ public class MainApp {
         for (Machine machine: machines) {
             machine.doWork();
             machine.stopWork();
-
         }
     }
 }
-
-
-
