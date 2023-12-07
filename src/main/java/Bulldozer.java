b9da985b764961ed844d13ed851142d5c306cc64
Maryna Bodina <bananafox2002@gmail.com>
2023-12-07 15:15:21 +0200

    Transport with checked maven

---
+++ src/main/java/Bulldozer.java
@@ -0,0 +1,10 @@
+public class Bulldozer extends Machine {
+    public void doWork() {
+        System.out.println("Bulldozer do work");
+    }
+
+    public void stopWork() {
+        System.out.println("Bulldozer stop work");
+    }
+}
+
