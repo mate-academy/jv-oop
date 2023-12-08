c3e344e688fe88fdbff3fb09c006a4e444653487
Rostyslav Olekha <ros123ole@gmail.com>
2023-12-08 17:47:00 +0200

    finished my work

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public  class Bulldozer extends Machine {
+    @Override
+    public  void doWork(){
+        System.out.print("Bulldozer started its work  ");
+    }
+    @Override
+    public  void stopWork(){
+        System.out.print("Bulldozer stopped working");
+    }
+}
