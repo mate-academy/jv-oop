386d62a2438eee61e493dcc0a94d4a28f2734de0
Ilia Artemchuk <illaartemhyk@gmail.com>
2023-12-12 23:42:55 +0200

    Було сторено класи та створено методи

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+
+    @Override
+    public void doWork() {
+        System.out.println("Work Excavator");
+    }
+
+    @Override
+    public void stopWor() {
+        System.out.println("Stop work Excavator");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,24 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+
+        Machine [] arrayMachine = new Machine[3];
+        arrayMachine[0]=truck;
+        arrayMachine[1]=excavator;
+        arrayMachine[2]=bulldozer;
+
+        for (Machine element : arrayMachine){
+            element.doWork();
+            element.stopWor();
+        }
+
+    }
+
+
+
+
 }
