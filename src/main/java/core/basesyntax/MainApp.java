810329af11d7fa3d861be8553866ee888fce4348
Marek Koscielewski <pwmarek96@gmail.com>
2023-12-13 13:59:16 +0100

    Main Class implementation

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machineArray = {new Truck(), new Bulldozer(), new Excavator()};
+        for (int i = 0; i < 3; i++) {
+            machineArray[i].doWork();
+            machineArray[i].stopWork();
+        }
+    }
 }
