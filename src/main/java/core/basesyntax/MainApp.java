35688420bd3868c1f7cd0812bf4e708a559437d5
Artem <artem.kozakevych@gmail.com>
2023-12-17 12:17:39 +0200

    new class and extends

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.machine.Buldozer;
+import core.basesyntax.machine.Excavator;
+import core.basesyntax.machine.Machine;
+import core.basesyntax.machine.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[]{
+                new Buldozer(),
+                new Excavator(),
+                new Truck()
+        };
+        System.out.println("Start Program");
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
