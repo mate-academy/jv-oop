e449177a597471f10877b1f1f6d2a18018648ff9
NekitCool1 <gnnikita97@gmail.com>
2023-12-24 18:49:34 +0200

    All mistakes were fixed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,9 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machines =  {new Truck(), new Bulldozer(), new Excavator()};
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
         for (int i = 0; i < machines.length; i++) {
             machines[i].doWork();
+            machines[i].stopWork();
         }
     }
 }
