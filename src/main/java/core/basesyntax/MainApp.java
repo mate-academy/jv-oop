36873d7455d2b0ee4035d105831a4ddee66500da
Yuliia <innishastudy@gmail.com>
2023-12-24 14:44:35 +0200

    In the MainApp called methods doWork() and stopWork() in a loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,29 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        String[] machines = {
+                "Truck",
+                "Bulldozer",
+                "Excavator"
+        };
 
+        for (String typeOfMachine : machines) {
+            switch (typeOfMachine) {
+                case "Truck" : Truck truck = new Truck();
+                              truck.doWork();
+                              truck.stopWork();
+                              break;
+                case "Bulldozer" : Bulldozer bulldozer = new Bulldozer();
+                              bulldozer.doWork();
+                              bulldozer.stopWork();
+                              break;
+                case "Excavator" : Excavator excavator = new Excavator();
+                              excavator.doWork();
+                              excavator.stopWork();
+                              break;
+                default : break;
+            }
+        }
+    }
 }
