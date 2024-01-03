4a1b61457379132efdb5f1c639e9bda87790aad8
Yana Stepanova <yanna.stepanova@gmail.com>
2024-01-03 21:53:14 +0200

    added fixes after review

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,17 +1,12 @@
 package core.basesyntax;
 
-import java.util.ArrayList;
-
 public class MainApp {
     public static void main(String[] args) {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-        ArrayList<Machine> arrayList = new ArrayList<Machine>();
-        arrayList.add(truck);
-        arrayList.add(bulldozer);
-        arrayList.add(excavator);
-        for (Machine machine : arrayList) {
+        Machine[] machines = {truck, bulldozer, excavator};
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
