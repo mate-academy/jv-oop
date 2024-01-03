d85daa7c2c23b65a9546e1b56245c86bfe2171f4
Yana Stepanova <yanna.stepanova@gmail.com>
2024-01-03 21:33:17 +0200

    Added a solution to the task.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
 
-public class MainApp {
+import java.util.ArrayList;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        ArrayList<Machine> arrayList = new ArrayList<Machine>();
+        arrayList.add(truck);
+        arrayList.add(bulldozer);
+        arrayList.add(excavator);
+        for (Machine machine : arrayList) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
