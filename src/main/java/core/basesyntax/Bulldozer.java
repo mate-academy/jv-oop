28eb88209ae22dcbbc57c5f3031ca17b5bdb40d9
Liza <lizabalashova7@gmail.com>
2024-01-05 10:57:15 +0200

    upd hw-oop

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,14 +1,12 @@
 package core.basesyntax;
 
-public class Bulldozer extends Machine{
+public class Bulldozer extends Machine {
     @Override
-    public void doWork(){
+    public void doWork() {
         System.out.println("Bulldozer start work");
     }
     @Override
     public void stopWork(){
         System.out.println("Bulldozer stop work");
     }
-
-
 }
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
-public class Excavator extends Machine{
+public class Excavator extends Machine {
     @Override
-    public void doWork(){
+    public void doWork() {
         System.out.println("Excavator start work");
     }
     @Override
-    public void stopWork(){
+    public void stopWork() {
         System.out.println("Excavator stop work");
     }
 }
