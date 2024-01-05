39eb2d074179135f47e8ef7b54a47a984f58ff4a
Liza <lizabalashova7@gmail.com>
2024-01-05 11:01:12 +0200

    upd_2 hw-oop

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -5,8 +5,9 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Bulldozer start work");
     }
+
     @Override
-    public void stopWork(){
+    public void stopWork() {
         System.out.println("Bulldozer stop work");
     }
 }
