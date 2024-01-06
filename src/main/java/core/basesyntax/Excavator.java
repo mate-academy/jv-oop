80435203475f0d6010ca74097b1c10d5ae60fc2b
ShevaUK <shevchuck00777@gmail.com>
2024-01-06 12:54:45 +0200

    Created abstract class and subclasses. Implements abstract methods in subclasses/ Created array with machines and call methods in loop.

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
-class Excavator extends Machine{
+class Excavator extends Machine {
     @Override
     public void doWork() {
         System.out.println("Excavator started its work.");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Excavator stopped working.");
