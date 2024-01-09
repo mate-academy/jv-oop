02f11a69ace0ed62dc024b444cec36d978a8347c
Oleksandr Farion <sashafarion@gmail.com>
2024-01-09 19:14:46 +0200

    added classes Machine, Truck, Bulldozer and Excavator. Overrided methods.

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,2 +1,16 @@
-package core.basesyntax;public class Excavator {
+package core.basesyntax;
+
+public class Excavator extends Machine {
+    public Excavator() {
+    }
+
+    @Override
+    public void doWork() {
+        System.out.println("The excavator doing the work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The excavator stopped working");
+    }
 }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,2 +1,16 @@
-package core.basesyntax;public class Truck {
+package core.basesyntax;
+
+public class Truck extends Machine {
+    public Truck() {
+    }
+
+    @Override
+    public void doWork() {
+        System.out.println("The truck doing the work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The truck stopped working");
+    }
 }
