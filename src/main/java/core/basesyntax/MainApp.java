36661a53d23c857c273344f4d6fd2e3b778a3f9f
moskalenkosergii <moskalenko.s.work@gmail.com>
2024-01-12 17:15:29 +0100

    implement task

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.vehicle.Bulldozer;
+import core.basesyntax.vehicle.Excavator;
+import core.basesyntax.vehicle.Machine;
+import core.basesyntax.vehicle.Truck;
 
+import javax.crypto.Mac;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+
+        for (Machine machine:machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
