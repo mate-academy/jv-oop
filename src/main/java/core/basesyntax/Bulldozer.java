5e8b114a087b2afaa4ce141e347e7e2771056f06
Sergey Sukhachov <ssukhachov@gmail.com>
2024-01-20 17:07:01 +0200

    machines created

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer does work: trrr trrr tududum");
+    }
+    public void stopWork() {
+        System.out.println("Bulldozer stops work");
+    }
+
+}
