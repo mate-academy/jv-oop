15b73dd58518087cc2aa019a51fbd40364eec6be
Artem Akymenko <akimenko.artem@gmail.com>
2024-01-21 11:20:56 +0200

    Fixed checkstyle.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,8 +4,8 @@ public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
         for (Machine machine : machines) {
-        machine.doWork();
-        machine.stopWork();
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
