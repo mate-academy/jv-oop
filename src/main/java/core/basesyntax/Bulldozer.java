82525235a920099626432430269d06f8806c5740
MacioJanisz <janiszewski.maciej1@gmail.com>
2024-01-22 14:23:10 +0100

    fixes after code review -remove empty lines

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -4,9 +4,7 @@ public class Bulldozer extends Machine {
     @Override
     public void doWork() {
         System.out.println("Machine is working");
-
     }
-
     @Override
     public void stopWork() {
         System.out.println("Machines stopped working");
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,12 +1,10 @@
 package core.basesyntax;
 
 public class Excavator extends Machine {
-
     @Override
     public void doWork() {
         System.out.println("Machines are working");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Machines stopped working");
