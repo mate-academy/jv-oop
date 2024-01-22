9b172542bf1a774a5ee9a40ba75b2d8681e92e91
MacioJanisz <janiszewski.maciej1@gmail.com>
2024-01-22 14:27:56 +0100

    fixes after code review by maven -add lines

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -5,6 +5,7 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Machine is working");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Machines stopped working");
