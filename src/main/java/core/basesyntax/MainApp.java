3c02445f8f74b191302d2febc1978750f94e9b0d
Владислав Чабак <vlad.chabak@gmail.com>
2024-01-23 14:25:32 +0000

    done5

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -6,7 +6,7 @@ public class MainApp {
 
         Machine[] machines;
 
-        machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+        machines = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
 
         for (Machine machine : machines) {
             machine.doWork();
