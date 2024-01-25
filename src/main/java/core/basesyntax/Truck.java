6d163c1a957b6bd5aa73deb28be07ab545659f8b
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-01-25 13:16:36 +0200

    added psvm method created abstract class Machine with abstract methods doWork and stopWord created classes Truck, Bulldozer, Excavator with overrided methods

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+import mypackage.Machine;
+
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck started working.");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped working.");
+    }
+}
