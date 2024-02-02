96cf3641335061233b80e491cac1fc7d845ac11a
TonyH277 <robhobs50@gmail.com>
2024-02-02 16:19:27 +0200

    Implement OOP Basic logic

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.model.Bulldozer;
+import core.basesyntax.model.Excavator;
+import core.basesyntax.model.Machine;
+import core.basesyntax.model.Truck;
 
+public class MainApp {
+    private static Machine truck = new Truck();
+    private static Machine bulldozer = new Bulldozer();
+    private static Machine excavator = new Excavator();
+    private static Machine[] machines = new Machine[]{truck, bulldozer, excavator};
+
+    public static void main(String[] args) {
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
