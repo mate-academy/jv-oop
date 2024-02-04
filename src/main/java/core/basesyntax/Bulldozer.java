964d1e8ee0d38a938889dcfe7b7603cb0cbd9df0
Taras Fedak <tarasfedak@me.com>
2024-02-04 15:03:35 +0200

    Completed task

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,2 +1,13 @@
-package core.basesyntax;public class Bulldozer {
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println(this.getClass().getSimpleName() + " starts working.");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println(this.getClass().getSimpleName() + " stops working.");
+    }
 }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,2 +1,14 @@
-package core.basesyntax;public class Truck {
+package core.basesyntax;
+
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println(this.getClass().getSimpleName() + " starts working.");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println(this.getClass().getSimpleName() + " stops working.");
+    }
 }
+
