d0092d7c76aa0a29aed3121017f27e28e7336357
Andriy Gromysh <andriygromysh@ukr.net>
2024-02-09 12:54:51 +0200

    Blank lines removed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,14 +2,13 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-
         Truck truck = new Truck();
         Bulldozer bulldozer = new Bulldozer();
         Excavator excavator = new Excavator();
 
-        Machine[] machines = new Machine[] {truck, bulldozer, excavator };
+        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
 
-        for (Machine machine: machines) {
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
