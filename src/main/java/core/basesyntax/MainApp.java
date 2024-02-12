b68419593e14f9858b9f9478cbf099eeba322d79
HAIOVIY <gaevoywork@gmail.com>
2024-02-12 15:28:30 +0200

    fix short names, remove white space from classes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machine = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
-        for (Machine m : machine) {
-            m.doWork();
-            m.stopWork();
+        for (Machine someMachine : machine) {
+            someMachine.doWork();
+            someMachine.stopWork();
         }
     }
 }
