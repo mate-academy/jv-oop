491734541a3e5dc56ae85b66a54bf48f529303fe
Pavlo Betsa <bigworld2303@gmail.com>
2024-02-13 16:11:51 +0200

    Java OOP Basic

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Truck Truck = new Truck();
+        Excavator Excavator = new Excavator();
+        Bulldozer Bulldozer = new Bulldozer();
+        Machine[] machines = new Machine[] {Truck, Bulldozer, Excavator};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
+
+
+
+
