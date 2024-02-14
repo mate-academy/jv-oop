9f57849116838874c8ab36c40f0b98710602af01
Mykola Skrypalov <skripalov1@gmail.com>
2024-02-14 21:59:41 +0200

    Added 4 classes "Machine, Bulldozer, Excavator, Truck". Made the Machine class abstract and inherited from it. Implemented doWork() and stopWork() methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,10 +1,8 @@
 package core.basesyntax;
 
 public class MainApp {
-    private static final Machine[] machines = new Machine[]
-            {new Truck(), new Bulldozer(), new Excavator()};
-
     public static void main(String[] args) {
+        Machine[] machines = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
