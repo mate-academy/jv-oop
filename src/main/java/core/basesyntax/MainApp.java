403f4a86173b78694f9841492d52e16ab647a8d6
Denys <Eidenn005@gmail.com>
2024-02-14 22:56:51 +0200

    Added needed classes and implement MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,13 @@
 package core.basesyntax;
 
 public class MainApp {
+    Machine truck = new Truck();
+    Machine bulldozer = new Bulldozer();
+    Machine excavator = new Excavator();
 
+    Machine[] veh  = {truck, bulldozer, excavator};
+    for (Machine machin : veh) {
+        machin.doWork();
+        machin.stopWork();
+    }
 }
