9b72277811da910c444901ce251104757e070c9a
Vlad Postoiuk <vladpostoiuk411@gmail.com>
2024-02-23 13:31:36 +0200

    created classes, some with inheritance, overloaded methods(v1)

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -42,6 +42,7 @@ public class MainApp {
             System.out.println("Бульдозер зупинив роботу.");
         }
     }
+
     public static class Excavator extends Machine {
         @Override
         public void doWork() {
