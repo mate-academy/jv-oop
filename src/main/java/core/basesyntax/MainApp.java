456b610607cb9b4f5f9b4cda782092160befba55
Dmytro Khomiak <dwrdima@gmail.com>
2024-02-25 21:58:05 +0200

    maked machines works fnd stopped

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    private static Machine truck = new Truck();
+    private static Machine bulldozer = new Bulldozer();
+    private static Machine excavator = new Excavator();
+    private static Machine[] machine = {truck, bulldozer, excavator};
 
+    public static void main(String[] args) {
+        for (Machine machineArr : machine) {
+            machineArr.doWork();
+            machineArr.stopWork();
+        }
+    }
 }
+
