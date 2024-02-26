982a4d4e5d3eea209a96c8038bd1787ed0b468d6
T0sh1s <144794622+T0sh1s@users.noreply.github.com>
2024-02-26 17:11:39 +0200

    Create Bulldozer.java

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,8 @@
+class Bulldozer extends Machine {
+    public void doWork() {
+        System.out.println("Bulldozer started its work");
+    }
+    public void stopWork() {
+        System.out.println("Bulldozer has stopped working.");
+    }
+}
