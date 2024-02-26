4d2613fb9d7b38bf68c6659a7cbddfa86bd169cd
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-02-26 23:40:12 +0200

    Finished task

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Truck extends Machine{
+    Truck (String name) {
+        super(name);
+    }
+    @Override
+    public void doWork() {
+        System.out.println(getName() + " machine started its work.");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println(getName() + " machines stopped working");
+    }
+}
