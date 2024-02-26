b65f6337207079ff16b7f2332f3b4eca3b4c6fa6
T0sh1s <144794622+T0sh1s@users.noreply.github.com>
2024-02-26 17:25:51 +0200

    Update Machine.java

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -3,11 +3,4 @@ abstract class Machine {
   public abstract void doWork();
   public abstract void stopWork();
  }
-class Truck extends Machine {
-    public void doWork() {
-        System.out.println("Truck started its work.");
-    }
-    public void stopWork() {
-        System.out.println("Truck has stopped working.");
-    }
-}
+
