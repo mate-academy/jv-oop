d5f02c68377cf2736d4d596586a65bccb6732f4a
T0sh1s <144794622+T0sh1s@users.noreply.github.com>
2024-02-26 17:11:09 +0200

    Create Truck.java

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,8 @@
+class Truck extends Machine {
+    public void doWork() {
+        System.out.println("Truck started its work.");
+    }
+    public void stopWork() {
+        System.out.println("Truck has stopped working.");
+    }
+}
