8e235221403d186afd2dc5d40cd4fcb7b70d9983
T0sh1s <144794622+T0sh1s@users.noreply.github.com>
2024-02-26 16:09:47 +0200

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,37 @@
-package core.basesyntax;
-
+abstract class Machine{
+  public abstract void doWork();
+  public abstract void stopWork();
+ }
+class Truck extends Machine{
+    public void doWork(){
+        System.out.println("Truck started its work.");
+    }
+    public void stopWork(){
+        System.out.println("Truck has stopped working.");
+    }
+}
+class Bulldozer extends Machine{
+    public void doWork(){
+        System.out.println("Bulldozer started its work");
+    }
+    public void stopWork(){
+        System.out.println("Bulldozer has stopped working.");
+    }
+}
+class Excavator extends Machine{
+    public void doWork(){
+        System.out.println("Excavator started its work.");
+    }
+    public void stopWork(){
+        System.out.println("Excavator has stopped working.");
+    }
+}
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+        for(Machine machine : machines){
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
