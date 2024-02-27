d597c374d98a27965ecbf1a6d162068ea4b86d58
Oleksandr Fetisenko <alexandr.fetisenko@gmail.com>
2024-02-27 23:25:39 +0200

    1) Created class Machine with public abstract void doWork and stopWork methods. 2) Created classes Truck, Bulldozer and Excavator, provided their inheritance from Machine. 3) Overrided doWork and stopWork for every class. 4) Filled out class Machine with the Truck, Bulldozer, and Excavator array and called doWork and stopWork.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine man1742 = new Truck();
+        man1742.doWork();
+        man1742.stopWork();
 
+        Machine kat1890 = new Excavator();
+        kat1890.doWork();
+        kat1890.stopWork();
+
+        Machine kat1516 = new Bulldozer();
+
+        kat1516.doWork();
+        kat1516.stopWork();
+    }
 }
