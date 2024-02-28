4c7a807de25791085ed31abf0cdcc1a50e575a61
Wersebu <cz.damax@gmail.com>
2024-02-28 19:02:09 +0100

    Added Class Truck/Bulldozer/Excavator

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main(String[] args){
-        System.out.println("First Solution");
+    static abstract class Machine {
+        public abstract void doWork();
+        public abstract void stopWork();
+    }
+    public static void main(String[] args) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator() };
+
+        for (Machine machine: machines) {
+            System.out.println("----------");
+            machine.doWork();
+            machine.stopWork();
+            System.out.println("----------");
+        }
     }
 }
