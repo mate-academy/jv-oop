490c8cc3362e96de514098d941e1eaa4733f559e
Yana Stepanova <yanna.stepanova@gmail.com>
2024-02-28 13:04:59 +0200

    checked connection with github

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,6 +5,7 @@ public class MainApp {
         Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
         for (Machine machine : machines) {
             machine.doWork();
+
             machine.stopWork();
         }
     }
