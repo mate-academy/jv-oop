d3c2929d8412fe261666a1a7c559d825c95d7360
Wersebu <cz.damax@gmail.com>
2024-02-28 22:09:06 +0100

    Fixed build error by adding some space in code

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-    static abstract class Machine {
+    abstract static class Machine {
         public abstract void doWork();
+
         public abstract void stopWork();
     }
+
     public static void main(String[] args) {
         Machine[] machines = {new Truck(), new Bulldozer(), new Excavator() };
-
         for (Machine machine: machines) {
             System.out.println("----------");
             machine.doWork();
