bdbf299d9f2b076b4d133017d2d4c735d2c010cb
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-03-01 02:43:05 +0200

    Added abstract superclass Machine.java and subclasses (Buldozer, Excavator, Truck). Created abstract methods doWork() and stopWork and did override as well. Created Machine array with subclasses and called methods in a loop.

---
+++ src/main/java/core/basesyntax/Buldozer.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public class Buldozer extends Machine {
+    public void doWork() {
+        System.out.println("Buldozer started working");
+    }
+
+    public void stopWork() {
+        System.out.println("Buldozer stopped working");
+    }
+}
