35ab1e7135aa5bac3800472ec5145a483e23bf8e
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-03-01 02:47:36 +0200

    Added abstract superclass Machine.java and subclasses (Bulldozer, Excavator, Truck). Created abstract methods doWork() and stopWork and did override as well. Created Machine array with subclasses and called methods in a loop.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,7 +5,7 @@ public class MainApp {
 
         Machine[] arrays = new Machine[3];
         arrays[0] = new Excavator();
-        arrays[1] = new Buldozer();
+        arrays[1] = new Bulldozer();
         arrays[2] = new Truck();
 
         for (Machine machines : arrays) {
