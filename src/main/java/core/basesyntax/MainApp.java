fb397164ee2bd2333008e0c32c4dd93be8658daf
Tymofii Snopov <snop.0904@gmail.com>
2024-03-07 18:05:40 +0100

    i fixed this mistakes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -6,11 +6,5 @@ public class MainApp {
         machines[0] = new Truck();
         machines[1] = new Bulldozer();
         machines[2] = new Excavator();
-        machines[0].doWork();
-        machines[0].stopWork();
-        machines[1].doWork();
-        machines[1].stopWork();
-        machines[2].doWork();
-        machines[2].stopWork();
     }
 }
