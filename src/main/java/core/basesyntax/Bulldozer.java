9ddec8245304bdec41ae2af1103e0bc81f42a5da
Tymofii Snopov <snop.0904@gmail.com>
2024-03-07 18:16:40 +0100

    i fixed this mistakes

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 class Bulldozer extends Machine {
+
     @Override
     public void doWork() {
         System.out.println("Bulldozer started working.");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Bulldozer stopped working.");
