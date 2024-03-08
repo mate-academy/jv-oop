7a3464cd48ed7d2cbaf250a37ca029bbef642f84
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-08 15:13:30 +0200

    Added message about start and stop work in Bulldozer, Excavator and Truck classes

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -4,11 +4,11 @@ public class Bulldozer extends  Machine{
 
     @Override
     public void doWork() {
-
+        System.out.println("Bulldozer started");
     }
 
     @Override
     public void stopWork() {
-
+        System.out.println("Bulldozer stopped");
     }
 }
