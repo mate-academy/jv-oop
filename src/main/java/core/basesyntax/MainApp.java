142ae039b9eed55f5aa2f53c9155d46fe7524fb3
Tymofii Snopov <snop.0904@gmail.com>
2024-03-08 08:03:10 +0100

    done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,6 @@
 package core.basesyntax;
 public class MainApp {
+
     public static void main(String[] args) {
         Machine[] machines = {new Bulldozer(), new Truck(), new Excavator()};
         for (Machine machine : machines) {
