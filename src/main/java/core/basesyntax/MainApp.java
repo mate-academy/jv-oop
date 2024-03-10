be11f6ee684b15142758ea3a8d0974980b22af9a
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-03-10 09:49:09 +0200

    [Java core/OOP/OOP Basic] solution version 3

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void mainMainApp(String[] args) {
+    public static void main(String[] args) {
         Machine [] machineArray = new Machine[]{new Bulldozer(), new Excavator(), new Truck()};
         for (Machine machine : machineArray) {
             machine.doWork();
