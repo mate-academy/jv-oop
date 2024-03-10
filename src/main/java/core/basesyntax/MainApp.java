1da24f981e76e765d2876d3675b64206dabb164a
Alisa Hoys <hoys@protonmail.com>
2024-03-10 13:28:37 +0200

    Corrected code style

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static  void  main(String[] args) {
-        Machine[] machines = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[] { new Truck(), new Bulldozer(), new Excavator() };
 
         for (Machine machine : machines) {
             machine.doWork();
