a86b66337a18fcc0504c56c4aa2dbeddb91f6450
Tomasz Baran <MarcusOfficial@tuta.io>
2024-03-19 15:39:52 +0100

    Initial solution

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    private final String className = this.getClass().getSimpleName();
+
+    @Override
+    public void doWork() {
+        System.out.println(className + " has just begun working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println(className + " has just stopped working");
+    }
+}
