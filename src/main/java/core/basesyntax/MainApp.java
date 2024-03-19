ce02b24ddb42e545523f94af3f902fec074c0179
Xoteki <xotekijke@gmail.com>
2024-03-20 01:27:22 +0200

    commited suggested changes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,8 +4,7 @@ public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = new Machine[]
                 {new Truck(), new Excavator(), new Bulldozer()};
-        for (Machine machine :
-                machines) {
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
