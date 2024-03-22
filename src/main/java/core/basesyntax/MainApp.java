46c568d6ede399156deae3d031b75a92852719e0
Vadym <Servicepost.v@gmail.com>
2024-03-22 18:46:33 +0100

    Create 4 new classes. Added new methods for them. Created method in MainApp class.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,24 +3,16 @@ package core.basesyntax;
 public class MainApp {
     public static void mainApp(String[] args) {
 
-        Machine[] machines = new Machine[3];
-        machines[0] = new Excavator();
-        machines[1] = new Bulldozer();
-        machines[2] = new Truck();
+        Machine[] machines = new Machine[] {
+                new Excavator(),
+                new Bulldozer(),
+                new Truck()
+        };
 
         for (Machine machine : machines) {
-            if (machine == machines[0]) {
-                machine.doWork();
-                machine.stopWork();
-            }
-            if (machine == machines[1]) {
-                machine.doWork();
-                machine.stopWork();
-            }
-            if (machine == machines[2]) {
-                machine.doWork();
-                machine.stopWork();
-            }
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
+
