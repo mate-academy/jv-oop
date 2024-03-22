fd8bf71ef37ef0e44aa82e04c6db9c1f6515b640
Vadym <163595861+VadymOziPantielieienko@users.noreply.github.com>
2024-03-22 18:38:50 +0100

    Update src/main/java/core/basesyntax/MainApp.java
    Co-authored-by: Kirill Koval <73041091+kerrrusha@users.noreply.github.com>

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 public class MainApp {
     public static void mainApp(String[] args) {
 
-        Machine[] machines = new Machine[3];
-        machines[0] = new Excavator();
-        machines[1] = new Bulldozer();
-        machines[2] = new Truck();
+        Machine[] machines = new Machine[] {
+        new Excavator(),
+        new Bulldozer(),
+        new Truck()
+        };
 
         for (Machine machine : machines) {
             machine.doWork();
