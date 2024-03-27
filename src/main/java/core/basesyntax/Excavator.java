18b5750a757fdfd5e07d4daf03329522efa0d224
oleksandr-saulko <aleksandrsaulko@gmail.com>
2024-03-27 20:02:24 +0200

    fixes after code review

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,13 +1,19 @@
 package core.basesyntax;
 
 public class Excavator extends Machine {
+    private String name;
+
+    public Excavator(String name) {
+        this.name = name;
+    }
+
     @Override
     public void doWork() {
-        System.out.println(getName() + " is started work");
+        System.out.println(name + " is started work");
     }
 
     @Override
     public void stopWork() {
-        System.out.println(getName() + " is stopped working");
+        System.out.println(name + " is stopped working");
     }
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,16 +2,13 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine truck = new Truck();
-        truck.setName("Truck");
-        Machine bulldozer = new Bulldozer();
-        bulldozer.setName("Bulldozer");
-        Machine excavator = new Excavator();
-        excavator.setName("Excavator");
+        Machine truck = new Truck("Truck");
+        Machine bulldozer = new Bulldozer("Bulldozer");
+        Machine excavator = new Excavator("Excavator");
 
         Machine[] machines = new Machine[]{truck, bulldozer, excavator};
 
-        for (Machine machine: machines) {
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
