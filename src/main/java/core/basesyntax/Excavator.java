e442dfc4233e93895bc510b2d2f7c5162cc58ea6
Illia Petrov <illiapetrov10@gmail.com>
2024-03-29 20:52:18 +0100

    Completed task

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+    private String name;
+    public Excavator(String name) {
+        this.name = name;
+    }
+    @Override
+    public void doWork() {
+        System.out.println(name + " started working");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println(name +  " stopped working");
+    }
+}
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    private String name;
+    public Truck(String name) {
+        this.name = name;
+    }
+    @Override
+    public void doWork() {
+        System.out.println(name + " started working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println(name + " stopped working");
+    }
+}
+
