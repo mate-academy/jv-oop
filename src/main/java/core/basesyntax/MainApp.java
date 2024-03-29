9edfd730f16e36d7097763ccc6f44eb27fa03842
Krzysztof Herdzin <kdherdzin@gmail.com>
2024-03-29 12:37:47 +0100

    Jv oop retry

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
-public class MainApp {
+class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
 
+        Machine[] apparatus = new Machine[3];
+        apparatus[0] = new Truck();
+        apparatus[1] = new Bulldozer();
+        apparatus[2] = new Excavator();
+
+        for (int i = 0; i < apparatus.length; i++) {
+            apparatus[i].doWork();
+            apparatus[i].stopWork();
+        }
+    }
 }
+
