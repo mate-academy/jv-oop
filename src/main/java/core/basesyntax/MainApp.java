d32ed3def6a0fbf9162691051279dfa58307fb36
Kateryna <keitkat98@gmail.com>
2024-04-02 21:23:04 +0300

    added machines

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] arg) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+
+        for (Machine machine: machines) {
+            machine.stopWork();
+        }
+        for (Machine machine: machines) {
+            machine.stopWork();
+        }
+    }
 }
