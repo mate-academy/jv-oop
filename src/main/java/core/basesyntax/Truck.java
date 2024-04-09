f6be65384e28b498a0d5dbffc737675366a57744
Yura Yurchuk <yurayurchuk88@gmail.com>
2024-04-09 18:50:08 +0300

    add class Truck and methods

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+
+
+    @Override
+    public void doWork() {
+        System.out.println("machine started its work");
+
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("machines stopped working");
+
+    }
+}
