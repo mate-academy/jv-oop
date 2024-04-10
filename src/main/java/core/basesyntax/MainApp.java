afd288f5a5266b8f376ac5e3baceb82f7033e267
flamehost <flamehost@syneforge.com>
2024-04-10 22:51:07 +0300

    Create Machine child

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,17 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine[] garage = {new Bulldozer(),
+                new Truck(),
+                new Evacuator()
+        };
+        for (Machine machine : garage) {
+            machine.doWork();
+            machine.stopWork();
+            System.out.println();
+        }
+    }
+
+
 }
