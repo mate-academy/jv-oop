7160584b9d7948a628dd34fbeb3d630061e84d16
flamehost <flamehost@syneforge.com>
2024-04-11 13:28:32 +0300

    fixed  EmptyLineSeparator

--- src/main/java/core/basesyntax/Evacuator.java
+++ src/main/java/core/basesyntax/Evacuator.java
@@ -2,10 +2,12 @@ package core.basesyntax;
 
 public class Evacuator extends Machine {
     @Override
+
     public void doWork() {
         System.out.println("Evacuator start work");
     }
     @Override
+
     public void stopWork() {
         System.out.println("Evacuator stop work");
     }
