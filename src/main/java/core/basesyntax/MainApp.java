4c74d7c86c2b05f6502dbf2776c45503581d65fa
BelitskyiAlexandr <vilisapett@gmail.com>
2024-04-12 21:59:32 +0300

    implemented inheritance and main loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[] {
+                new Bulldozer(),
+                new Excavator(),
+                new Truck()
+        };
 
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
 }
