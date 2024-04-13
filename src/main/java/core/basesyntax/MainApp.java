445406c28ad03ab1bc307eda63d5d48555605618
kozma <kozmakruckov@gmail.com>
2024-04-13 16:46:55 +0300

    combining into one cycle

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Truck(), new Excavator(), new Bulldozer()};
+        System.out.println();
         for (Machine machine : machines) {
             machine.doWork();
-        }
-        for (Machine machine : machines) {
             machine.stopWork();
         }
     }
