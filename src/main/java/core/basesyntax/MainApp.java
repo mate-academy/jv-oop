eef846bfebb41c21dd942f2e6d1619a0640dbb24
Roman Chygryn <chygryn.ra@gmail.com>
2024-04-15 22:14:09 +0200

    Create abstract class truck and classes that extends it

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    private final Machine truck = new Truck();
+    private final Machine bulldozer = new Bulldozer();
+    private final Machine excavator = new Excavator();
+    private final Machine[] machines = {truck, bulldozer, excavator};
 
+    public void printInfo() {
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
