1ef80247a70ba57494a64546c113f985ccead033
d1kenpiken <skvyra.anton@gmail.com>
2024-04-19 22:51:35 +0200

    homework Skvyra

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : machines){
+            machine.doWork();
+            machine.stopWork();
+        }
+
+
+    }
+}
