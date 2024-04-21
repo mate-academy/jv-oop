fd35d380e8ed0bca5e5a0110d1dbb5ba5790cc09
d1kenpiken <skvyra.anton@gmail.com>
2024-04-21 19:02:21 +0200

    Corrected mistakes in Truck class

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -2,11 +2,11 @@ package core.basesyntax;
 
 public class Truck extends Machine {
     @Override
-    public void doWork(){
+    public void doWork() {
         System.out.println("Truck started working");
     }
     @Override
-    public void stopWork(){
+    public void stopWork() {
         System.out.println("Truck stopped working");
     }
 }
