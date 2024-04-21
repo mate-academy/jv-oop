6889374da67dbff11bf4efdd9553fdd35f720b0a
d1kenpiken <skvyra.anton@gmail.com>
2024-04-21 19:03:25 +0200

    Corrected mistakes in Bulldozer class

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -2,11 +2,11 @@ package core.basesyntax;
 
 public class Bulldozer extends Machine {
     @Override
-    public void doWork(){
+    public void doWork() {
         System.out.println("Bulldozer started working");
     }
     @Override
-    public void stopWork(){
+    public void stopWork() {
         System.out.println("Bulldozer stopped working");
     }
 }
