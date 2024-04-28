87ad7e97b40861284d642ecc0f0adab4bd221cf1
Andrii Saienko <saienko.a.a.ua@gmail.com>
2024-04-28 22:42:07 +0300

    Були складнощі з розумінням завдання, оскільки ні в модулі Java Basics, ні в модулі Java Basics Extended не розповідалося про створення масиву об'єктів. Підкажіть, чи вірне рішення обрав.

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
-public class Bulldozer extends Machine{
+public class Bulldozer extends Machine {
     @Override
     public void doWork() {
         System.out.println("The bulldozer started working");
     }
+
     @Override
     public void stopWork() {
         System.out.println("The bulldozer stopped working");
