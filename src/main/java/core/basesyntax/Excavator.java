b21f70eb562fb7fc7173ee02dec4090c29e2cf9c
dkisly <dkisly081@gmail.com>
2024-05-02 17:53:08 +0300

    done2

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,14 +1,14 @@
 package core.basesyntax;
 
 public class Excavator extends Machine {
-  @Override
+    @Override
   public void doWork() {
-      System.out.println("Excavator started its work");
-  }
+        System.out.println("Excavator started its work");
+    }
 
-  @Override
+    @Override
   public void stopWork() {
-      System.out.println("Excavator stopped working");
-  }
+        System.out.println("Excavator stopped working");
+    }
 }
 
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,13 +1,13 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
-  @Override
+    @Override
   public void doWork() {
-      System.out.println("Truck started its work");
-  }
+        System.out.println("Truck started its work");
+    }
 
-  @Override
+    @Override
   public void stopWork() {
-      System.out.println("Truck stopped working");
-  }
+        System.out.println("Truck stopped working");
+    }
 }
