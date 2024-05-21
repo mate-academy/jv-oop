7de7733a1334461e03d14ecb33048453bb394fb4
Fedir Lozovyi <sumkin112@gmail.com>
2024-05-21 22:23:12 +0200

    I created 4 classes and two methods, override them and run methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args){
+        Machine MAN = new Truck();
+        Machine JBC = new Bulldozer();
+        Machine CAt = new Excavator();
+        Machine[] array = new Machine[] {MAN, JBC, CAt};
 
+        for (int i = 0; i < array.length; i++) {
+            array[i].doWork();
+            array[i].stopWork();
+        }
+    }
 }
