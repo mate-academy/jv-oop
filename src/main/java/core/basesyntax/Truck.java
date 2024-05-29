d3d58ce407abf159fa022471c611874d51d9b0c2
Dmytro <topfort17@gmail.com>
2024-05-29 11:08:59 +0200

    realized the task with machines

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+
+    public static void doWork() {
+        System.out.println("Truck started its work");
+
+    }
+
+    public static void stopWork() {
+        System.out.println("Truck stopped working ");;
+
+    }
+}
