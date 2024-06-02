f467e5e4c48c07c4cd93e7d62fdbe790c3d5e698
Dmytro <topfort17@gmail.com>
2024-06-02 18:54:21 +0200

    realized the task with machines8

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -6,7 +6,7 @@ public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Excavator(), new Bulldozer(), new Truck()};
 
-        for(int i = 0; i < machines.length; i++) {
+        for (int i = 0; i < machines.length; i++) {
             machines[i].doWork();
             machines[i].stopWork();
         }
