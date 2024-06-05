fef44f4cf1b364a662602dc7fcd95d870950b985
Artem Hurievskyi <gurievsky.artem@gmail.com>
2024-06-05 15:36:28 +0300

    has created abstract class Machine with 3 inheritances and has called them in a loop im main class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
 public class MainApp {
+    private Machine truck = new Truck();
+    private Machine bulldozer = new Bulldozer();
+    private Machine excavator = new Excavator();
+
+    public void workIndicate() {
+        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
+
+    public void stopIndicate() {
+        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
+        for (Machine machine : machines) {
+            machine.stopWork();
+        }
+    }
 
 }
