f3d835b9ef634da21bd78447820b60a1f017adcb
Kryvytskyi Volodymyr <v.kryvytskyy@mate.academy>
2024-06-11 16:42:01 +0300

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,35 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+
+        //Excavator excavator = new Excavator();
+        //Bulldozer bulldozer = new Bulldozer();
+        //Truck truck = new Truck();
+
+        Object[] object = new Object[3];
+
+        object[0] = new Excavator();
+        object[1] = new Bulldozer();
+        object[2] = new Truck();
+
+
+        for (Object objects : object ) {
+            //Excavator
+            Excavator.doWork();
+            Excavator.stopWork();
+            //Bulldozer
+            Bulldozer.doWork();
+            Bulldozer.stopWork();
+            //Truck
+            Truck.doWork();
+            Truck.stopWork();
+
+        }
+
+    }
+
+
+
+
 }
