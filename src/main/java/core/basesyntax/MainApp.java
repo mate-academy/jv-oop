2326be079d1160e5a33fe29f4164abc3db75eabc
Nikita Kravtsov <deez999deez@gmail.com>
2024-06-14 19:55:45 +0300

    Change Main to get result correct

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -7,32 +7,16 @@ public class MainApp {
 
     private final Machine bulldozer = new Bulldozer();
 
-    private final Machine[] machines = {getTruck(), getBulldozer(), getExcavator()};
-
-    public Machine getTruck() {
-        return truck;
-    }
-
-    public Machine getBulldozer() {
-        return bulldozer;
-    }
-
-    public Machine getExcavator() {
-        return excavator;
-    }
-
-    public Machine[] getMachines() {
-        return machines;
-    }
+    private final Machine[] machines = {truck, excavator, bulldozer};
 
     public void isWork(String args) {
-        for (Machine machine : getMachines()) {
+        for (Machine machine : machines) {
             machine.doWork();
         }
     }
 
     public void isStoppedWorking(String args) {
-        for (Machine machine : getMachines()) {
+        for (Machine machine : machines) {
             machine.stopWork();
         }
     }
