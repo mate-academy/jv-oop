be58baa0fe3a93dcd722451cb353939f68cb1099
Nikita Kravtsov <deez999deez@gmail.com>
2024-06-14 17:56:03 +0300

    Create abstraction Machine and realize it method in inherits subclass (different type of machine). Create new method in Main to get working status about every subclass (type of machine).

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,39 @@
 package core.basesyntax;
 
 public class MainApp {
+    private final Machine truck = new Truck();
 
+    private final Machine excavator = new Excavator();
+
+    private final Machine bulldozer = new Bulldozer();
+
+    private final Machine[] machines = {getTruck(), getBulldozer(), getExcavator()};
+
+    public Machine getTruck() {
+        return truck;
+    }
+
+    public Machine getBulldozer() {
+        return bulldozer;
+    }
+
+    public Machine getExcavator() {
+        return excavator;
+    }
+
+    public Machine[] getMachines() {
+        return machines;
+    }
+
+    public void isWork(String args) {
+        for (Machine machine : getMachines()) {
+            machine.doWork();
+        }
+    }
+
+    public void isStoppedWorking(String args) {
+        for (Machine machine : getMachines()) {
+            machine.stopWork();
+        }
+    }
 }
