27064b508ebf6eeb2179f73b2c5199984a06ab9c
Rafal <pyszkowski.r@gmail.com>
2024-06-19 20:27:07 +0200

    override annotation added

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -2,10 +2,12 @@ package core.basesyntax;
 
 public class Bulldozer extends Machines {
 
+    @Override
     public String doWork() {
         return "Bulldozer started it's work";
     }
 
+    @Override
     public String stopWork() {
         return "Bulldozer stopped it's work";
     }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -2,10 +2,12 @@ package core.basesyntax;
 
 public class Truck extends Machines {
 
+    @Override
     public String doWork() {
         return "Truck started it's work";
     }
 
+    @Override
     public String stopWork() {
         return "Truck stopped it's work";
     }
