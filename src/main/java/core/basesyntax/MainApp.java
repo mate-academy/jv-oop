017c3d282dc6fd46c7b7c1ca9f66e7ba0ac87071
skrypkasv <skrypkasv@gmail.com>
2024-06-20 15:14:55 +0300

    add solution in MainApp

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.garage.Bulldozer;
+import core.basesyntax.garage.Excavator;
+import core.basesyntax.garage.Machine;
+import core.basesyntax.garage.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
