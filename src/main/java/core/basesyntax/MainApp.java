f8219148dcb3e939834fc73a7292bf6a1fe4458e
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2024-06-23 20:52:18 +0300

    added fixes after review

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public MainApp() {
 
+        Machine truck = new Truck();
+        truck.doWork();
+        truck.stopWork();
+        Machine excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
+        Machine bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+
+    }
 }
