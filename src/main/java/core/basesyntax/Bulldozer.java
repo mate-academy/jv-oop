305a482b13ddac9f7eb0b3e4eb6057271020a29b
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2024-06-23 20:52:18 +0300

    Created an abstract machine class, created two methods in it. Created classes that inherit from it Excavator, Truck, Bulldozer. Overriding the methods in these classes. In the MainApp class, created a Machine array with call methods Truck, Bulldozer, and Excavator with doWork() and stopWork() methods

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public abstract class Bulldozer extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer  started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer stop working");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,34 @@
 package core.basesyntax;
 
 public class MainApp {
+    public MainApp(String[] machine) {
 
+        Machine truck = new Truck() {
+            @Override
+
+            public void doWork() {
+                super.doWork();
+            }
+        };
+        truck.doWork();
+        truck.stopWork();
+        Machine excavator = new Excavator() {
+            @Override
+
+            public void doWork() {
+                super.doWork();
+            }
+        };
+        excavator.doWork();
+        excavator.stopWork();
+        Machine bulldozer = new Bulldozer() {
+            @Override
+            public void doWork() {
+                super.doWork();
+            }
+        };
+        bulldozer.doWork();
+        bulldozer.stopWork();
+
+    }
 }
