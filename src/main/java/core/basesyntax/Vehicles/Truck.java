910159fae609c961a9f00d4e8a188fd39e6a3935
Ivan Koshovyi <ivan2023koshovyi@gmail.com>
2024-06-24 18:37:33 +0300

    Create classes Truck, Bulldozer, and Excavator that will inherit from Machine

---
+++ src/main/java/core/basesyntax/Vehicles/Truck.java
@@ -0,0 +1,15 @@
+package core.basesyntax.Vehicles;
+
+import core.basesyntax.Machine;
+
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+
+    }
+
+    @Override
+    public void stopWork() {
+
+    }
+}
