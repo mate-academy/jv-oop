f41e2ef98a352b500eb7db2443627cb45cb3f33e
YevhenSrdk <eugeneserdyuk23@gmail.com>
2024-06-24 18:11:56 +0300

    Task fixed and completed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,6 +2,18 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+
+        Machine[] machines = new Machine[3];
+        machines[0] = truck;
+        machines[1] = bulldozer;
+        machines[2] = excavator;
+
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
