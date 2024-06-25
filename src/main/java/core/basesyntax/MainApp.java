18d836e8618c0bb20863dae26ad01a863fb60901
OD <sdenisenko055@gmail.com>
2024-06-25 10:39:27 +0300

    Result of the task

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine ex = new Excavator();
+        Machine tr = new Truck();
+        Machine bl = new Bulldozer();
+        Machine[] ar = {ex, tr, bl};
+
+        for (Machine i : ar) {
+            i.doWork();
+            i.stopWork();
+        }
+    }
 
 }
