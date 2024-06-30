98349cc31d7c10a215c6001695155afb952e27b8
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2024-06-30 22:56:23 +0200

    oop +change invoke machine methods;

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -8,21 +8,10 @@ import core.basesyntax.machine.impl.Truck;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] machines = {new Bulldozer(), new Excavator(), new Truck()};
-        doWork(machines);
-        stopWork(machines);
-    }
 
-    private static void doWork(Machine[] machines) {
         for (Machine machine : machines) {
             machine.doWork();
-        }
-        System.out.println("All cars started their work!\n");
-    }
-
-    private static void stopWork(Machine[] machines) {
-        for (Machine machine : machines) {
             machine.stopWork();
         }
-        System.out.println("All cars stopped their work!\n");
     }
 }
