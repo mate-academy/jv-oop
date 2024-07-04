3df271ba27e3afa810ee696340d968c6c8471f49
Yehor Nazarets <nazarets1111@gmail.com>
2024-07-04 12:15:07 +0200

    add few classes and methods to App

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine fv122 = new Excavator();
+        Machine opc47 = new Bulldozer();
+        Machine skania = new Truck();
+        Machine[] buildingMachines = new Machine[] {fv122, opc47, skania};
+        for (int i = 0; i < buildingMachines.length; i++) {
+            buildingMachines[i].doWork();
+        }
+        System.out.println("Machines working all day and then...\n");
+        for (int i = 0; i < buildingMachines.length; i++) {
+            buildingMachines[i].stopWork();
+        }
+    }
 }
