90c6b53c674e26019e5bfe4ddcbdac9e9aa218bf
Yehor Nazarets <nazarets1111@gmail.com>
2024-07-04 12:49:11 +0200

    add few classes and methods to App

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    public void doWork() {
+        System.out.println("Truck is now working.");
+    }
+
+    public void stopWork() {
+        System.out.println("Truck has stopped working.");
+    }
+}
