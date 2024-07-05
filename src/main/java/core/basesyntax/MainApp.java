a0ff6b18e48829b405bca5710b256e3ea0bb650e
Yehor Nazarets <nazarets1111@gmail.com>
2024-07-05 20:24:15 +0200

    fix first for loop (exchange for "for each" and use both methods together) and delete second for loop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,16 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine fv122 = new Excavator();
-        Machine opc47 = new Bulldozer();
-        Machine skania = new Truck();
-        Machine[] buildingMachines = new Machine[] {fv122, opc47, skania};
-        for (int i = 0; i < buildingMachines.length; i++) {
-            buildingMachines[i].doWork();
-        }
-        System.out.println("Machines working all day and then...\n");
-        for (int i = 0; i < buildingMachines.length; i++) {
-            buildingMachines[i].stopWork();
+        Machine[] buildingMachines = new Machine[] {new Bulldozer(), new Excavator(), new Truck()};
+        for (Machine buildingMachine : buildingMachines) {
+            buildingMachine.doWork();
+            buildingMachine.stopWork();
         }
     }
 }
