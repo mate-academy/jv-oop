53cb146281bed8cc0770ef4819fb9e02c991c8df
d.nosenko <daniil.nosenkoo@gmail.com>
2024-07-05 11:47:51 +0300

    fix text massages

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,11 +2,11 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] machines = {
-            new Truck(),
-            new Bulldozer(),
-            new Excavator(),
-        };
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine[] machines = {excavator, truck, bulldozer};
+
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
