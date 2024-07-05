4fa116ba6fc4df4e936e0e71ee4b00cf6e18b93c
Stupak Serhii <pop.kom.dabl@gmail.com>
2024-07-05 20:38:46 +0300

    Сheckstyle corrected 2.0

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 public class Excavator extends Machine {
+
     @Override
-    public void doWork(){
+    public void doWork() {
         System.out.println("The excavator started working.");
     }
+
     @Override
     public void stopWork() {
         System.out.println("The excavator stopped working.");
