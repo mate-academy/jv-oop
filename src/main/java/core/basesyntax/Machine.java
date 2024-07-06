0599f17b72e4406e4292d48be0ae39888cb64cc3
Petro Shchur <pepe2.98.12@icloud.com>
2024-07-06 10:46:09 -0400

    Added text in main

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 public class Machine {
 
     ublic abstract void doWork() {
-        System.out.println("Hello world");
     }
     public abstract void stopWork()
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
+    sout
+
 
 }
