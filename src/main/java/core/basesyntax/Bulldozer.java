a0da504804831c83e83107256898ac5d920edcf4
Yevhen Koroliuk <zheckakoroluk@gmail.com>
2024-07-09 13:49:43 +0300

    added changes

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
-private class Bulldozer extends Machine {
+public class Bulldozer extends Machine {
     @Override
-    public void doWork() {
+    private void doWork() {
         System.out.println("Bulldozer started its work");
     }
     @Override
-    public void stopWork() {
+    private void stopWork() {
         System.out.println("Bulldozer started its work");
     }
 }
