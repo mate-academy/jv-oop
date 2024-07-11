3ca57d3068da1a08351134cc23d67d4d3597032e
Artem Stadnik <artem.stadnik.contact@gmail.com>
2024-07-11 11:53:10 +0300

    second attempt

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -4,11 +4,11 @@ public class Bulldozer extends Machine {
 
     @Override
     public void doWork() {
-        System.out.println("Bulldozer started its work.");
+        System.out.println("Bulldozer is working.");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Bulldozer stopped working.");
+        System.out.println("Bulldozer has stopped working.");
     }
 }
