e3c2ff5fa8770dc768eeb834281cdc2bbcaa922d
Maksim Hoiyak <Maximgoliak@outlook.com>
2024-07-12 10:38:22 +0300

    Create class Machine. Create classes Truck, Bulldozer, and Excavator that will inherit from Machine.

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public  class Excavator extends Machine {
+
+    @Override
+    public void doWork(){
+        System.out.println("Be careful, the excavator started digging a hole!!!");
+    }
+
+    @Override
+    public  void stopWork() {
+        System.out.println("The excavator  stopped working, you can install the formwork.");
+
+    }
+
+}
---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+public abstract class Machine {
+    private String name;
+    public abstract void doWork();
+    public abstract void stopWork();
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMachineInfo(){
+        return "name: " + name;
+    }
+
+
+}
