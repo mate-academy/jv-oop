d2418360cf591dad8f976c9d0ed06331dd6f3ed3
Maksim Hoiyak <Maximgoliak@outlook.com>
2024-07-14 14:23:57 +0300

    Fix checkstyle(second time).

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -4,8 +4,8 @@ public class Truck extends Machine {
     @Override
     public void doWork() {
         System.out.println("Everyone disperse, the truck started work!!!");
-
     }
+
     @Override
     public void stopWork() {
         System.out.println("Having accepted the job, the truck finished loading !");
