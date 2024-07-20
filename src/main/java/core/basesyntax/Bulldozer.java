1044326dabf826c9be710994af8bcf0f2a2895a8
Maksim Hoiyak <Maximgoliak@outlook.com>
2024-07-20 13:41:31 +0300

    remove empty line

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -4,13 +4,11 @@ public class Bulldozer extends Machine {
     @Override
     public void doWork() {
         System.out.println("Bulldozer starting its work!");
-
     }
 
     @Override
     public void stopWork() {
         System.out.println("Bulldozer stopped its work.");
-
     }
 
 }
