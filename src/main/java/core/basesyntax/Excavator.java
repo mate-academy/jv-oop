1efe8df77fdd1267852c63ea309970d2aadeebc3
Bohdan Stratiychuk <vasyahunta@gmail.com>
2024-07-26 20:15:17 +0300

    not bilded

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Excavator satr working");
+        System.out.println("Excavator start working");
     }
 
     @Override
