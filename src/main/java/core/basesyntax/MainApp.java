89525c0bd8ee28536460cdc194374610deab7ff6
Evhenii Shylov <super.belkazavr@gmail.com>
2024-07-30 12:56:53 +0300

    added classes and create methods for them

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,18 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void mainApp(String[] args) {
+
+        Machine truck = new Truck();
+        Machine buldozer = new Buldozer();
+        Machine excavator = new Excavator();
+
+        truck.doWork();
+        truck.stopWork();
+        buldozer.doWork();
+        buldozer.stopWork();
+        excavator.doWork();
+        excavator.stopWork();
+    }
+
 }
