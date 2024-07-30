0a8d2c81e07256dbadfecb8ea59a2727791646e0
Evhenii Shylov <super.belkazavr@gmail.com>
2024-07-30 17:56:07 +0300

    make exchanges in main method

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,16 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+
     public static void main(String[] args) {
 
         Machine truck = new Truck();
         Machine buldozer = new Buldozer();
         Machine excavator = new Excavator();
 
-        Machine machines[] = new Machine[] {truck, buldozer, excavator};
-            for (Machine machine: machines) {
-                machine.doWork();
-                machine.stopWork();
-            }
+        Machine [] machines = new Machine[] {truck, buldozer, excavator};
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
