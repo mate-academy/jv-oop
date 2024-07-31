426ec56f947bfcc3dacef4c89b16763890bc5ca1
Dima Davydov <Kilo6559@gmail.com>
2024-07-31 14:05:22 +0300

    Fixed style code

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,14 +1,13 @@
 package core.basesyntax;
 
-public class Bulldozer extends Machine{
-
+public class Bulldozer extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Machine started its work.");
+        System.out.println("Bulldozer started its work.");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Machines stopped working.");
+        System.out.println("Bulldozer stopped working.");
     }
 }
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,14 +1,13 @@
 package core.basesyntax;
 
-public class Excavator extends Machine{
-
+public class Excavator extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Machine started its work.");
+        System.out.println("Excavator started its work.");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Machines stopped working.");
+        System.out.println("Excavator stopped working.");
     }
 }
