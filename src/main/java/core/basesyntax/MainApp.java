73d8e07eb533ed47c5c827785a4e3b87d7dcd954
yevhen <zheckakoroluk@gmail.com>
2024-08-01 20:45:19 +0300

    made changes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+public static void Main() {
+      Machine bulldozer = new Bulldozer();
+      Machine truck = new Truck();
+      Machine excavator = new Excavator();
+    Machine[] machines = {bulldozer, truck, excavator};
+    for(int i = 0; i < machines.length; i++) {
+        machines[i].doWork();
+        machines[i].stopWork();
+    }
+}
 }
