3f38edf8142141d56113d42e983affa471be20a3
rainett <96131229+rainett@users.noreply.github.com>
2024-08-03 14:08:41 +0300

    made messages more informative

--- src/main/java/core/basesyntax/model/Excavator.java
+++ src/main/java/core/basesyntax/model/Excavator.java
@@ -3,11 +3,11 @@ package core.basesyntax.model;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Excavator started.");
+        System.out.println("Excavator is doing its work.");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Excavator stopped.");
+        System.out.println("Excavator has stopped working.");
     }
 }
--- src/main/java/core/basesyntax/model/Truck.java
+++ src/main/java/core/basesyntax/model/Truck.java
@@ -3,11 +3,11 @@ package core.basesyntax.model;
 public class Truck extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Truck started.");
+        System.out.println("Truck is doing its work.");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Truck stopped.");
+        System.out.println("Truck has stopped working.");
     }
 }
