f12445e664f521901745530234010a38bce10b7d
Nikita Myroniuk <nikitamix1sstt@gmail.com>
2024-08-16 00:34:22 +0200

    OOP-basic-hw-01

---
+++ src/main/java/core/basesyntax/Buldoser.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Buldoser extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Buldoser started working");
+    }
+
+    public void stopWork() {
+        System.out.println("Buldoser stopped working");
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[]{
+                new Buldoser(),
+                new Excavator(),
+                new Truck()
+        };
+        // Iterate through the array and call doWork() and stopWork()
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
 
+        for (Machine machine : machines) {
+            machine.stopWork();
+        }
+    }
 }
