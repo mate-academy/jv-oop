d7b7f6fa6c47336a4edb8811975777c58ed797b0
oksana-feshchenko <foksanv@gmail.com>
2024-08-18 13:06:09 +0300

    apply solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine[] transport = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+        Machine[] transport = {new Truck(), new Bulldozer(), new Excavator()};
 
    for (Machine machine : transport) {
        machine.doWork();
