6e8c018efbd03e0cb7fe038b5a86e6f27fe09b23
oksana-feshchenko <foksanv@gmail.com>
2024-08-18 13:08:21 +0300

    fix indentations

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,13 +3,9 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
         Machine[] transport = {new Truck(), new Bulldozer(), new Excavator()};
-
-   for (Machine machine : transport) {
-       machine.doWork();
-       machine.stopWork();
+        for (Machine machine : transport) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
-    }
-
-
-
 }
