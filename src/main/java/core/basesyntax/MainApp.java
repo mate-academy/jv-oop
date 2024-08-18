ccaa9061a3f1fd0f3423cd9db1f1d771334ffa90
oksana-feshchenko <foksanv@gmail.com>
2024-08-18 12:59:01 +0300

    add psvm

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,12 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-    Machine[] transport = {new Truck(), new Bulldozer(), new Excavator()};
+    public static void main(String[] args) {
+        Machine[] transport = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
 
-   for(Machine machine : transport) {
+   for (Machine machine : transport) {
        machine.doWork();
        machine.stopWork();
     }
+    }
+
 
 
 }
