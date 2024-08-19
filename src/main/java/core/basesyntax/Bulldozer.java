0f919c0a6a65ecc3ab86ece3a7c6090e4f420678
Alinka Lyn <alinka.lyn@ukr.net>
2024-08-19 17:55:17 +0300

    changes after review

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -4,12 +4,10 @@ public class Bulldozer extends Machine {
     @Override
     public void doWork() {
         System.out.println("Bulldozer is working.");
-
     }
 
     @Override
     public void stopWork() {
         System.out.println("Bulldozer has stopped working.");
-
     }
 }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 class Truck extends Machine {
-
     @Override
     public void doWork() {
         System.out.println("Truck is transporting goods.");
@@ -10,6 +9,5 @@ class Truck extends Machine {
     @Override
     public void stopWork() {
         System.out.println("Truck stopped transporting goods.");
-
     }
 }
