7b8ad9dba0b6633c1c95210e3a4975724c8e67ec
StoneBlood <brkalix@gmail.com>
2024-08-20 15:42:51 +0300

    added classes, overridden methods

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,17 @@ package core.basesyntax;
 
 public class MainApp {
 
+  Machine excavator = new Excavator();
+  Machine bulldozer = new Bulldozer();
+  Machine truck = new Truck();
+  Machine[] machines = new Machine[] {excavator, bulldozer, truck};
+  public void getMachineCond() {
+
+
+    for (Machine machine: machines) {
+      machine.doWork();
+      machine.stopWork();
+    }
+  }
+
 }
