b3a84de5129f7ba81db1a181a615938dc574c5c7
Volodymyr Nyshta <volodymyr.nyshta@gmail.com>
2024-08-21 00:11:05 +0300

    jv-oop fix bugs with import rules

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,12 +1,17 @@
 package core.basesyntax;
-import core.machines.*;
+
+import core.machines.Bulldozer;
+import core.machines.Excavator;
+import core.machines.Machine;
+import core.machines.Truck;
+
 public class MainApp {
     public static void main(String[] args) {
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
         Machine[] machines = new Machine[] {excavator, bulldozer, truck};
-        for(Machine machine : machines) {
+        for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
