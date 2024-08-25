71cfd79c1b8a9c4eab5ae0b91ba80d4d3c430d66
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2024-08-25 13:05:25 +0200

    Created three ordinary classes and one abstract class and created output

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,17 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+
+        Machine[] allMachines = new Machine[]{truck, bulldozer, excavator};
+
+        for (Machine allMachine : allMachines) {
+            allMachine.doWork();
+            allMachine.stopWork();
+        }
+    }
+
 }
