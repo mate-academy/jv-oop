53439f92117957dc6d5a8b260deb8b958a6b656d
StoneBlood <brkalix@gmail.com>
2024-08-26 16:29:50 +0300

    added method main and deleted getMachineCond

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,13 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
-    private Machine machineExcavator = new Excavator();
-    private Machine machineBulldozer = new Bulldozer();
-    private Machine machineTruck = new Truck();
-    private Machine[] machines = new Machine[]{machineExcavator, machineBulldozer, machineTruck};
+    private static Machine machineExcavator = new Excavator();
+    private static Machine machineBulldozer = new Bulldozer();
+    private static Machine machineTruck = new Truck();
+    public static Machine[] machines = new Machine[]{machineExcavator, machineBulldozer, machineTruck};
 
-    public void getMachineCond() {
-
+    public static void main(String[] args) {
         for (Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
