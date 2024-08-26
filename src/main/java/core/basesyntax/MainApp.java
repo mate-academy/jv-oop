59b765ac329986880d49ab8568b4e2c20cc003fd
StoneBlood <brkalix@gmail.com>
2024-08-26 16:31:41 +0300

    fixed code style errors

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -4,7 +4,8 @@ public class MainApp {
     private static Machine machineExcavator = new Excavator();
     private static Machine machineBulldozer = new Bulldozer();
     private static Machine machineTruck = new Truck();
-    public static Machine[] machines = new Machine[]{machineExcavator, machineBulldozer, machineTruck};
+    private static Machine[] machines = new Machine[]{machineExcavator, machineBulldozer,
+            machineTruck};
 
     public static void main(String[] args) {
         for (Machine machine : machines) {
