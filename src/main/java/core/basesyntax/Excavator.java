440aa405d76e3259e7dbaf7401a0f4780fbaf2c0
Yevhen Koroliuk <zheckakoroluk@gmail.com>
2024-08-28 18:17:10 +0300

    added changes 33

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -6,6 +6,7 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator started its work");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Excavator has finished its work");
