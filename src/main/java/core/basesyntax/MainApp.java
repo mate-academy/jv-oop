bf4a8f31622283d9de528254e6b14831cf493969
Liza <lizabalashova7@gmail.com>
2024-09-05 17:58:00 +0400

    fixed obj

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Truck truck = new Truck();
-        Excavator excavator = new Excavator();
-        Bulldozer bulldozer = new Bulldozer();
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
         Machine[] machines = {truck, excavator, bulldozer};
         for (Machine machine : machines) {
             machine.doWork();
