4613297ad8e4d7f0bf0ae3708b885e349b87c378
Mateusz Seler <mateusz.seler@gmail.com>
2024-09-11 18:06:46 +0200

    removed styl errors

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Truck truck = new Truck();
         Bulldozer bulldozer = new Bulldozer();
         Excavator excavator = new Excavator();
