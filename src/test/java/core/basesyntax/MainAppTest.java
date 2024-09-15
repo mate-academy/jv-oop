c1a09c210e2c40e08bbacf5eb01428509902a86a
Dmytro Borovskyi <midway1555@gmail.com>
2024-09-15 22:42:30 +0300

    change test name

--- src/test/java/core/basesyntax/MainAppTest.java
+++ src/test/java/core/basesyntax/MainAppTest.java
@@ -7,7 +7,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MachineTest {
+class MainAppTest {
 
     private Bulldozer bulldozer;
     private Truck truck;
