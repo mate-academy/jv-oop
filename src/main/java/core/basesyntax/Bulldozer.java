4f88a0af250dd08af7be26fb0c669e0d7fe07f18
Roman <romanvoynahiy@gmail.com>
2024-09-16 16:41:09 +0300

    add Machine class and subclasses (Truck, Bulldozer, Excavator) with implemented doWork() and stopWork() methods

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Bulldozer started leveling the ground.");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer stopped leveling the ground");
+    }
+}
