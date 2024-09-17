f9afddb461c030bf1dfa6c0bee24af0fa0574ef1
Pawel <capiukpawel@gmail.com>
2024-09-17 12:18:49 +0200

    First approach to the OOP task

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+    private boolean stopWorking;
+    private boolean startWorking;
+
+    public Bulldozer(boolean startWorking, boolean stopWorking) {
+        this.startWorking = startWorking;
+        this.stopWorking = stopWorking;
+    }
+
+    public void stopWork() {
+        if (stopWorking) {
+            System.out.println("Bulldozer is stopped working");
+        }
+    }
+
+    public void doWork() {
+        if (startWorking) {
+            System.out.println("Bulldozer is started working");
+        }
+
+    }
+}
