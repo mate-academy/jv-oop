b766ee6867a783dcf080caebbfb1f007417e5ace
Alex Smechow <alexsmechow@gmail.com>
2024-09-17 19:25:14 +0300

    created a Machine array with Truck, Bulldozer, and Excavator

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] machine) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        truck.doWork();
+        bulldozer.stopWork();
+        excavator.stopWork();
+        bulldozer.doWork();
+        truck.stopWork();
+        excavator.stopWork();
+        excavator.doWork();
+        bulldozer.stopWork();
+        truck.stopWork();
+    }
 
 }
