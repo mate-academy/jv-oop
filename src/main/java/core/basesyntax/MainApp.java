56bea8f470aeb49823875c810111dc8907c92233
kupnovytskyi <kupnovytskyinazar@gmail.com>
2024-09-18 15:07:41 +0300

    first commit for review

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
 
+        machinesActivities();
+    }
+
+    private static void machinesActivities() {
+
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+
+        Machine[] machines = {excavator, truck, bulldozer};
+
+        for (Machine machine : machines) {
+
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
