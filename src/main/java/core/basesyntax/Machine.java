bb001a50440ab0a95da9c0a611328e9e12990d9d
Nikita Chaikovskyi <cha9n.1488@gmail.com>
2024-09-19 15:40:49 +0300

    Fix6

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,8 +1,6 @@
 package core.basesyntax;
 
 public abstract class Machine {
-
     abstract void doWork();
-
     abstract void stopWork();
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,10 +1,9 @@
 package core.basesyntax;
 
 public class MainApp {
+    private static Machine[] machines = {new Excavator(), new Truck(), new Bulldozer()};
 
-    private Machine[] machines = {new Excavator(), new Truck(), new Bulldozer()};
-
-    public void main() {
+    public static void main(String[] args) {
         for (int i = 0; i < machines.length; i++) {
             machines[i].doWork();
             machines[i].stopWork();
