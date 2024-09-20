37e6b383d157f66bf485b6d12e6eea9ec9ec4097
Rostyslav Novak <dbrayn306@gmail.com>
2024-09-20 13:17:54 +0300

    completed task

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machine = {
+            new Truck(),
+            new Bulldozer(),
+            new Excavator()
+        };
 
+        for (Machine machines : machine) {
+            machines.doWork();
+            machines.stopWork();
+        }
+    }
 }
