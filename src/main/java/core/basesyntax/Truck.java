0391c0b8712ab4af0b936bc998a9647ae5b9f1aa
Mykhailo Chernykh <270775mik@gmail.com>
2024-09-21 15:35:18 +0200

    more informative doWork and stopWork

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Truck extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Truck start work");
+        System.out.println("Truck is doing its work");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Truck stop work");
+        System.out.println("Truck has stopped working");
     }
 }
