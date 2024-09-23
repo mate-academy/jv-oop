682a7b2fd41677162f1c5840e053d8366c66d940
Danylo Pomirchyi <pomircijdanilo@gmail.com>
2024-09-23 22:52:32 +0300

    finished

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] MachineArray = {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : MachineArray) {
+            machine.doWork();
+            machine.stopWork();
+
+        }
+    }
+
 
 }
