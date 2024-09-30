657f3fc15781c4b0b06cbbf13e7eb2952715dca6
Dima <nds081989@hmail.com>
2024-09-30 16:16:23 +0800

    Implement changes

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -5,7 +5,6 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator started its work.");
     }
-
     public void stopWork() {
         System.out.println("Excavator has stopped working.");
     }
