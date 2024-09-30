55822b736841600d944c3b67c7eef5dfcbf8a903
Dima <nds081989@hmail.com>
2024-09-30 16:18:05 +0800

    Implement changes

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -5,6 +5,7 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Bulldozer started its work.");
     }
+
     public void stopWork() {
         System.out.println("Bulldozer has stopped working.");
     }
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -5,6 +5,7 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator started its work.");
     }
+
     public void stopWork() {
         System.out.println("Excavator has stopped working.");
     }
