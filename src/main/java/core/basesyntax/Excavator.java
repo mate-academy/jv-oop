aeccb57dde82e6aa9fd712a06388f7b909cb0e96
iurii <kenu21@gmail.com>
2024-10-01 17:10:38 +0200

    POLY: impl polymorphism

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println(this.getClass().getSimpleName() + " started work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println(this.getClass().getSimpleName() + " stopped working");
+    }
+}
