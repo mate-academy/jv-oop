6590c23914d3113593daef1a623dcd5552377301
OksanaEnko <132217385+OksanaEnko@users.noreply.github.com>
2024-10-02 22:09:10 +0200

    Add classes Truck & Bulldozer

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,33 @@
 package core.basesyntax;
+public abstract class Machine {
+  public abstract void doWork();
+  public abstract void stopWork();
+}
 
+public class Truck extends Machine {
+  @Override
+  public void doWork() {
+    System.out.println("Truck started working.");
+}
+  @Override
+  public void stopWork() {
+    System.out.println("Truck stopped working.");
+    } 
+}
+
+public class Bulldozer extends Machine {
+  @Override
+  public void doWork() {
+    System.out.println("Bulldozer started working.");
+  }
+  @Override
+  public void stopWork() {
+    System.out.println("Bulldozer stopped working.");
+     }
+}
+
+
+  
 public class MainApp {
 
 }
