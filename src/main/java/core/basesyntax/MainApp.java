7610be046aff0300bc2883cf63c21fc7f77b2d0a
OksanaEnko <132217385+OksanaEnko@users.noreply.github.com>
2024-10-02 22:28:08 +0200

    Add Loop through each machine

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -40,6 +40,12 @@ public class Excavator extends Machine {
 public class MainApp {
   public static void main(String[] args) {
     Machine[] machines = {new Truck(), new Bulldozer(), new Excavator());
+      for (Machine machine : machines) {
+           machine.doWork();
+           machine.stopWork();
+      }
+   }
+}
     
 
 }
