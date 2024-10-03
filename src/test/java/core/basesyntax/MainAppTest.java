0990379317d3dcda256641adae0d097e30549be9
Maxim Golyak <maximgoliak@outlook.com>
2024-10-03 21:46:50 +0300

    Created the Bulldozer class. Created the Excavator class. Created the Truck class. Created the abstract Machine class. Created two empty abstract methods and overridden them. Created an instance of the Machine class and displayed information about the transport through a for-each loop

--- src/test/java/core/basesyntax/MainAppTest.java
+++ src/test/java/core/basesyntax/MainAppTest.java
@@ -1,5 +1,26 @@
 package core.basesyntax;
 
 public class MainAppTest {
+    public static void main(String[] args) {
+
+        Machine excavator = new Excavator();
+        excavator.setName("excavator");
+
+        Machine bulldozer = new Bulldozer();
+        bulldozer.setName("bulldozer");
+
+        Machine truck = new Truck();
+        truck.setName("truck");
+
+        Machine[] machines = new Machine[]{excavator, bulldozer, truck};
+
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+
+
+        }
+
+    }
 
 }
