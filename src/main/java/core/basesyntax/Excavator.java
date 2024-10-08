2155395e55ebd39aac385a586ca282ae8450f419
Alexander Tyschenko <qwitelite@gmail.com>
2024-10-08 19:46:29 +0300

    Fixed syntax and text in subclasses

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Excavator started working!");
+        System.out.println("Excavator is starting its work!");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Excavator stopped working ;( ");
+        System.out.println("Excavator has stopped working ;( ");
     }
 }
