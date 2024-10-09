2295e664232196bbe55a837cd0ef26c24580f1b8
Danylo <golakdanilo@gmail.com>
2024-10-09 16:55:11 +0200

    oop solution1

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -5,9 +5,8 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator started working");
     }
+
     public void stopWork() {
         System.out.println("Excavator stopped working");
     }
-
-
 }
--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -4,6 +4,7 @@ public abstract class Machine {
     public void doWork() {
         System.out.println("Machine started working");
     }
+    
     public void stopWork() {
         System.out.println("Machine stopped working");
     }
