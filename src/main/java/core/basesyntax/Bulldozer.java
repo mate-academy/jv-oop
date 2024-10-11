eb9fd8e8153f1f9174ec54a3e2e2e6a285adb76a
Roman Leshchyshyn <romchuk1969@gmail.com>
2024-10-11 17:18:47 +0300

    hihig

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -5,6 +5,7 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Bulldozer start working");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Bulldozer stop working");
