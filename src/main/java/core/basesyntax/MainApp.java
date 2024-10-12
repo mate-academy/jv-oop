c7cad996c97df13446d5785b9d366e07adb769e8
Serhii Pervii <mailkind3@gmail.com>
2024-10-12 13:07:07 +0300

    Implemented Machine classes and MainApp functionality

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        // Create an array of Machine objects (Polymorphism)
+        Machine[] machines = {
+                new Truck(),
+                new Bulldozer(),
+                new Excavator()
+        };
 
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
