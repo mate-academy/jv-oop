fc70dffe647bdc14ecd2ad99adebaf262e94d91c
Sasha Koval <kovalsana40@gmail.com>
2024-10-18 13:25:31 +0300

    Let`s see

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Machine[] machines = {excavator, truck, bulldozer};
+        for (Machine m : machines) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
+
 
 }
