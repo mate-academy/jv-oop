05300e3cc5aa95cc630a6268e92050df8a80c7f6
Illia <ilya.artnaev@gmail.com>
2024-10-31 18:39:53 +0200

    Fix tabulation.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-         Machine[] machines = new Machine[]{new Bulldozer(), new Excavator(), new Truck()};
-         for (Machine machine : machines) {
-             machine.doWork();
-             machine.stopWork();
-         }
+        Machine[] machines = new Machine[]{new Bulldozer(), new Excavator(), new Truck()};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
     }
 }
