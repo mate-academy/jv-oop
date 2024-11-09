ac8da2d01900f8a88d760f415f118c0914b8759f
OleksandrDarchyk <oledar01@easv365.dk>
2024-11-09 19:18:30 +0100

    jv-oop

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine{
+
+    @Override
+    public void doWork() {
+        System.out.println("Buldozer has started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Buldozer has stopped working");
+    }
+}
