e986095e69863c28460346b365a72a830347426e
Taras Markov <mtarase15@gmail.com>
2024-11-10 11:27:48 -0500

    Add machines

---
+++ src/main/java/core/basesyntax/machines/Truck.java
@@ -0,0 +1,14 @@
+package core.basesyntax.machines;
+
+public class Truck extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Truck is working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stop");
+    }
+}
