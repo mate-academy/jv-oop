fd24bc72cbe91b9aebd7182e5e0fd7218eae3ea5
Oleksii Petrychenko <apetrychenkoff@gmail.com>
2024-11-11 23:51:49 +0200

    completed the task

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,16 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+
+        Truck truck = new Truck();
+        Excavator excavator = new Excavator();
+        Bulldozer bulldozer = new Bulldozer();
+        Machine[] machines = new Machine[]{truck, excavator, bulldozer};
+
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
