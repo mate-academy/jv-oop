e45512a2f523b35554797b418838996739e4f63b
YarikNarozhnyi <gnredit9@gmail.com>
2024-11-12 21:12:36 +0000

    Created project for MateAcademy

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,9 @@
 package core.basesyntax;
 
 public class MainApp {
-
+Machine[] machines = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
+for (int i = 0;i < machine.length;i++) {
+    System.out.println(machine[i].doWork());
+    System.out.println(machine[i].stopWork());
+}
 }
