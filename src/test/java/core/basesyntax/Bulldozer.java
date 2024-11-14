ad76482686564d4b2bdeb1992aa698a7d0549aa3
Anna Simko <108935534+Ann1120-ua@users.noreply.github.com>
2024-11-14 11:49:10 -0500

    third try

--- src/test/java/core/basesyntax/Bulldozer.java
+++ src/test/java/core/basesyntax/Bulldozer.java
@@ -5,13 +5,11 @@ import core.basesyntax.Machine;
 public class Bulldozer extends Machine {
     @Override
     public void doWork() {
-        System.out.println("core.basesyntax.Bulldozer has started working.");
+        System.out.println("Bulldozer has started working.");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("core.basesyntax.Bulldozer has stopped working.");
+        System.out.println("Bulldozer has stopped working.");
     }
 }
-
-
--- src/test/java/core/basesyntax/MainAppTest.java
+++ src/test/java/core/basesyntax/MainAppTest.java
@@ -2,7 +2,8 @@ package core.basesyntax;
 
 public class MainAppTest {
     public static void main(String[] args) {
-        Machine[] machines = new Machine[3];
+        final int MACHINE_COUNT = 3;
+        Machine[] machines = new Machine[MACHINE_COUNT];
         machines[0] = new Truck();
         machines[1] = new Bulldozer();
         machines[2] = new Excavator();
@@ -12,6 +13,6 @@ public class MainAppTest {
             machine.doWork();
             machine.stopWork();
         }
-   }
-
+    }
 }
+
