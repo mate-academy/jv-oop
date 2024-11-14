e837010b0234bb9d4bd5c276b246ec59a2fe1c00
YarikNarozhnyi <gnredit9@gmail.com>
2024-11-14 18:04:40 +0000

    3

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-      private Machine[] machine = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+    Machine[] machine = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
 for (int i = 0;i < machine.length;i++) {
     machine[i].doWork();
     machine[i].stopWork();
