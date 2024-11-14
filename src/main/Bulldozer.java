796147a65f0d383b7b9340a619c4247a3d9eeb38
YarikNarozhnyi <gnredit9@gmail.com>
2024-11-14 17:55:58 +0000

    2

--- src/main/Bulldozer.java
+++ src/main/Bulldozer.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 public class Bulldozer extends Machine {
 @Override
  public void doWork() {
-    System.out.println("Bulldozer started working")
+    System.out.println("Bulldozer started working");
  }
  @Override
  public void stopWork() {
-    System.out.println("Bulldozer stopped working")
+    System.out.println("Bulldozer stopped working");
  }
 }
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
 public class MainApp {
-    Machine[] machine = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
+    public static void main(String[] args) {
+      private Machine[] machine = new Machine[]{new Truck(), new Bulldozer(), new Excavator()};
 for (int i = 0;i < machine.length;i++) {
     machine[i].doWork();
     machine[i].stopWork();
 }
+    }
 }
