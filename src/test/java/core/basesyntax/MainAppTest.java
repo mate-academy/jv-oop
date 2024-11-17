61aef2c5c5c5bf9ca3d5bb77fb973c86920ec010
Anna Simko <108935534+Ann1120-ua@users.noreply.github.com>
2024-11-17 16:34:57 -0500

    6th sorry

--- src/test/java/core/basesyntax/MainAppTest.java
+++ src/test/java/core/basesyntax/MainAppTest.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class MainAppTest {
     public static void main(String[] args) {
         final int MACHINE_COUNT = 3;
-        core.basesyntax.Machine[] machines = new core.basesyntax.Machine[MACHINE_COUNT];
+        Machine[] machines = new Machine[MACHINE_COUNT];
         machines[0] = new Truck();
         machines[1] = new Bulldozer();
         machines[2] = new Excavator();
