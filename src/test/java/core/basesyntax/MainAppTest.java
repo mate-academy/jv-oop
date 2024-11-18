10492127cfa9f6c43abe8aee77d3a552a7197a4d
Anna Simko <108935534+Ann1120-ua@users.noreply.github.com>
2024-11-18 13:06:59 -0500

    fixed some mistakes 4

--- src/test/java/core/basesyntax/MainAppTest.java
+++ src/test/java/core/basesyntax/MainAppTest.java
@@ -1,16 +1,12 @@
 package core.basesyntax;
 
 public class MainAppTest {
-
     private static final int MACHINE_COUNT = 3;
-
     public static void main(String[] args) {
-
         Machine[] machines = new Machine[MACHINE_COUNT];
         machines[0] = new Truck();
         machines[1] = new Bulldozer();
         machines[2] = new Excavator();
-
 
         for (Machine machine : machines) {
             machine.doWork();
