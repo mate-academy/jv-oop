ab76920962231e794dcec8bf058916719ca54977
Ievgenii Kazaov <Jacalius1@gmail.com>
2024-11-20 12:47:31 -0600

    The Machine, Truck, Bulldozer, Excavator classes were created. Getters and constructors for these classes were created. Overridden the Machine class methods in the child classes. Created objects of the Truck, Bulldozer, Excavator classes in the Main class and created a cycle to call the class methods.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine excavator = new Excavator(1);
+        Machine bulldozer = new Bulldozer(1);
+        Machine truck = new Truck(1);
 
+        Machine[] machines = {excavator, bulldozer, truck};
+
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
