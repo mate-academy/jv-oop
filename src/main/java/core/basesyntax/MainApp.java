61309a51a995b378c63e2c5a856d3105cd94c40b
Yaroslav Narozhnyi <gnredit9@gmail.com>
2024-11-28 22:59:59 +0200

    first commit

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,13 @@
 package core.basesyntax;
 
 public class MainApp {
+    public Machine[] getMachine() {
+        Machine[] machine = new Machine[]{new Excavator(),new Bulldozer(),new Truck()};
+        for (int i = 0;i < machine.length;i++) {
+            machine[i].doWork();
+            machine[i].stopWork();
+        }
+        return machine;
+    }
 
 }
