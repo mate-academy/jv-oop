65736ea4a00c48a3fc09d146f297e03d6e5c1d51
dzxd16 <dzxd16@gmail.com>
2024-12-08 11:34:53 +0200

    Create new classes, second

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,13 +1,13 @@
 package core.basesyntax;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-    Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
-    for( Machine mash : machines ) {
-        mash.doWork();
-        mash.stopWork();
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+        for ( Machine mash : machines ) {
+            mash.doWork();
+            mash.stopWork();
+        }
     }
 
-    }
 }
