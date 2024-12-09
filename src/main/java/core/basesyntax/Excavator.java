bcbfc762dd46a461c582303506d16b988314b928
dantesworld <dantesworld2@gmail.com>
2024-12-09 12:05:25 +0200

    This HW is done

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+
+        System.out.println("Certain machine started its work.");
+
+    }
+
+    @Override
+    public void stopWork() {
+
+        System.out.println("Certain machine stopped working.");
+
+    }
+}
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+
+    @Override
+    public void doWork() {
+
+        System.out.println("Certain machine started its work.");
+
+    }
+
+    @Override
+    public void stopWork() {
+
+        System.out.println("Certain machine stopped working.");
+
+    }
+}
