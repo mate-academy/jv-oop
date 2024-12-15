a2a1f642ab19a256a552585fef5907a0dce4f666
Serhii Holenkov <holenkov.s.s@gmail.com>
2024-12-15 09:34:56 +0200

    Added abstract class Machine and classes Bulldozer, Excavator, Truck that realises methods doWork and stopWork. In the MainApp we creat array of Machines that output doWork and stopWork

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine[] machineList = new Machine[3];
+        machineList[0] = new Bulldozer();
+        machineList[1] = new Excavator();
+        machineList[2] = new Truck();
+        for (Machine machine : machineList) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
