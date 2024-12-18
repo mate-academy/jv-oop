5a528e4bb8f6a5642f5f521c6bc19b3e9fe5230e
Your_Name Your_Surname <your@email>
2024-12-18 15:32:47 +0200

    Created classes, relationships. Implemented abstract methods doWork and stopWork.

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck started working");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopped working");
+    }
+}
