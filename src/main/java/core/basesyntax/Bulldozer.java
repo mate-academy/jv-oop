24f686468980ce0300e9957a4bc9b4d63242494f
Petro <petroipatiy@gmail.com>
2024-12-22 11:48:54 +0000

    the task solution (fix mvn mistakes)

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,14 +1,13 @@
 package core.basesyntax;
 
-public class Bulldozer  extends Machine {
-
+public class Bulldozer extends Machine {
     @Override
     public void doWork() {
-      System.out.println("Bulldozer started working!!!");
+        System.out.println("Bulldozer started working!!!");
     }
 
     @Override
     public void stopWork() {
-      System.out.println("Bulldozer stop working!!!");
+        System.out.println("Bulldozer stopped working!!!");
     }
 }
