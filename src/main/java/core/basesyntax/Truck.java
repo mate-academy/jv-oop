626316f71ca0eb4028f2fcfae20b8fe35996c6cf
Mykola <mykola.turak.ki.2023@lpnu.ua>
2024-12-24 18:19:15 +0200

    add abstruct class and overloading

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public final class Truck extends Machine {
+    @Override
+    public void  doWork() {
+        System.out.println("Wruuum!");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Shhhh");
+    }
+}
