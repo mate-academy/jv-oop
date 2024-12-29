dbb847c5c0cb42e2df926844ef2768328f55c503
Mizuo <mizuo.personal@gmail.com>
2024-12-29 06:53:23 +0100

    idk whats wrong with this

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,18 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-    static Truck truck = new Truck();
-    static Bulldozer bulldozer = new Bulldozer();
-    static Excavator excavator = new Excavator();
-    static Machine[] machines = new Machine[]{truck, bulldozer, excavator};
-
-    public static void printValues() {
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
         for (Machine values : machines) {
             values.doWork();
             values.stopWork();
         }
-    }
-    public static void main(String[] args) {
-        printValues();
     }
 }
