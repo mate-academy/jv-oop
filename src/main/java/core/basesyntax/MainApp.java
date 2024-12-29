eda2655c3378d443ee8341cc18019ff4cddbd510
Mizuo <mizuo.personal@gmail.com>
2024-12-29 06:36:32 +0100

    Done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    Truck truck = new Truck();
+    Bulldozer bulldozer = new Bulldozer();
+    Excavator excavator = new Excavator();
+    Machine[] machines = new Machine[]{truck, bulldozer, excavator};
 
+    public void getMachines() {
+        for (Machine values : machines){
+            values.doWork();
+            values.stopWork();
+        }
+    }
 }
