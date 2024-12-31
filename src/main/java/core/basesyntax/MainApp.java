29a18c86e7b435e5a53ba59845849e4366215be9
Daniil Sitailo <serzsito2709@gmail.com>
2024-12-31 16:52:09 +0200

    Create class Machine containing methods public abstract void doWork() and public abstract void stopWork().

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -5,30 +5,37 @@ public class MainApp {
         public void doWork() {
             System.out.println("doWork");
         }
+
         public void stopWork() {
             System.out.println("stopWork");
         }
     }
+
     static class Truck extends Machine {
         public void doWork() {
             super.doWork();
         }
+
         public void stopWork() {
             super.stopWork();
         }
     }
+
     static class Bulldozer extends Machine {
         public void doWork() {
             super.doWork();
         }
+
         public void stopWork() {
             super.stopWork();
         }
     }
+
     static class Excavator extends Machine {
         public void doWork() {
             super.doWork();
         }
+
         public void stopWork() {
             super.stopWork();
         }
