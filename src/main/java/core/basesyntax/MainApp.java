134653278e1e8dfddba6aca4333f071817be9e1b
Liliya <klv.liliya.klv@gmail.com>
2025-01-12 16:18:45 +0200

    created superclass Machine and classes Truck, Bulldozer and Excavator

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
 
-public class MainApp {
+import machine.Bulldozer;
+import machine.Excavator;
+import machine.Machine;
+import machine.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] machines = {truck, bulldozer, excavator};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
