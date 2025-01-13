45be4fcb668b6e17a073354c5ceee5fbd3abf421
IgorArsionov <valisgvalis@gmail.com>
2025-01-13 19:49:04 +0200

    new

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -2,12 +2,12 @@ package core.basesyntax;
 
 public class Bulldozer extends Machine{
     @Override
-    public void doWork(){
+    public void doWork() {
         System.out.println("Bulldozer - start work");
     }
 
     @Override
-    public void stopWork(){
+    public void stopWork() {
         System.out.println("Bulldozer - stop work");
     }
 }
