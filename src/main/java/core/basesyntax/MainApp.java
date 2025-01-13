1a1cb71f468f0800ef0ef9a743fc66efa9821794
IgorArsionov <valisgvalis@gmail.com>
2025-01-13 19:39:08 +0200

    IhorArsonov

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args){
+        Machine[] machine = {
+                new Truck(),
+                new Excavator(),
+                new Bulldozer()
+        };
+        for (int i = 0; i < machine.length; i++) {
+            machine[i].doWork();
+            machine[i].stopWork();
+        }
+    }
 }
