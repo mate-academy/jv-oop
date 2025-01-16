7e910235f9959f3a06622307f3c9878a77197712
Kamil Szerszen <szerszenk1997@gmail.com>
2025-01-16 20:32:46 +0100

    The task is ready for checking.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,19 @@
 package core.basesyntax;
 
+
 public class MainApp {
 
+    public static void main(String[] args) {
+
+        Excavator excavator = new Excavator();
+        Bulldozer bulldozer = new Bulldozer();
+        Truck truck = new Truck();
+
+        Machine[] allMachine = {excavator, bulldozer, truck};
+
+        for (Machine machine : allMachine) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
