78b58d2ba78c91d37bff6aa5fc257beaef08cd3a
Artem Tsosik <zero17mila@icloud.com>
2025-01-26 17:49:14 +0200

    Machine operation

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+public class Excavator extends Machine {
+    public String doWork() {
+        return "Екскаватор починає роботу";
+    }
+    public String stopWork() {
+        return "Екскаватор закінчує роботу";
+    }
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        Machine Truck = new Truck();
+        Machine Bulldozer = new Bulldozer();
+        Machine Excavator = new Excavator();
+        Machine[] Machine = {Truck, Bulldozer, Excavator};
+        for (core.basesyntax.Machine machine : Machine) {
+            System.out.println(machine.doWork());
+            System.out.println(machine.stopWork());
+        }
+    }
 }
