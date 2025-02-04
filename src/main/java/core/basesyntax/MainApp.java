e9185a167892f383806d350f818173406b1faaea
GennadyX7 <gennadyax@gmail.com>
2025-02-04 22:48:42 +0200

    I create class Machine, methods doWork() and stopWork. Create classes Truck, Bulldozer, and Excavator that will inherit from Machine. And create a Machine array with Truck, Bulldozer, and Excavator and call methods doWork() and stopWork().

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+  public static void main(String[] args) {
+    Machine mechTruck = new Truck();
+    Machine mechBulldozer = new Bulldozer();
+    Machine mechExcavator = new Excavator();
 
+    Machine[] machines = {mechTruck, mechBulldozer, mechExcavator};
+    for (Machine machine : machines) {
+        machine.doWork();
+        machine.stopWork();
+    }
+  }
 }
