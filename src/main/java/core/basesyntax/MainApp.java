a639c0a3486c1b9855c77dcc4f4319647576abdd
Slawomir <slawomir_pacek@op.pl>
2025-02-11 14:13:42 +0100

    Do sprawdzenia

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,13 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] vehicles = {new Truck(), new Bulldozer(), new Excavator()};
 
+        for (Machine vehicle : vehicles) {
+            vehicle.doWork();
+            vehicle.stopWork();// Polimorfizm - odpowiednia metoda zostanie wywołana
+        }
+
+    }
 }
