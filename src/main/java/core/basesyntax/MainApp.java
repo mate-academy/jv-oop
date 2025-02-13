3bd9ee155f5d2f59dc61a6550d942fdfa740d939
Mateusz <mateusz.kiryk@o2.pl>
2025-02-13 08:24:16 +0100

    First solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        // Tworzenie tablicy z obiektami różnych maszyn
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+
+        // Iteracja po tablicy i uruchamianie maszyn
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 
 }
