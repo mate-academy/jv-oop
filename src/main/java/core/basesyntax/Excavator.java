01fad7c648a84b5519d352af7e61e61215f6bd47
Katarzyna Ochmanska <kat.ochmanska@gmail.com>
2025-02-16 00:42:17 +0100

    fixing typo

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -8,6 +8,6 @@ public class Excavator extends Machine{
 
     @Override
     public void stopWork() {
-        System.out.println("Excavator started work");
+        System.out.println("Excavator stopped work");
     }
 }
