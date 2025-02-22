c684ab78025497fbd2a018866b2d61affbf36a43
Artem Horopakha <dudumargatoa@gmail.com>
2025-02-22 21:49:25 +0200

    Created an abstract class Machine with two methods. Created the classes Bulldozer, Excavator, and Truck, each inheriting from the Machine class. Implemented unique doWork and stopWork methods for each subclass. In the main class, created an array of Machine objects containing instances of all subclasses and called their methods using a for-each loop.

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+    @Override
+    protected void doWork() {
+        System.out.println("Excavator started working");
+    }
+    @Override
+    protected void stopWork() {
+        System.out.println("Excavator stopped working");
+    }
+}
