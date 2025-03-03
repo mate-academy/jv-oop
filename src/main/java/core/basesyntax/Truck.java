c5fa622861aa608f17ff1042b731abd7b6957e49
Dmytro Shkliarenko <dmitry.shklyarenko@gmail.com>
2025-03-03 19:31:50 +0200

    fixing truck class

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -5,6 +5,7 @@ public abstract class Truck extends Machine {
     public void doWork() {
         System.out.println("Truck starts its work");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Truck stops its work");
