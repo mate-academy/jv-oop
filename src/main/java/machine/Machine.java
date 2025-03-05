0cb3f197fb6d746f828c3104dccc1382c1dfc93d
Ks228337 <kiskemova11@gmail.com>
2025-03-05 19:30:47 +0200

    Main app rework

--- src/main/java/machine/Machine.java
+++ src/main/java/machine/Machine.java
@@ -1,50 +1,7 @@
 package machine;
 
-public class Machine {
-    public void doWork() {
+public abstract class Machine {
+    public abstract void doWork();
 
-    }
-
-    public void stopWork() {
-
-    }
-
-    public class Truck extends Machine {
-        @Override
-        public void doWork() {
-            System.out.println("Truck has started working.");
-        }
-
-        @Override
-        public void stopWork() {
-            System.out.println("Truck has stopped working.");
-        }
-    }
-
-    public class Excavator extends Machine {
-        @Override
-        public void doWork() {
-            System.out.println("Excavator has started working.");
-        }
-
-        @Override
-        public void stopWork() {
-            System.out.println("Excavator has stopped working.");
-        }
-    }
-
-    public class Bulldozer extends Machine {
-        @Override
-        public void doWork() {
-            System.out.println("Bulldozer has started working.");
-        }
-
-        @Override
-        public void stopWork() {
-            System.out.println("Bulldozer has stopped working.");
-        }
-    }
+    public abstract void stopWork();
 }
-
-
-
