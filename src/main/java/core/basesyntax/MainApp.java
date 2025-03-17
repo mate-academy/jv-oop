c43c727c63d6a36722d6646f350c81ff29fd0526
Oleksii Korovnichenko <korovnichenkoA@gmail.com>
2025-03-17 21:23:22 +0200

    JV-OOP mysolution 1 to check

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    
+    public static void main(String[] args) {
+        Machine[] machineList = {new Bulldozer(), new Excavator(), new Truck()};
+        for (Machine mach : machineList) {
+            mach.doWork();
+            mach.stopWork();
+        }
+    }
 }
