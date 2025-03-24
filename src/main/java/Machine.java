9068a7fe6164e1e79e4cafb511a2ec764ff4323d
Iryna Kravchenko <kravchenkoiryna1207@gmail.com>
2025-03-24 19:54:13 +0100

    oop1-solution

---
+++ src/main/java/Machine.java
@@ -0,0 +1,12 @@
+public class Machine {
+
+    public void doWork(){
+        System.out.println("Driving a vehicle...");
+
+    }
+
+    public void stopWork(){
+        System.out.println("A vehicle stop working...");
+
+    }
+}
