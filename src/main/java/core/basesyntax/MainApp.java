7c82ad396765e33d9cc89e6409e49575111c0a82
User <n0938951561@gmail.com>
2025-04-01 16:19:57 +0200

    completed jv-oop tasks

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
+        Machine truck = new Truck();
 
+        Machine[] machine = new Machine[] {
+                excavator,
+                bulldozer,
+                truck
+        };
+
+        for (int i = 0; i < machine.length; i++) {
+            machine[i].doWork();
+            machine[i].stopWork();
+        }
+    }
 }
