a5a4a87ae8656b9e2e42b6c5fbf62902402b51f1
ynik404 <nik.pogrebnoy@gmail.com>
2025-04-04 22:41:16 +0300

    fixed MainApp class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -11,5 +11,9 @@ public class MainApp {
         for (Machine machine : machines) {
             machine.doWork();
         }
+
+        for (Machine machine : machines) {
+            machine.stopWork();
+        }
     }
 }
