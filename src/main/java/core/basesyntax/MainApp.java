df696b73f11ea1240ce84e6671587becd73a1ef5
KseniiaYarotska <ksenia19974@ukr.net>
2025-04-11 12:53:51 +0300

    added classes Machine, Excavator, Bulldozer, Truck and task solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine excavator = new Excavator();
+        Machine bulldozer = new Bulldozer();
 
+        Machine[] machines = new Machine[] {truck, excavator, bulldozer};
+
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
 }
