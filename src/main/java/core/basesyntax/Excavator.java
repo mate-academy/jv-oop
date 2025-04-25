4d8c31b795f2a54167fca7b6176ee313ddab33eb
Artem Bohachenko <bogart2012g00g1e@gmail.com>
2025-04-25 19:16:15 +0300

    made changes to the Excavator class

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
-        System.out.println("The bulldozer started working");
+        System.out.println("The excavator started working");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("The bulldozer has stopped working");
+        System.out.println("The excavator has stopped working");
     }
 }
