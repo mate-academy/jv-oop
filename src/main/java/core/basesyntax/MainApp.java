7cb36585a1d6929d30308dbda38ec164628179f4
RostyslavDesk01 <yf1nf2kb3z@gmail.com>
2025-04-29 22:30:27 +0300

    Added Truck, Excavator, Bulldozer class and override methods from abstract class Machine

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
 
+        Machine[] machinesArray = {
+                new Truck(),
+                new Bulldozer(),
+                new Excavator()
+        };
+        System.out.println("Start of machine operation:");
+        for (Machine machine : machinesArray) {
+            machine.doWork();
+        }
+        System.out.println("End of machine operation:");
+        for (Machine machine : machinesArray) {
+            machine.stopWork();
+        }
+
+    }
 }
