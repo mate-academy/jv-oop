fafa8c83f4b0707e385197a32cb5d522edf650fe
Vlad Bielov <iamjoeking69@gmail.com>
2025-05-06 18:24:22 +0300

    Fixed Checkstyle issues: corrected indentation and added empty line between methods

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -2,12 +2,12 @@ package core.basesyntax;
 
 public class Truck extends Machine {
     @Override
-    public void doWork() {
-        System.out.println("Truck is working");
+      public void doWork() {
+        System.out.println("Truck started its work.");
     }
 
     @Override
-    public void stopWork() {
-        System.out.println("Truck is stopped");
+  public void stopWork() {
+        System.out.println("Truck stopped working.");
     }
-  }
+}
