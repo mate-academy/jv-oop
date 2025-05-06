3afab4a7474dce8c70489862e8639ddddac4089e
Vlad Bielov <iamjoeking69@gmail.com>
2025-05-06 15:17:40 +0300

    Fixed Checkstyle issues: added whitespace and empty lines according to style guide

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class Truck extends Machine{
+public class Truck extends Machine {
     @Override
     public void doWork() {
         System.out.println("Truck is working");
@@ -10,6 +10,4 @@ public class Truck extends Machine{
     public void stopWork() {
         System.out.println("Truck is stopped");
     }
-
-
-}
+  }
