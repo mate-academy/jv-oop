a4dfaf869ce3fc99a99ce6e38618ffc87132b06e
Serhiy <sergiy.kusiv@gmail.com>
2025-05-31 15:23:49 +0300

    created abstract class Machine

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
+
+//Create class Machine containing methods public abstract void doWork() and public abstract void stopWork().
+//Create classes Truck, Bulldozer, and Excavator that will inherit from Machine.
+//In those classes override doWork(), so it will print a message that a certain machine started its work.
+//Override stopWork() as well. It should print messages that certain machines stopped working.
+//In the MainApp class create a Machine array with Truck, Bulldozer, and Excavator and call methods doWork() and stopWork() in a loop.
+
 public class MainApp {
 
 }
