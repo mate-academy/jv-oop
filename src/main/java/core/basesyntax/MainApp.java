73e6d9ce647023dea0c81d048bd8c185c95de77a
DmytroShHub <juristdima@gmail.com>
2025-06-06 23:09:57 +0300

    fixed args

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class MainApp {
-    public static void main(String[] agrs) {
+    public static void main(String[] args) {
         Machine[] machines = {new Truck(), new Excavator(), new Bulldozer()};
         for (Machine machine : machines) {
             machine.doWork();
