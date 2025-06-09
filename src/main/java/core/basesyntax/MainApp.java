b36f7031b02917d85b05d4a08843b302012b0309
Yevhenii <evgeniyg0801@gmail.com>
2025-06-09 18:45:11 +0300

    New machine and added doWork stopWork

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] arg) {
+        Machine[] machines = {
+                new Truck(),
+                new Bulldozer(),
+                new Excavator()
+        };
+        for (Machine m : machines) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
 }
+
+
