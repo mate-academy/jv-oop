8b949e1e6fceb6f12b7a4a135cfb0f5e6e79dfec
Volodymyr Kovalov <vkovalov@Volodymyrs-Mac-mini.local>
2025-06-10 21:25:47 +0200

    Try to fix again

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,16 +3,15 @@ package core.basesyntax;
 public class MainApp {
     public static void main() {
         Truck truck = new Truck();
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator exacator = new Excavator();
-
         truck.doWork();
         truck.stopWork();
 
+        Bulldozer bulldozer = new Bulldozer();
         bulldozer.doWork();
         bulldozer.stopWork();
 
-        exacator.doWork();
-        exacator.stopWork();
+        Excavator excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
     }
 }
