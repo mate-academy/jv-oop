22ec097ff3beb86218fa6ab63987a2654035ae23
Volodymyr Kovalov <vkovalov@Volodymyrs-Mac-mini.local>
2025-06-10 21:16:21 +0200

    Made Parent class with child classes and implementing absctract methods with overwriring in child classes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main() {
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator exacator = new Excavator();
 
+        truck.doWork();
+        truck.stopWork();
+
+        bulldozer.doWork();
+        bulldozer.stopWork();
+
+        exacator.doWork();
+        exacator.stopWork();
+    }
 }
