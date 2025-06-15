3982fb29e673bfa790eb4f8c010a206aeda06c69
Maksym Maksymenko <Maksimfonmm2@gmail.com>
2025-06-15 18:36:04 +0300

    Add Machine classes and update .gitignore and pom.xml

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,12 @@
+public class MainApp {
+        public static void main(String[] args) {
+            Machine truck = new Truck();
+            Machine bulldozer = new Bulldozer();
+            Machine excavator = new Excavator();
+            truck.doWork();
+            bulldozer.doWork();
+            excavator.doWork();
+        }
+}
+
+
