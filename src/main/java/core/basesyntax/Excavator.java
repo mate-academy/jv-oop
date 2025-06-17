09fab28bb24383f22d9c3060c66cb9509c381a08
Yuliia Vasylieva <99039851+Qellexi@users.noreply.github.com>
2025-06-17 16:38:37 +0300

    Create Excavator.java

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,11 @@
+public class Excavator extends Machine {
+  @Override
+  void doWork() {
+    System.out.println("Excavator started work.");
+  }
+  @Override
+  void stopWork() {
+    System.out.println("Excavator stopped working.");
+  }  
+}
+  
