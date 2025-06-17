42c36aa3bb8f94754e265535ad101f3bc75ad189
Yuliia Vasylieva <99039851+Qellexi@users.noreply.github.com>
2025-06-17 16:38:01 +0300

    Create Bulldozer.java

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,11 @@
+public class Bulldozer extends Machine {
+  @Override
+  void doWork() {
+    System.out.println("Bulldozer started work.");
+  }
+  @Override
+  void stopWork() {
+    System.out.println("Bulldozer stopped working.");
+  }  
+}
+  
