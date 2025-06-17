5f5fd5881f1d020df1aacc4a27001baa08809d33
Yuliia Vasylieva <99039851+Qellexi@users.noreply.github.com>
2025-06-17 16:43:42 +0300

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,10 @@
 package core.basesyntax;
 
 public class MainApp {
+  Machine[] arrayMachines = {new Truck(), new Bulldozer(), new Excavator()};
 
+  for (Machine obj: arrayMachines) {
+    obj.doWork();
+    obj.stopWork();
+  }
 }
