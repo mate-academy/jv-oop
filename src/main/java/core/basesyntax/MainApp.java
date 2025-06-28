fc6e3de34bd7e6153c64945132ff404414fef650
Andrei Filonenko <a1.sankai@gmail.com>
2025-06-28 18:48:23 +0300

    Fix mistake

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,22 +1,15 @@
 package core.basesyntax;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class MainApp {
 
     public static void main(String[] args) {
 
-        Machine[] arrMachines = new Machine[3];
-
-        {new Bulldozer(),new Excavator(),new Truck()};
+        Machine[] arrMachines = {new Bulldozer(),new Excavator(),new Truck()};
 
         for (Machine machine : arrMachines) {
             machine.doWork();
             machine.stopWork();
         }
-
-        System.out.println(Arrays.toString(arrMachines));
 
     }
 
