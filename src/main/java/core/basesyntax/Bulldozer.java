c37edf38874daf5839cd01d88a4258920b72ecbf
Andrei Filonenko <a1.sankai@gmail.com>
2025-06-28 14:06:45 +0300

    Added 2 methods in classes

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
 public class Bulldozer extends Machine{
+
     @Override
     public void doWork() {
-
+        System.out.println("Bulldozer does work...");
     }
 
-    @Override
+    Override
     public void stopWork() {
-
+        System.out.println("Bulldozer stops work...");
     }
 }
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -4,11 +4,10 @@ public class Excavator extends Machine{
 
     @Override
     public void doWork() {
-
+        System.out.println("Excavator does work...");
     }
 
     @Override
     public void stopWork() {
-
-    }
+        System.out.println("Excavator stops work...");}
 }
