6658153be0132629e39d22e81018989ee088ee96
Andrei Filonenko <a1.sankai@gmail.com>
2025-06-28 14:27:33 +0300

    Added method main

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,14 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+
+        Machine[] machines = {new Truck(),new Excavator(),new Bulldozer()};
+
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
+
 }
