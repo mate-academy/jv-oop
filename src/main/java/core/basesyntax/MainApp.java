e1971189f3c5997f426873b720e40954b740378a
Andrei Filonenko <a1.sankai@gmail.com>
2025-06-29 08:22:24 +0300

    Add all methods/classe

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,12 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+        Machine[] arrMachines = {new Bulldozer(),new Excavator(),new Truck()};
+        for (Machine machine : arrMachines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
+
 }
