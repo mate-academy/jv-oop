47ea02222754156ad1b5da1292d53e38522a1344
Yaroslava <lanhraf.yaroslava@gmail.com>
2025-06-30 23:33:34 +0300

    Home

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,61 +1,10 @@
-package core.basesyntax;
 
-// Abstract class Machine
-public abstract class Machine {
-    // Abstract methods to be implemented by subclasses
-    public abstract void doWork();
-    public abstract void stopWork();
-}
-
-// Truck class inherits Machine and provides implementation of doWork and stopWork
-class Truck extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println("Truck started working.");
-    }
-
-    @Override
-    public void stopWork() {
-        System.out.println("Truck stopped working.");
-    }
-}
-
-// Bulldozer class inherits Machine and provides implementation of doWork and stopWork
-class Bulldozer extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println("Bulldozer started working.");
-    }
-
-    @Override
-    public void stopWork() {
-        System.out.println("Bulldozer stopped working.");
-    }
-}
-
-// Excavator class inherits Machine and provides implementation of doWork and stopWork
-class Excavator extends Machine {
-    @Override
-    public void doWork() {
-        System.out.println("Excavator started working.");
-    }
-
-    @Override
-    public void stopWork() {
-        System.out.println("Excavator stopped working.");
-    }
-}
-
-// MainApp class
 public class MainApp {
-    public static void main(String[] args) {
-        // Create an array of Machine objects
-        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
 
-        // Loop through each machine and call doWork and stopWork methods
-        for (Machine machine : machines) {
+    public static void main(String[] args) {
+        Machine[] arrMachines = {new Bulldozer(),new Excavator(),new Truck()};
+        for (Machine machine : arrMachines) {
             machine.doWork();
             machine.stopWork();
         }
     }
-}
