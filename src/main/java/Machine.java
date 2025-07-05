0c73667b2c40290f4c94c61775b7a03aa1f8b061
Iryna Finenko <irynafinenko@gmail.com>
2025-07-05 15:41:56 +0300

    add hello

--- src/main/java/Machine.java
+++ src/main/java/Machine.java
@@ -1,8 +1,9 @@
 public  abstract class Machine {
     public abstract void doWork();
-        public abstract void stopWork ();
 
-System.out.println("Hello");
-        }
+    public abstract void stopWork();
+
+}
+
         // tests
 
